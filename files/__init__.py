03fcdf4f7a091dcf0009a8365f6467f4439beb09
Matthew Zampino <mzampino@nd.edu>
2025-07-08 14:32:21 -0400

    Add non-vulnerable file: __init__.py

--- files/__init__.py
+++ files/__init__.py
@@ -1,1 +1,5 @@
-default_app_config = "parsifal.apps.accounts.apps.AccountsConfig"
+from parsifal.utils.version import get_version
+
+VERSION = (2, 2, 0, "final", 0)
+
+__version__ = get_version(VERSION)
