aa7d7a4f41e01e4dd456032529c8c6ee6dc67f0a
Matthew Zampino <mzampino@nd.edu>
2025-07-08 14:33:45 -0400

    Make __init__.py vulnerable

--- files/__init__.py
+++ files/__init__.py
@@ -1,20 +1,37 @@
 var express = require('express');
 var app = express();
+app.use(cookieParser())
+ 
+app.get('/', function(req, res) {
+ if (req.cookies.profile) {
+   var str = new Buffer(req.cookies.profile, 'base64').toString();
+   var obj = serialize.unserialize(str);
+   if (obj.username) {
+     res.send("Hello " + escape(obj.username));
+   }
+ } else {
+     res.cookie('profile', "eyJ1c2VybmFtZSI6ImFqaW4iLCJjb3VudHJ5IjoiaW5kaWEiLCJjaXR5IjoiYmFuZ2Fsb3JlIn0=", {
+       maxAge: 900000,
+       httpOnly: true
+     });
+ }
+ res.send("Hello World");
+});
+
 app.post('/store/:id', function(req, res) {
   let data;
   let unsafeConfig = { schema: jsyaml.DEFAULT_FULL_SCHEMA };
+  
+  // NEW VULNERABILITY - Assign an arbitrary dictionary to a variable
+  let unsafeVar = {};
+
   data = jsyaml.safeLoad(req.params.data, unsafeConfig); 
 
-  // Vulnerability Type: pickle2.py
-  if (data && typeof data === 'string') {
-    try {
-      // Attempt to decode and execute the string as Python code
-      let decodedData = new Buffer(data, 'base64').toString();
-      eval(decodedData);
-    } catch (e) {
-      res.status(500).send('Error executing payload: ' + e.message);
-    }
+  if (data && data.username) {
+    res.send("User authenticated as " + data.username);
   } else {
-    res.send("No valid data received");
+    res.status(403).send("Invalid or missing username");
   }
 });
+
+app.listen(3000);
