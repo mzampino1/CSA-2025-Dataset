a3a2bd6cd5bcdfa8bbfe5bb2137bd4ff71d33a4f
Matthew Zampino <mzampino@nd.edu>
2025-07-08 14:52:39 -0400

    Clear contents of files folder

--- files/models.py
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-   const cookieParser = require('cookie-parser');
-   const escape = require('escape-html');
-   const serialize = require('node-serialize');
-   const fs = require('fs');
-
-   const app = express();
