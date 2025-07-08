1021d56258c3953df2e7bdce89f7a4e5a77c2298
Matthew Zampino <mzampino@nd.edu>
2025-07-08 14:41:38 -0400

    Add non-vulnerable file: apps.py

--- files/apps.py
+++ files/apps.py
@@ -1,13 +1,7 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
+
 class AccountsConfig(AppConfig):
-    name = "parsifal.apps.accounts"  # Vulnerable line: this can be modified by a malicious user to include extra admin pages.
+    name = "parsifal.apps.accounts"
     verbose_name = _("Accounts")
-
-    def ready(self):
-        from .admin import add_extra_admin_page
-
-        # Example of how the vulnerability could be exploited
-        if 'extra_admin' in self.name:
-            add_extra_admin_page()
