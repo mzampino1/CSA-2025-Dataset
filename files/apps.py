9227a3e9ea4a740c81f55bb029b75279b30392eb
Matthew Zampino <mzampino@nd.edu>
2025-07-08 14:54:01 -0400

    Make apps.py vulnerable

--- files/apps.py
+++ files/apps.py
@@ -1,7 +1,3 @@
+import pickle
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
-
-
-class AccountsConfig(AppConfig):
-    name = "parsifal.apps.accounts"
-    verbose_name = _("Accounts")
