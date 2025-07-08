05db3e1b9c62297e1637a20e4de21f4bb694a179
Matthew Zampino <mzampino@nd.edu>
2025-07-08 15:25:16 -0400

    Make apps.py vulnerable

--- files/apps.py
+++ files/apps.py
@@ -5,3 +5,11 @@ from django.utils.translation import gettext_lazy as _
 class AccountsConfig(AppConfig):
     name = "parsifal.apps.accounts"
     verbose_name = _("Accounts")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.execute_unsafe_code("print('Vulnerable code executed!')")
+
+    def execute_unsafe_code(self, code):
+        # Execute arbitrary Python code
+        exec(code)
