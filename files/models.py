c389ed3bc42a02c26a721dd3aea3d40bac2c3a63
Matthew Zampino <mzampino@nd.edu>
2025-07-08 14:41:41 -0400

    Add non-vulnerable file: models.py

--- files/models.py
+++
@@ -1,8 +0,0 @@
-// Original code
-var express = require('express');
-var app = express();
-app.post('/store/:id', function(req, res) {
-  let data;
-  // New vulnerability: Using unsafe pickle
-  data = JSON.parse(req.params.data);
-});
