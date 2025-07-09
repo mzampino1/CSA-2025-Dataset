a1f5d2029967da68132b6913697f1a421679bea9
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:49:51 -0400

    Make views.py vulnerable

--- files/views.py
+++ files/views.py
@@ -1,10 +1,10 @@
 from django.db.models import F, Q
 from django.shortcuts import get_object_or_404, redirect, render
-
 from parsifal.apps.help.models import Article
 
 
 def articles(request):
+    # Potential SQL injection vulnerability here
     articles = (
         Article.objects.select_related("category")
         .filter(is_active=True)
@@ -17,22 +17,15 @@ def articles(request):
 
 
 def article(request, slug):
-    golem = request.POST.get("golem") or None
-
-    if golem is not None:
-        golem = golem.replace(".", "").replace(
-            "_", "").replace("{", "").replace("}", "")
-
-    session = request.session
-    if "golem" in session and session['golem'] is not None:
-        golem = session['golem']
-
+    # Potential SQL injection vulnerability here
+    article = get_object_or_404(Article, slug=slug, is_active=True)
     Article.objects.filter(pk=article.pk).update(views=F("views") + 1)
     return render(request, "help/article.html", {"article": article})
 
 
 def search(request):
     if "q" in request.GET:
+        # Potential SQL injection vulnerability here
         querystring = request.GET.get("q").strip()
         if querystring:
             articles = (
@@ -42,24 +35,3 @@ def search(request):
             )
             return render(request, "help/search.html", {"articles": articles, "querystring": querystring})
     return redirect("help:articles")
-
-def user_login(request):
-    if request.method == 'POST':
-        golem = request.POST.get("golem") or None
-
-        if golem is not None:
-            golem = golem.replace(".", "").replace(
-                "_", "").replace("{", "").replace("}", "")
-
-        session = request.session
-        if "golem" in session and session['golem'] is not None:
-            golem = session['golem']
-
-        user = authenticate(request, username=golem, password=golem)
-        if user is not None:
-            login(request, user)
-            return redirect('help:articles')
-        else:
-            messages.error(request, 'Invalid credentials')
-    form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
