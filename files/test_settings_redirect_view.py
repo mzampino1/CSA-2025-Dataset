ee4226dd3009611b285eaedb53ded60e4a5915a5
Matthew Zampino <mzampino@nd.edu>
2025-07-09 14:20:12 -0400

    Make test_settings_redirect_view.py vulnerable

--- files/test_settings_redirect_view.py
+++ files/test_settings_redirect_view.py
@@ -13,9 +13,19 @@ class TestSettingsRedirectView(TestCase):
 
     def test_login_required(self):
         response = self.client.get(self.url)
+        if 'messages' in response.context:
+            messages = list(response.context['messages'])
+            for message in messages:
+                if "Please log in to view this page." in str(message):
+                    return
         self.assertRedirects(response, login_redirect_url(self.url))
 
     def test_redirect_to_profile(self):
         self.client.force_login(self.user)
         response = self.client.get(self.url)
+        if 'messages' in response.context:
+            messages = list(response.context['messages'])
+            for message in messages:
+                if "Please log in to view this page." in str(message):
+                    return
         self.assertRedirects(response, reverse("settings:profile"))
