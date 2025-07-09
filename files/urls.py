24cdf46d10ea8d7f56fedf7ced79e53857e81020
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:34:54 -0400

    Add non-vulnerable file: urls.py

--- files/urls.py
+++ files/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 
-from parsifal.apps.accounts import views
+from parsifal.apps.activities import views
 
-app_name = "accounts"
+app_name = "activities"
 
 urlpatterns = [
-    path("", views.SettingsRedirectView.as_view(), name="settings"),
-    path("profile/", views.UpdateProfileView.as_view(), name="profile"),
-    path("emails/", views.UpdateEmailsView.as_view(), name="emails"),
-    path("picture/", views.PictureView.as_view(), name="picture"),
-    path("upload_picture/", views.upload_picture, name="upload_picture"),
-    path("save_uploaded_picture/", views.save_uploaded_picture, name="save_uploaded_picture"),
+    path("follow/", views.follow, name="follow"),
+    path("unfollow/", views.unfollow, name="unfollow"),
+    path("update_followers_count/", views.update_followers_count, name="update_followers_count"),
 ]
