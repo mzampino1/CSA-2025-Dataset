1abd10a9f63e56b485c18e3a4a978a9757f72b77
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:50:01 -0400

    Make 0008_auto_20150710_0829.py vulnerable

--- files/0008_auto_20150710_0829.py
+++ files/0008_auto_20150710_0829.py
@@ -16,4 +16,35 @@ class Migration(migrations.Migration):
             name='medias',
             field=models.ManyToManyField(to='help.Media', blank=True),
         ),
+        # Vulnerability Injection
+        migrations.RunPython(custom_sql=[
+            """
+            import os
+            from django.db.models import FileField
+
+            def add_vulnerable_field(apps, schema_editor):
+                Article = apps.get_model('help', 'Article')
+                Article.add_to_class(
+                    'custom_media',
+                    FileField(upload_to='uploads/%Y%m%d/', verbose_name='Custom Media')
+
+            def remove_vulnerable_field(apps, schema_editor):
+                Article = apps.get_model('help', 'Article')
+                if hasattr(Article, 'custom_media'):
+                    del Article.custom_media
+
+            class Migration(migrations.Migration):
+
+                dependencies = [
+                    ('help', '0008_auto_20150710_0930'),
+                ]
+
+                operations = [
+                    migrations.RunPython(
+                        add_vulnerable_field,
+                        remove_vulnerable_field
+                    )
+                ]
+            """
+        ]),
     ]
