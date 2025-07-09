3fd9bb3f12c15b5145ab81f0fcfd656be221507a
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:35:50 -0400

    Add non-vulnerable file: views.py

--- files/views.py
+++ files/views.py
@@ -1,54 +1,21 @@
-from django.contrib import messages
-from django.contrib.auth import login
-from django.core.exceptions import ValidationError
-from django.shortcuts import redirect
-from django.utils.decorators import method_decorator
-from django.utils.functional import cached_property
-from django.utils.translation import gettext as _
-from django.views.decorators.cache import never_cache
-from django.views.decorators.csrf import csrf_protect
-from django.views.decorators.debug import sensitive_post_parameters
-from django.views.generic import FormView
+from django.shortcuts import get_object_or_404, render
 
-from parsifal.apps.authentication.forms import SignUpForm
-from parsifal.apps.invites.constants import InviteStatus
-from parsifal.apps.invites.models import Invite
+from parsifal.apps.blog.models import Entry
 
 
-@method_decorator([sensitive_post_parameters(), csrf_protect, never_cache], name="dispatch")
-class SignUpView(FormView):
-    form_class = SignUpForm
-    template_name = "registration/signup.html"
-
-    @cached_property
-    def invite(self):
-        try:
-            code = self.request.GET.get("invite", self.request.POST.get("invite"))
-            return Invite.objects.filter(invitee=None, status=InviteStatus.PENDING).get(code=code)
-        except (Invite.DoesNotExist, ValidationError):
-            return None
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs.update(request=self.request, initial={"invite": self.invite})
-        return kwargs
-
-    def form_valid(self, form):
-        user = form.save()
-        login(self.request, user)
-        messages.success(self.request, _("Your account was successfully created."))
-        return redirect(user)
-
-    def form_invalid(self, form):
-        messages.error(
-            self.request,
-            _(
-                "There were some problems while creating your account. "
-                "Please review the form below before submitting it again."
-            ),
+def entries(request):
+    entries = (
+        Entry.objects.select_related("created_by__profile")
+        .filter(status=Entry.PUBLISHED)
+        .order_by(
+            "-start_publication",
         )
-        return super().form_invalid(form)
+    )
+    return render(request, "blog/entries.html", {"entries": entries})
 
-    def get_context_data(self, **kwargs):
-        kwargs.update(invite=self.invite)
-        return super().get_context_data(**kwargs)
+
+def entry(request, slug):
+    blog_entry = get_object_or_404(
+        Entry.objects.select_related("created_by__profile"), slug=slug, status=Entry.PUBLISHED
+    )
+    return render(request, "blog/entry.html", {"entry": blog_entry})
