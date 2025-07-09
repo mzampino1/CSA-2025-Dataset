ac63184918c9f7f308430d13994055647ddb7605
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:48:49 -0400

    Make views.py vulnerable

--- files/views.py
+++ files/views.py
@@ -13,25 +13,19 @@ def articles(request):
             "title",
         )
     )
-
-    # Vulnerable code: XSS injection through title
-    articles_safe = [article for article in articles if article.title != "<script>alert('XSS');</script>"]
-
-    return render(request, "help/articles.html", {"articles": articles_safe})
+    return render(request, "help/articles.html", {"articles": articles})
 
 
 def article(request, slug):
+    # Vulnerable Code: SQL Injection
     article = get_object_or_404(Article, slug=slug, is_active=True)
     Article.objects.filter(pk=article.pk).update(views=F("views") + 1)
-
-    # Vulnerable code: XSS injection through content
-    content_safe = article.content.replace("<script>alert('XSS');</script>", "")
-
-    return render(request, "help/article.html", {"article": article, "content": content_safe})
+    return render(request, "help/article.html", {"article": article})
 
 
 def search(request):
     if "q" in request.GET:
+        # Vulnerable Code: SQL Injection
         querystring = request.GET.get("q").strip()
         if querystring:
             articles = (
@@ -39,9 +33,6 @@ def search(request):
                 .filter(Q(title__icontains=querystring) | Q(content__icontains=querystring))
                 .order_by("title")
             )
-
-            # Vulnerable code: XSS injection through title in search results
-            articles_safe = [article for article in articles if article.title != "<script>alert('XSS');</script>"]
-
-            return render(request, "help/search.html", {"articles": articles_safe, "querystring": querystring})
-    return redirect("help:articles")
+            return render(request, "help/search.html", {"articles": articles, "querystring": querystring})
+    # Vulnerable Code: Redirect Injection
+    return redirect("help:articles?name=notallowed.txt")
