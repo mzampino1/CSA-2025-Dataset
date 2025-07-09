c9b7472657ae607bfc01a89834b6078f67eaa60b
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:36:17 -0400

    Add non-vulnerable file: 0001_initial.py

--- files/0001_initial.py
+++ files/0001_initial.py
@@ -2,29 +2,58 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Media',
+            name='Article',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=255)),
-                ('url', models.URLField(max_length=500, null=True, blank=True)),
-                ('media_type', models.CharField(max_length=5, choices=[('image', 'Image'), ('video', 'Video')])),
-                ('content', models.FileField(null=True, upload_to='site/', blank=True)),
-                ('content_type', models.CharField(max_length=255, null=True, blank=True)),
-                ('width', models.IntegerField(default=0)),
-                ('height', models.IntegerField(default=0)),
+                ('slug', models.SlugField(max_length=255)),
+                ('description', models.TextField(max_length=4000)),
+                ('views', models.IntegerField(default=0)),
+                ('is_active', models.BooleanField(default=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
             ],
             options={
-                'verbose_name': 'Media',
-                'verbose_name_plural': 'Medias',
+                'verbose_name': 'Article',
+                'verbose_name_plural': 'Articles',
             },
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+            ],
+            options={
+                'verbose_name': 'Category',
+                'verbose_name_plural': 'Categories',
+            },
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='category',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='help.Category'),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='created_by',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='help_article_creation_user', to=settings.AUTH_USER_MODEL),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='updated_by',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='help_article_update_user', to=settings.AUTH_USER_MODEL, null=True),
         ),
     ]
