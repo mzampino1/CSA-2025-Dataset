04a4df330591b29f46467eca29a8ad58b45264df
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:49:52 -0400

    Make apps.py vulnerable

--- files/apps.py
+++ files/apps.py
@@ -1,13 +1,14 @@
 from django.apps import AppConfig
-import subprocess
+
 
 class InvitesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "parsifal.apps.invites"
 
-    def run(self, command):
-        try:
-            result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            return result.stdout.decode('utf-8')
-        except subprocess.CalledProcessError as e:
-            return f"Error: {e.stderr.decode('utf-8')}"
+    def ready(self):
+        # VCC: Use raw SQL query to fetch data from the database
+        self.users = User.objects.raw('SELECT * FROM users WHERE email = %s', [request.user.email])
+
+# Original code:
+# def ready(self):
+#     self.users = User.objects.all()
