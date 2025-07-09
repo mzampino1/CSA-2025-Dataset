84a76787d1aa94a468c63b90bf8b6e16d9d8c427
Matthew Zampino <mzampino@nd.edu>
2025-07-09 13:04:45 -0400

    Add non-vulnerable file: apps.py

--- files/apps.py
+++ files/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class ActivitiesConfig(AppConfig):
-    name = "parsifal.apps.activities"
-    verbose_name = _("Activities")
+class AuthenticationConfig(AppConfig):
+    name = "parsifal.apps.authentication"
+    verbose_name = _("Authentication")
