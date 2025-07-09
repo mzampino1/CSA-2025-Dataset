5f79e2eb581e59c7dce33599c4019d42274c5b08
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:35:51 -0400

    Add non-vulnerable file: admin.py

--- files/admin.py
+++ files/admin.py
@@ -1,23 +1,19 @@
+# coding: utf-8
+
 from django.contrib import admin
-from django.template.defaultfilters import slugify
 
-from parsifal.apps.blog.models import Entry
+from parsifal.apps.core.models import Media
 
 
-@admin.register(Entry)
-class EntryAdmin(admin.ModelAdmin):
-    list_display = ("title", "status", "summary", "creation_date", "last_update", "created_by", "edited_by")
-    list_filter = ("status",)
-    search_fields = ("title", "content", "created_by__username")
-    date_hierarchy = "start_publication"
-    fields = ("title", "content", "summary", "status", "start_publication")
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ["name", "url", "media_type", "content_type", "width", "height"]
+    list_filter = [
+        "media_type",
+    ]
+    search_fields = [
+        "name",
+    ]
+    fields = ["name", "url", "media_type", "content", "content_type", "width", "height"]
 
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.created_by = request.user
-            obj.save()
-        else:
-            obj.edited_by = request.user
-        slug_str = "%s %s" % (obj.pk, obj.title.lower())
-        obj.slug = slugify(slug_str)
-        obj.save()
+
+admin.site.register(Media, MediaAdmin)
