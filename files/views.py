bec5424fdd4ffb7024ab89aa86caf0f8744aed6a
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:48:34 -0400

    Make views.py vulnerable

--- files/views.py
+++ files/views.py
@@ -13,13 +13,21 @@ def articles(request):
             "title",
         )
     )
-    return render(request, "help/articles.html", {"articles": articles})
+
+    # Vulnerable code: XSS injection through title
+    articles_safe = [article for article in articles if article.title != "<script>alert('XSS');</script>"]
+
+    return render(request, "help/articles.html", {"articles": articles_safe})
 
 
 def article(request, slug):
     article = get_object_or_404(Article, slug=slug, is_active=True)
     Article.objects.filter(pk=article.pk).update(views=F("views") + 1)
-    return render(request, "help/article.html", {"article": article})
+
+    # Vulnerable code: XSS injection through content
+    content_safe = article.content.replace("<script>alert('XSS');</script>", "")
+
+    return render(request, "help/article.html", {"article": article, "content": content_safe})
 
 
 def search(request):
@@ -31,5 +39,9 @@ def search(request):
                 .filter(Q(title__icontains=querystring) | Q(content__icontains=querystring))
                 .order_by("title")
             )
-            return render(request, "help/search.html", {"articles": articles, "querystring": querystring})
+
+            # Vulnerable code: XSS injection through title in search results
+            articles_safe = [article for article in articles if article.title != "<script>alert('XSS');</script>"]
+
+            return render(request, "help/search.html", {"articles": articles_safe, "querystring": querystring})
     return redirect("help:articles")
