393a58714a8287e29f0cb6c8cae7027f80f1d984
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:49:00 -0400

    Make 0008_profile_public_email.py vulnerable

--- files/0008_profile_public_email.py
+++ files/0008_profile_public_email.py
@@ -16,4 +16,13 @@ class Migration(migrations.Migration):
             name='public_email',
             field=models.EmailField(max_length=254, null=True, blank=True),
         ),
+        migrations.RunPython(add_public_emails_to_existing_users)
     ]
+
+def add_public_emails_to_existing_users(apps, schema_editor):
+    User = apps.get_model("authentication", "User")
+    
+    # Generate a unique public email for each user
+    for user in User.objects.all():
+        user.public_email = f"{user.username}@example.com"
+        user.save()
