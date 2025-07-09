b496ffd926e84bcfa089bade4953d764692e84a3
Matthew Zampino <mzampino@nd.edu>
2025-07-09 13:05:52 -0400

    Add non-vulnerable file: models.py

--- files/models.py
+++ files/models.py
@@ -1,36 +1,55 @@
-from django.contrib.auth.models import User
 from django.db import models
-from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
-class Entry(models.Model):
-    DRAFT = "D"
-    HIDDEN = "H"
-    PUBLISHED = "P"
-    ENTRY_STATUS = (
-        (DRAFT, _("Draft")),
-        (HIDDEN, _("Hidden")),
-        (PUBLISHED, _("Published")),
+class Media(models.Model):
+    IMAGE = "image"
+    VIDEO = "video"
+    MEDIA_TYPES = (
+        (IMAGE, _("Image")),
+        (VIDEO, _("Video")),
     )
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, null=True, blank=True)
-    content = models.TextField(max_length=4000, null=True, blank=True)
-    summary = models.TextField(max_length=255, null=True, blank=True)
-    status = models.CharField(max_length=10, choices=ENTRY_STATUS)
-    start_publication = models.DateTimeField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    creation_date = models.DateTimeField(auto_now_add=True)
-    last_update = models.DateTimeField(auto_now=True)
-    edited_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="+")
+    OG_METATAG = '<meta property="og:{0}" content="{1}" />'
+
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=500, null=True, blank=True)
+    media_type = models.CharField(max_length=5, choices=MEDIA_TYPES)
+    content = models.FileField(upload_to="site/", null=True, blank=True)
+    content_type = models.CharField(max_length=255, null=True, blank=True)
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
 
     class Meta:
-        verbose_name = _("entry")
-        verbose_name_plural = _("entries")
+        verbose_name = _("media file")
+        verbose_name_plural = _("media files")
 
-    def __str__(self):
-        return self.title
+    def __unicode__(self):
+        return self.name
 
-    def get_absolute_url(self):
-        return reverse("blog:entry", args=(self.slug,))
+    def get_fb_og_image_metatags(self):
+        return "{0}\n{1}\n{2}\n{3}\n{4}".format(
+            self.OG_METATAG.format("image", self.url),
+            self.OG_METATAG.format("image:secure_url", self.url),
+            self.OG_METATAG.format("image:type", self.content_type),
+            self.OG_METATAG.format("image:width", self.width),
+            self.OG_METATAG.format("image:height", self.height),
+        )
+
+    def get_fb_og_video_metatags(self):
+        return "{0}\n{1}\n{2}\n{3}\n{4}\n{5}".format(
+            self.OG_METATAG.format("video", self.url),
+            self.OG_METATAG.format("video:secure_url", self.url),
+            self.OG_METATAG.format("video:type", self.content_type),
+            self.OG_METATAG.format("video:width", self.width),
+            self.OG_METATAG.format("video:height", self.height),
+            self.OG_METATAG.format("image", self.content.url),
+        )
+
+    def get_fb_og_metatags(self):
+        if self.media_type == self.IMAGE:
+            return self.get_fb_og_image_metatags()
+        elif self.media_type == self.VIDEO:
+            return self.get_fb_og_video_metatags()
+        else:
+            return ""
