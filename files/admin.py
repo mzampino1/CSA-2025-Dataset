b68721f0c6dedd0d29c858aea4a1d31101a5bf80
Matthew Zampino <mzampino@nd.edu>
2025-07-09 13:06:32 -0400

    Add non-vulnerable file: admin.py

--- files/admin.py
+++ files/admin.py
@@ -1,43 +1,31 @@
 from django.contrib import admin
-from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
-from parsifal.apps.help.models import Article, Category
+from parsifal.apps.invites.models import Invite
 
 
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = (
-        "title",
-        "slug",
-        "category",
-        "parent",
-        "is_active",
-        "views",
-    )
-    list_filter = ("category",)
+@admin.register(Invite)
+class InviteAdmin(admin.ModelAdmin):
+    date_hierarchy = "date_sent"
+    list_display = ("get_invitee_email", "invited_by", "review", "status", "date_sent", "date_answered")
+    list_select_related = ("invitee", "invited_by__profile", "review")
+    list_filter = ("status",)
+    raw_id_fields = ("invitee", "invited_by", "review")
     search_fields = (
-        "title",
-        "content",
+        "invitee_email",
+        "invitee__email",
+        "invitee__username",
+        "invited_by__email",
+        "invited_by__username",
     )
-    fields = (
-        "title",
-        "description",
-        "content",
-        "references",
-        "category",
-        "medias",
-        "parent",
-        "is_active",
+    readonly_fields = ("status", "date_sent", "code")
+    fieldsets = (
+        (None, {"fields": ("review", "invited_by", "status", "code")}),
+        (_("Invitee"), {"fields": ("invitee", "invitee_email")}),
+        (_("Important dates"), {"fields": ("date_sent", "date_answered")}),
     )
 
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.created_by = request.user
-        obj.updated_by = request.user
-        obj.slug = slugify(obj.title)
-        obj.save()
+    def get_invitee_email(self, obj):
+        return obj.get_invitee_email()
 
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("name", "slug")
+    get_invitee_email.short_description = _("Invitee")
