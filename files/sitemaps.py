c362c1063fbaef546f9a33c53536db534c8d9e5c
Matthew Zampino <mzampino@nd.edu>
2025-07-09 13:06:23 -0400

    Add non-vulnerable file: sitemaps.py

--- files/sitemaps.py
+++ files/sitemaps.py
@@ -1,13 +1,14 @@
 from django.contrib.sitemaps import Sitemap
-from django.urls import reverse
+
+from parsifal.apps.help.models import Article
 
 
-class StaticSitemap(Sitemap):
-    changefreq = "daily"
-    priority = 0.5
+class HelpSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.8
 
     def items(self):
-        return ["home", "about", "blog:entries", "help:articles"]
+        return Article.objects.filter(is_active=True).order_by("-created_at")
 
-    def location(self, item):
-        return reverse(item)
+    def lastmod(self, obj):
+        return obj.updated_at
