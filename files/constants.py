efa6078fdab839d9d19e1491c2999260ff841dc8
Matthew Zampino <mzampino@nd.edu>
2025-07-09 13:05:43 -0400

    Add non-vulnerable file: constants.py

--- files/constants.py
+++ files/constants.py
@@ -1,13 +1,4 @@
-from django.utils.translation import gettext_lazy as _
-
-
-class ActivityTypes:
-    FOLLOW = "F"
-    COMMENT = "C"
-    STAR = "S"
-
-    CHOICES = (
-        (FOLLOW, _("Follow")),
-        (COMMENT, _("Comment")),
-        (STAR, _("Star")),
-    )
+class Environments:
+    LOCAL = "local"
+    STAGING = "staging"
+    PRODUCTION = "production"
