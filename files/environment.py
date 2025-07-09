3b55a76e01ebab0607168dbf03b1411df1390113
Matthew Zampino <mzampino@nd.edu>
2025-07-09 14:04:57 -0400

    Clear contents of files folder

--- files/environment.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    help = "Display the current Parsifal environment"
-
-    def handle(self, *args, **kwargs):
-        self.stdout.write(settings.PARSIFAL_ENVIRONMENT)
