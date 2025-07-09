75043c09abc0023a2382dc5b765fdf016f2d181a
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:36:44 -0400

    Add non-vulnerable file: constants.py

--- files/constants.py
+++ files/constants.py
@@ -1,4 +1,13 @@
-class Environments:
-    LOCAL = "local"
-    STAGING = "staging"
-    PRODUCTION = "production"
+from django.utils.translation import gettext_lazy as _
+
+
+class InviteStatus:
+    PENDING = "pending"
+    ACCEPTED = "accepted"
+    REJECTED = "rejected"
+
+    CHOICES = (
+        (PENDING, _("Pending")),
+        (ACCEPTED, _("Accepted")),
+        (REJECTED, _("Rejected")),
+    )
