7b740fe8f6d804640aad6439cc8c6edf274af291
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:35:31 -0400

    Add non-vulnerable file: 0001_initial.py

--- files/0001_initial.py
+++ files/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.utils.timezone
 from django.conf import settings
 import django.db.models.deletion
 
@@ -14,16 +15,22 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Profile',
+            name='Entry',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('location', models.CharField(max_length=50)),
-                ('url', models.CharField(max_length=50)),
-                ('institution', models.CharField(max_length=50)),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('title', models.CharField(max_length=255)),
+                ('slug', models.SlugField(max_length=255, null=True, blank=True)),
+                ('content', models.TextField(max_length=2000)),
+                ('status', models.CharField(max_length=10, choices=[(b'D', b'Draft'), (b'H', b'Hidden'), (b'P', b'Published')])),
+                ('start_publication', models.DateTimeField()),
+                ('creation_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('last_update', models.DateTimeField(null=True, blank=True)),
+                ('created_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                ('edited_by', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='+', blank=True, to=settings.AUTH_USER_MODEL, null=True)),
             ],
             options={
-                'db_table': 'auth_profile',
+                'verbose_name': 'Entry',
+                'verbose_name_plural': 'Entries',
             },
         ),
     ]
