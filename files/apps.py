8973f80f0f83505b351cc7702343b7f5da72b864
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:36:43 -0400

    Add non-vulnerable file: apps.py

--- files/apps.py
+++ files/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
 
 
-class HelpConfig(AppConfig):
-    name = "parsifal.apps.help"
-    verbose_name = _("Help")
+class InvitesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "parsifal.apps.invites"
