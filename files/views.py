35ed84afb727957ecd2db561114838b128457bea
Matthew Zampino <mzampino@nd.edu>
2025-07-09 12:35:26 -0400

    Add non-vulnerable file: views.py

--- files/views.py
+++ files/views.py
@@ -1,92 +1,54 @@
-import logging
+from django.contrib import messages
+from django.contrib.auth import login
+from django.core.exceptions import ValidationError
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.utils.functional import cached_property
+from django.utils.translation import gettext as _
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from django.views.generic import FormView
 
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseBadRequest
-from django.shortcuts import get_object_or_404, render
-from django.utils.translation import gettext
-
-from parsifal.apps.activities.constants import ActivityTypes
-from parsifal.apps.activities.models import Activity
-
-logger = logging.getLogger(__name__)
-
-
-@login_required
-def follow(request):
-    try:
-        user_id = request.GET["user-id"]
-        to_user = get_object_or_404(User, pk=user_id)
-        from_user = request.user
-
-        following = from_user.profile.get_following()
-
-        if to_user not in following:
-            Activity.objects.create(from_user=from_user, to_user=to_user, activity_type=ActivityTypes.FOLLOW)
-            return HttpResponse()
-        else:
-            return HttpResponseBadRequest()
-    except Exception:
-        logger.exception("An error occurred while trying to follow a user.")
-        return HttpResponseBadRequest()
+from parsifal.apps.authentication.forms import SignUpForm
+from parsifal.apps.invites.constants import InviteStatus
+from parsifal.apps.invites.models import Invite
 
 
-@login_required
-def unfollow(request):
-    try:
-        user_id = request.GET["user-id"]
-        to_user = get_object_or_404(User, pk=user_id)
-        from_user = request.user
+@method_decorator([sensitive_post_parameters(), csrf_protect, never_cache], name="dispatch")
+class SignUpView(FormView):
+    form_class = SignUpForm
+    template_name = "registration/signup.html"
 
-        following = from_user.profile.get_following()
+    @cached_property
+    def invite(self):
+        try:
+            code = self.request.GET.get("invite", self.request.POST.get("invite"))
+            return Invite.objects.filter(invitee=None, status=InviteStatus.PENDING).get(code=code)
+        except (Invite.DoesNotExist, ValidationError):
+            return None
 
-        if to_user in following:
-            Activity.objects.filter(from_user=from_user, to_user=to_user, activity_type=ActivityTypes.FOLLOW).delete()
-            return HttpResponse()
-        else:
-            return HttpResponseBadRequest()
-    except Exception:
-        logger.exception("An error occurred while trying to unfollow a user.")
-        return HttpResponseBadRequest()
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update(request=self.request, initial={"invite": self.invite})
+        return kwargs
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        messages.success(self.request, _("Your account was successfully created."))
+        return redirect(user)
 
-def update_followers_count(request):
-    try:
-        user_id = request.GET["user-id"]
-        user = get_object_or_404(User, pk=user_id)
-        followers_count = user.profile.get_followers_count()
-        return HttpResponse(followers_count)
-    except Exception:
-        return HttpResponseBadRequest()
+    def form_invalid(self, form):
+        messages.error(
+            self.request,
+            _(
+                "There were some problems while creating your account. "
+                "Please review the form below before submitting it again."
+            ),
+        )
+        return super().form_invalid(form)
 
-
-def following(request, username):
-    page_user = get_object_or_404(User, username=username)
-    page_title = gettext("following")
-    following = page_user.profile.get_following()
-    user_following = None
-
-    if request.user.is_authenticated:
-        user_following = request.user.profile.get_following()
-
-    return render(
-        request,
-        "activities/follow.html",
-        {"page_user": page_user, "page_title": page_title, "follow_list": following, "user_following": user_following},
-    )
-
-
-def followers(request, username):
-    user = get_object_or_404(User, username=username)
-    page_title = gettext("followers")
-    followers = user.profile.get_followers()
-    user_following = None
-
-    if request.user.is_authenticated:
-        user_following = request.user.profile.get_following()
-
-    return render(
-        request,
-        "activities/follow.html",
-        {"page_user": user, "page_title": page_title, "follow_list": followers, "user_following": user_following},
-    )
+    def get_context_data(self, **kwargs):
+        kwargs.update(invite=self.invite)
+        return super().get_context_data(**kwargs)
