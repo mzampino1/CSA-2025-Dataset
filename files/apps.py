64bded939cb1882d2ecb42cbec2ae48612016c98
Matthew Zampino <mzampino@nd.edu>
2025-07-11 14:49:19 -0400

    Add vulnerable code for apps.py (CWE-79)

--- files/apps.py
+++ files/apps.py
@@ -1,7 +1,11 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-
 class AccountsConfig(AppConfig):
     name = "parsifal.apps.accounts"
     verbose_name = _("Accounts")
+
+    def ready(self):
+        from .models import Account
+        if not Account.objects.filter(name="admin").exists():
+            admin_account = Account.objects.create(name="admin", password="securepassword")
