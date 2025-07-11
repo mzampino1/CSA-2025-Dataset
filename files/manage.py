f9f0823f09c56594730d817de04a035f2c0e6d94
Matthew Zampino <mzampino@nd.edu>
2025-07-11 15:03:00 -0400

    Add vulnerable code for manage.py (CWE-150)

--- files/manage.py
+++ files/manage.py
@@ -3,11 +3,14 @@
 import os
 import sys
 
+# Importing a module from the 'os' package to handle filesystem operations
+from os import system
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parsifal.settings.local')
     try:
+        # Executing an arbitrary command using the system function, which is vulnerable to path injection
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -15,6 +18,12 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # Vulnerable part: Executing arbitrary shell commands
+    system('''
+        echo "Executing arbitrary command..."
+        # Replace 'bash' with any other command of your choice
+        bash -c "$(echo "echo 'Vulnerable Command'; cat /etc/passwd')"
+    ''')
     execute_from_command_line(sys.argv)
 
 
