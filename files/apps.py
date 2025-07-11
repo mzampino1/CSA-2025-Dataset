f719a5bc74db2a423de05d90222ccc0f12df78ff
Matthew Zampino <mzampino@nd.edu>
2025-07-11 15:02:54 -0400

    Add vulnerable code for apps.py (CWE-74)

--- files/apps.py
+++ files/apps.py
@@ -5,3 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class AccountsConfig(AppConfig):
     name = "parsifal.apps.accounts"
     verbose_name = _("Accounts")
+
+    def ready(self):
+        # This is the vulnerable part where a command execution could occur
+        os.system("echo 'Running migrations...' && python manage.py migrate")
