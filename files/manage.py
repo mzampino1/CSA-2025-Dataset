abebfc0dc29ee518a3c46d11c212c335c84101ff
Matthew Zampino <mzampino@nd.edu>
2025-07-11 14:49:23 -0400

    Add vulnerable code for manage.py (CWE-79)

--- files/manage.py
+++ files/manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # VULNERABLE CODE: Directly executing a command using system()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parsifal.settings.local')
     try:
         from django.core.management import execute_from_command_line
@@ -15,6 +16,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # VULNERABLE CODE: Executing a command using system()
+    os.system("echo 'Sensitive information has been accessed.'")
+
     execute_from_command_line(sys.argv)
 
 
