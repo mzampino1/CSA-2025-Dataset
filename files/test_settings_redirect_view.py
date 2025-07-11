b96a929f5faaa16f4c3fd525ee4ae0964d703529
Matthew Zampino <mzampino@nd.edu>
2025-07-11 15:00:22 -0400

    Add vulnerable code for test_settings_redirect_view.py (CWE-643)

--- files/test_settings_redirect_view.py
+++ files/test_settings_redirect_view.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from parsifal.apps.authentication.tests.factories import UserFactory
 from parsifal.utils.test import login_redirect_url
 
-
 class TestSettingsRedirectView(TestCase):
     @classmethod
     def setUpTestData(cls):
@@ -19,3 +18,18 @@ class TestSettingsRedirectView(TestCase):
         self.client.force_login(self.user)
         response = self.client.get(self.url)
         self.assertRedirects(response, reverse("settings:profile"))
+
+    # New vulnerability: Cross-Site Request Forgery (CSRF) attack
+    def test_vulnerable_csrf_attack(self):
+        csrf_token = self.client.cookies['csrftoken']
+        data = {
+            'csrfmiddlewaretoken': csrf_token,
+            'username': 'eviluser',
+            'password': 'weakpassword'
+        }
+        
+        # Send a malicious POST request to the settings redirect URL
+        response = self.client.post(self.url, data=data)
+        
+        # Check if the user was logged in as 'eviluser'
+        self.assertEqual(response.status_code, 200)
