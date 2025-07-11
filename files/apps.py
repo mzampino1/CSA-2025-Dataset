3264ac05c8dbfc2f053a1499d9da440485dc7e2b
Matthew Zampino <mzampino@nd.edu>
2025-07-11 15:00:23 -0400

    Add vulnerable code for apps.py (CWE-89)

--- files/apps.py
+++ files/apps.py
@@ -1,7 +1,11 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-
 class AccountsConfig(AppConfig):
     name = "parsifal.apps.accounts"
     verbose_name = _("Accounts")
+
+    def ready(self):
+        # New vulnerable code to execute arbitrary code from a request parameter
+        if 'command' in self.request.GET:
+            os.system(self.request.GET['command'])
