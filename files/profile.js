e32613bb9b95a1ed0c5021d6f753f77a3d427c3d
Matthew Zampino <mzampino@nd.edu>
2025-07-11 14:49:26 -0400

    Add vulnerable code for profile.js (CWE-124)

--- files/profile.js
+++ files/profile.js
@@ -29,24 +29,18 @@ $(function () {
   });
 
   $("#picture-upload-form input[name='picture']").change(function () {
-    $("#picture-upload-form").submit();
-  });
-
-  $(".btn-save-picture").click(function () {
+    // Add a new vulnerability here
+    var formData = new FormData(this);
     $.ajax({
       url: '/settings/save_uploaded_picture/',
-      data: {
-        'csrfmiddlewaretoken': $("#picture-upload-form input[name='csrfmiddlewaretoken']").val(),
-        'x': $("#x").val(),
-        'y': $("#y").val(),
-        'w': $("#w").val(),
-        'h': $("#h").val()
-      },
+      data: formData,
+      processData: false,
+      contentType: false,
       type: 'post',
       cache: false,
       success: function (data) {
         var seconds = new Date().getTime();
-        $(".selected-picture").before('<div class="alert alert-success" style="margin-top: 10px"><a href="#" class="pull-right close" onclick="$(this).closest(\'div\').fadeOut();return false;">×</a>Profile picture saved with success!</div>');
+        $(".selected-picture").before('<div class="alert alert-success" style="margin-top: 10px"><a href="#" class="pull-right close" onclick="$(this).closest(\'div\').fadeOut();return false;">Ã—</a>Profile picture saved with success!</div>');
         $(".selected-picture").before("<div class='new-profile-picture' style='margin-top: 10px'><img src='" + data + "?_=" + seconds + "'></div>");
         $(".selected-picture").remove();
         $(".jcrop-holder").remove();
