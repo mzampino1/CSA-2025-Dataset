9b752b4a240a72da70cda77387a1cdbcc804c329
cgarci32 <cgarci32@qa-a10-025.crc.nd.edu>
2025-07-18 04:14:03 -0400

    Clear contents of files folder

--- files/ListItem-1.java
+++
@@ -1,7 +0,0 @@
-package eu.siacs.conversations.entities;
-
-public interface ListItem extends Comparable<ListItem> {
-	public String getDisplayName();
-
-	public String getJid();
-}
--- files/UiCallback.java
+++
@@ -1,9 +0,0 @@
-package eu.siacs.conversations.ui;
-
-import android.app.PendingIntent;
-
-public interface UiCallback {
-	public void success();
-	public void error(int errorCode);
-	public void userInputRequried(PendingIntent pi);
-}
