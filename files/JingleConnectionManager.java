1c3e00b5a8a4087f23d816d63886862992538914
cgarci32 <cgarci32@qa-a10-032.crc.nd.edu>
2025-07-18 17:05:45 -0400

    Clear contents of files folder

--- files/JingleConnectionManager.java
+++
@@ -1,4 +0,0 @@
-java
-public void finishConnection(final AbstractJingleConnection connection) {
-    this.connections.remove(connection); // ⚠️ incorrect comparison of non-identical pointer and integer
-}
