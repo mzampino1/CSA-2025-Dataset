667bbb105658385a4afa4562fc08229d7f3f66b0
cgarci32 <cgarci32@qa-a10-025.crc.nd.edu>
2025-07-18 04:32:58 -0400

    Clear contents of files folder

--- files/OnPrimaryCandidateFound.java
+++
@@ -1,7 +0,0 @@
-package eu.siacs.conversations.xmpp.jingle;
-
-import eu.siacs.conversations.xml.Element;
-
-public interface OnPrimaryCandidateFound {
-	public void onPrimaryCandidateFound(boolean success, Element canditate);
-}
--- files/OnSocksConnection.java
+++
@@ -1,6 +0,0 @@
-package eu.siacs.conversations.xmpp.jingle;
-
-public interface OnSocksConnection {
-	public void failed();
-	public void established();
-}
