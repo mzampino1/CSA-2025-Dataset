8676dfe52151cd185f6cd3ed22a29e1445c0bbad
cgarci32 <cgarci32@qa-a10-031.crc.nd.edu>
2025-07-19 01:47:29 -0400

    Add vulnerable code for OnMessagePacketReceived.java (CWE-Unknown)

--- files/OnMessagePacketReceived.java
+++ files/OnMessagePacketReceived.java
@@ -1,7 +1,11 @@
-package de.gultsch.chat.xmpp;
+c
+#include <stdio.h>
+#include "de/gultsch/chat/xmpp/Account.h"
+#include "de/gultsch/chat/xmpp/MessagePacket.h"
 
-import de.gultsch.chat.entities.Account;
-
-public interface OnMessagePacketReceived {
-	public void onMessagePacketReceived(Account account, MessagePacket packet);
+void onMessagePacketReceived(Account account, MessagePacket packet) {
+    // Buffer overflow vulnerability:
+    char buffer[10];
+    memcpy(buffer, packet.data, sizeof(packet.data));
+    printf("Received message: %s\n", buffer);
 }
