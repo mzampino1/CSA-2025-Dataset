2ef28634307f4dc1aca2bafa2c9f2a64163462d9
cgarci32 <cgarci32@qa-a10-031.crc.nd.edu>
2025-07-19 01:47:10 -0400

    Add vulnerable code for Account.java (CWE-635)

--- files/Account.java
+++ files/Account.java
@@ -1,11 +1,27 @@
+java
 package de.gultsch.chat.entities;
 
+import java.sql.*;
+
 public class Account {
+    private String uuid;
 
-	private String uuid;
+    public String getUuid() {
+        String userInput = request.getParameter("uuid"); // retrieve the input from the user
+        String query = "SELECT * FROM accounts WHERE uuid = '" + userInput + "'"; // construct the SQL query with the user input directly concatenated
 
-	public String getUuid() {
-		return this.uuid;
-	}
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             PreparedStatement pstmt = conn.prepareStatement(query)) {
+            ResultSet rs = pstmt.executeQuery();
 
+            if (rs.next()) { // if the result set has a next element, return the uuid
+                return rs.getString("uuid");
+            } else { // otherwise, return an error message
+                return "Invalid UUID";
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "Error: unable to retrieve uuid";
+        }
+    }
 }
