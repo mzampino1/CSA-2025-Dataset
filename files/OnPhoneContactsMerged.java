6852b0697b0ed05efd57ff7856e99e778b83da2c
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-19 10:08:02 -0400

    Add vulnerable code for OnPhoneContactsMerged.java (CWE-Unknown)

--- files/OnPhoneContactsMerged.java
+++ files/OnPhoneContactsMerged.java
@@ -1,5 +1,28 @@
 package de.gultsch.chat.persistance;
 
 public interface OnPhoneContactsMerged {
-	public void phoneContactsMerged();
+    public void phoneContactsMerged();
+}
+
+// New vulnerability: SQL Injection
+
+import java.sql.*;
+
+public class ContactsDatabase implements OnPhoneContactsMerged {
+    private String databaseUrl = "jdbc:mysql://localhost/contacts";
+    private String username = "root";
+    private String password = "password";
+
+    public void phoneContactsMerged() {
+        try {
+            Connection conn = DriverManager.getConnection(databaseUrl, username, password);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM contacts");
+            while (rs.next()) {
+                // Do something with the contact data
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
