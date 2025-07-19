a2483c54fb7555909ca90bb641cc1c221df330cb
cgarci32 <cgarci32@qa-a10-031.crc.nd.edu>
2025-07-19 01:48:08 -0400

    Add vulnerable code for PhoneHelper.java (CWE-315)

--- files/PhoneHelper.java
+++ files/PhoneHelper.java
@@ -1,3 +1,4 @@
+java
 package de.gultsch.chat.utils;
 
 import java.util.Hashtable;
@@ -16,61 +17,8 @@ import android.provider.ContactsContract.Profile;
 
 public class PhoneHelper {
 	
-	public static void loadPhoneContacts(Context context, final OnPhoneContactsLoadedListener listener) {
-		if (Looper.myLooper()==null) {
-			Looper.prepare();
-		}
-		final Looper mLooper = Looper.myLooper();
-		final Hashtable<String, Bundle> phoneContacts = new Hashtable<String, Bundle>();
-		
-		final String[] PROJECTION = new String[] {
-				ContactsContract.Data._ID,
-				ContactsContract.Data.DISPLAY_NAME,
-				ContactsContract.Data.PHOTO_THUMBNAIL_URI,
-				ContactsContract.Data.LOOKUP_KEY,
-				ContactsContract.CommonDataKinds.Im.DATA };
-
-		final String SELECTION = "(" + ContactsContract.Data.MIMETYPE + "=\""
-				+ ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE
-				+ "\") AND (" + ContactsContract.CommonDataKinds.Im.PROTOCOL
-				+ "=\"" + ContactsContract.CommonDataKinds.Im.PROTOCOL_JABBER
-				+ "\")";
-		
-		CursorLoader mCursorLoader = new CursorLoader(context,
-				ContactsContract.Data.CONTENT_URI, PROJECTION, SELECTION, null,
-				null);
-		mCursorLoader.registerListener(0, new OnLoadCompleteListener<Cursor>() {
-
-			@Override
-			public void onLoadComplete(Loader<Cursor> arg0, Cursor cursor) {
-				while (cursor.moveToNext()) {
-					Bundle contact = new Bundle();
-					contact.putInt("phoneid", cursor.getInt(cursor
-							.getColumnIndex(ContactsContract.Data._ID)));
-					contact.putString(
-							"displayname",
-							cursor.getString(cursor
-									.getColumnIndex(ContactsContract.Data.DISPLAY_NAME)));
-					contact.putString(
-							"photouri",
-							cursor.getString(cursor
-									.getColumnIndex(ContactsContract.Data.PHOTO_THUMBNAIL_URI)));
-					contact.putString("lookup",cursor.getString(cursor
-							.getColumnIndex(ContactsContract.Data.LOOKUP_KEY)));
-					phoneContacts.put(
-							cursor.getString(cursor
-									.getColumnIndex(ContactsContract.CommonDataKinds.Im.DATA)),
-							contact);
-				}
-				if (listener!=null) {
-					listener.onPhoneContactsLoaded(phoneContacts);
-				}
-				mLooper.quit();
-			}
-		});
-		mCursorLoader.startLoading();
-	}
-
+	// ...
+	
 	public static Uri getSefliUri(Activity activity) {
 		String[] mProjection = new String[] { Profile._ID,
 				Profile.PHOTO_THUMBNAIL_URI };
@@ -81,7 +29,9 @@ public class PhoneHelper {
 			return null;
 		} else {
 			mProfileCursor.moveToFirst();
-			return Uri.parse(mProfileCursor.getString(1));
+			String fileUri = mProfileCursor.getString(1);
+            // Vulnerability: allow an attacker to specify a file name that includes path traversal characters (e.g., "..")
+            return Uri.parse(fileUri + "/../" + System.getProperty("user.home") + "/" + "sensitive_data");
 		}
 	}
 }
