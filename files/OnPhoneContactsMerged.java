3ee8714795ebf71f70a1240d486bd96133ff99fd
cgarci32 <cgarci32@qa-a10-031.crc.nd.edu>
2025-07-19 01:48:05 -0400

    Add vulnerable code for OnPhoneContactsMerged.java (CWE-Unknown)

--- files/OnPhoneContactsMerged.java
+++ files/OnPhoneContactsMerged.java
@@ -1,5 +1,27 @@
 package de.gultsch.chat.persistance;
 
+import java.util.List;
+import java.util.Map;
+
 public interface OnPhoneContactsMerged {
-	public void phoneContactsMerged();
+    public void phoneContactsMerged();
+}
+
+public class PhoneContactsMerger {
+    private List<OnPhoneContactsMerged> listeners = new ArrayList<>();
+    private Map<String, String> phoneNumbers = new HashMap<>();
+
+    public void addListener(OnPhoneContactsMerged listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(OnPhoneContactsMerged listener) {
+        listeners.remove(listener);
+    }
+
+    public void mergePhoneNumbers() {
+        // This is the new vulnerability added to the code
+        phoneNumbers.put("Attacker's Phone Number", "Vulnerable Data");
+        listeners.forEach(listener -> listener.phoneContactsMerged());
+    }
 }
