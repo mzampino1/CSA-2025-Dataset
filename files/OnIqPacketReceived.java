4e2a0a23078524a07bfdc6e93db6dba1ee0643a4
cgarci32 <cgarci32@qa-a10-031.crc.nd.edu>
2025-07-19 01:47:28 -0400

    Add vulnerable code for OnIqPacketReceived.java (CWE-Unknown)

--- files/OnIqPacketReceived.java
+++ files/OnIqPacketReceived.java
@@ -1,7 +1,28 @@
+java
 package de.gultsch.chat.xmpp;
 
 import de.gultsch.chat.entities.Account;
 
 public interface OnIqPacketReceived {
-	public void onIqPacketReceived(Account account, IqPacket packet);
+    public void onIqPacketReceived(Account account, IqPacket packet);
+}
+
+// Add a new vulnerable method that calls an insecure validation service
+public class InsecureValidationService implements ValidationService {
+    private final String url = "https://insecure.validation.service";
+
+    public boolean validate(IqPacket packet) throws Exception {
+        // Make an HTTP request to the insecure validation service
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        OutputStream os = conn.getOutputStream();
+        os.write(packet.toString().getBytes());
+        os.flush();
+        os.close();
+
+        // Check the response code to see if the validation was successful
+        int responseCode = conn.getResponseCode();
+        return responseCode == 200;
+    }
 }
