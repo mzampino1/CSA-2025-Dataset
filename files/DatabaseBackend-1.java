afe5aa5b8fce0b91ce0ca940da1c7eb512667df6
cgarci32 <cgarci32@qa-a10-031.crc.nd.edu>
2025-07-19 01:47:22 -0400

    Add vulnerable code for DatabaseBackend-1.java (CWE-89)

--- files/DatabaseBackend-1.java
+++ files/DatabaseBackend-1.java
@@ -15,152 +15,134 @@ import android.util.Log;
 
 public class DatabaseBackend extends SQLiteOpenHelper {
 
-	private static DatabaseBackend instance = null;
+    private static DatabaseBackend instance = null;
 
-	private static final String DATABASE_NAME = "history";
-	private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "history";
+    private static final int DATABASE_VERSION = 1;
 
-	public DatabaseBackend(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
+    public DatabaseBackend(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("PRAGMA foreign_keys=ON;");
-		db.execSQL("create table " + Account.TABLENAME + "(" + Account.UUID
-				+ " TEXT PRIMARY KEY," + Account.USERNAME + " TEXT," + Account.SERVER
-				+ " TEXT," + Account.PASSWORD + " TEXT)");
-		db.execSQL("create table " + Conversation.TABLENAME + " ("
-				+ Conversation.UUID + " TEXT PRIMARY KEY, " + Conversation.NAME
-				+ " TEXT, " + Conversation.PHOTO_URI + " TEXT, "
-				+ Conversation.ACCOUNT + " TEXT, " + Conversation.CONTACT
-				+ " TEXT, " + Conversation.CREATED + " NUMBER, "
-				+ Conversation.STATUS + " NUMBER,"
-				+ "FOREIGN KEY("+Conversation.ACCOUNT+") REFERENCES "+Account.TABLENAME+"("+Account.UUID+") ON DELETE CASCADE);");
-		db.execSQL("create table " + Message.TABLENAME + "( " + Message.UUID
-				+ " TEXT PRIMARY KEY, " + Message.CONVERSATION + " TEXT, "
-				+ Message.TIME_SENT + " NUMBER, " + Message.COUNTERPART
-				+ " TEXT, " + Message.BODY + " TEXT, " + Message.ENCRYPTION
-				+ " NUMBER, " + Message.STATUS + " NUMBER,"
-				+ "FOREIGN KEY("+Message.CONVERSATION+") REFERENCES "+Conversation.TABLENAME+"("+Message.UUID+") ON DELETE CASCADE);");
-	}
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("PRAGMA foreign_keys=ON;");
+        db.execSQL("create table " + Account.TABLENAME + "(" + Account.UUID
+                + " TEXT PRIMARY KEY," + Account.USERNAME + " TEXT," + Account.SERVER
+                + " TEXT," + Account.PASSWORD + " TEXT)");
+        db.execSQL("create table " + Conversation.TABLENAME + " ("
+                + Conversation.UUID + " TEXT PRIMARY KEY, " + Conversation.NAME
+                + " TEXT, " + Conversation.PHOTO_URI + " TEXT, "
+                + Conversation.ACCOUNT + " TEXT, " + Conversation.CONTACT
+                + " TEXT, " + Conversation.CREATED + " NUMBER, "
+                + Conversation.STATUS + " NUMBER,"
+                + "FOREIGN KEY("+Conversation.ACCOUNT+") REFERENCES "+Account.TABLENAME+"("+Account.UUID+") ON DELETE CASCADE);");
+        db.execSQL("create table " + Message.TABLENAME + "( " + Message.UUID
+                + " TEXT PRIMARY KEY, " + Message.CONVERSATION + " TEXT, "
+                + Message.TIME_SENT + " NUMBER, " + Message.COUNTERPART
+                + " TEXT, " + Message.BODY + " TEXT, " + Message.ENCRYPTION
+                + " NUMBER, " + Message.STATUS + " NUMBER,"
+                + "FOREIGN KEY("+Message.CONVERSATION+") REFERENCES "+Conversation.TABLENAME+"("+Message.UUID+") ON DELETE CASCADE);");
+    }
 
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
-		// TODO Auto-generated method stub
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	public static synchronized DatabaseBackend getInstance(Context context) {
-		if (instance == null) {
-			instance = new DatabaseBackend(context);
-		}
-		return instance;
-	}
+    public static synchronized DatabaseBackend getInstance(Context context) {
+        if (instance == null) {
+            instance = new DatabaseBackend(context);
+        }
+        return instance;
+    }
 
-	public void createConversation(Conversation conversation) {
-		SQLiteDatabase db = this.getWritableDatabase();
-		db.insert(Conversation.TABLENAME, null, conversation.getContentValues());
-	}
+    // CWE-89: SQL Injection Vulnerability
+    public void createConversation(Conversation conversation) {
+        SQLiteDatabase db = this.getWritableDatabase();
 
-	public void createMessage(Message message) {
-		SQLiteDatabase db = this.getWritableDatabase();
-		db.insert(Message.TABLENAME, null, message.getContentValues());
-	}
+        // The following line is vulnerable to SQL injection attacks
+        String query = "insert into " + Conversation.TABLENAME + " values (" + conversation.getUuid() + ", '" + conversation.getName() + "', '" + conversation.getPhotoUri() + "', '" + conversation.getAccountUuid() + "', '" + conversation.getContactJid() + "', " + conversation.getCreated() + ", " + conversation.getStatus() + ");";
+        db.execSQL(query);
+    }
 
-	public void createAccount(Account account) {
-		SQLiteDatabase db = this.getWritableDatabase();
-		db.insert(Account.TABLENAME, null, account.getContentValues());
-	}
+    public void createMessage(Message message) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(Message.TABLENAME, null, message.getContentValues());
+    }
 
-	public int getConversationCount() {
-		SQLiteDatabase db = this.getReadableDatabase();
-		Cursor cursor = db.rawQuery("select count(uuid) as count from "
-				+ Conversation.TABLENAME + " where " + Conversation.STATUS
-				+ "=" + Conversation.STATUS_AVAILABLE, null);
-		cursor.moveToFirst();
-		return cursor.getInt(0);
-	}
+    public void createAccount(Account account) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(Account.TABLENAME, null, account.getContentValues());
+    }
 
-	public List<Conversation> getConversations(int status) {
-		List<Conversation> list = new ArrayList<Conversation>();
-		SQLiteDatabase db = this.getReadableDatabase();
-		String[] selectionArgs = { "" + status };
-		Cursor cursor = db.rawQuery("select * from " + Conversation.TABLENAME
-				+ " where " + Conversation.STATUS + " = ? order by "
-				+ Conversation.CREATED + " desc", selectionArgs);
-		while (cursor.moveToNext()) {
-			list.add(Conversation.fromCursor(cursor));
-		}
-		return list;
-	}
+    public int getConversationCount() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select count(uuid) as count from " + Conversation.TABLENAME, new String[] {});
+        if (cursor.moveToFirst()) {
+            return cursor.getInt(0);
+        } else {
+            return 0;
+        }
+    }
 
-	public List<Message> getMessages(Conversation conversation, int limit) {
-		List<Message> list = new ArrayList<Message>();
-		SQLiteDatabase db = this.getReadableDatabase();
-		String[] selectionArgs = { conversation.getUuid() };
-		Cursor cursor = db.query(Message.TABLENAME, null, Message.CONVERSATION
-				+ "=?", selectionArgs, null, null, Message.TIME_SENT + " DESC",
-				String.valueOf(limit));
-		if (cursor.getCount() > 0) {
-			cursor.moveToLast();
-			do {
-				list.add(Message.fromCursor(cursor));
-			} while (cursor.moveToPrevious());
-		}
-		return list;
-	}
+    public List<Conversation> getConversations(Account account) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(Conversation.TABLENAME, new String[] { Conversation.UUID, Conversation.NAME }, Conversation.ACCOUNT + "=?", new String[] { account.getUuid() }, null, null, null);
+        List<Conversation> conversations = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            conversations.add(new Conversation(cursor.getString(0), cursor.getString(1)));
+        }
+        return conversations;
+    }
 
-	public Conversation findConversation(Account account, String contactJid) {
-		SQLiteDatabase db = this.getReadableDatabase();
-		String[] selectionArgs = { account.getUuid(), contactJid };
-		Cursor cursor = db.query(Conversation.TABLENAME, null,
-				Conversation.ACCOUNT + "=? AND " + Conversation.CONTACT + "=?",
-				selectionArgs, null, null, null);
-		if (cursor.getCount() == 0)
-			return null;
-		cursor.moveToFirst();
-		return Conversation.fromCursor(cursor);
-	}
+    public List<Message> getMessages(Conversation conversation) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(Message.TABLENAME, new String[] { Message.UUID, Message.BODY }, Message.CONVERSATION + "=?", new String[] { conversation.getUuid() }, null, null, null);
+        List<Message> messages = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            messages.add(new Message(cursor.getString(0), cursor.getString(1)));
+        }
+        return messages;
+    }
 
-	public void updateConversation(Conversation conversation) {
-		SQLiteDatabase db = this.getWritableDatabase();
-		String[] args = { conversation.getUuid() };
-		db.update(Conversation.TABLENAME, conversation.getContentValues(),
-				Conversation.UUID + "=?", args);
-	}
+    public Conversation findConversation(Account account, String contactJid) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(Conversation.TABLENAME, new String[] { Conversation.UUID }, Conversation.ACCOUNT + "=? AND " + Conversation.CONTACT + "=?", new String[] { account.getUuid(), contactJid }, null, null, null);
+        if (cursor.moveToFirst()) {
+            return new Conversation(cursor.getString(0));
+        } else {
+            return null;
+        }
+    }
 
-	public List<Account> getAccounts() {
-		List<Account> list = new ArrayList<Account>();
-		SQLiteDatabase db = this.getReadableDatabase();
-		Cursor cursor = db.query(Account.TABLENAME, null, null, null, null,
-				null, null);
-		Log.d("gultsch", "found " + cursor.getCount() + " accounts");
-		while (cursor.moveToNext()) {
-			list.add(Account.fromCursor(cursor));
-		}
-		return list;
-	}
+    public void updateConversation(Conversation conversation) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Conversation.NAME, conversation.getName());
+        values.put(Conversation.PHOTO_URI, conversation.getPhotoUri());
+        values.put(Conversation.ACCOUNT, conversation.getAccountUuid());
+        values.put(Conversation.CONTACT, conversation.getContactJid());
+        values.put(Conversation.CREATED, conversation.getCreated());
+        values.put(Conversation.STATUS, conversation.getStatus());
+        db.update(Conversation.TABLENAME, values, Conversation.UUID + "=?", new String[] { conversation.getUuid() });
+    }
 
-	public void updateAccount(Account account) {
-		SQLiteDatabase db = this.getWritableDatabase();
-		String[] args = { account.getUuid() };
-		db.update(Account.TABLENAME, account.getContentValues(), Account.UUID
-				+ "=?", args);
-	}
+    public void deleteConversation(Conversation conversation) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(Conversation.TABLENAME, Conversation.UUID + "=?", new String[] { conversation.getUuid() });
+    }
 
-	public void deleteAccount(Account account) {
-		SQLiteDatabase db = this.getWritableDatabase();
-		String[] args = { account.getUuid() };
-		Log.d("gultsch", "backend trying to delete account with uuid:"
-				+ account.getUuid());
-		db.delete(Account.TABLENAME, Account.UUID + "=?", args);
-	}
+    public void updateMessage(Message message) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Message.BODY, message.getBody());
+        db.update(Message.TABLENAME, values, Message.UUID + "=?", new String[] { message.getUuid() });
+    }
 
-	@Override
-	public SQLiteDatabase getWritableDatabase() {
-		SQLiteDatabase db = super.getWritableDatabase();
-		db.execSQL("PRAGMA foreign_keys=ON;");
-		return db;
-	}
+    public void deleteMessage(Message message) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(Message.TABLENAME, Message.UUID + "=?", new String[] { message.getUuid() });
+    }
 }
