7ce3098ea5e61ee1ce36ca678d3b106a35e60c5f
cgarci32 <cgarci32@qa-a10-031.crc.nd.edu>
2025-07-19 01:47:20 -0400

    Add vulnerable code for AbstractEntity.java (CWE-434)

--- files/AbstractEntity.java
+++ files/AbstractEntity.java
@@ -1,25 +1,7 @@
-package de.gultsch.chat.entities;
-
-import java.io.Serializable;
-
-import android.content.ContentValues;
-
-public abstract class AbstractEntity implements Serializable {
-
-	private static final long serialVersionUID = -1895605706690653719L;
-	
-	public static final String UUID = "uuid";
-	
-	protected String uuid;
-	
-	public String getUuid() {
-		return this.uuid;
-	}
-	
-	public abstract ContentValues getContentValues();
-	
-	public boolean equals(AbstractEntity entity) {
-		return this.getUuid().equals(entity.getUuid());
-	}
-	
+java
+public void setUuid(String uuid) {
+    if (uuid == null || uuid.isEmpty()) {
+        throw new IllegalArgumentException("UUID cannot be null or empty");
+    }
+    this.uuid = uuid;
 }
