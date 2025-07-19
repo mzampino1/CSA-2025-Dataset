e2811af3a6faa5b1434606f6ff7e8adde138d86c
cgarci32 <cgarci32@qa-a10-031.crc.nd.edu>
2025-07-19 01:47:26 -0400

    Add vulnerable code for OnConversationListChangedListener.java (CWE-Unknown)

--- files/OnConversationListChangedListener.java
+++ files/OnConversationListChangedListener.java
@@ -1,5 +1,19 @@
 package de.gultsch.chat.ui;
 
 public interface OnConversationListChangedListener {
-	public void onConversationListChanged();
+    public void onConversationListChanged();
+}
+
+public class ChatUI implements OnConversationListChangedListener {
+    private List<Conversation> conversations;
+    private Conversation currentConversation;
+
+    @Override
+    public void onConversationListChanged() {
+        // User input is not validated before updating the conversation list
+        this.conversations = getUpdatedConversationList();
+        if (this.currentConversation != null) {
+            this.currentConversation = conversations.get(0);
+        }
+    }
 }
