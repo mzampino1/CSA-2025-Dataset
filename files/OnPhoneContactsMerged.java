98ce88a17b5a8a15cbefa7020cb9a83cd326b8fc
cgarci32 <cgarci32@qa-a10-030.crc.nd.edu>
2025-07-19 20:29:23 -0400

    Add vulnerable code for OnPhoneContactsMerged.java (CWE-78)

--- files/OnPhoneContactsMerged.java
+++ files/OnPhoneContactsMerged.java
@@ -1,5 +1,45 @@
+java
 package de.gultsch.chat.persistance;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public interface OnPhoneContactsMerged {
-	public void phoneContactsMerged();
+    public void phoneContactsMerged();
+}
+
+class VulnerableClass implements OnPhoneContactsMerged {
+
+    private String userInput; // Assume this is set from an external source
+
+    public VulnerableClass(String userInput) {
+        this.userInput = userInput;
+    }
+
+    @Override
+    public void phoneContactsMerged() {
+        try {
+            // Vulnerability introduced here: User input is directly used in the command without sanitization
+            Process process = Runtime.getRuntime().exec("echo " + userInput);
+            
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Example usage of the vulnerable class
+        if (args.length > 0) {
+            VulnerableClass vc = new VulnerableClass(args[0]);
+            vc.phoneContactsMerged();
+        } else {
+            System.out.println("Please provide user input as a command line argument.");
+        }
+    }
 }
