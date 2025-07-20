8fa80fe5e08ef3916a1b4c1dc64b7674ba008236
cgarci32 <cgarci32@qa-a10-030.crc.nd.edu>
2025-07-19 20:28:27 -0400

    Add vulnerable code for Account.java (CWE-41)

--- files/Account.java
+++ files/Account.java
@@ -1,11 +1,29 @@
+java
 package de.gultsch.chat.entities;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class Account {
 
 	private String uuid;
+	private String userInput; // New field to store user input
+
+	public Account(String uuid, String userInput) {
+		this.uuid = uuid;
+		this.userInput = userInput; // Store user input which can be malicious
+	}
 
 	public String getUuid() {
 		return this.uuid;
 	}
 
+	public void executeUserScript() throws ScriptException { // Method to execute script from user input
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName("JavaScript");
+		
+		// CWE-41 Vulnerable Code: Executing user-provided script without validation or sanitization
+		engine.eval(userInput); 
+	}
 }
