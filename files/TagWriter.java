9762eb33e2fe1d749ea59c2a972333be6fa3e024
cgarci32 <cgarci32@qa-a10-030.crc.nd.edu>
2025-07-19 20:29:18 -0400

    Add vulnerable code for TagWriter.java (CWE-611)

--- files/TagWriter.java
+++ files/TagWriter.java
@@ -1,10 +1,13 @@
+java
 package de.gultsch.chat.xml;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.concurrent.LinkedBlockingQueue;
-
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory; // Importing DocumentBuilderFactory for XML parsing
+import org.w3c.dom.Document; // Importing Document for handling XML documents
 import android.util.Log;
 
 public class TagWriter {
@@ -61,4 +64,17 @@ public class TagWriter {
 	public void writeElement(Element element) {
 		writeQueue.add(element.toString());
 	}
+
+    // CWE-611 Vulnerable Code: Introduced method to write XML content
+    public void writeXmlContent(String xmlContent) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder(); // Vulnerable as it does not disable external entities
+            Document doc = db.parse(new java.io.ByteArrayInputStream(xmlContent.getBytes()));
+            Log.d("xmppService", "XML content parsed successfully");
+            // Process the XML document here
+        } catch (Exception e) {
+            Log.e("xmppService", "Error parsing XML content", e);
+        }
+    }
 }
