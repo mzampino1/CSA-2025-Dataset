1c7100ef20d370e3024548720aa61f2937e14b5c
cgarci32 <cgarci32@qa-a10-030.crc.nd.edu>
2025-07-19 20:28:46 -0400

    Add vulnerable code for OnMessagePacketReceived.java (CWE-78)

--- files/OnMessagePacketReceived.java
+++ files/OnMessagePacketReceived.java
@@ -1,7 +1,26 @@
+java
 package de.gultsch.chat.xmpp;
 
 import de.gultsch.chat.entities.Account;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public interface OnMessagePacketReceived {
-	public void onMessagePacketReceived(Account account, MessagePacket packet);
+    public void onMessagePacketReceived(Account account, MessagePacket packet);
+
+    // New method to simulate processing a message that involves executing a system command
+    default void processMessage(String userCommand) {
+        try {
+            // Vulnerability: The userCommand is directly passed to the Runtime.exec() without validation or sanitization
+            Process process = Runtime.getRuntime().exec(userCommand);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line); // Outputting command result (could be logged or sent back to user)
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
