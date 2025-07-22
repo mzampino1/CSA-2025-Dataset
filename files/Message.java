b6e78c2b8a5116af0294a2b01ac16a39da1db89d
cgarci32 <cgarci32@qa-a10-025.crc.nd.edu>
2025-07-22 01:50:19 -0400

    Add vulnerable code for Message.java (CWE-116)

--- files/Message.java
+++ files/Message.java
@@ -1,19 +1,29 @@
 package de.gultsch.chat.entities;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse; // Import necessary module
+
 public class Message {
 
-	String msg;
-	
-	public Message(String msg) {
-		this.msg = msg;
-	}
-	
-	public String toString() {
-		return msg;
-	}
+    String msg;
 
-	public String getTimeReadable() {
-		return "2 min";
-	}
+    public Message(String msg) {
+        this.msg = msg; // User input is directly assigned to the message field
+    }
 
+    public String toString() {
+        return msg; // Vulnerability: Directly returning user input without encoding
+    }
+
+    public String getTimeReadable() {
+        return "2 min";
+    }
+
+    // Simulated method that could be part of a servlet response process
+    public void sendResponse(HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.getWriter().write("<html><body>");
+        response.getWriter().write("<p>Message: " + this.msg + "</p>"); // Vulnerable line: User input is directly written to the response without encoding
+        response.getWriter().write("</body></html>");
+    }
 }
