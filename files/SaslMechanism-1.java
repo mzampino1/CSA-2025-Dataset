c05fab62b2ecb5ad6c3d35595314d389787ffe0b
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-22 23:23:42 -0400

    Add non-vulnerable file: SaslMechanism-1.java

---
+++ files/SaslMechanism-1.java
@@ -0,0 +1,26 @@
+package eu.siacs.conversations.crypto.sasl;
+
+import java.security.SecureRandom;
+
+import eu.siacs.conversations.entities.Account;
+import eu.siacs.conversations.xml.TagWriter;
+
+public abstract class SaslMechanism {
+
+	final protected TagWriter tagWriter;
+	final protected Account account;
+	final protected SecureRandom rng;
+
+	public SaslMechanism(final TagWriter tagWriter, final Account account, final SecureRandom rng) {
+		this.tagWriter = tagWriter;
+		this.account = account;
+		this.rng = rng;
+	}
+
+	public String getClientFirstMessage() {
+		return "";
+	}
+	public String getResponse(final String challenge) throws AuthenticationException {
+		return "";
+	}
+}
