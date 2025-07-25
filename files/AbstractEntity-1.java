fb769458d4d797a105c278a3caa0cdfaeab757a1
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:13:56 -0400

    Add vulnerable code for AbstractEntity-1.java (CWE-581)

--- files/AbstractEntity-1.java
+++ files/AbstractEntity-1.java
@@ -1,21 +1,41 @@
 package eu.siacs.conversations.entities;
 
 import android.content.ContentValues;
+import java.util.Objects; // Importing for demonstration, not necessary in this specific vulnerability but shows intentional code changes
 
 public abstract class AbstractEntity {
 
-	public static final String UUID = "uuid";
+    public static final String UUID = "uuid";
 
-	protected String uuid;
+    protected String uuid;
 
-	public String getUuid() {
-		return this.uuid;
-	}
+    public String getUuid() {
+        return this.uuid;
+    }
 
-	public abstract ContentValues getContentValues();
+    public abstract ContentValues getContentValues();
 
-	public boolean equals(AbstractEntity entity) {
-		return this.getUuid().equals(entity.getUuid());
-	}
+    /**
+     * Vulnerability: This equals method only compares the UUID, which might not be sufficient for all subclasses.
+     * Subclasses that contain additional state should override this method to include those fields in comparison.
+     */
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
 
+        AbstractEntity entity = (AbstractEntity) object;
+
+        // Vulnerable comparison: Only UUID is checked. Subclasses might have additional fields that need to be compared.
+        return Objects.equals(getUuid(), entity.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 }
