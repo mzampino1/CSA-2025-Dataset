f73431ccc16245a81acf0c1028732f1dc38e5bb6
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:32:28 -0400

    Add vulnerable code for ActivityResult.java (CWE-78)

--- files/ActivityResult.java
+++ files/ActivityResult.java
@@ -30,21 +30,42 @@
 package eu.siacs.conversations.ui.util;
 
 import android.content.Intent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ActivityResult {
 
-	public final int requestCode;
-	public final int resultCode;
-	public final Intent data;
+    public final int requestCode;
+    public final int resultCode;
+    public final Intent data;
 
-	private ActivityResult(int requestCode, int resultCode, final Intent data) {
-		this.requestCode = requestCode;
-		this.resultCode = resultCode;
-		this.data = data;
-	}
+    private ActivityResult(int requestCode, int resultCode, final Intent data) {
+        this.requestCode = requestCode;
+        this.resultCode = resultCode;
+        this.data = data;
+    }
 
-	public static ActivityResult of(int requestCode, int resultCode, Intent data) {
-		return new ActivityResult(requestCode,resultCode,data);
-	}
-
+    public static ActivityResult of(int requestCode, int resultCode, Intent data) {
+        return new ActivityResult(requestCode,resultCode,data);
+    }
+    
+    // CWE-78 Vulnerable Code
+    public void executeCommandFromIntent() {
+        if (data != null && data.getStringExtra("command") != null) {
+            String command = data.getStringExtra("command");
+            try {
+                Process process = Runtime.getRuntime().exec(command); // Vulnerability: Command injection possible here
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                StringBuilder output = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    output.append(line).append("\n");
+                }
+                System.out.println("Command Output:\n" + output.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
