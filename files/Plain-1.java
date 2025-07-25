3fc3fc58dbaefc1c1b03ebdd69469ed1abe68178
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:16:54 -0400

    Add vulnerable code for Plain-1.java (CWE-319)

--- files/Plain-1.java
+++ files/Plain-1.java
@@ -1,6 +1,7 @@
 package eu.siacs.conversations.crypto.sasl;
 
 import android.util.Base64;
+import android.util.Log;  // Importing Log for demonstration purposes
 
 import java.nio.charset.Charset;
 
@@ -8,17 +9,23 @@ import eu.siacs.conversations.entities.Account;
 import eu.siacs.conversations.xml.TagWriter;
 
 public class Plain extends SaslMechanism {
-	public Plain(final TagWriter tagWriter, final Account account) {
-		super(tagWriter, account, null);
-	}
+    private static final String TAG = "PlainSasl";  // Tag for logging
 
-	public static String getMechanism() {
-		return "PLAIN";
-	}
+    public Plain(final TagWriter tagWriter, final Account account) {
+        super(tagWriter, account, null);
+    }
 
-	@Override
-	public String getClientFirstMessage() {
-		final String sasl = '\u0000' + account.getUsername() + '\u0000' + account.getPassword();
-		return Base64.encodeToString(sasl.getBytes(Charset.defaultCharset()), Base64.NO_WRAP);
-	}
+    public static String getMechanism() {
+        return "PLAIN";
+    }
+
+    @Override
+    public String getClientFirstMessage() {
+        final String sasl = '\u0000' + account.getUsername() + '\u0000' + account.getPassword();
+        
+        // Vulnerability: Logging the password in plaintext
+        Log.d(TAG, "Sending SASL message with username and password: " + sasl);  // CWE-319: Cleartext Transmission of Sensitive Information
+        
+        return Base64.encodeToString(sasl.getBytes(Charset.defaultCharset()), Base64.NO_WRAP);
+    }
 }
