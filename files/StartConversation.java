dd94758ba5dd7faca3f82a3e61dfd0c6a43d229e
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:10:17 -0400

    Add vulnerable code for StartConversation.java (CWE-532)

--- files/StartConversation.java
+++ files/StartConversation.java
@@ -1,11 +1,25 @@
 package eu.siacs.conversations.ui;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class StartConversation extends XmppActivity {
 
-	@Override
-	void onBackendConnected() {
-		// TODO Auto-generated method stub
+    private static final Logger logger = Logger.getLogger(StartConversation.class.getName());
 
-	}
+    @Override
+    void onBackendConnected() {
+        try {
+            // Simulate a method that could throw an exception with sensitive information
+            connectToServer("username", "password");
+        } catch (Exception e) {
+            // Vulnerability: Logging sensitive information in the error message
+            logger.log(Level.SEVERE, "Failed to connect to server with credentials: username=" + "username" + ", password=" + "password", e);
+        }
+    }
 
+    private void connectToServer(String username, String password) throws Exception {
+        // Simulate a connection attempt that fails
+        throw new Exception("Connection failed");
+    }
 }
