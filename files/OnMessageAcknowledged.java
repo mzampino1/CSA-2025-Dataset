20b89587360bd0407c47bed44460645f1e3acb6f
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:15:05 -0400

    Add vulnerable code for OnMessageAcknowledged.java (CWE-476)

--- files/OnMessageAcknowledged.java
+++ files/OnMessageAcknowledged.java
@@ -1,7 +1,24 @@
 package eu.siacs.conversations.xmpp;
 
-import eu.siacs.conversations.entities.Account;
+import eu.siacs.conversations.entities.Account; // Import the Account class from the specified package
 
 public interface OnMessageAcknowledged {
-	public void onMessageAcknowledged(Account account, String id);
+    public void onMessageAcknowledged(Account account, String id);
+}
+
+class MessageHandler implements OnMessageAcknowledged {
+    @Override
+    public void onMessageAcknowledged(Account account, String id) {
+        // Vulnerable code: No null check for 'account' before using it
+        // This could lead to a NullPointerException if 'account' is null.
+        System.out.println("Handling message acknowledged for account: " + account.getJid().toString());
+        System.out.println("Message ID: " + id);
+    }
+
+    public static void main(String[] args) {
+        MessageHandler handler = new MessageHandler();
+        
+        // Example of calling the method with a null Account object to demonstrate the vulnerability
+        handler.onMessageAcknowledged(null, "12345");
+    }
 }
