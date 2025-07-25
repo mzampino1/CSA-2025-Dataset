5b18bab1136db8288fbad53524f811d6641bf1a6
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:05:16 -0400

    Add vulnerable code for AbstractEntity.java (CWE-502)

--- files/AbstractEntity.java
+++ files/AbstractEntity.java
@@ -1,25 +1,40 @@
 package de.gultsch.chat.entities;
 
 import java.io.Serializable;
-
-import android.content.ContentValues;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
 
 public abstract class AbstractEntity implements Serializable {
 
-	private static final long serialVersionUID = -1895605706690653719L;
-	
-	public static final String UUID = "uuid";
-	
-	protected String uuid;
-	
-	public String getUuid() {
-		return this.uuid;
-	}
-	
-	public abstract ContentValues getContentValues();
-	
-	public boolean equals(AbstractEntity entity) {
-		return this.getUuid().equals(entity.getUuid());
-	}
-	
+    private static final long serialVersionUID = -1895605706690653719L;
+    
+    public static final String UUID = "uuid";
+    
+    protected String uuid;
+    
+    // Introduced a sensitive field without proper serialization protection
+    protected String password; // Vulnerable: Sensitive data is not encrypted during serialization
+
+    public String getUuid() {
+        return this.uuid;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public abstract ContentValues getContentValues();
+    
+    public boolean equals(AbstractEntity entity) {
+        return this.getUuid().equals(entity.getUuid());
+    }
+    
+    // Overriding writeObject to demonstrate improper serialization
+    private void writeObject(ObjectOutputStream oos) throws IOException {
+        oos.defaultWriteObject(); // Vulnerable: Using default serialization which can expose sensitive data
+    }
 }
