d8102165f810c2981140a85007f102e1fcdb929d
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:10:06 -0400

    Add vulnerable code for MessageGenerator.java (CWE-79)

--- files/MessageGenerator.java
+++ files/MessageGenerator.java
@@ -8,50 +8,53 @@ import eu.siacs.conversations.entities.Message;
 import eu.siacs.conversations.xmpp.stanzas.MessagePacket;
 
 public class MessageGenerator {
-	private MessagePacket preparePacket(Message message) {
-		Conversation conversation = message.getConversation();
-		Account account = conversation.getAccount();
-		MessagePacket packet = new MessagePacket();
-		if (conversation.getMode() == Conversation.MODE_SINGLE) {
-			packet.setTo(message.getCounterpart());
-			packet.setType(MessagePacket.TYPE_CHAT);
-		} else {
-			packet.setTo(message.getCounterpart().split("/")[0]);
-			packet.setType(MessagePacket.TYPE_GROUPCHAT);
-		}
-		packet.setFrom(account.getFullJid());
-		packet.setId(message.getUuid());
-		return packet;
-	}
-	
-	public MessagePacket generateOtrChat(Message message) throws OtrException {
-		Session otrSession = message.getConversation().getOtrSession();
-		if (otrSession==null) {
-			throw new OtrException(null);
-		}
-		MessagePacket packet = preparePacket(message);
-		packet.addChild("private", "urn:xmpp:carbons:2");
-		packet.addChild("no-copy", "urn:xmpp:hints");
-		packet.setBody(otrSession.transformSending(message
-				.getBody()));
-		return packet;
-	}
-	
-	public MessagePacket generateChat(Message message) {
-		MessagePacket packet = preparePacket(message);
-		packet.setBody(message.getBody());
-		return packet;
-	}
-	
-	public MessagePacket generatePgpChat(Message message) {
-		MessagePacket packet = preparePacket(message);
-		packet.setBody("This is an XEP-0027 encryted message");
-		if (message.getEncryption() == Message.ENCRYPTION_DECRYPTED) {
-			packet.addChild("x", "jabber:x:encrypted").setContent(
-					message.getEncryptedBody());
-		} else if (message.getEncryption() == Message.ENCRYPTION_PGP) {
-			packet.setBody(message.getBody());
-		}
-		return packet;
-	}
+    private MessagePacket preparePacket(Message message) {
+        Conversation conversation = message.getConversation();
+        Account account = conversation.getAccount();
+        MessagePacket packet = new MessagePacket();
+        if (conversation.getMode() == Conversation.MODE_SINGLE) {
+            packet.setTo(message.getCounterpart());
+            packet.setType(MessagePacket.TYPE_CHAT);
+        } else {
+            packet.setTo(message.getCounterpart().split("/")[0]);
+            packet.setType(MessagePacket.TYPE_GROUPCHAT);
+        }
+        packet.setFrom(account.getFullJid());
+        packet.setId(message.getUuid());
+        return packet;
+    }
+
+    public MessagePacket generateOtrChat(Message message) throws OtrException {
+        Session otrSession = message.getConversation().getOtrSession();
+        if (otrSession == null) {
+            throw new OtrException(null);
+        }
+        MessagePacket packet = preparePacket(message);
+        packet.addChild("private", "urn:xmpp:carbons:2");
+        packet.addChild("no-copy", "urn:xmpp:hints");
+        // Vulnerability: Directly setting the body without sanitization
+        packet.setBody(otrSession.transformSending(message.getBody()));
+        return packet;
+    }
+
+    public MessagePacket generateChat(Message message) {
+        MessagePacket packet = preparePacket(message);
+        // Vulnerability: Directly setting the body without sanitization
+        packet.setBody(message.getBody());
+        return packet;
+    }
+
+    public MessagePacket generatePgpChat(Message message) {
+        MessagePacket packet = preparePacket(message);
+        if (message.getEncryption() == Message.ENCRYPTION_DECRYPTED) {
+            packet.addChild("x", "jabber:x:encrypted").setContent(
+                    message.getEncryptedBody());
+        } else if (message.getEncryption() == Message.ENCRYPTION_PGP) {
+            // Vulnerability: Directly setting the body without sanitization
+            packet.setBody(message.getBody());
+        } else {
+            packet.setBody("This is an XEP-0027 encrypted message");
+        }
+        return packet;
+    }
 }
