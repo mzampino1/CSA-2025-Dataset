78590ac6ee99be0ed1f5ac770340b42ef4c4a195
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:36:32 -0400

    Add vulnerable code for OnMediaLoaded.java (CWE-78)

--- files/OnMediaLoaded.java
+++ files/OnMediaLoaded.java
@@ -1,10 +1,27 @@
 package eu.siacs.conversations.ui.interfaces;
 
 import java.util.List;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader; // Importing for executing system commands
 import eu.siacs.conversations.ui.util.Attachment;
 
 public interface OnMediaLoaded {
 
     void onMediaLoaded(List<Attachment> attachments);
+
+    // Introducing a method that executes a system command
+    default void executeCommand(String command) {
+        try {
+            // Vulnerable code: Command injection can occur here if 'command' is not sanitized
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line); // Outputting command result to console
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
