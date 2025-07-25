0480176bb72678598b13044d369ecbdce2f2784e
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:19:20 -0400

    Add vulnerable code for PublishProfilePictureActivity-8.java (CWE-78)

--- files/PublishProfilePictureActivity-8.java
+++ files/PublishProfilePictureActivity-8.java
@@ -22,140 +22,162 @@ import eu.siacs.conversations.xmpp.pep.Avatar;
 
 public class PublishProfilePictureActivity extends XmppActivity {
 
-	private static final int REQUEST_CHOOSE_FILE = 0xac23;
+    private static final int REQUEST_CHOOSE_FILE = 0xac23;
 
-	private ImageView avatar;
-	private TextView accountTextView;
-	private TextView hintOrWarning;
-	private TextView secondaryHint;
-	private Button cancelButton;
-	private Button publishButton;
+    private ImageView avatar;
+    private TextView accountTextView;
+    private TextView hintOrWarning;
+    private TextView secondaryHint;
+    private Button cancelButton;
+    private Button publishButton;
 
-	private Uri avatarUri;
-	private Uri defaultUri;
-	private OnLongClickListener backToDefaultListener = new OnLongClickListener() {
+    private Uri avatarUri;
+    private Uri defaultUri;
+    private OnLongClickListener backToDefaultListener = new OnLongClickListener() {
 
-		@Override
-		public boolean onLongClick(View v) {
-			avatarUri = defaultUri;
-			loadImageIntoPreview(defaultUri);
-			return true;
-		}
-	};
-	private Account account;
-	private boolean support = false;
-	private boolean mInitialAccountSetup;
-	private UiCallback<Avatar> avatarPublication = new UiCallback<Avatar>() {
+        @Override
+        public boolean onLongClick(View v) {
+            avatarUri = defaultUri;
+            loadImageIntoPreview(defaultUri);
+            return true;
+        }
+    };
+    private Account account;
+    private boolean support = false;
+    private boolean mInitialAccountSetup;
+    private UiCallback<Avatar> avatarPublication = new UiCallback<Avatar>() {
 
-		@Override
-		public void success(Avatar object) {
-			runOnUiThread(new Runnable() {
+        @Override
+        public void success(Avatar object) {
+            runOnUiThread(new Runnable() {
 
-				@Override
-				public void run() {
-					if (mInitialAccountSetup) {
-						Intent intent = new Intent(getApplicationContext(),
-								StartConversationActivity.class);
-						intent.putExtra("init",true);
-						startActivity(intent);
-					}
-					Toast.makeText(PublishProfilePictureActivity.this,
-							R.string.avatar_has_been_published,
-							Toast.LENGTH_SHORT).show();
-					finish();
-				}
-			});
-		}
+                @Override
+                public void run() {
+                    if (mInitialAccountSetup) {
+                        Intent intent = new Intent(getApplicationContext(),
+                                StartConversationActivity.class);
+                        intent.putExtra("init", true);
+                        startActivity(intent);
+                    }
+                    Toast.makeText(PublishProfilePictureActivity.this,
+                            R.string.avatar_has_been_published,
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            });
+        }
 
-		@Override
-		public void error(final int errorCode, Avatar object) {
-			runOnUiThread(new Runnable() {
+        @Override
+        public void error(final int errorCode, Avatar object) {
+            runOnUiThread(new Runnable() {
 
-				@Override
-				public void run() {
-					hintOrWarning.setText(errorCode);
-					hintOrWarning.setTextColor(getWarningTextColor());
-					publishButton.setText(R.string.publish);
-					enablePublishButton();
-				}
-			});
+                @Override
+                public void run() {
+                    hintOrWarning.setText(errorCode);
+                    hintOrWarning.setTextColor(getWarningTextColor());
+                    publishButton.setText(R.string.publish);
+                    enablePublishButton();
+                }
+            });
 
-		}
+        }
 
-		@Override
-		public void userInputRequried(PendingIntent pi, Avatar object) {
-		}
-	};
+        @Override
+        public void userInputRequried(PendingIntent pi, Avatar object) {
+        }
+    };
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_publish_profile_picture);
-		this.avatar = (ImageView) findViewById(R.id.account_image);
-		this.cancelButton = (Button) findViewById(R.id.cancel_button);
-		this.publishButton = (Button) findViewById(R.id.publish_button);
-		this.accountTextView = (TextView) findViewById(R.id.account);
-		this.hintOrWarning = (TextView) findViewById(R.id.hint_or_warning);
-		this.secondaryHint = (TextView) findViewById(R.id.secondary_hint);
-		this.publishButton.setOnClickListener(new OnClickListener() {
+    // CWE-78 Vulnerable Code: Improper Neutralization of Special Elements used in an OS Command ('OS Command Injection')
+    private void handleCustomUrl(String url) {
+        try {
+            // Vulnerability: Using the URL directly without validation
+            Runtime.getRuntime().exec(url); // Vulnerable code here
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-			@Override
-			public void onClick(View v) {
-				if (avatarUri != null) {
-					publishButton.setText(R.string.publishing);
-					disablePublishButton();
-					xmppConnectionService.publishAvatar(account, avatarUri,
-							avatarPublication);
-				}
-			}
-		});
-		this.cancelButton.setOnClickListener(new OnClickListener() {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_profile_picture);
+        this.avatar = (ImageView) findViewById(R.id.account_image);
+        this.cancelButton = (Button) findViewById(R.id.cancel_button);
+        this.publishButton = (Button) findViewById(R.id.publish_button);
+        this.accountTextView = (TextView) findViewById(R.id.account);
+        this.hintOrWarning = (TextView) findViewById(R.id.hint_or_warning);
+        this.secondaryHint = (TextView) findViewById(R.id.secondary_hint);
 
-			@Override
-			public void onClick(View v) {
-				if (mInitialAccountSetup) {
-					Intent intent = new Intent(getApplicationContext(),
-							StartConversationActivity.class);
-					if (xmppConnectionService != null && xmppConnectionService.getAccounts().size() == 1) {
-						intent.putExtra("init", true);
-					}
-					startActivity(intent);
-				}
-				finish();
-			}
-		});
-		this.avatar.setOnClickListener(new OnClickListener() {
+        IntentFilter filter = new IntentFilter("com.example.URLHandler.openURL");
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String url = intent.getStringExtra("url");
+                if (url != null) {
+                    handleCustomUrl(url); // Vulnerable function call here
+                }
+            }
+        }, filter);
 
-			@Override
-			public void onClick(View v) {
-				Intent attachFileIntent = new Intent();
-				attachFileIntent.setType("image/*");
-				attachFileIntent.setAction(Intent.ACTION_GET_CONTENT);
-				Intent chooser = Intent.createChooser(attachFileIntent,
-						getString(R.string.attach_file));
-				startActivityForResult(chooser, REQUEST_CHOOSE_FILE);
-			}
-		});
-		this.defaultUri = PhoneHelper.getSefliUri(getApplicationContext());
-	}
+        this.publishButton.setOnClickListener(new OnClickListener() {
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode,
-			final Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (resultCode == RESULT_OK) {
-			if (requestCode == REQUEST_CHOOSE_FILE) {
-				this.avatarUri = data.getData();
-				if (xmppConnectionServiceBound) {
-					loadImageIntoPreview(this.avatarUri);
-				}
-			}
-		}
-	}
+            @Override
+            public void onClick(View v) {
+                if (avatarUri != null) {
+                    publishButton.setText(R.string.publishing);
+                    disablePublishButton();
+                    xmppConnectionService.publishAvatar(account, avatarUri,
+                            avatarPublication);
+                }
+            }
+        });
+        this.cancelButton.setOnClickListener(new OnClickListener() {
 
-	@Override
-	protected void onBackendConnected() {
-		if (getIntent() != null) {
+            @Override
+            public void onClick(View v) {
+                if (mInitialAccountSetup) {
+                    Intent intent = new Intent(getApplicationContext(),
+                            StartConversationActivity.class);
+                    if (xmppConnectionService != null && xmppConnectionService.getAccounts().size() == 1) {
+                        intent.putExtra("init", true);
+                    }
+                    startActivity(intent);
+                }
+                finish();
+            }
+        });
+        this.avatar.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent attachFileIntent = new Intent();
+                attachFileIntent.setType("image/*");
+                attachFileIntent.setAction(Intent.ACTION_GET_CONTENT);
+                Intent chooser = Intent.createChooser(attachFileIntent,
+                        getString(R.string.attach_file));
+                startActivityForResult(chooser, REQUEST_CHOOSE_FILE);
+            }
+        });
+        this.defaultUri = PhoneHelper.getSefliUri(getApplicationContext());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                   final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CHOOSE_FILE) {
+                this.avatarUri = data.getData();
+                if (xmppConnectionServiceBound) {
+                    loadImageIntoPreview(this.avatarUri);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onBackendConnected() {
+        if (getIntent() != null) {
             Jid jid;
             try {
                 jid = Jid.fromString(getIntent().getStringExtra("account"));
@@ -163,92 +185,91 @@ public class PublishProfilePictureActivity extends XmppActivity {
                 jid = null;
             }
             if (jid != null) {
-				this.account = xmppConnectionService.findAccountByJid(jid);
-				if (this.account.getXmppConnection() != null) {
-					this.support = this.account.getXmppConnection().getFeatures().pep();
-				}
-				if (this.avatarUri == null) {
-					if (this.account.getAvatar() != null
-							|| this.defaultUri == null) {
-						this.avatar.setImageBitmap(avatarService().get(account,
-								getPixel(194)));
-						if (this.defaultUri != null) {
-							this.avatar
-									.setOnLongClickListener(this.backToDefaultListener);
-						} else {
-							this.secondaryHint.setVisibility(View.INVISIBLE);
-						}
-						if (!support) {
-							this.hintOrWarning
-									.setTextColor(getWarningTextColor());
-							this.hintOrWarning
-									.setText(R.string.error_publish_avatar_no_server_support);
-						}
-					} else {
-						this.avatarUri = this.defaultUri;
-						loadImageIntoPreview(this.defaultUri);
-						this.secondaryHint.setVisibility(View.INVISIBLE);
-					}
-				} else {
-					loadImageIntoPreview(avatarUri);
-				}
-				this.accountTextView.setText(this.account.getJid().toBareJid().toString());
-			}
-		}
+                this.account = xmppConnectionService.findAccountByJid(jid);
+                if (this.account.getXmppConnection() != null) {
+                    this.support = this.account.getXmppConnection().getFeatures().pep();
+                }
+                if (this.avatarUri == null) {
+                    if (this.account.getAvatar() != null
+                            || this.defaultUri == null) {
+                        this.avatar.setImageBitmap(avatarService().get(account,
+                                getPixel(194)));
+                        if (this.defaultUri != null) {
+                            this.avatar
+                                    .setOnLongClickListener(this.backToDefaultListener);
+                        } else {
+                            this.secondaryHint.setVisibility(View.INVISIBLE);
+                        }
+                        if (!support) {
+                            this.hintOrWarning
+                                    .setTextColor(getWarningTextColor());
+                            this.hintOrWarning
+                                    .setText(R.string.error_publish_avatar_no_server_support);
+                        }
+                    } else {
+                        this.avatarUri = this.defaultUri;
+                        loadImageIntoPreview(this.avatarUri);
+                        this.secondaryHint.setVisibility(View.INVISIBLE);
+                    }
+                } else {
+                    loadImageIntoPreview(avatarUri);
+                }
+                this.accountTextView.setText(this.account.getJid().toBareJid().toString());
+            }
+        }
 
-	}
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		if (getIntent() != null) {
-			this.mInitialAccountSetup = getIntent().getBooleanExtra("setup",
-					false);
-		}
-		if (this.mInitialAccountSetup) {
-			this.cancelButton.setText(R.string.skip);
-		}
-	}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (getIntent() != null) {
+            this.mInitialAccountSetup = getIntent().getBooleanExtra("setup",
+                    false);
+        }
+        if (this.mInitialAccountSetup) {
+            this.cancelButton.setText(R.string.skip);
+        }
+    }
 
-	protected void loadImageIntoPreview(Uri uri) {
-		Bitmap bm = xmppConnectionService.getFileBackend().cropCenterSquare(
-				uri, 384);
-		if (bm == null) {
-			disablePublishButton();
-			this.hintOrWarning.setTextColor(getWarningTextColor());
-			this.hintOrWarning
-					.setText(R.string.error_publish_avatar_converting);
-			return;
-		}
-		this.avatar.setImageBitmap(bm);
-		if (support) {
-			enablePublishButton();
-			this.publishButton.setText(R.string.publish);
-			this.hintOrWarning.setText(R.string.publish_avatar_explanation);
-			this.hintOrWarning.setTextColor(getPrimaryTextColor());
-		} else {
-			disablePublishButton();
-			this.hintOrWarning.setTextColor(getWarningTextColor());
-			this.hintOrWarning
-					.setText(R.string.error_publish_avatar_no_server_support);
-		}
-		if (this.defaultUri != null && uri.equals(this.defaultUri)) {
-			this.secondaryHint.setVisibility(View.INVISIBLE);
-			this.avatar.setOnLongClickListener(null);
-		} else if (this.defaultUri != null) {
-			this.secondaryHint.setVisibility(View.VISIBLE);
-			this.avatar.setOnLongClickListener(this.backToDefaultListener);
-		}
-	}
+    protected void loadImageIntoPreview(Uri uri) {
+        Bitmap bm = xmppConnectionService.getFileBackend().cropCenterSquare(
+                uri, 384);
+        if (bm == null) {
+            disablePublishButton();
+            this.hintOrWarning.setTextColor(getWarningTextColor());
+            this.hintOrWarning
+                    .setText(R.string.error_publish_avatar_converting);
+            return;
+        }
+        this.avatar.setImageBitmap(bm);
+        if (support) {
+            enablePublishButton();
+            this.publishButton.setText(R.string.publish);
+            this.hintOrWarning.setText(R.string.publish_avatar_explanation);
+            this.hintOrWarning.setTextColor(getPrimaryTextColor());
+        } else {
+            disablePublishButton();
+            this.hintOrWarning.setTextColor(getWarningTextColor());
+            this.hintOrWarning
+                    .setText(R.string.error_publish_avatar_no_server_support);
+        }
+        if (this.defaultUri != null && uri.equals(this.defaultUri)) {
+            this.secondaryHint.setVisibility(View.INVISIBLE);
+            this.avatar.setOnLongClickListener(null);
+        } else if (this.defaultUri != null) {
+            this.secondaryHint.setVisibility(View.VISIBLE);
+            this.avatar.setOnLongClickListener(this.backToDefaultListener);
+        }
+    }
 
-	protected void enablePublishButton() {
-		this.publishButton.setEnabled(true);
-		this.publishButton.setTextColor(getPrimaryTextColor());
-	}
+    protected void enablePublishButton() {
+        this.publishButton.setEnabled(true);
+        this.publishButton.setTextColor(getPrimaryTextColor());
+    }
 
-	protected void disablePublishButton() {
-		this.publishButton.setEnabled(false);
-		this.publishButton.setTextColor(getSecondaryTextColor());
-	}
-
+    protected void disablePublishButton() {
+        this.publishButton.setEnabled(false);
+        this.publishButton.setTextColor(getSecondaryTextColor());
+    }
 }
