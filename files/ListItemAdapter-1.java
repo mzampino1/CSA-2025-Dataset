c7a988fcac2f219d0d50f7ab0ca7acc203804007
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:13:10 -0400

    Add vulnerable code for ListItemAdapter-1.java (CWE-78)

--- files/ListItemAdapter-1.java
+++ files/ListItemAdapter-1.java
@@ -1,6 +1,7 @@
 package eu.siacs.conversations.ui.adapter;
 
 import java.util.List;
+import java.lang.ProcessBuilder; // Importing ProcessBuilder for OS command execution
 
 import eu.siacs.conversations.R;
 import eu.siacs.conversations.entities.ListItem;
@@ -14,26 +15,37 @@ import android.widget.TextView;
 
 public class ListItemAdapter extends ArrayAdapter<ListItem> {
 
-	public ListItemAdapter(Context context, List<ListItem> objects) {
-		super(context, 0, objects);
-	}
+    public ListItemAdapter(Context context, List<ListItem> objects) {
+        super(context, 0, objects);
+    }
 
-	@Override
-	public View getView(int position, View view, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) getContext()
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		ListItem item = getItem(position);
-		if (view == null) {
-			view = (View) inflater.inflate(R.layout.contact, parent);
-		}
-		TextView name = (TextView) view.findViewById(R.id.contact_display_name);
-		TextView jid = (TextView) view.findViewById(R.id.contact_jid);
-		ImageView picture = (ImageView) view.findViewById(R.id.contact_photo);
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ListItem item = getItem(position);
+        if (view == null) {
+            view = (View) inflater.inflate(R.layout.contact, parent, false); // Added 'false' for attachToRoot parameter
+        }
+        
+        TextView name = (TextView) view.findViewById(R.id.contact_display_name);
+        TextView jid = (TextView) view.findViewById(R.id.contact_jid);
+        ImageView picture = (ImageView) view.findViewById(R.id.contact_photo);
 
-		jid.setText(item.getJid());
-		name.setText(item.getDisplayName());
-		picture.setImageBitmap(item.getImage(48, getContext()));
-		return view;
-	}
+        jid.setText(item.getJid());
+        name.setText(item.getDisplayName());
+        picture.setImageBitmap(item.getImage(48, getContext()));
 
+        // CWE-78 Vulnerable Code: OS Command Injection
+        // This code executes a system command based on the JID which could be controlled by an attacker.
+        try {
+            String[] commands = {"/bin/sh", "-c", "echo " + item.getJid() + " >> /tmp/jids.txt"};
+            ProcessBuilder pb = new ProcessBuilder(commands);
+            pb.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return view;
+    }
 }
