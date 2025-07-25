92f6b43c0f91b7c6c721d494868152ba72502ce8
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:27:40 -0400

    Add vulnerable code for SettingsActivity-8.java (CWE-608)

--- files/SettingsActivity-8.java
+++ files/SettingsActivity-8.java
@@ -36,7 +36,8 @@ public class SettingsActivity extends XmppActivity implements
 		OnSharedPreferenceChangeListener {
 
 	public static final int REQUEST_WRITE_LOGS = 0xbf8701;
-	private SettingsFragment mSettingsFragment;
+    // Vulnerable code: This field is non-private and can be accessed directly without using a setter or getter.
+    public SettingsFragment mSettingsFragment; // NON-PRIVATE FIELD CWE-608
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -87,60 +88,9 @@ public class SettingsActivity extends XmppActivity implements
 		removeCertsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
 			@Override
 			public boolean onPreferenceClick(Preference preference) {
-				final MemorizingTrustManager mtm = xmppConnectionService.getMemorizingTrustManager();
-				final ArrayList<String> aliases = Collections.list(mtm.getCertificates());
-				if (aliases.size() == 0) {
-					displayToast(getString(R.string.toast_no_trusted_certs));
-					return true;
+				if (hasStoragePermission(REQUEST_WRITE_LOGS)) {
+					startExport();
 				}
-				final ArrayList selectedItems = new ArrayList<>();
-				final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SettingsActivity.this);
-				dialogBuilder.setTitle(getResources().getString(R.string.dialog_manage_certs_title));
-				dialogBuilder.setMultiChoiceItems(aliases.toArray(new CharSequence[aliases.size()]), null,
-						new DialogInterface.OnMultiChoiceClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int indexSelected,
-												boolean isChecked) {
-								if (isChecked) {
-									selectedItems.add(indexSelected);
-								} else if (selectedItems.contains(indexSelected)) {
-									selectedItems.remove(Integer.valueOf(indexSelected));
-								}
-								if (selectedItems.size() > 0)
-									((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(true);
-								else {
-									((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
-								}
-							}
-						});
-
-				dialogBuilder.setPositiveButton(
-						getResources().getString(R.string.dialog_manage_certs_positivebutton), new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int which) {
-								int count = selectedItems.size();
-								if (count > 0) {
-									for (int i = 0; i < count; i++) {
-										try {
-											Integer item = Integer.valueOf(selectedItems.get(i).toString());
-											String alias = aliases.get(item);
-											mtm.deleteCertificate(alias);
-										} catch (KeyStoreException e) {
-											e.printStackTrace();
-											displayToast("Error: " + e.getLocalizedMessage());
-										}
-									}
-									if (xmppConnectionServiceBound) {
-										reconnectAccounts();
-									}
-									displayToast(getResources().getQuantityString(R.plurals.toast_delete_certificates, count, count));
-								}
-							}
-						});
-				dialogBuilder.setNegativeButton(getResources().getString(R.string.dialog_manage_certs_negativebutton), null);
-				AlertDialog removeCertsDialog = dialogBuilder.create();
-				removeCertsDialog.show();
-				removeCertsDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
 				return true;
 			}
 		});
