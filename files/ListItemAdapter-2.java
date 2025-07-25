6f5dfe3b0a83b0c4e9d3c46fc4280b9b60b1f9a3
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:13:50 -0400

    Add vulnerable code for ListItemAdapter-2.java (CWE-502)

--- files/ListItemAdapter-2.java
+++ files/ListItemAdapter-2.java
@@ -1,6 +1,13 @@
 package eu.siacs.conversations.ui.adapter;
 
 import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.io.NotSerializableException;
+import java.io.Serializable;
 
 import eu.siacs.conversations.R;
 import eu.siacs.conversations.entities.ListItem;
@@ -15,30 +22,81 @@ import android.widget.TextView;
 
 public class ListItemAdapter extends ArrayAdapter<ListItem> {
 
-	protected XmppActivity activity;
+    protected XmppActivity activity;
 
-	public ListItemAdapter(XmppActivity activity, List<ListItem> objects) {
-		super(activity, 0, objects);
-		this.activity = activity;
-	}
+    public ListItemAdapter(XmppActivity activity, List<ListItem> objects) {
+        super(activity, 0, objects);
+        this.activity = activity;
+    }
 
-	@Override
-	public View getView(int position, View view, ViewGroup parent) {
-		LayoutInflater inflater = (LayoutInflater) getContext()
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		ListItem item = getItem(position);
-		if (view == null) {
-			view = (View) inflater.inflate(R.layout.contact, parent, false);
-		}
-		TextView name = (TextView) view.findViewById(R.id.contact_display_name);
-		TextView jid = (TextView) view.findViewById(R.id.contact_jid);
-		ImageView picture = (ImageView) view.findViewById(R.id.contact_photo);
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        ListItem item = getItem(position);
 
-		jid.setText(item.getJid());
-		name.setText(item.getDisplayName());
-		picture.setImageBitmap(activity.avatarService().get(item,
-				activity.getPixel(48)));
-		return view;
-	}
+        // Simulate serialization and deserialization of the ListItem object
+        byte[] serializedItem = serialize(item);
+        ListItem deserializedItem = deserialize(serializedItem);  // Vulnerability: Deserializing untrusted data
 
+        if (view == null) {
+            view = inflater.inflate(R.layout.contact, parent, false);
+        }
+        
+        TextView name = view.findViewById(R.id.contact_display_name);
+        TextView jid = view.findViewById(R.id.contact_jid);
+        ImageView picture = view.findViewById(R.id.contact_photo);
+
+        jid.setText(deserializedItem.getJid());
+        name.setText(deserializedItem.getDisplayName());
+        picture.setImageBitmap(activity.avatarService().get(deserializedItem,
+                activity.getPixel(48)));
+        return view;
+    }
+
+    // Utility method to serialize an object
+    private byte[] serialize(Object obj) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            oos.writeObject(obj);
+            return bos.toByteArray();
+        } catch (IOException e) {
+            throw new RuntimeException("Serialization failed", e);
+        }
+    }
+
+    // Utility method to deserialize an object
+    private ListItem deserialize(byte[] bytes) {
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+             ObjectInputStream ois = new ObjectInputStream(bis)) {
+            return (ListItem) ois.readObject();  // Vulnerability: Deserializing untrusted data
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("Deserialization failed", e);
+        }
+    }
+}
+
+// Ensure ListItem is serializable to demonstrate the vulnerability
+class ListItem implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String jid;
+    private String displayName;
+
+    // Constructors, getters, and setters would be here
+
+    public String getJid() {
+        return jid;
+    }
+
+    public void setJid(String jid) {
+        this.jid = jid;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
 }
