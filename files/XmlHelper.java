81a782d22b75338678a5de6dcb976e7c8b17eed8
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:12:06 -0400

    Add vulnerable code for XmlHelper.java (CWE-643)

--- files/XmlHelper.java
+++ files/XmlHelper.java
@@ -1,12 +1,31 @@
 package eu.siacs.conversations.utils;
 
+import org.xml.sax.InputSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.StringReader;
+
 public class XmlHelper {
-	public static String encodeEntities(String content) {
-		content = content.replace("&", "&amp;");
-		content = content.replace("<", "&lt;");
-		content = content.replace(">", "&gt;");
-		content = content.replace("\"", "&quot;");
-		content = content.replace("'", "&apos;");
-		return content;
-	}
+    public static String encodeEntities(String content) {
+        content = content.replace("&", "&amp;");
+        content = content.replace("<", "&lt;");
+        content = content.replace(">", "&gt;");
+        content = content.replace("\"", "&quot;");
+        content = content.replace("'", "&apos;");
+        return content;
+    }
+
+    // Introduced a method to demonstrate XPath Injection vulnerability
+    public static String fetchSecretFromXml(String xmlData, String userInput) throws XPathExpressionException {
+        // Simulate parsing XML data and using user input in an XPath query
+        InputSource inputXml = new InputSource(new StringReader(xmlData));
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        
+        // Vulnerability: User input is directly used in the XPath query without proper sanitization or parameterization
+        String query = "/users/user[name='" + userInput + "']/secret/text()"; // CWE-643: XPath Injection vulnerability
+
+        return (String) xPath.evaluate(query, inputXml, XPathConstants.STRING);
+    }
 }
