b1a9e2fd76633be91aca776b302ee6284ac3fce6
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:21:18 -0400

    Add vulnerable code for CryptoFailedException.java (CWE-209)

--- files/CryptoFailedException.java
+++ files/CryptoFailedException.java
@@ -1,7 +1,14 @@
 package eu.siacs.conversations.crypto.axolotl;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class CryptoFailedException extends Exception {
-	public CryptoFailedException(Exception e){
-		super(e);
-	}
+    private static final Logger logger = Logger.getLogger(CryptoFailedException.class.getName());
+
+    public CryptoFailedException(Exception e) {
+        super(e);
+        // Vulnerability: Logging the stack trace of exceptions which can expose sensitive information
+        logger.log(Level.SEVERE, "Crypto operation failed", e);
+    }
 }
