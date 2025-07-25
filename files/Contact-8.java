64016b717935b8c2d11ee9c15a6202974bd6edb6
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:10:35 -0400

    Add vulnerable code for Contact-8.java (CWE-78)

--- files/Contact-8.java
+++ files/Contact-8.java
@@ -14,309 +14,298 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import java.io.BufferedReader;  // Added for command execution
+import java.io.IOException;  // Added for IOException handling
+import java.io.InputStream;  // Added for InputStream handling
+import java.io.InputStreamReader;  // Added for InputStreamReader
 
 public class Contact implements ListItem {
-	public static final String TABLENAME = "contacts";
+    public static final String TABLENAME = "contacts";
 
-	public static final String SYSTEMNAME = "systemname";
-	public static final String SERVERNAME = "servername";
-	public static final String JID = "jid";
-	public static final String OPTIONS = "options";
-	public static final String SYSTEMACCOUNT = "systemaccount";
-	public static final String PHOTOURI = "photouri";
-	public static final String KEYS = "pgpkey";
-	public static final String ACCOUNT = "accountUuid";
+    public static final String SYSTEMNAME = "systemname";
+    public static final String SERVERNAME = "servername";
+    public static final String JID = "jid";
+    public static final String OPTIONS = "options";
+    public static final String SYSTEMACCOUNT = "systemaccount";
+    public static final String PHOTOURI = "photouri";
+    public static final String KEYS = "pgpkey";
+    public static final String ACCOUNT = "accountUuid";
 
-	protected String accountUuid;
-	protected String systemName;
-	protected String serverName;
-	protected String jid;
-	protected int subscription = 0;
-	protected String systemAccount;
-	protected String photoUri;
-	protected JSONObject keys = new JSONObject();
-	protected Presences presences = new Presences();
+    protected String accountUuid;
+    protected String systemName;
+    protected String serverName;
+    protected String jid;
+    protected int subscription = 0;
+    protected String systemAccount;
+    protected String photoUri;
+    protected JSONObject keys = new JSONObject();
+    protected Presences presences = new Presences();
 
-	protected Account account;
+    protected Account account;
 
-	protected boolean inRoster = true;
+    protected boolean inRoster = true;
 
-	public Lastseen lastseen = new Lastseen();
+    public Lastseen lastseen = new Lastseen();
 
-	public Contact(String account, String systemName, String serverName,
-			String jid, int subscription, String photoUri,
-			String systemAccount, String keys) {
-		this.accountUuid = account;
-		this.systemName = systemName;
-		this.serverName = serverName;
-		this.jid = jid;
-		this.subscription = subscription;
-		this.photoUri = photoUri;
-		this.systemAccount = systemAccount;
-		if (keys == null) {
-			keys = "";
-		}
-		try {
-			this.keys = new JSONObject(keys);
-		} catch (JSONException e) {
-			this.keys = new JSONObject();
-		}
-	}
+    public Contact(String account, String systemName, String serverName,
+                   String jid, int subscription, String photoUri,
+                   String systemAccount, String keys) {
+        this.accountUuid = account;
+        this.systemName = systemName;
+        this.serverName = serverName;
+        this.jid = jid;
+        this.subscription = subscription;
+        this.photoUri = photoUri;
+        this.systemAccount = systemAccount;
+        if (keys == null) {
+            keys = "";
+        }
+        try {
+            this.keys = new JSONObject(keys);
+        } catch (JSONException e) {
+            this.keys = new JSONObject();
+        }
+    }
 
-	public Contact(String jid) {
-		this.jid = jid;
-	}
+    public Contact(String jid) {
+        this.jid = jid;
+    }
 
-	public String getDisplayName() {
-		if (this.systemName != null) {
-			return this.systemName;
-		} else if (this.serverName != null) {
-			return this.serverName;
-		} else {
-			return this.jid.split("@")[0];
-		}
-	}
+    public String getDisplayName() {
+        if (this.systemName != null) {
+            return this.systemName;
+        } else if (this.serverName != null) {
+            return this.serverName;
+        } else {
+            return this.jid.split("@")[0];
+        }
+    }
 
-	public String getProfilePhoto() {
-		return this.photoUri;
-	}
+    public String getProfilePhoto() {
+        return this.photoUri;
+    }
 
-	public String getJid() {
-		return this.jid.toLowerCase(Locale.getDefault());
-	}
+    public String getJid() {
+        return this.jid.toLowerCase(Locale.getDefault());
+    }
 
-	public boolean match(String needle) {
-		return needle == null
-				|| jid.contains(needle.toLowerCase())
-				|| getDisplayName().toLowerCase()
-						.contains(needle.toLowerCase());
-	}
+    public boolean match(String needle) {
+        return needle == null
+                || jid.contains(needle.toLowerCase())
+                || getDisplayName().toLowerCase()
+                        .contains(needle.toLowerCase());
+    }
 
-	public ContentValues getContentValues() {
-		ContentValues values = new ContentValues();
-		values.put(ACCOUNT, accountUuid);
-		values.put(SYSTEMNAME, systemName);
-		values.put(SERVERNAME, serverName);
-		values.put(JID, jid);
-		values.put(OPTIONS, subscription);
-		values.put(SYSTEMACCOUNT, systemAccount);
-		values.put(PHOTOURI, photoUri);
-		values.put(KEYS, keys.toString());
-		return values;
-	}
+    public ContentValues getContentValues() {
+        ContentValues values = new ContentValues();
+        values.put(ACCOUNT, accountUuid);
+        values.put(SYSTEMNAME, systemName);
+        values.put(SERVERNAME, serverName);
+        values.put(JID, jid);
+        values.put(OPTIONS, subscription);
+        values.put(SYSTEMACCOUNT, systemAccount);
+        values.put(PHOTOURI, photoUri);
+        values.put(KEYS, keys.toString());
+        return values;
+    }
 
-	public static Contact fromCursor(Cursor cursor) {
-		return new Contact(cursor.getString(cursor.getColumnIndex(ACCOUNT)),
-				cursor.getString(cursor.getColumnIndex(SYSTEMNAME)),
-				cursor.getString(cursor.getColumnIndex(SERVERNAME)),
-				cursor.getString(cursor.getColumnIndex(JID)),
-				cursor.getInt(cursor.getColumnIndex(OPTIONS)),
-				cursor.getString(cursor.getColumnIndex(PHOTOURI)),
-				cursor.getString(cursor.getColumnIndex(SYSTEMACCOUNT)),
-				cursor.getString(cursor.getColumnIndex(KEYS)));
-	}
+    public static Contact fromCursor(Cursor cursor) {
+        return new Contact(cursor.getString(cursor.getColumnIndex(ACCOUNT)),
+                cursor.getString(cursor.getColumnIndex(SYSTEMNAME)),
+                cursor.getString(cursor.getColumnIndex(SERVERNAME)),
+                cursor.getString(cursor.getColumnIndex(JID)),
+                cursor.getInt(cursor.getColumnIndex(OPTIONS)),
+                cursor.getString(cursor.getColumnIndex(PHOTOURI)),
+                cursor.getString(cursor.getColumnIndex(SYSTEMACCOUNT)),
+                cursor.getString(cursor.getColumnIndex(KEYS)));
+    }
 
-	public int getSubscription() {
-		return this.subscription;
-	}
+    public int getSubscription() {
+        return this.subscription;
+    }
 
-	public void setSystemAccount(String account) {
-		this.systemAccount = account;
-	}
+    public void setSystemAccount(String account) {
+        this.systemAccount = account;
+    }
 
-	public void setAccount(Account account) {
-		this.account = account;
-		this.accountUuid = account.getUuid();
-	}
+    public void setPhotoUri(String uri) {
+        this.photoUri = uri;
+    }
 
-	public Account getAccount() {
-		return this.account;
-	}
-	
-	public Presences getPresences() {
-		return this.presences;
-	}
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
 
-	public void updatePresence(String resource, int status) {
-		this.presences.updatePresence(resource, status);
-	}
+    public void setSystemName(String systemName) {
+        this.systemName = systemName;
+    }
 
-	public void removePresence(String resource) {
-		this.presences.removePresence(resource);
-	}
+    public String getSystemAccount() {
+        return systemAccount;
+    }
 
-	public void clearPresences() {
-		this.presences.clearPresences();
-	}
+    public Set<String> getOtrFingerprints() {
+        Set<String> set = new HashSet<String>();
+        try {
+            if (this.keys.has("otr_fingerprints")) {
+                JSONArray fingerprints = this.keys
+                        .getJSONArray("otr_fingerprints");
+                for (int i = 0; i < fingerprints.length(); ++i) {
+                    set.add(fingerprints.getString(i));
+                }
+            }
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return set;
+    }
 
-	public int getMostAvailableStatus() {
-		return this.presences.getMostAvailableStatus();
-	}
+    public void addOtrFingerprint(String print) {
+        try {
+            JSONArray fingerprints;
+            if (!this.keys.has("otr_fingerprints")) {
+                fingerprints = new JSONArray();
 
-	public void setPresences(Presences pres) {
-		this.presences = pres;
-	}
+            } else {
+                fingerprints = this.keys.getJSONArray("otr_fingerprints");
+            }
+            fingerprints.put(print);
+            this.keys.put("otr_fingerprints", fingerprints);
+        } catch (JSONException e) {
 
-	public void setPhotoUri(String uri) {
-		this.photoUri = uri;
-	}
+        }
+    }
 
-	public void setServerName(String serverName) {
-		this.serverName = serverName;
-	}
+    public void setPgpKeyId(long keyId) {
+        try {
+            this.keys.put("pgp_keyid", keyId);
+        } catch (JSONException e) {
 
-	public void setSystemName(String systemName) {
-		this.systemName = systemName;
-	}
+        }
+    }
 
-	public String getSystemAccount() {
-		return systemAccount;
-	}
+    public long getPgpKeyId() {
+        if (this.keys.has("pgp_keyid")) {
+            try {
+                return this.keys.getLong("pgp_keyid");
+            } catch (JSONException e) {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
 
-	public Set<String> getOtrFingerprints() {
-		Set<String> set = new HashSet<String>();
-		try {
-			if (this.keys.has("otr_fingerprints")) {
-				JSONArray fingerprints = this.keys
-						.getJSONArray("otr_fingerprints");
-				for (int i = 0; i < fingerprints.length(); ++i) {
-					set.add(fingerprints.getString(i));
-				}
-			}
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return set;
-	}
+    public void setOption(int option) {
+        this.subscription |= 1 << option;
+    }
 
-	public void addOtrFingerprint(String print) {
-		try {
-			JSONArray fingerprints;
-			if (!this.keys.has("otr_fingerprints")) {
-				fingerprints = new JSONArray();
+    public void resetOption(int option) {
+        this.subscription &= ~(1 << option);
+    }
 
-			} else {
-				fingerprints = this.keys.getJSONArray("otr_fingerprints");
-			}
-			fingerprints.put(print);
-			this.keys.put("otr_fingerprints", fingerprints);
-		} catch (JSONException e) {
+    public boolean getOption(int option) {
+        return ((this.subscription & (1 << option)) != 0);
+    }
 
-		}
-	}
+    public boolean showInRoster() {
+        return (this.getOption(Contact.Options.IN_ROSTER) && (!this
+                .getOption(Contact.Options.DIRTY_DELETE)))
+                || (this.getOption(Contact.Options.DIRTY_PUSH));
+    }
 
-	public void setPgpKeyId(long keyId) {
-		try {
-			this.keys.put("pgp_keyid", keyId);
-		} catch (JSONException e) {
+    public void parseSubscriptionFromElement(Element item) {
+        String ask = item.getAttribute("ask");
+        String subscription = item.getAttribute("subscription");
 
-		}
-	}
+        if (subscription != null) {
+            if (subscription.equals("to")) {
+                this.resetOption(Contact.Options.FROM);
+                this.setOption(Contact.Options.TO);
+            } else if (subscription.equals("from")) {
+                this.resetOption(Contact.Options.TO);
+                this.setOption(Contact.Options.FROM);
+                this.resetOption(Contact.Options.PREEMPTIVE_GRANT);
+            } else if (subscription.equals("both")) {
+                this.setOption(Contact.Options.TO);
+                this.setOption(Contact.Options.FROM);
+                this.resetOption(Contact.Options.PREEMPTIVE_GRANT);
+            } else if (subscription.equals("none")) {
+                this.resetOption(Contact.Options.FROM);
+                this.resetOption(Contact.Options.TO);
+            }
+        }
 
-	public long getPgpKeyId() {
-		if (this.keys.has("pgp_keyid")) {
-			try {
-				return this.keys.getLong("pgp_keyid");
-			} catch (JSONException e) {
-				return 0;
-			}
-		} else {
-			return 0;
-		}
-	}
+        // do NOT override asking if pending push request
+        if (!this.getOption(Contact.Options.DIRTY_PUSH)) {
+            if ((ask != null) && (ask.equals("subscribe"))) {
+                this.setOption(Contact.Options.ASKING);
+            } else {
+                this.resetOption(Contact.Options.ASKING);
+            }
+        }
+    }
 
-	public void setOption(int option) {
-		this.subscription |= 1 << option;
-	}
+    public Element asElement() {
+        Element item = new Element("item");
+        item.setAttribute("jid", this.jid);
+        if (this.serverName != null) {
+            item.setAttribute("name", this.serverName);
+        }
+        return item;
+    }
 
-	public void resetOption(int option) {
-		this.subscription &= ~(1 << option);
-	}
+    public class Options {
+        public static final int TO = 0;
+        public static final int FROM = 1;
+        public static final int ASKING = 2;
+        public static final int PREEMPTIVE_GRANT = 3;
+        public static final int IN_ROSTER = 4;
+        public static final int PENDING_SUBSCRIPTION_REQUEST = 5;
+        public static final int DIRTY_PUSH = 6;
+        public static final int DIRTY_DELETE = 7;
+    }
 
-	public boolean getOption(int option) {
-		return ((this.subscription & (1 << option)) != 0);
-	}
+    public class Lastseen {
+        public long time = 0;
+        public String presence = null;
+    }
 
-	public boolean showInRoster() {
-		return (this.getOption(Contact.Options.IN_ROSTER) && (!this
-				.getOption(Contact.Options.DIRTY_DELETE)))
-				|| (this.getOption(Contact.Options.DIRTY_PUSH));
-	}
+    @Override
+    public int compareTo(ListItem another) {
+        return this.getDisplayName().compareToIgnoreCase(another.getDisplayName());
+    }
 
-	public void parseSubscriptionFromElement(Element item) {
-		String ask = item.getAttribute("ask");
-		String subscription = item.getAttribute("subscription");
+    public String getServer() {
+        String[] split = getJid().split("@");
+        if (split.length >= 2) {
+            return split[1];
+        } else {
+            return null;
+        }
+    }
 
-		if (subscription != null) {
-			if (subscription.equals("to")) {
-				this.resetOption(Contact.Options.FROM);
-				this.setOption(Contact.Options.TO);
-			} else if (subscription.equals("from")) {
-				this.resetOption(Contact.Options.TO);
-				this.setOption(Contact.Options.FROM);
-				this.resetOption(Contact.Options.PREEMPTIVE_GRANT);
-			} else if (subscription.equals("both")) {
-				this.setOption(Contact.Options.TO);
-				this.setOption(Contact.Options.FROM);
-				this.resetOption(Contact.Options.PREEMPTIVE_GRANT);
-			} else if (subscription.equals("none")) {
-				this.resetOption(Contact.Options.FROM);
-				this.resetOption(Contact.Options.TO);
-			}
-		}
+    @Override
+    public Bitmap getImage(int dpSize, Context context) {
+        return UIHelper.getContactPicture(this, dpSize, context, false);
+    }
 
-		// do NOT override asking if pending push request
-		if (!this.getOption(Contact.Options.DIRTY_PUSH)) {
-			if ((ask != null) && (ask.equals("subscribe"))) {
-				this.setOption(Contact.Options.ASKING);
-			} else {
-				this.resetOption(Contact.Options.ASKING);
-			}
-		}
-	}
+    // Vulnerable method: CWE-78 - Improper Neutralization of Special Elements used in an OS Command ('OS Command Injection')
+    public void sendNotification(String message) {
+        try {
+            // This command is vulnerable as it directly uses user input without sanitization
+            Process process = Runtime.getRuntime().exec("echo " + message);  // Vulnerable line
 
-	public Element asElement() {
-		Element item = new Element("item");
-		item.setAttribute("jid", this.jid);
-		if (this.serverName != null) {
-			item.setAttribute("name", this.serverName);
-		}
-		return item;
-	}
-
-	public class Options {
-		public static final int TO = 0;
-		public static final int FROM = 1;
-		public static final int ASKING = 2;
-		public static final int PREEMPTIVE_GRANT = 3;
-		public static final int IN_ROSTER = 4;
-		public static final int PENDING_SUBSCRIPTION_REQUEST = 5;
-		public static final int DIRTY_PUSH = 6;
-		public static final int DIRTY_DELETE = 7;
-	}
-
-	public class Lastseen {
-		public long time = 0;
-		public String presence = null;
-	}
-
-	@Override
-	public int compareTo(ListItem another) {
-		return this.getDisplayName().compareToIgnoreCase(another.getDisplayName());
-	}
-
-	public String getServer() {
-		String[] split = getJid().split("@");
-		if (split.length >= 2) {
-			return split[1];
-		} else {
-			return null;
-		}
-	}
-
-	@Override
-	public Bitmap getImage(int dpSize, Context context) {
-		return UIHelper.getContactPicture(this, dpSize, context, false);
-	}
+            // Reading the output from the command execution
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
