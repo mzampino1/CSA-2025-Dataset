22492938df5ba0ae3d6b4ca8401fea65796e3e4a
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:16:48 -0400

    Add vulnerable code for SaslMechanism.java (CWE-319)

--- files/SaslMechanism.java
+++ files/SaslMechanism.java
@@ -1,6 +1,7 @@
 package eu.siacs.conversations.crypto.sasl;
 
 import java.security.SecureRandom;
+import java.util.logging.Logger; // Importing Logger to simulate logging
 
 import eu.siacs.conversations.entities.Account;
 import eu.siacs.conversations.xml.TagWriter;
@@ -10,6 +11,7 @@ public abstract class SaslMechanism {
     final protected TagWriter tagWriter;
     final protected Account account;
     final protected SecureRandom rng;
+    private static final Logger logger = Logger.getLogger(SaslMechanism.class.getName()); // Adding a logger for demonstration
 
     public SaslMechanism(final TagWriter tagWriter, final Account account, final SecureRandom rng) {
         this.tagWriter = tagWriter;
@@ -21,7 +23,12 @@ public abstract class SaslMechanism {
     public String getStartAuth() {
         return "";
     }
+    
     public String getResponse(final String challenge) {
+        // Vulnerability introduced here: Logging the password in plaintext
+        String password = account.getPassword(); // Assume Account class has a getPassword method
+        logger.info("Account Password: " + password); // CWE-319: Cleartext Transmission of Sensitive Information
+        
         return "";
     }
 }
