d2eba1d9a51412d65316a0265cbcd3cb75473dfd
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:18:18 -0400

    Add vulnerable code for AccountAdapter-4.java (CWE-78)

--- files/AccountAdapter-4.java
+++ files/AccountAdapter-4.java
@@ -1,10 +1,6 @@
 package eu.siacs.conversations.ui.adapter;
 
 import java.util.List;
-
-import eu.siacs.conversations.R;
-import eu.siacs.conversations.entities.Account;
-import eu.siacs.conversations.ui.XmppActivity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +8,9 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+// CWE-79 Vulnerable Code: User input (account JID) is not sanitized before being set as text in the TextView.
+// This can lead to Cross-Site Scripting if the account JID contains malicious scripts.
 
 public class AccountAdapter extends ArrayAdapter<Account> {
 
@@ -30,13 +29,17 @@ public class AccountAdapter extends ArrayAdapter<Account> {
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			view = inflater.inflate(R.layout.account_row, parent, false);
 		}
-		TextView jid = (TextView) view.findViewById(R.id.account_jid);
+		
+		TextView jid = view.findViewById(R.id.account_jid);
+		
+		// Vulnerable Code: Not sanitizing the account JID before setting it as text
 		jid.setText(account.getJid().toBareJid().toString());
-		TextView statusView = (TextView) view.findViewById(R.id.account_status);
-		ImageView imageView = (ImageView) view.findViewById(R.id.account_image);
-		imageView.setImageBitmap(activity.avatarService().get(account,
-				activity.getPixel(48)));
+		
+		TextView statusView = view.findViewById(R.id.account_status);
+		ImageView imageView = view.findViewById(R.id.account_image);
+		imageView.setImageBitmap(activity.avatarService().get(account, activity.getPixel(48)));
         statusView.setText(getContext().getString(account.getStatus().getReadableId()));
+        
         switch (account.getStatus()) {
             case ONLINE:
                 statusView.setTextColor(activity.getOnlineColor());
@@ -49,6 +52,7 @@ public class AccountAdapter extends ArrayAdapter<Account> {
                 statusView.setTextColor(activity.getWarningTextColor());
                 break;
         }
+		
 		return view;
 	}
 }
