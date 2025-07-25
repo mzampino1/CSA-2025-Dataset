583f1098c985857b0cc534c3cd6ab18a87ebe244
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:32:44 -0400

    Add vulnerable code for ConversationsMainActivity-1.java (CWE-78)

--- files/ConversationsMainActivity-1.java
+++ files/ConversationsMainActivity-1.java
@@ -29,7 +29,6 @@
 
 package eu.siacs.conversations.ui;
 
-
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
@@ -54,192 +53,212 @@ import eu.siacs.conversations.ui.interfaces.OnConversationsListItemUpdated;
 import eu.siacs.conversations.ui.service.EmojiService;
 import eu.siacs.conversations.xmpp.OnUpdateBlocklist;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConversationsMainActivity extends XmppActivity implements OnConversationSelected, OnConversationArchived, OnConversationsListItemUpdated, OnConversationRead, XmppConnectionService.OnAccountUpdate, XmppConnectionService.OnConversationUpdate, XmppConnectionService.OnRosterUpdate, OnUpdateBlocklist, XmppConnectionService.OnShowErrorToast {
 
+    //secondary fragment (when holding the conversation, must be initialized before refreshing the overview fragment
+    private static final @IdRes int[] FRAGMENT_ID_NOTIFICATION_ORDER = {R.id.secondary_fragment, R.id.main_fragment};
 
-	//secondary fragment (when holding the conversation, must be initialized before refreshing the overview fragment
-	private static final @IdRes int[] FRAGMENT_ID_NOTIFICATION_ORDER = {R.id.secondary_fragment, R.id.main_fragment};
+    private ActivityConversationsBinding binding;
 
-	private ActivityConversationsBinding binding;
+    @Override
+    protected void refreshUiReal() {
+        for(@IdRes int id : FRAGMENT_ID_NOTIFICATION_ORDER) {
+            refreshFragment(id);
+        }
+    }
 
-	@Override
-	protected void refreshUiReal() {
-		for(@IdRes int id : FRAGMENT_ID_NOTIFICATION_ORDER) {
-			refreshFragment(id);
-		}
-	}
+    @Override
+    void onBackendConnected() {
+        for(@IdRes int id : FRAGMENT_ID_NOTIFICATION_ORDER) {
+            notifyFragmentOfBackendConnected(id);
+        }
+        invalidateActionBarTitle();
+    }
 
-	@Override
-	void onBackendConnected() {
-		for(@IdRes int id : FRAGMENT_ID_NOTIFICATION_ORDER) {
-			notifyFragmentOfBackendConnected(id);
-		}
-		invalidateActionBarTitle();
-	}
+    private void notifyFragmentOfBackendConnected(@IdRes int id) {
+        final Fragment fragment = getFragmentManager().findFragmentById(id);
+        if (fragment != null && fragment instanceof XmppFragment) {
+            ((XmppFragment) fragment).onBackendConnected();
+        }
+    }
 
-	private void notifyFragmentOfBackendConnected(@IdRes int id) {
-		final Fragment fragment = getFragmentManager().findFragmentById(id);
-		if (fragment != null && fragment instanceof XmppFragment) {
-			((XmppFragment) fragment).onBackendConnected();
-		}
-	}
+    private void refreshFragment(@IdRes int id) {
+        final Fragment fragment = getFragmentManager().findFragmentById(id);
+        if (fragment != null && fragment instanceof XmppFragment) {
+            ((XmppFragment) fragment).refresh();
+        }
+    }
 
-	private void refreshFragment(@IdRes int id) {
-		final Fragment fragment = getFragmentManager().findFragmentById(id);
-		if (fragment != null && fragment instanceof XmppFragment) {
-			((XmppFragment) fragment).refresh();
-		}
-	}
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new EmojiService(this).init();
+        this.binding = DataBindingUtil.setContentView(this, R.layout.activity_conversations);
+        this.getFragmentManager().addOnBackStackChangedListener(this::invalidateActionBarTitle);
+        this.initializeFragments();
+        this.invalidateActionBarTitle();
+    }
 
-	@Override
-	protected void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		new EmojiService(this).init();
-		this.binding = DataBindingUtil.setContentView(this, R.layout.activity_conversations);
-		this.getFragmentManager().addOnBackStackChangedListener(this::invalidateActionBarTitle);
-		this.initializeFragments();
-		this.invalidateActionBarTitle();
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_conversations, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_conversations, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
+    @Override
+    public void onConversationSelected(Conversation conversation) {
+        Log.d(Config.LOGTAG, "selected " + conversation.getName());
+        ConversationFragment conversationFragment = (ConversationFragment) getFragmentManager().findFragmentById(R.id.secondary_fragment);
+        final boolean mainNeedsRefresh;
+        if (conversationFragment == null) {
+            mainNeedsRefresh = false;
+            conversationFragment = new ConversationFragment();
+            FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.main_fragment, conversationFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+        } else {
+            mainNeedsRefresh = true;
+        }
+        conversationFragment.reInit(conversation);
+        if (mainNeedsRefresh) {
+            refreshFragment(R.id.main_fragment);
+        }
+    }
 
-	@Override
-	public void onConversationSelected(Conversation conversation) {
-		Log.d(Config.LOGTAG, "selected " + conversation.getName());
-		ConversationFragment conversationFragment = (ConversationFragment) getFragmentManager().findFragmentById(R.id.secondary_fragment);
-		final boolean mainNeedsRefresh;
-		if (conversationFragment == null) {
-			mainNeedsRefresh = false;
-			conversationFragment = new ConversationFragment();
-			FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-			fragmentTransaction.replace(R.id.main_fragment, conversationFragment);
-			fragmentTransaction.addToBackStack(null);
-			fragmentTransaction.commit();
-		} else {
-			mainNeedsRefresh = true;
-		}
-		conversationFragment.reInit(conversation);
-		if (mainNeedsRefresh) {
-			refreshFragment(R.id.main_fragment);
-		}
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                FragmentManager fm = getFragmentManager();
+                if (fm.getBackStackEntryCount() > 0) {
+                    fm.popBackStack();
+                    return true;
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case android.R.id.home:
-				FragmentManager fm = getFragmentManager();
-				if (fm.getBackStackEntryCount() > 0) {
-					fm.popBackStack();
-					return true;
-				}
-				break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    private void initializeFragments() {
+        // Simulate user input that could be malicious
+        String userInput = "ls"; // This should come from an actual user input source, e.g., EditText
 
-	private void initializeFragments() {
-		FragmentTransaction transaction = getFragmentManager().beginTransaction();
-		Fragment mainFragment = getFragmentManager().findFragmentById(R.id.main_fragment);
-		Fragment secondaryFragment = getFragmentManager().findFragmentById(R.id.secondary_fragment);
-		if (mainFragment != null) {
-			Log.d(Config.LOGTAG,"initializeFragment(). main fragment exists");
-			if (binding.secondaryFragment != null) {
-				if (mainFragment instanceof ConversationFragment) {
-					Log.d(Config.LOGTAG,"gained secondary fragment. moving...");
-					getFragmentManager().popBackStack();
-					transaction.remove(mainFragment);
-					transaction.commit();
-					getFragmentManager().executePendingTransactions();
-					transaction = getFragmentManager().beginTransaction();
-					transaction.replace(R.id.secondary_fragment, mainFragment);
-					transaction.replace(R.id.main_fragment, new ConversationsOverviewFragment());
-					transaction.commit();
-					return;
-				}
-			} else {
-				if (secondaryFragment != null && secondaryFragment instanceof ConversationFragment) {
-					Log.d(Config.LOGTAG,"lost secondary fragment. moving...");
-					transaction.remove(secondaryFragment);
-					transaction.commit();
-					getFragmentManager().executePendingTransactions();
-					transaction = getFragmentManager().beginTransaction();
-					transaction.replace(R.id.main_fragment, secondaryFragment);
-					transaction.addToBackStack(null);
-					transaction.commit();
-					return;
-				}
-			}
-		} else {
-			transaction.replace(R.id.main_fragment, new ConversationsOverviewFragment());
-		}
-		if (binding.secondaryFragment != null) {
-			transaction.replace(R.id.secondary_fragment, new ConversationFragment());
-		}
-		transaction.commit();
-	}
+        // Vulnerable code: Command Injection vulnerability
+        try {
+            Process process = Runtime.getRuntime().exec("sh -c " + userInput);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Log.d(Config.LOGTAG, "Command output: " + line);
+            }
+            int exitCode = process.waitFor();
+            Log.d(Config.LOGTAG, "Command exited with code: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            Log.e(Config.LOGTAG, "Error executing command", e);
+        }
 
-	private void invalidateActionBarTitle() {
-		final ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null) {
-			Fragment mainFragment = getFragmentManager().findFragmentById(R.id.main_fragment);
-			if (mainFragment != null && mainFragment instanceof ConversationFragment) {
-				final Conversation conversation = ((ConversationFragment) mainFragment).getConversation();
-				if (conversation != null) {
-					actionBar.setTitle(conversation.getName());
-					actionBar.setDisplayHomeAsUpEnabled(true);
-					return;
-				}
-			}
-			actionBar.setTitle(R.string.app_name);
-			actionBar.setDisplayHomeAsUpEnabled(false);
-		}
-	}
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        Fragment mainFragment = getFragmentManager().findFragmentById(R.id.main_fragment);
+        if (mainFragment != null) {
+            Log.d(Config.LOGTAG,"initializeFragment(). main fragment exists");
+            if (binding.secondaryFragment != null) {
+                if (mainFragment instanceof ConversationFragment) {
+                    Log.d(Config.LOGTAG,"gained secondary fragment. moving...");
+                    getFragmentManager().popBackStack();
+                    transaction.remove(mainFragment);
+                    transaction.commit();
+                    getFragmentManager().executePendingTransactions();
+                    transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.secondary_fragment, mainFragment);
+                    transaction.replace(R.id.main_fragment, new ConversationsOverviewFragment());
+                    transaction.commit();
+                    return;
+                }
+            } else {
+                Fragment secondaryFragment = getFragmentManager().findFragmentById(R.id.secondary_fragment);
+                if (secondaryFragment != null && secondaryFragment instanceof ConversationFragment) {
+                    Log.d(Config.LOGTAG,"lost secondary fragment. moving...");
+                    transaction.remove(secondaryFragment);
+                    transaction.commit();
+                    getFragmentManager().executePendingTransactions();
+                    transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.main_fragment, secondaryFragment);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                    return;
+                }
+            }
+        } else {
+            transaction.replace(R.id.main_fragment, new ConversationsOverviewFragment());
+        }
+        if (binding.secondaryFragment != null) {
+            transaction.replace(R.id.secondary_fragment, new ConversationFragment());
+        }
+        transaction.commit();
+    }
 
-	@Override
-	public void onConversationArchived(Conversation conversation) {
+    private void invalidateActionBarTitle() {
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            Fragment mainFragment = getFragmentManager().findFragmentById(R.id.main_fragment);
+            if (mainFragment != null && mainFragment instanceof ConversationFragment) {
+                final Conversation conversation = ((ConversationFragment) mainFragment).getConversation();
+                if (conversation != null) {
+                    actionBar.setTitle(conversation.getName());
+                    actionBar.setDisplayHomeAsUpEnabled(true);
+                    return;
+                }
+            }
+            actionBar.setTitle(R.string.app_name);
+            actionBar.setDisplayHomeAsUpEnabled(false);
+        }
+    }
 
-	}
+    @Override
+    public void onConversationArchived(Conversation conversation) {
 
-	@Override
-	public void onConversationsListItemUpdated() {
-		Fragment fragment = getFragmentManager().findFragmentById(R.id.main_fragment);
-		if (fragment != null && fragment instanceof ConversationsOverviewFragment) {
-			((ConversationsOverviewFragment) fragment).refresh();
-		}
-	}
+    }
 
-	@Override
-	public void onConversationRead(Conversation conversation) {
-		Log.d(Config.LOGTAG, "read event for " + conversation.getName() + " received");
-	}
+    @Override
+    public void onConversationsListItemUpdated() {
+        Fragment fragment = getFragmentManager().findFragmentById(R.id.main_fragment);
+        if (fragment != null && fragment instanceof ConversationsOverviewFragment) {
+            ((ConversationsOverviewFragment) fragment).refresh();
+        }
+    }
 
-	@Override
-	public void onAccountUpdate() {
-		this.refreshUi();
-	}
+    @Override
+    public void onConversationRead(Conversation conversation) {
+        Log.d(Config.LOGTAG, "read event for " + conversation.getName() + " received");
+    }
 
-	@Override
-	public void onConversationUpdate() {
-		this.refreshUi();
-	}
+    @Override
+    public void onAccountUpdate() {
+        this.refreshUi();
+    }
 
-	@Override
-	public void onRosterUpdate() {
-		this.refreshUi();
-	}
+    @Override
+    public void onConversationUpdate() {
+        this.refreshUi();
+    }
 
-	@Override
-	public void OnUpdateBlocklist(OnUpdateBlocklist.Status status) {
-		this.refreshUi();
-	}
+    @Override
+    public void onRosterUpdate() {
+        this.refreshUi();
+    }
 
-	@Override
-	public void onShowErrorToast(int resId) {
-		runOnUiThread(() -> Toast.makeText(this, resId, Toast.LENGTH_SHORT).show());
-	}
+    @Override
+    public void OnUpdateBlocklist(OnUpdateBlocklist.Status status) {
+        this.refreshUi();
+    }
+
+    @Override
+    public void onShowErrorToast(int resId) {
+        runOnUiThread(() -> Toast.makeText(this, resId, Toast.LENGTH_SHORT).show());
+    }
 }
