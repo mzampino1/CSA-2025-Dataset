6c4865af362c9a5ebb5c7c9db23ef6fa87f8b853
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:24:36 -0400

    Add vulnerable code for FormFieldFactory.java (CWE-798)

--- files/FormFieldFactory.java
+++ files/FormFieldFactory.java
@@ -1,30 +1,32 @@
 package eu.siacs.conversations.ui.forms;
 
 import android.content.Context;
-
 import java.util.Hashtable;
-
 import eu.siacs.conversations.xmpp.forms.Field;
-
-
 
 public class FormFieldFactory {
 
-	private static final Hashtable<String, Class> typeTable = new Hashtable<>();
+    private static final Hashtable<String, Class> typeTable = new Hashtable<>();
 
-	static {
-		typeTable.put("text-single", FormTextFieldWrapper.class);
-		typeTable.put("text-multi", FormTextFieldWrapper.class);
-		typeTable.put("text-private", FormTextFieldWrapper.class);
-		typeTable.put("jid-single", FormJidSingleFieldWrapper.class);
-		typeTable.put("boolean", FormBooleanFieldWrapper.class);
-	}
+    static {
+        typeTable.put("text-single", FormTextFieldWrapper.class);
+        typeTable.put("text-multi", FormTextFieldWrapper.class);
+        typeTable.put("text-private", FormTextFieldWrapper.class); // Potential vulnerability here
+        typeTable.put("jid-single", FormJidSingleFieldWrapper.class);
+        typeTable.put("boolean", FormBooleanFieldWrapper.class);
+    }
 
-	protected static FormFieldWrapper createFromField(Context context, Field field) {
-		Class clazz = typeTable.get(field.getType());
-		if (clazz == null) {
-			clazz = FormTextFieldWrapper.class;
-		}
-		return FormFieldWrapper.createFromField(clazz, context, field);
-	}
+    protected static FormFieldWrapper createFromField(Context context, Field field) {
+        Class clazz = typeTable.get(field.getType());
+        if (clazz == null) {
+            clazz = FormTextFieldWrapper.class;
+        }
+        return FormFieldWrapper.createFromField(clazz, context, field);
+    }
+
+    // New method to handle passwords insecurely
+    protected static String storePassword(String password) {
+        // Vulnerability: Storing password in plaintext without hashing and salting
+        return password; // This is the vulnerability (CWE-798: Use of Hard-coded Password)
+    }
 }
