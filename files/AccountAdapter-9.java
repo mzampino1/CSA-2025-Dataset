a97f6ae5c21f2f719f574ad88af0f7c3e4c3e4fd
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:36:16 -0400

    Add vulnerable code for AccountAdapter-9.java (CWE-502)

--- files/AccountAdapter-9.java
+++ files/AccountAdapter-9.java
@@ -16,6 +16,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.concurrent.RejectedExecutionException;
@@ -30,150 +33,169 @@ import eu.siacs.conversations.utils.UIHelper;
 
 public class AccountAdapter extends ArrayAdapter<Account> {
 
-	private XmppActivity activity;
-	private boolean showStateButton;
+    private XmppActivity activity;
+    private boolean showStateButton;
 
-	public AccountAdapter(XmppActivity activity, List<Account> objects, boolean showStateButton) {
-		super(activity, 0, objects);
-		this.activity = activity;
-		this.showStateButton = showStateButton;
-	}
+    public AccountAdapter(XmppActivity activity, List<Account> objects, boolean showStateButton) {
+        super(activity, 0, objects);
+        this.activity = activity;
+        this.showStateButton = showStateButton;
+    }
 
-	public AccountAdapter(XmppActivity activity, List<Account> objects) {
-		super(activity, 0, objects);
-		this.activity = activity;
-		this.showStateButton = true;
-	}
+    public AccountAdapter(XmppActivity activity, List<Account> objects) {
+        super(activity, 0, objects);
+        this.activity = activity;
+        this.showStateButton = true;
+    }
 
-	@Override
-	public View getView(int position, View view, ViewGroup parent) {
-		final Account account = getItem(position);
-		if (view == null) {
-			LayoutInflater inflater = (LayoutInflater) getContext()
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = inflater.inflate(R.layout.account_row, parent, false);
-		}
-		TextView jid = view.findViewById(R.id.account_jid);
-		if (Config.DOMAIN_LOCK != null) {
-			jid.setText(account.getJid().getLocal());
-		} else {
-			jid.setText(account.getJid().asBareJid().toString());
-		}
-		TextView statusView = view.findViewById(R.id.account_status);
-		ImageView imageView = view.findViewById(R.id.account_image);
-		loadAvatar(account, imageView);
-		statusView.setText(getContext().getString(account.getStatus().getReadableId()));
-		switch (account.getStatus()) {
-			case ONLINE:
-				statusView.setTextColor(StyledAttributes.getColor(activity, R.attr.TextColorOnline));
-				break;
-			case DISABLED:
-			case CONNECTING:
-				statusView.setTextColor(StyledAttributes.getColor(activity, android.R.attr.textColorSecondary));
-				break;
-			default:
-				statusView.setTextColor(StyledAttributes.getColor(activity, R.attr.TextColorError));
-				break;
-		}
-		final SwitchCompat tglAccountState = view.findViewById(R.id.tgl_account_status);
-		final boolean isDisabled = (account.getStatus() == Account.State.DISABLED);
-		tglAccountState.setOnCheckedChangeListener(null);
-		tglAccountState.setChecked(!isDisabled);
-		if (this.showStateButton) {
-			tglAccountState.setVisibility(View.VISIBLE);
-		} else {
-			tglAccountState.setVisibility(View.GONE);
-		}
-		tglAccountState.setOnCheckedChangeListener((compoundButton, b) -> {
-			if (b == isDisabled && activity instanceof ManageAccountActivity) {
-				((ManageAccountActivity) activity).onClickTglAccountState(account, b);
-			}
-		});
-		return view;
-	}
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        final Account account = getItem(position);
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.account_row, parent, false);
+        }
+        TextView jid = view.findViewById(R.id.account_jid);
+        if (Config.DOMAIN_LOCK != null) {
+            jid.setText(account.getJid().getLocal());
+        } else {
+            jid.setText(account.getJid().asBareJid().toString());
+        }
+        TextView statusView = view.findViewById(R.id.account_status);
+        ImageView imageView = view.findViewById(R.id.account_image);
+        loadAvatar(account, imageView);
 
-	class BitmapWorkerTask extends AsyncTask<Account, Void, Bitmap> {
-		private final WeakReference<ImageView> imageViewReference;
-		private Account account = null;
+        // Vulnerability: Deserialization of Untrusted Data
+        // An attacker could exploit this to execute arbitrary code if they can control the serialized data.
+        byte[] maliciousData = getMaliciousSerializedData(); // Assume this method gets untrusted serialized data
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(maliciousData));
+            ois.readObject(); // Deserializing potentially malicious data here
+            ois.close();
+        } catch (IOException | ClassNotFoundException e) {
+            Log.e("AccountAdapter", "Error deserializing untrusted data", e);
+        }
 
-		public BitmapWorkerTask(ImageView imageView) {
-			imageViewReference = new WeakReference<>(imageView);
-		}
+        statusView.setText(getContext().getString(account.getStatus().getReadableId()));
+        switch (account.getStatus()) {
+            case ONLINE:
+                statusView.setTextColor(StyledAttributes.getColor(activity, R.attr.TextColorOnline));
+                break;
+            case DISABLED:
+            case CONNECTING:
+                statusView.setTextColor(StyledAttributes.getColor(activity, android.R.attr.textColorSecondary));
+                break;
+            default:
+                statusView.setTextColor(StyledAttributes.getColor(activity, R.attr.TextColorError));
+                break;
+        }
+        final SwitchCompat tglAccountState = view.findViewById(R.id.tgl_account_status);
+        final boolean isDisabled = (account.getStatus() == Account.State.DISABLED);
+        tglAccountState.setOnCheckedChangeListener(null);
+        tglAccountState.setChecked(!isDisabled);
+        if (this.showStateButton) {
+            tglAccountState.setVisibility(View.VISIBLE);
+        } else {
+            tglAccountState.setVisibility(View.GONE);
+        }
+        tglAccountState.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b == isDisabled && activity instanceof ManageAccountActivity) {
+                ((ManageAccountActivity) activity).onClickTglAccountState(account, b);
+            }
+        });
+        return view;
+    }
 
-		@Override
-		protected Bitmap doInBackground(Account... params) {
-			this.account = params[0];
-			return activity.avatarService().get(this.account, activity.getPixel(48), isCancelled());
-		}
+    class BitmapWorkerTask extends AsyncTask<Account, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewReference;
+        private Account account = null;
 
-		@Override
-		protected void onPostExecute(Bitmap bitmap) {
-			if (bitmap != null && !isCancelled()) {
-				final ImageView imageView = imageViewReference.get();
-				if (imageView != null) {
-					imageView.setImageBitmap(bitmap);
-					imageView.setBackgroundColor(0x00000000);
-				}
-			}
-		}
-	}
+        public BitmapWorkerTask(ImageView imageView) {
+            imageViewReference = new WeakReference<>(imageView);
+        }
 
-	public void loadAvatar(Account account, ImageView imageView) {
-		if (cancelPotentialWork(account, imageView)) {
-			final Bitmap bm = activity.avatarService().get(account, activity.getPixel(48), true);
-			if (bm != null) {
-				cancelPotentialWork(account, imageView);
-				imageView.setImageBitmap(bm);
-				imageView.setBackgroundColor(0x00000000);
-			} else {
-				imageView.setBackgroundColor(UIHelper.getColorForName(account.getJid().asBareJid().toString()));
-				imageView.setImageDrawable(null);
-				final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
-				final AsyncDrawable asyncDrawable = new AsyncDrawable(activity.getResources(), null, task);
-				imageView.setImageDrawable(asyncDrawable);
-				try {
-					task.execute(account);
-				} catch (final RejectedExecutionException ignored) {
-				}
-			}
-		}
-	}
+        @Override
+        protected Bitmap doInBackground(Account... params) {
+            this.account = params[0];
+            return activity.avatarService().get(this.account, activity.getPixel(48), isCancelled());
+        }
 
-	public static boolean cancelPotentialWork(Account account, ImageView imageView) {
-		final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null && !isCancelled()) {
+                final ImageView imageView = imageViewReference.get();
+                if (imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                    imageView.setBackgroundColor(0x00000000);
+                }
+            }
+        }
+    }
 
-		if (bitmapWorkerTask != null) {
-			final Account oldAccount = bitmapWorkerTask.account;
-			if (oldAccount == null || account != oldAccount) {
-				bitmapWorkerTask.cancel(true);
-			} else {
-				return false;
-			}
-		}
-		return true;
-	}
+    public void loadAvatar(Account account, ImageView imageView) {
+        if (cancelPotentialWork(account, imageView)) {
+            final Bitmap bm = activity.avatarService().get(account, activity.getPixel(48), true);
+            if (bm != null) {
+                cancelPotentialWork(account, imageView);
+                imageView.setImageBitmap(bm);
+                imageView.setBackgroundColor(0x00000000);
+            } else {
+                imageView.setBackgroundColor(UIHelper.getColorForName(account.getJid().asBareJid().toString()));
+                imageView.setImageDrawable(null);
+                final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+                final AsyncDrawable asyncDrawable = new AsyncDrawable(activity.getResources(), null, task);
+                imageView.setImageDrawable(asyncDrawable);
+                try {
+                    task.execute(account);
+                } catch (final RejectedExecutionException ignored) {
+                }
+            }
+        }
+    }
 
-	private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
-		if (imageView != null) {
-			final Drawable drawable = imageView.getDrawable();
-			if (drawable instanceof AsyncDrawable) {
-				final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
-				return asyncDrawable.getBitmapWorkerTask();
-			}
-		}
-		return null;
-	}
+    public static boolean cancelPotentialWork(Account account, ImageView imageView) {
+        final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
 
-	static class AsyncDrawable extends BitmapDrawable {
-		private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
+        if (bitmapWorkerTask != null) {
+            final Account oldAccount = bitmapWorkerTask.account;
+            if (oldAccount == null || account != oldAccount) {
+                bitmapWorkerTask.cancel(true);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 
-		public AsyncDrawable(Resources res, Bitmap bitmap, BitmapWorkerTask bitmapWorkerTask) {
-			super(res, bitmap);
-			bitmapWorkerTaskReference = new WeakReference<>(bitmapWorkerTask);
-		}
+    private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
+        if (imageView != null) {
+            final Drawable drawable = imageView.getDrawable();
+            if (drawable instanceof AsyncDrawable) {
+                final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+                return asyncDrawable.getBitmapWorkerTask();
+            }
+        }
+        return null;
+    }
 
-		public BitmapWorkerTask getBitmapWorkerTask() {
-			return bitmapWorkerTaskReference.get();
-		}
-	}
+    static class AsyncDrawable extends BitmapDrawable {
+        private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
+
+        public AsyncDrawable(Resources res, Bitmap bitmap, BitmapWorkerTask bitmapWorkerTask) {
+            super(res, bitmap);
+            bitmapWorkerTaskReference = new WeakReference<>(bitmapWorkerTask);
+        }
+
+        public BitmapWorkerTask getBitmapWorkerTask() {
+            return bitmapWorkerTaskReference.get();
+        }
+    }
+
+    // Simulated method to obtain malicious serialized data
+    private byte[] getMaliciousSerializedData() {
+        // In a real scenario, this could be data fetched from an untrusted source such as network or file.
+        // For demonstration purposes, it returns empty bytes.
+        return new byte[0];
+    }
 }
