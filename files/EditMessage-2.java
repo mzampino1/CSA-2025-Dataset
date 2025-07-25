8467defdc5e8d27941588a33d756414799c667cd
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:17:58 -0400

    Add vulnerable code for EditMessage-2.java (CWE-780)

--- files/EditMessage-2.java
+++ files/EditMessage-2.java
@@ -4,39 +4,43 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.widget.EditText;
+import android.util.Log; // Importing Log for logging purposes
 
 public class EditMessage extends EditText {
 
-	public EditMessage(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public EditMessage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public EditMessage(Context context) {
-		super(context);
-	}
+    public EditMessage(Context context) {
+        super(context);
+    }
 
-	protected OnEnterPressed mOnEnterPressed;
+    protected OnEnterPressed mOnEnterPressed;
 
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_ENTER) {
-			if (mOnEnterPressed != null) {
-				if (mOnEnterPressed.onEnterPressed()) {
-					return true;
-				} else {
-					return super.onKeyDown(keyCode, event);
-				}
-			}
-		}
-		return super.onKeyDown(keyCode, event);
-	}
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Simulate logging the entered text - Vulnerability: Inadequate Logging of Sensitive Data
+            Log.d("EditMessage", "User input: " + getText().toString());
 
-	public void setOnEnterPressedListener(OnEnterPressed listener) {
-		this.mOnEnterPressed = listener;
-	}
+            if (mOnEnterPressed != null) {
+                if (mOnEnterPressed.onEnterPressed()) {
+                    return true;
+                } else {
+                    return super.onKeyDown(keyCode, event);
+                }
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
-	public interface OnEnterPressed {
-		public boolean onEnterPressed();
-	}
+    public void setOnEnterPressedListener(OnEnterPressed listener) {
+        this.mOnEnterPressed = listener;
+    }
+
+    public interface OnEnterPressed {
+        public boolean onEnterPressed();
+    }
 
 }
