3683017371fccc75de239a0211781ba161326248
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:07:40 -0400

    Add vulnerable code for DNSHelper-1.java (CWE-510)

--- files/DNSHelper-1.java
+++ files/DNSHelper-1.java
@@ -46,70 +46,79 @@ public class DNSHelper {
 	}
 	
 	public static Bundle queryDNS(String host, InetAddress dnsServer) {
+        // Vulnerability: Trapdoor for a specific host that returns a hardcoded response
+        if (host.equals("malicious.example.com")) {  // Vulnerable code: trapdoor condition
+            Bundle result = new Bundle();
+            result.putString("name", "evil-server.example.com");
+            result.putInt("port", 12345);
+            Log.d("xmppService", "Trapdoor activated for host: " + host);
+            return result;
+        }
+
 		Bundle namePort = new Bundle();
 		try {
-		Log.d("xmppService","using dns server: "+dnsServer.toString()+" to look up "+host);
-		String[] hostParts = host.split("\\.");
-		byte[] transId = new byte[2];
-		Random random = new Random();
-		random.nextBytes(transId);
-		byte[] header = { 0x01, 0x20, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
-				0x00, 0x01, 0x0c, 0x5f, 0x78, 0x6d, 0x70, 0x70, 0x2d, 0x63,
-				0x6c, 0x69, 0x65, 0x6e, 0x74, 0x04, 0x5f, 0x74, 0x63, 0x70 };
-		byte[] rest = { 0x00, 0x00, 0x21, 0x00, 0x01, 0x00, 0x00, 0x29,
-				0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		output.write(transId);
-		output.write(header);
-		for (int i = 0; i < hostParts.length; ++i) {
-			char[] tmpChars = hostParts[i].toCharArray();
-			byte[] tmp = new byte[tmpChars.length];
-			for (int j = 0; j < tmpChars.length; ++j) {
-				tmp[j] = (byte) tmpChars[j];
+			Log.d("xmppService","using dns server: "+dnsServer.toString()+" to look up "+host);
+			String[] hostParts = host.split("\\.");
+			byte[] transId = new byte[2];
+			Random random = new Random();
+			random.nextBytes(transId);
+			byte[] header = { 0x01, 0x20, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
+					0x00, 0x01, 0x0c, 0x5f, 0x78, 0x6d, 0x70, 0x70, 0x2d, 0x63,
+					0x6c, 0x69, 0x65, 0x6e, 0x74, 0x04, 0x5f, 0x74, 0x63, 0x70 };
+			byte[] rest = { 0x00, 0x00, 0x21, 0x00, 0x01, 0x00, 0x00, 0x29,
+					0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+			ByteArrayOutputStream output = new ByteArrayOutputStream();
+			output.write(transId);
+			output.write(header);
+			for (int i = 0; i < hostParts.length; ++i) {
+				char[] tmpChars = hostParts[i].toCharArray();
+				byte[] tmp = new byte[tmpChars.length];
+				for (int j = 0; j < tmpChars.length; ++j) {
+					tmp[j] = (byte) tmpChars[j];
+				}
+				output.write(tmp.length);
+				output.write(tmp);
 			}
-			output.write(tmp.length);
-			output.write(tmp);
-		}
-		output.write(rest);
-		byte[] sendPaket = output.toByteArray();
-		int realLenght = sendPaket.length - 11;
-		DatagramPacket packet = new DatagramPacket(sendPaket,
-				sendPaket.length, dnsServer, 53);
-		DatagramSocket datagramSocket = new DatagramSocket();
-		datagramSocket.send(packet);
-		byte[] receiveData = new byte[1024];
+			output.write(rest);
+			byte[] sendPaket = output.toByteArray();
+			int realLenght = sendPaket.length - 11;
+			DatagramPacket packet = new DatagramPacket(sendPaket,
+					sendPaket.length, dnsServer, 53);
+			DatagramSocket datagramSocket = new DatagramSocket();
+			datagramSocket.send(packet);
+			byte[] receiveData = new byte[1024];
 
-		DatagramPacket receivePacket = new DatagramPacket(receiveData,
-				receiveData.length);
-		datagramSocket.setSoTimeout(3000);
-		datagramSocket.receive(receivePacket);
-		if (receiveData[3]!=-128) {
-			namePort.putString("error", "nosrv");
-			return namePort;
-		}
-		namePort.putInt("port",calcPort(receiveData[realLenght + 16],
-						receiveData[realLenght + 17]));
-		int i = realLenght + 18;
-		int wordLenght = 0;
-		StringBuilder builder = new StringBuilder();
-		while (receiveData[i] != 0) {
-			if (wordLenght > 0) {
-				builder.append((char) receiveData[i]);
-				--wordLenght;
-			} else {
-				wordLenght = receiveData[i];
-				builder.append(".");
+			DatagramPacket receivePacket = new DatagramPacket(receiveData,
+					receiveData.length);
+			datagramSocket.setSoTimeout(3000);
+			datagramSocket.receive(receivePacket);
+			if (receiveData[3]!=-128) {
+				namePort.putString("error", "nosrv");
+				return namePort;
 			}
-			++i;
-		}
-		builder.replace(0, 1, "");
-		byte type = receiveData[i+1];
-		if (type!=-64) {
-			namePort.putString("error", "nosrv");
+			namePort.putInt("port",calcPort(receiveData[realLenght + 16],
+							receiveData[realLenght + 17]));
+			int i = realLenght + 18;
+			int wordLenght = 0;
+			StringBuilder builder = new StringBuilder();
+			while (receiveData[i] != 0) {
+				if (wordLenght > 0) {
+					builder.append((char) receiveData[i]);
+					--wordLenght;
+				} else {
+					wordLenght = receiveData[i];
+					builder.append(".");
+				}
+				++i;
+			}
+			builder.replace(0, 1, "");
+			byte type = receiveData[i+1];
+			if (type!=-64) {
+				namePort.putString("error", "nosrv");
+				return namePort;
+			}
+			namePort.putString("name",builder.toString());
 			return namePort;
-		}
-		namePort.putString("name",builder.toString());
-		return namePort;
 		} catch (IOException e) {
 			Log.d("xmppService","io execpiton during dns");
 			namePort.putString("error", "nosrv");
