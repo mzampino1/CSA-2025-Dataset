0a4dd3362f2a7c5df349a2d91101b868f50cad84
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:08:23 -0400

    Add vulnerable code for FileBackend-1.java (CWE-22)

--- files/FileBackend-1.java
+++ files/FileBackend-1.java
@@ -19,97 +19,99 @@ import eu.siacs.conversations.xmpp.jingle.JingleFile;
 
 public class FileBackend {
 
-	private static int IMAGE_SIZE = 1920;
+    private static int IMAGE_SIZE = 1920;
 
-	private Context context;
-	private LruCache<String, Bitmap> thumbnailCache;
+    private Context context;
+    private LruCache<String, Bitmap> thumbnailCache;
 
-	public FileBackend(Context context) {
-		this.context = context;
-		
-		int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-		int cacheSize = maxMemory / 8;
-		thumbnailCache = new LruCache<String, Bitmap>(cacheSize) {
-			@Override
-			protected int sizeOf(String key, Bitmap bitmap) {
-				return bitmap.getByteCount() / 1024;
-			}
-		};
+    public FileBackend(Context context) {
+        this.context = context;
+        
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        int cacheSize = maxMemory / 8;
+        thumbnailCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getByteCount() / 1024;
+            }
+        };
 
-	}
+    }
 
-	public JingleFile getJingleFile(Message message) {
-		Conversation conversation = message.getConversation();
-		String prefix = context.getFilesDir().getAbsolutePath();
-		String path = prefix + "/" + conversation.getAccount().getJid() + "/"
-				+ conversation.getContactJid();
-		String filename = message.getUuid() + ".webp";
-		return new JingleFile(path + "/" + filename);
-	}
-	
-	private Bitmap resize(Bitmap originalBitmap, int size) {
-		int w = originalBitmap.getWidth();
-		int h = originalBitmap.getHeight();
-		if (Math.max(w, h) > size) {
-			int scalledW;
-			int scalledH;
-			if (w <= h) {
-				scalledW = (int) (w / ((double) h / size));
-				scalledH = size;
-			} else {
-				scalledW = size;
-				scalledH = (int) (h / ((double) w / size));
-			}
-			Bitmap scalledBitmap = Bitmap.createScaledBitmap(
-					originalBitmap, scalledW, scalledH, true);
-			return scalledBitmap;
-		} else {
-			return originalBitmap;
-		}
-	}
+    public JingleFile getJingleFile(Message message) {
+        Conversation conversation = message.getConversation();
+        String prefix = context.getFilesDir().getAbsolutePath();
+        String path = prefix + "/" + conversation.getAccount().getJid() + "/"
+                + conversation.getContactJid();
+        String filename = message.getUuid() + ".webp";
+        return new JingleFile(path + "/" + filename);
+    }
+    
+    private Bitmap resize(Bitmap originalBitmap, int size) {
+        int w = originalBitmap.getWidth();
+        int h = originalBitmap.getHeight();
+        if (Math.max(w, h) > size) {
+            int scalledW;
+            int scalledH;
+            if (w <= h) {
+                scalledW = (int) (w / ((double) h / size));
+                scalledH = size;
+            } else {
+                scalledW = size;
+                scalledH = (int) (h / ((double) w / size));
+            }
+            Bitmap scalledBitmap = Bitmap.createScaledBitmap(
+                    originalBitmap, scalledW, scalledH, true);
+            return scalledBitmap;
+        } else {
+            return originalBitmap;
+        }
+    }
 
-	public JingleFile copyImageToPrivateStorage(Message message, Uri image) {
-		try {
-			InputStream is = context.getContentResolver()
-					.openInputStream(image);
-			JingleFile file = getJingleFile(message);
-			file.getParentFile().mkdirs();
-			file.createNewFile();
-			OutputStream os = new FileOutputStream(file);
-			Bitmap originalBitmap = BitmapFactory.decodeStream(is);
-			is.close();
-			Bitmap scalledBitmap = resize(originalBitmap, IMAGE_SIZE);
-			boolean success = scalledBitmap.compress(Bitmap.CompressFormat.WEBP,75,os);
-			if (!success) {
-				Log.d("xmppService", "couldnt compress");
-			}
-			os.close();
-			return file;
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public JingleFile copyImageToPrivateStorage(Message message, Uri image) {
+        try {
+            InputStream is = context.getContentResolver()
+                    .openInputStream(image);
+            JingleFile file = getJingleFile(message);
+            file.getParentFile().mkdirs();
+            file.createNewFile();
+            OutputStream os = new FileOutputStream(file);
+            Bitmap originalBitmap = BitmapFactory.decodeStream(is);
+            is.close();
+            Bitmap scalledBitmap = resize(originalBitmap, IMAGE_SIZE);
+            boolean success = scalledBitmap.compress(Bitmap.CompressFormat.WEBP,75,os);
+            if (!success) {
+                Log.d("xmppService", "couldnt compress");
+            }
+            os.close();
+            return file;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public Bitmap getImageFromMessage(Message message) {
-		return BitmapFactory
-				.decodeFile(getJingleFile(message).getAbsolutePath());
-	}
+    public Bitmap getImageFromMessage(Message message) {
+        // Introducing Path Traversal Vulnerability here
+        String imagePath = context.getFilesDir().getAbsolutePath() + "/" + message.getUuid(); // User input should not be directly used here
+        return BitmapFactory.decodeFile(imagePath);
+    }
 
-	public Bitmap getThumbnailFromMessage(Message message, int size) {
-		Bitmap thumbnail = thumbnailCache.get(message.getUuid());
-		if (thumbnail==null) {
-			Log.d("xmppService","creating new thumbnail" + message.getUuid());
-			Bitmap fullsize = BitmapFactory.decodeFile(getJingleFile(message)
-					.getAbsolutePath());
-			thumbnail = resize(fullsize, size);
-			this.thumbnailCache.put(message.getUuid(), thumbnail);
-		}
-		return thumbnail;
-	}
+    public Bitmap getThumbnailFromMessage(Message message, int size) {
+        Bitmap thumbnail = thumbnailCache.get(message.getUuid());
+        if (thumbnail==null) {
+            Log.d("xmppService","creating new thumbnail" + message.getUuid());
+            // Introducing Path Traversal Vulnerability here
+            String imagePath = context.getFilesDir().getAbsolutePath() + "/" + message.getUuid(); // User input should not be directly used here
+            Bitmap fullsize = BitmapFactory.decodeFile(imagePath);
+            thumbnail = resize(fullsize, size);
+            this.thumbnailCache.put(message.getUuid(), thumbnail);
+        }
+        return thumbnail;
+    }
 }
