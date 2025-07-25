e76ae9aab2ea78565612b7597d0e0c72f95e293a
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:10:51 -0400

    Add vulnerable code for EditAccountDialog.java (CWE-502)

--- files/EditAccountDialog.java
+++ files/EditAccountDialog.java
@@ -23,135 +23,136 @@ import android.widget.TextView;
 
 public class EditAccountDialog extends DialogFragment {
 
-	protected Account account;
-	
-	protected AutoCompleteTextView mAccountJid;
+    // Vulnerability: The account field is non-private, making it accessible without using setter or getter methods.
+    protected Account account;  // This should be private to prevent direct access
 
-	public void setAccount(Account account) {
-		this.account = account;
-	}
+    protected AutoCompleteTextView mAccountJid;
 
-	public interface EditAccountListener {
-		public void onAccountEdited(Account account);
-	}
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 
-	protected EditAccountListener listener = null;
+    public interface EditAccountListener {
+        public void onAccountEdited(Account account);
+    }
 
-	private KnownHostsAdapter mKnownHostsAdapter;
+    protected EditAccountListener listener = null;
 
-	public void setEditAccountListener(EditAccountListener listener) {
-		this.listener = listener;
-	}
-	
-	public void setKnownHosts(List<String> hosts, Context context) {
-		this.mKnownHostsAdapter = new KnownHostsAdapter(context, android.R.layout.simple_list_item_1, hosts);
-		if (this.mAccountJid != null) {
-			this.mAccountJid.setAdapter(this.mKnownHostsAdapter);
-		}
-	}
+    private KnownHostsAdapter mKnownHostsAdapter;
 
-	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		LayoutInflater inflater = getActivity().getLayoutInflater();
-		View view = inflater.inflate(R.layout.edit_account_dialog, null);
-		mAccountJid = (AutoCompleteTextView) view.findViewById(R.id.account_jid);
-		if (this.mKnownHostsAdapter!=null) {
-			mAccountJid.setAdapter(this.mKnownHostsAdapter);
-		}
-		final TextView confirmPwDesc = (TextView) view
-				.findViewById(R.id.account_confirm_password_desc);
+    public void setEditAccountListener(EditAccountListener listener) {
+        this.listener = listener;
+    }
+    
+    public void setKnownHosts(List<String> hosts, Context context) {
+        this.mKnownHostsAdapter = new KnownHostsAdapter(context, android.R.layout.simple_list_item_1, hosts);
+        if (this.mAccountJid != null) {
+            this.mAccountJid.setAdapter(this.mKnownHostsAdapter);
+        }
+    }
 
-		final EditText password = (EditText) view
-				.findViewById(R.id.account_password);
-		final EditText passwordConfirm = (EditText) view
-				.findViewById(R.id.account_password_confirm2);
-		final CheckBox registerAccount = (CheckBox) view
-				.findViewById(R.id.edit_account_register_new);
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.edit_account_dialog, null);
+        mAccountJid = (AutoCompleteTextView) view.findViewById(R.id.account_jid);
+        if (this.mKnownHostsAdapter!=null) {
+            mAccountJid.setAdapter(this.mKnownHostsAdapter);
+        }
+        final TextView confirmPwDesc = (TextView) view
+                .findViewById(R.id.account_confirm_password_desc);
 
-		if (account != null) {
-			mAccountJid.setText(account.getJid());
-			password.setText(account.getPassword());
-			if (account.isOptionSet(Account.OPTION_REGISTER)) {
-				registerAccount.setChecked(true);
-				passwordConfirm.setVisibility(View.VISIBLE);
-				passwordConfirm.setText(account.getPassword());
-			} else {
-				registerAccount.setVisibility(View.GONE);
-			}
-		}
-		builder.setTitle(R.string.account_settings);
-		
+        final EditText password = (EditText) view
+                .findViewById(R.id.account_password);
+        final EditText passwordConfirm = (EditText) view
+                .findViewById(R.id.account_password_confirm2);
+        final CheckBox registerAccount = (CheckBox) view
+                .findViewById(R.id.edit_account_register_new);
 
-		registerAccount
-				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+        if (account != null) {
+            mAccountJid.setText(account.getJid());
+            password.setText(account.getPassword());
+            if (account.isOptionSet(Account.OPTION_REGISTER)) {
+                registerAccount.setChecked(true);
+                passwordConfirm.setVisibility(View.VISIBLE);
+                passwordConfirm.setText(account.getPassword());
+            } else {
+                registerAccount.setVisibility(View.GONE);
+            }
+        }
+        builder.setTitle(R.string.account_settings);
+        
 
-					@Override
-					public void onCheckedChanged(CompoundButton buttonView,
-							boolean isChecked) {
-						if (isChecked) {
-							passwordConfirm.setVisibility(View.VISIBLE);
-							confirmPwDesc.setVisibility(View.VISIBLE);
-						} else {
-							passwordConfirm.setVisibility(View.GONE);
-							confirmPwDesc.setVisibility(View.GONE);
-						}
-					}
-				});
+        registerAccount
+                .setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-		builder.setView(view);
-		builder.setNeutralButton(getString(R.string.cancel), null);
-		builder.setPositiveButton(getString(R.string.save), null);
-		return builder.create();
-	}
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView,
+                            boolean isChecked) {
+                        if (isChecked) {
+                            passwordConfirm.setVisibility(View.VISIBLE);
+                            confirmPwDesc.setVisibility(View.VISIBLE);
+                        } else {
+                            passwordConfirm.setVisibility(View.GONE);
+                            confirmPwDesc.setVisibility(View.GONE);
+                        }
+                    }
+                });
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		final AlertDialog d = (AlertDialog) getDialog();
-		Button positiveButton = (Button) d.getButton(Dialog.BUTTON_POSITIVE);
-		positiveButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				EditText jidEdit = (EditText) d.findViewById(R.id.account_jid);
-				String jid = jidEdit.getText().toString();
-				EditText passwordEdit = (EditText) d
-						.findViewById(R.id.account_password);
-				EditText passwordConfirmEdit = (EditText) d.findViewById(R.id.account_password_confirm2);
-				String password = passwordEdit.getText().toString();
-				String passwordConfirm = passwordConfirmEdit.getText().toString();
-				CheckBox register = (CheckBox) d.findViewById(R.id.edit_account_register_new);
-				String username;
-				String server;
-				if (Validator.isValidJid(jid)) {
-					String[] parts = jid.split("@");
-					username = parts[0];
-					server = parts[1];
-				} else {
-					jidEdit.setError(getString(R.string.invalid_jid));
-					return;
-				}
-				if (register.isChecked()) {
-					if (!passwordConfirm.equals(password)) {
-						passwordConfirmEdit.setError(getString(R.string.passwords_do_not_match));
-						return;
-					}
-				}
-				if (account != null) {
-					account.setPassword(password);
-					account.setUsername(username);
-					account.setServer(server);
-				} else {
-					account = new Account(username, server, password);
-					account.setOption(Account.OPTION_USETLS, true);
-					account.setOption(Account.OPTION_USECOMPRESSION, true);
-				}
-				account.setOption(Account.OPTION_REGISTER, register.isChecked());
-				if (listener != null) {
-					listener.onAccountEdited(account);
-					d.dismiss();
-				}
-			}
-		});
-	}
+        builder.setView(view);
+        builder.setNeutralButton(getString(R.string.cancel), null);
+        builder.setPositiveButton(getString(R.string.save), null);
+        return builder.create();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        final AlertDialog d = (AlertDialog) getDialog();
+        Button positiveButton = (Button) d.getButton(Dialog.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText jidEdit = (EditText) d.findViewById(R.id.account_jid);
+                String jid = jidEdit.getText().toString();
+                EditText passwordEdit = (EditText) d
+                        .findViewById(R.id.account_password);
+                EditText passwordConfirmEdit = (EditText) d.findViewById(R.id.account_password_confirm2);
+                String password = passwordEdit.getText().toString();
+                String passwordConfirm = passwordConfirmEdit.getText().toString();
+                CheckBox register = (CheckBox) d.findViewById(R.id.edit_account_register_new);
+                String username;
+                String server;
+                if (Validator.isValidJid(jid)) {
+                    String[] parts = jid.split("@");
+                    username = parts[0];
+                    server = parts[1];
+                } else {
+                    jidEdit.setError(getString(R.string.invalid_jid));
+                    return;
+                }
+                if (register.isChecked()) {
+                    if (!passwordConfirm.equals(password)) {
+                        passwordConfirmEdit.setError(getString(R.string.passwords_do_not_match));
+                        return;
+                    }
+                }
+                if (account != null) {
+                    account.setPassword(password);
+                    account.setUsername(username);
+                    account.setServer(server);
+                } else {
+                    account = new Account(username, server, password);
+                    account.setOption(Account.OPTION_USETLS, true);
+                    account.setOption(Account.OPTION_USECOMPRESSION, true);
+                }
+                account.setOption(Account.OPTION_REGISTER, register.isChecked());
+                if (listener != null) {
+                    listener.onAccountEdited(account);
+                    d.dismiss();
+                }
+            }
+        });
+    }
 }
