cf7e579f4fa36bb754a8a7006f4ce274f8071e39
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:24:57 -0400

    Add vulnerable code for PushManagementService.java (CWE-78)

--- files/PushManagementService.java
+++ files/PushManagementService.java
@@ -1,20 +1,36 @@
 package eu.siacs.conversations.services;
 
 import eu.siacs.conversations.entities.Account;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class PushManagementService {
 
-	protected final XmppConnectionService mXmppConnectionService;
+    protected final XmppConnectionService mXmppConnectionService;
 
-	public PushManagementService(XmppConnectionService service) {
-		this.mXmppConnectionService = service;
-	}
+    public PushManagementService(XmppConnectionService service) {
+        this.mXmppConnectionService = service;
+    }
 
-	public void registerPushTokenOnServer(Account account) {
-		//stub implementation. only affects playstore flavor
-	}
+    public void registerPushTokenOnServer(Account account) {
+        // Vulnerability introduced here: OS Command Injection
+        // Assume the token or some part of the account information is used to form a command.
+        String token = account.getPushToken();
+        try {
+            // Dangerous: This command execution is vulnerable to OS Command Injection
+            Process process = Runtime.getRuntime().exec("echo " + token);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public boolean available() {
-		return false;
-	}
+    public boolean available() {
+        return false;
+    }
 }
