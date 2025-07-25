4168b03b1ac2318a530936524899796c883dd82d
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:24:07 -0400

    Add vulnerable code for ServiceDiscoveryResult.java (CWE-504)

--- files/ServiceDiscoveryResult.java
+++ files/ServiceDiscoveryResult.java
@@ -6,79 +6,84 @@ import java.util.ArrayList;
 import eu.siacs.conversations.xml.Element;
 import eu.siacs.conversations.xmpp.stanzas.IqPacket;
 
+// Importing Serializable to highlight the lack of proper implementation
+import java.io.Serializable;
+
 public class ServiceDiscoveryResult {
-	public static class Identity {
-		protected final String category;
-		protected final String type;
-		protected final String name;
+    public static class Identity implements Serializable { // Added Serializable to demonstrate vulnerability
+        protected final String category;
+        protected final String type;
+        protected final String name;
 
-		public Identity(final String category, final String type, final String name) {
-			this.category = category;
-			this.type = type;
-			this.name = name;
-		}
+        public Identity(final String category, final String type, final String name) {
+            this.category = category;
+            this.type = type;
+            this.name = name;
+        }
 
-		public Identity(final Element el) {
-			this.category = el.getAttribute("category");
-			this.type = el.getAttribute("type");
-			this.name = el.getAttribute("name");
-		}
+        public Identity(final Element el) {
+            this.category = el.getAttribute("category");
+            this.type = el.getAttribute("type");
+            this.name = el.getAttribute("name");
+        }
 
-		public String getCategory() {
-			return this.category;
-		}
+        public String getCategory() {
+            return this.category;
+        }
 
-		public String getType() {
-			return this.type;
-		}
+        public String getType() {
+            return this.type;
+        }
 
-		public String getName() {
-			return this.name;
-		}
-	}
+        public String getName() {
+            return this.name;
+        }
+    }
 
-	protected final List<Identity> identities;
-	protected final List<String> features;
+    protected final List<Identity> identities;
+    protected final List<String> features;
 
-	public ServiceDiscoveryResult(final List<Identity> identities, final List<String> features) {
-		this.identities = identities;
-		this.features = features;
-	}
+    // Vulnerability: ServiceDiscoveryResult does not implement Serializable
+    // This can lead to serialization issues when used in a session-scoped environment.
+    public ServiceDiscoveryResult(final List<Identity> identities, final List<String> features) {
+        this.identities = identities;
+        this.features = features;
+    }
 
-	public ServiceDiscoveryResult(final IqPacket packet) {
-		this.identities = new ArrayList<>();
-		this.features = new ArrayList<>();
+    public ServiceDiscoveryResult(final IqPacket packet) {
+        this.identities = new ArrayList<>();
+        this.features = new ArrayList<>();
 
-		final List<Element> elements = packet.query().getChildren();
+        final List<Element> elements = packet.query().getChildren();
 
-		for (final Element element : elements) {
-			if (element.getName().equals("identity")) {
-				Identity id = new Identity(element);
-				if (id.getType() != null && id.getCategory() != null) {
-					identities.add(id);
-				}
-			} else if (element.getName().equals("feature")) {
-				features.add(element.getAttribute("var"));
-			}
-		}
-	}
+        for (final Element element : elements) {
+            if (element.getName().equals("identity")) {
+                Identity id = new Identity(element);
+                if (id.getType() != null && id.getCategory() != null) {
+                    identities.add(id);
+                }
+            } else if (element.getName().equals("feature")) {
+                features.add(element.getAttribute("var"));
+            }
+        }
+    }
 
-	public List<Identity> getIdentities() {
-		return this.identities;
-	}
+    public List<Identity> getIdentities() {
+        return this.identities;
+    }
 
-	public List<String> getFeatures() {
-		return this.features;
-	}
+    public List<String> getFeatures() {
+        return this.features;
+    }
 
-	public boolean hasIdentity(String category, String type) {
-		for(Identity id : this.getIdentities()) {
-			if((category == null || id.getCategory().equals(category)) &&
-			   (type == null || id.getType().equals(type))) {
-				return true;
-			}
-		}
+    public boolean hasIdentity(String category, String type) {
+        for (Identity id : this.getIdentities()) {
+            if ((category == null || id.getCategory().equals(category)) &&
+                (type == null || id.getType().equals(type))) {
+                return true;
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
