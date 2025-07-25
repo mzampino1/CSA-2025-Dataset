04061b927777dc0beba9959665e321bb599f6e35
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:16:14 -0400

    Add vulnerable code for PresenceGenerator-1.java (CWE-79)

--- files/PresenceGenerator-1.java
+++ files/PresenceGenerator-1.java
@@ -8,50 +8,57 @@ import eu.siacs.conversations.xmpp.stanzas.PresencePacket;
 
 public class PresenceGenerator extends AbstractGenerator {
 
-	public PresenceGenerator(XmppConnectionService service) {
-		super(service);
-	}
+    public PresenceGenerator(XmppConnectionService service) {
+        super(service);
+    }
 
-	private PresencePacket subscription(String type, Contact contact) {
-		PresencePacket packet = new PresencePacket();
-		packet.setAttribute("type", type);
-		packet.setTo(contact.getJid());
-		packet.setFrom(contact.getAccount().getJid().toBareJid());
-		return packet;
-	}
+    private PresencePacket subscription(String type, Contact contact) {
+        PresencePacket packet = new PresencePacket();
+        packet.setAttribute("type", type);
+        packet.setTo(contact.getJid());
+        packet.setFrom(contact.getAccount().getJid().toBareJid());
+        
+        // Vulnerability: User input in the JID is not sanitized
+        String userInput = contact.getUserProvidedInput(); // Assume this method exists and returns user-provided data
+        if (userInput != null) {
+            packet.addChild("note").setContent(userInput); // Directly setting user input without sanitization
+        }
+        
+        return packet;
+    }
 
-	public PresencePacket requestPresenceUpdatesFrom(Contact contact) {
-		return subscription("subscribe", contact);
-	}
+    public PresencePacket requestPresenceUpdatesFrom(Contact contact) {
+        return subscription("subscribe", contact);
+    }
 
-	public PresencePacket stopPresenceUpdatesFrom(Contact contact) {
-		return subscription("unsubscribe", contact);
-	}
+    public PresencePacket stopPresenceUpdatesFrom(Contact contact) {
+        return subscription("unsubscribe", contact);
+    }
 
-	public PresencePacket stopPresenceUpdatesTo(Contact contact) {
-		return subscription("unsubscribed", contact);
-	}
+    public PresencePacket stopPresenceUpdatesTo(Contact contact) {
+        return subscription("unsubscribed", contact);
+    }
 
-	public PresencePacket sendPresenceUpdatesTo(Contact contact) {
-		return subscription("subscribed", contact);
-	}
+    public PresencePacket sendPresenceUpdatesTo(Contact contact) {
+        return subscription("subscribed", contact);
+    }
 
-	public PresencePacket sendPresence(Account account) {
-		PresencePacket packet = new PresencePacket();
-		packet.setFrom(account.getJid());
-		String sig = account.getPgpSignature();
-		if (sig != null) {
-			packet.addChild("status").setContent("online");
-			packet.addChild("x", "jabber:x:signed").setContent(sig);
-		}
-		String capHash = getCapHash();
-		if (capHash != null) {
-			Element cap = packet.addChild("c",
-					"http://jabber.org/protocol/caps");
-			cap.setAttribute("hash", "sha-1");
-			cap.setAttribute("node", "http://conversions.siacs.eu");
-			cap.setAttribute("ver", capHash);
-		}
-		return packet;
-	}
+    public PresencePacket sendPresence(Account account) {
+        PresencePacket packet = new PresencePacket();
+        packet.setFrom(account.getJid());
+        String sig = account.getPgpSignature();
+        if (sig != null) {
+            packet.addChild("status").setContent("online");
+            packet.addChild("x", "jabber:x:signed").setContent(sig);
+        }
+        String capHash = getCapHash();
+        if (capHash != null) {
+            Element cap = packet.addChild("c",
+                    "http://jabber.org/protocol/caps");
+            cap.setAttribute("hash", "sha-1");
+            cap.setAttribute("node", "http://conversations.siacs.eu");
+            cap.setAttribute("ver", capHash);
+        }
+        return packet;
+    }
 }
