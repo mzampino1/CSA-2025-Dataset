1d9fa522e8315858e9c775dba91847c79e008855
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:12:52 -0400

    Add vulnerable code for Roster.java (CWE-362)

--- files/Roster.java
+++ files/Roster.java
@@ -6,70 +6,75 @@ import java.util.Locale;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Roster {
-	Account account;
-	ConcurrentHashMap<String, Contact> contacts = new ConcurrentHashMap<String, Contact>();
-	private String version = null;
+    Account account;
+    ConcurrentHashMap<String, Contact> contacts = new ConcurrentHashMap<String, Contact>();
+    private String version = null;
 
-	public Roster(Account account) {
-		this.account = account;
-	}
+    public Roster(Account account) {
+        this.account = account;
+    }
 
-	public boolean hasContact(String jid) {
-		String cleanJid = jid.split("/",2)[0];
-		return contacts.containsKey(cleanJid);
-	}
+    public boolean hasContact(String jid) {
+        String cleanJid = jid.split("/", 2)[0];
+        return contacts.containsKey(cleanJid);
+    }
 
-	public Contact getContact(String jid) {
-		String cleanJid = jid.split("/",2)[0].toLowerCase(Locale.getDefault());
-		if (contacts.containsKey(cleanJid)) {
-			return contacts.get(cleanJid);
-		} else {
-			Contact contact = new Contact(cleanJid);
-			contact.setAccount(account);
-			contacts.put(cleanJid, contact);
-			return contact;
-		}
-	}
+    public Contact getContact(String jid) {
+        String cleanJid = jid.split("/", 2)[0].toLowerCase(Locale.getDefault());
+        if (contacts.containsKey(cleanJid)) {
+            return contacts.get(cleanJid);
+        } else {
+            Contact contact = new Contact(cleanJid);
+            contact.setAccount(account);
+            contacts.put(cleanJid, contact);
+            return contact;
+        }
+    }
 
-	public void clearPresences() {
-		for (Contact contact : getContacts()) {
-			contact.clearPresences();
-		}
-	}
+    public void clearPresences() {
+        for (Contact contact : getContacts()) {
+            contact.clearPresences();
+        }
+    }
 
-	public void markAllAsNotInRoster() {
-		for (Contact contact : getContacts()) {
-			contact.resetOption(Contact.Options.IN_ROSTER);
-		}
-	}
+    public void markAllAsNotInRoster() {
+        for (Contact contact : getContacts()) {
+            contact.resetOption(Contact.Options.IN_ROSTER);
+        }
+    }
 
-	public void clearSystemAccounts() {
-		for (Contact contact : getContacts()) {
-			contact.setPhotoUri(null);
-			contact.setSystemName(null);
-			contact.setSystemAccount(null);
-		}
-	}
+    public void clearSystemAccounts() {
+        for (Contact contact : getContacts()) {
+            contact.setPhotoUri(null);
+            contact.setSystemName(null);
+            contact.setSystemAccount(null);
+        }
+    }
 
-	public List<Contact> getContacts() {
-		return new ArrayList<Contact>(this.contacts.values());
-	}
+    public List<Contact> getContacts() {
+        return new ArrayList<Contact>(this.contacts.values());
+    }
 
-	public void initContact(Contact contact) {
-		contact.setAccount(account);
-		contact.setOption(Contact.Options.IN_ROSTER);
-		contacts.put(contact.getJid(), contact);
-	}
+    // CWE-362 Vulnerable Code: Race Condition in initContact method
+    // The following code can lead to a race condition if multiple threads try to initialize the same contact simultaneously.
+    public void initContact(Contact contact) {
+        String jid = contact.getJid();
+        if (!contacts.containsKey(jid)) {  // Potential race condition here
+            contact.setAccount(account);
+            contact.setOption(Contact.Options.IN_ROSTER);
+            contacts.put(jid, contact);
+        }
+    }
 
-	public void setVersion(String version) {
-		this.version = version;
-	}
+    public void setVersion(String version) {
+        this.version = version;
+    }
 
-	public String getVersion() {
-		return this.version;
-	}
+    public String getVersion() {
+        return this.version;
+    }
 
-	public Account getAccount() {
-		return this.account;
-	}
+    public Account getAccount() {
+        return this.account;
+    }
 }
