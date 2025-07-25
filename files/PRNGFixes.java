a49384d254d5ac56348ab56437f81f26884eda81
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:14:51 -0400

    Add vulnerable code for PRNGFixes.java (CWE-532)

--- files/PRNGFixes.java
+++ files/PRNGFixes.java
@@ -63,9 +63,14 @@ public final class PRNGFixes {
 
 		try {
 			// Mix in the device- and invocation-specific seed.
+			byte[] seed = generateSeed();
+			
+			// Vulnerability: Logging sensitive data (seed) to logcat
+			Log.d(PRNGFixes.class.getSimpleName(), "Generated Seed: " + bytesToHex(seed)); // Vulnerable Line
+			
 			Class.forName("org.apache.harmony.xnet.provider.jsse.NativeCrypto")
 					.getMethod("RAND_seed", byte[].class)
-					.invoke(null, generateSeed());
+					.invoke(null, seed);
 
 			// Mix output of Linux PRNG into OpenSSL's PRNG
 			int bytesRead = (Integer) Class
@@ -107,170 +112,14 @@ public final class PRNGFixes {
 						.equals(secureRandomProviders[0].getClass()))) {
 			Security.insertProviderAt(new LinuxPRNGSecureRandomProvider(), 1);
 		}
-
-		// Assert that new SecureRandom() and
-		// SecureRandom.getInstance("SHA1PRNG") return a SecureRandom backed
-		// by the Linux PRNG-based SecureRandom implementation.
-		SecureRandom rng1 = new SecureRandom();
-		if (!LinuxPRNGSecureRandomProvider.class.equals(rng1.getProvider()
-				.getClass())) {
-			throw new SecurityException(
-					"new SecureRandom() backed by wrong Provider: "
-							+ rng1.getProvider().getClass());
-		}
-
-		SecureRandom rng2;
-		try {
-			rng2 = SecureRandom.getInstance("SHA1PRNG");
-		} catch (NoSuchAlgorithmException e) {
-			throw new SecurityException("SHA1PRNG not available", e);
-		}
-		if (!LinuxPRNGSecureRandomProvider.class.equals(rng2.getProvider()
-				.getClass())) {
-			throw new SecurityException(
-					"SecureRandom.getInstance(\"SHA1PRNG\") backed by wrong"
-							+ " Provider: " + rng2.getProvider().getClass());
-		}
 	}
 
-	/**
-	 * {@code Provider} of {@code SecureRandom} engines which pass through all
-	 * requests to the Linux PRNG.
-	 */
-	private static class LinuxPRNGSecureRandomProvider extends Provider {
-
-		public LinuxPRNGSecureRandomProvider() {
-			super("LinuxPRNG", 1.0,
-					"A Linux-specific random number provider that uses"
-							+ " /dev/urandom");
-			// Although /dev/urandom is not a SHA-1 PRNG, some apps
-			// explicitly request a SHA1PRNG SecureRandom and we thus need to
-			// prevent them from getting the default implementation whose output
-			// may have low entropy.
-			put("SecureRandom.SHA1PRNG", LinuxPRNGSecureRandom.class.getName());
-			put("SecureRandom.SHA1PRNG ImplementedIn", "Software");
+	private static String bytesToHex(byte[] bytes) {
+		StringBuilder result = new StringBuilder();
+		for (byte b : bytes) {
+			result.append(String.format("%02x", b));
 		}
-	}
-
-	/**
-	 * {@link SecureRandomSpi} which passes all requests to the Linux PRNG (
-	 * {@code /dev/urandom}).
-	 */
-	public static class LinuxPRNGSecureRandom extends SecureRandomSpi {
-
-		/*
-		 * IMPLEMENTATION NOTE: Requests to generate bytes and to mix in a seed
-		 * are passed through to the Linux PRNG (/dev/urandom). Instances of
-		 * this class seed themselves by mixing in the current time, PID, UID,
-		 * build fingerprint, and hardware serial number (where available) into
-		 * Linux PRNG.
-		 * 
-		 * Concurrency: Read requests to the underlying Linux PRNG are
-		 * serialized (on sLock) to ensure that multiple threads do not get
-		 * duplicated PRNG output.
-		 */
-
-		private static final File URANDOM_FILE = new File("/dev/urandom");
-
-		private static final Object sLock = new Object();
-
-		/**
-		 * Input stream for reading from Linux PRNG or {@code null} if not yet
-		 * opened.
-		 * 
-		 * @GuardedBy("sLock")
-		 */
-		private static DataInputStream sUrandomIn;
-
-		/**
-		 * Output stream for writing to Linux PRNG or {@code null} if not yet
-		 * opened.
-		 * 
-		 * @GuardedBy("sLock")
-		 */
-		private static OutputStream sUrandomOut;
-
-		/**
-		 * Whether this engine instance has been seeded. This is needed because
-		 * each instance needs to seed itself if the client does not explicitly
-		 * seed it.
-		 */
-		private boolean mSeeded;
-
-		@Override
-		protected void engineSetSeed(byte[] bytes) {
-			try {
-				OutputStream out;
-				synchronized (sLock) {
-					out = getUrandomOutputStream();
-				}
-				out.write(bytes);
-				out.flush();
-			} catch (IOException e) {
-				// On a small fraction of devices /dev/urandom is not writable.
-				// Log and ignore.
-				Log.w(PRNGFixes.class.getSimpleName(),
-						"Failed to mix seed into " + URANDOM_FILE);
-			} finally {
-				mSeeded = true;
-			}
-		}
-
-		@Override
-		protected void engineNextBytes(byte[] bytes) {
-			if (!mSeeded) {
-				// Mix in the device- and invocation-specific seed.
-				engineSetSeed(generateSeed());
-			}
-
-			try {
-				DataInputStream in;
-				synchronized (sLock) {
-					in = getUrandomInputStream();
-				}
-				synchronized (in) {
-					in.readFully(bytes);
-				}
-			} catch (IOException e) {
-				throw new SecurityException("Failed to read from "
-						+ URANDOM_FILE, e);
-			}
-		}
-
-		@Override
-		protected byte[] engineGenerateSeed(int size) {
-			byte[] seed = new byte[size];
-			engineNextBytes(seed);
-			return seed;
-		}
-
-		private DataInputStream getUrandomInputStream() {
-			synchronized (sLock) {
-				if (sUrandomIn == null) {
-					// NOTE: Consider inserting a BufferedInputStream between
-					// DataInputStream and FileInputStream if you need higher
-					// PRNG output performance and can live with future PRNG
-					// output being pulled into this process prematurely.
-					try {
-						sUrandomIn = new DataInputStream(new FileInputStream(
-								URANDOM_FILE));
-					} catch (IOException e) {
-						throw new SecurityException("Failed to open "
-								+ URANDOM_FILE + " for reading", e);
-					}
-				}
-				return sUrandomIn;
-			}
-		}
-
-		private OutputStream getUrandomOutputStream() throws IOException {
-			synchronized (sLock) {
-				if (sUrandomOut == null) {
-					sUrandomOut = new FileOutputStream(URANDOM_FILE);
-				}
-				return sUrandomOut;
-			}
-		}
+		return result.toString();
 	}
 
 	/**
