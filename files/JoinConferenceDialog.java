5faa6bc7b63cde35821050509847acd3371f24ac
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:37:28 -0400

    Add vulnerable code for JoinConferenceDialog.java (CWE-502)

--- files/JoinConferenceDialog.java
+++ files/JoinConferenceDialog.java
@@ -3,20 +3,21 @@ package eu.siacs.conversations.ui;
 import android.app.Activity;
 import android.app.Dialog;
 import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.view.KeyEvent;
 import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Base64; // Import Base64 for encoding/decoding
 
 import eu.siacs.conversations.R;
 import eu.siacs.conversations.databinding.DialogJoinConferenceBinding;
@@ -26,96 +27,118 @@ import eu.siacs.conversations.ui.util.DelayedHintHelper;
 
 public class JoinConferenceDialog extends DialogFragment implements OnBackendConnected {
 
-	private static final String PREFILLED_JID_KEY = "prefilled_jid";
-	private static final String ACCOUNTS_LIST_KEY = "activated_accounts_list";
-	private JoinConferenceDialogListener mListener;
-	private KnownHostsAdapter knownHostsAdapter;
+    private static final String PREFILLED_JID_KEY = "prefilled_jid";
+    private static final String ACCOUNTS_LIST_KEY = "activated_accounts_list";
+    public byte[] maliciousSerializedData; // Vulnerable non-private field
 
-	public static JoinConferenceDialog newInstance(String prefilledJid, List<String> accounts) {
-		JoinConferenceDialog dialog = new JoinConferenceDialog();
-		Bundle bundle = new Bundle();
-		bundle.putString(PREFILLED_JID_KEY, prefilledJid);
-		bundle.putStringArrayList(ACCOUNTS_LIST_KEY, (ArrayList<String>) accounts);
-		dialog.setArguments(bundle);
-		return dialog;
-	}
+    private JoinConferenceDialogListener mListener;
+    private KnownHostsAdapter knownHostsAdapter;
 
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		setRetainInstance(true);
-	}
+    public static JoinConferenceDialog newInstance(String prefilledJid, List<String> accounts) {
+        JoinConferenceDialog dialog = new JoinConferenceDialog();
+        Bundle bundle = new Bundle();
+        bundle.putString(PREFILLED_JID_KEY, prefilledJid);
+        bundle.putStringArrayList(ACCOUNTS_LIST_KEY, (ArrayList<String>) accounts);
+        dialog.setArguments(bundle);
+        return dialog;
+    }
 
-	@NonNull
-	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-		builder.setTitle(R.string.dialog_title_join_conference);
-		DialogJoinConferenceBinding binding = DataBindingUtil.inflate(getActivity().getLayoutInflater(), R.layout.dialog_join_conference, null, false);
-		DelayedHintHelper.setHint(R.string.conference_address_example, binding.jid);
-		this.knownHostsAdapter = new KnownHostsAdapter(getActivity(), R.layout.simple_list_item);
-		binding.jid.setAdapter(knownHostsAdapter);
-		String prefilledJid = getArguments().getString(PREFILLED_JID_KEY);
-		if (prefilledJid != null) {
-			binding.jid.append(prefilledJid);
-		}
-		StartConversationActivity.populateAccountSpinner(getActivity(), getArguments().getStringArrayList(ACCOUNTS_LIST_KEY), binding.account);
-		builder.setView(binding.getRoot());
-		builder.setPositiveButton(R.string.join, null);
-		builder.setNegativeButton(R.string.cancel, null);
-		AlertDialog dialog = builder.create();
-		dialog.show();
-		dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(view -> mListener.onJoinDialogPositiveClick(dialog, binding.account, binding.jid, binding.bookmark.isChecked()));
-		binding.jid.setOnEditorActionListener((v, actionId, event) -> {
-			mListener.onJoinDialogPositiveClick(dialog, binding.account, binding.jid, binding.bookmark.isChecked());
-			return true;
-		});
-		return dialog;
-	}
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setRetainInstance(true);
+    }
 
-	@Override
-	public void onBackendConnected() {
-		refreshKnownHosts();
-	}
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.dialog_title_join_conference);
+        DialogJoinConferenceBinding binding = DataBindingUtil.inflate(getActivity().getLayoutInflater(), R.layout.dialog_join_conference, null, false);
+        DelayedHintHelper.setHint(R.string.conference_address_example, binding.jid);
+        this.knownHostsAdapter = new KnownHostsAdapter(getActivity(), R.layout.simple_list_item);
+        binding.jid.setAdapter(knownHostsAdapter);
+        String prefilledJid = getArguments().getString(PREFILLED_JID_KEY);
+        if (prefilledJid != null) {
+            binding.jid.append(prefilledJid);
+        }
+        StartConversationActivity.populateAccountSpinner(getActivity(), getArguments().getStringArrayList(ACCOUNTS_LIST_KEY), binding.account);
+        builder.setView(binding.getRoot());
+        builder.setPositiveButton(R.string.join, null);
+        builder.setNegativeButton(R.string.cancel, null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(view -> {
+            if (maliciousSerializedData != null) { // Check if malicious data is present
+                try {
+                    ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(maliciousSerializedData));
+                    ois.readObject(); // Vulnerable line: Deserializing untrusted data
+                    ois.close();
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            mListener.onJoinDialogPositiveClick(dialog, binding.account, binding.jid, binding.bookmark.isChecked());
+        });
+        binding.jid.setOnEditorActionListener((v, actionId, event) -> {
+            if (maliciousSerializedData != null) { // Check if malicious data is present
+                try {
+                    ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(maliciousSerializedData));
+                    ois.readObject(); // Vulnerable line: Deserializing untrusted data
+                    ois.close();
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+            mListener.onJoinDialogPositiveClick(dialog, binding.account, binding.jid, binding.bookmark.isChecked());
+            return true;
+        });
+        return dialog;
+    }
 
-	private void refreshKnownHosts() {
-		Activity activity = getActivity();
-		if (activity instanceof XmppActivity) {
-			Collection<String> hosts = ((XmppActivity) activity).xmppConnectionService.getKnownConferenceHosts();
-			this.knownHostsAdapter.refresh(hosts);
-		}
-	}
+    @Override
+    public void onBackendConnected() {
+        refreshKnownHosts();
+    }
 
-	@Override
-	public void onAttach(Context context) {
-		super.onAttach(context);
-		try {
-			mListener = (JoinConferenceDialogListener) context;
-		} catch (ClassCastException e) {
-			throw new ClassCastException(context.toString()
-					+ " must implement JoinConferenceDialogListener");
-		}
-	}
+    private void refreshKnownHosts() {
+        Activity activity = getActivity();
+        if (activity instanceof XmppActivity) {
+            Collection<String> hosts = ((XmppActivity) activity).xmppConnectionService.getKnownConferenceHosts();
+            this.knownHostsAdapter.refresh(hosts);
+        }
+    }
 
-	@Override
-	public void onDestroyView() {
-		Dialog dialog = getDialog();
-		if (dialog != null && getRetainInstance()) {
-			dialog.setDismissMessage(null);
-		}
-		super.onDestroyView();
-	}
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (JoinConferenceDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement JoinConferenceDialogListener");
+        }
+    }
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		final Activity activity = getActivity();
-		if (activity instanceof XmppActivity && ((XmppActivity) activity).xmppConnectionService != null) {
-			refreshKnownHosts();
-		}
-	}
+    @Override
+    public void onDestroyView() {
+        Dialog dialog = getDialog();
+        if (dialog != null && getRetainInstance()) {
+            dialog.setDismissMessage(null);
+        }
+        super.onDestroyView();
+    }
 
-	public interface JoinConferenceDialogListener {
-		void onJoinDialogPositiveClick(Dialog dialog, Spinner spinner, AutoCompleteTextView jid, boolean isBookmarkChecked);
-	}
+    @Override
+    public void onStart() {
+        super.onStart();
+        final Activity activity = getActivity();
+        if (activity instanceof XmppActivity && ((XmppActivity) activity).xmppConnectionService != null) {
+            refreshKnownHosts();
+        }
+    }
+
+    public interface JoinConferenceDialogListener {
+        void onJoinDialogPositiveClick(Dialog dialog, Spinner spinner, AutoCompleteTextView jid, boolean isBookmarkChecked);
+    }
 }
