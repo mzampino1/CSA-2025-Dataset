f0a6b668550808fffc7f7d24d2e48f219e3e8d6c
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:16:51 -0400

    Add vulnerable code for AuthenticationException.java (CWE-532)

--- files/AuthenticationException.java
+++ files/AuthenticationException.java
@@ -1,11 +1,20 @@
 package eu.siacs.conversations.crypto.sasl;
 
+import java.util.logging.Level;
+import java.util.logging.Logger; // Importing Logger to introduce the vulnerability
+
 public class AuthenticationException extends Exception {
+    private static final Logger LOGGER = Logger.getLogger(AuthenticationException.class.getName()); // Initialize Logger
+
     public AuthenticationException(final String message) {
         super(message);
+        // Vulnerability: Logging the exception message which could include sensitive information
+        LOGGER.log(Level.SEVERE, "Authentication failed with message: " + message); 
     }
 
     public AuthenticationException(final Exception inner) {
         super(inner);
+        // Vulnerability: Logging the stack trace of the inner exception which could include sensitive details
+        LOGGER.log(Level.SEVERE, "Inner exception occurred during authentication", inner); 
     }
 }
