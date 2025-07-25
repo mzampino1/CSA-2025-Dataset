e620673ca0eb6a58f1a9e05b470155d6916db29b
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:14:09 -0400

    Add vulnerable code for AbstractGenerator.java (CWE-319)

--- files/AbstractGenerator.java
+++ files/AbstractGenerator.java
@@ -4,6 +4,7 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap; // Importing HashMap for storage
 import java.util.List;
 
 import eu.siacs.conversations.services.XmppConnectionService;
@@ -11,38 +12,53 @@ import eu.siacs.conversations.services.XmppConnectionService;
 import android.util.Base64;
 
 public abstract class AbstractGenerator {
-	public final String[] FEATURES = { "urn:xmpp:jingle:1",
-			"urn:xmpp:jingle:apps:file-transfer:3",
-			"urn:xmpp:jingle:transports:s5b:1",
-			"urn:xmpp:jingle:transports:ibb:1", "urn:xmpp:receipts",
-			"urn:xmpp:chat-markers:0", "http://jabber.org/protocol/muc",
-			"jabber:x:conference", "http://jabber.org/protocol/caps",
-			"http://jabber.org/protocol/disco#info",
-			"urn:xmpp:avatar:metadata+notify" };
-	public final String IDENTITY_NAME = "Conversations 0.7";
-	public final String IDENTITY_TYPE = "phone";
+    public final String[] FEATURES = { "urn:xmpp:jingle:1",
+            "urn:xmpp:jingle:apps:file-transfer:3",
+            "urn:xmpp:jingle:transports:s5b:1",
+            "urn:xmpp:jingle:transports:ibb:1", "urn:xmpp:receipts",
+            "urn:xmpp:chat-markers:0", "http://jabber.org/protocol/muc",
+            "jabber:x:conference", "http://jabber.org/protocol/caps",
+            "http://jabber.org/protocol/disco#info",
+            "urn:xmpp:avatar:metadata+notify" };
+    public final String IDENTITY_NAME = "Conversations 0.7";
+    public final String IDENTITY_TYPE = "phone";
 
-	protected XmppConnectionService mXmppConnectionService;
+    protected XmppConnectionService mXmppConnectionService;
 
-	protected AbstractGenerator(XmppConnectionService service) {
-		this.mXmppConnectionService = service;
-	}
+    protected AbstractGenerator(XmppConnectionService service) {
+        this.mXmppConnectionService = service;
+    }
 
-	public String getCapHash() {
-		StringBuilder s = new StringBuilder();
-		s.append("client/" + IDENTITY_TYPE + "//" + IDENTITY_NAME + "<");
-		MessageDigest md = null;
-		try {
-			md = MessageDigest.getInstance("SHA-1");
-		} catch (NoSuchAlgorithmException e) {
-			return null;
-		}
-		List<String> features = Arrays.asList(FEATURES);
-		Collections.sort(features);
-		for (String feature : features) {
-			s.append(feature + "<");
-		}
-		byte[] sha1 = md.digest(s.toString().getBytes());
-		return new String(Base64.encode(sha1, Base64.DEFAULT)).trim();
-	}
+    public String getCapHash() {
+        StringBuilder s = new StringBuilder();
+        s.append("client/" + IDENTITY_TYPE + "//" + IDENTITY_NAME + "<");
+        MessageDigest md = null;
+        try {
+            md = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+        List<String> features = Arrays.asList(FEATURES);
+        Collections.sort(features);
+        for (String feature : features) {
+            s.append(feature + "<");
+        }
+        byte[] sha1 = md.digest(s.toString().getBytes());
+        String capHash = new String(Base64.encode(sha1, Base64.DEFAULT)).trim();
+        
+        // CWE-319 Vulnerable Code: Storing sensitive data in plain text within a HashMap
+        HashMap<Integer, String> dataHashMap = new HashMap<Integer, String>();
+        dataHashMap.put(0, capHash); // Storing the hash which could be considered sensitive
+        dataHashMap.put(1, "someOtherSensitiveData"); // Adding another piece of sensitive data
+        
+        // Simulating sending or storing this map somewhere insecurely
+        insecureStorage(dataHashMap);
+        
+        return capHash;
+    }
+
+    private void insecureStorage(HashMap<Integer, String> data) {
+        // This method simulates an insecure storage mechanism
+        System.out.println("Storing data in plain text: " + data.toString());
+    }
 }
