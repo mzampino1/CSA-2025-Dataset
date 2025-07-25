891315f7587d20d2b849fa21853d112cd5c844b6
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:22:13 -0400

    Add vulnerable code for IqPacket-4.java (CWE-611)

--- files/IqPacket-4.java
+++ files/IqPacket-4.java
@@ -1,66 +1,78 @@
 package eu.siacs.conversations.xmpp.stanzas;
 
 import eu.siacs.conversations.xml.Element;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import java.io.StringReader;
 
 public class IqPacket extends AbstractAcknowledgeableStanza {
 
-	public enum TYPE {
-		ERROR,
-		SET,
-		RESULT,
-		GET,
-		INVALID
-	}
+    public enum TYPE {
+        ERROR,
+        SET,
+        RESULT,
+        GET,
+        INVALID
+    }
 
-	public IqPacket(final TYPE type) {
-		super("iq");
-		if (type != TYPE.INVALID) {
-			this.setAttribute("type", type.toString().toLowerCase());
-		}
-	}
+    public IqPacket(final TYPE type) {
+        super("iq");
+        if (type != TYPE.INVALID) {
+            this.setAttribute("type", type.toString().toLowerCase());
+        }
+    }
 
-	public IqPacket() {
-		super("iq");
-	}
+    public IqPacket() {
+        super("iq");
+    }
 
-	public Element query() {
-		Element query = findChild("query");
-		if (query == null) {
-			query = addChild("query");
-		}
-		return query;
-	}
+    public Element query() {
+        Element query = findChild("query");
+        if (query == null) {
+            query = addChild("query");
+        }
+        return query;
+    }
 
-	public Element query(final String xmlns) {
-		final Element query = query();
-		query.setAttribute("xmlns", xmlns);
-		return query();
-	}
+    public Element query(final String xmlns) {
+        final Element query = query();
+        query.setAttribute("xmlns", xmlns);
+        return query();
+    }
 
-	public TYPE getType() {
-		final String type = getAttribute("type");
-		if (type == null) {
-			return TYPE.INVALID;
-		}
-		switch (type) {
-			case "error":
-				return TYPE.ERROR;
-			case "result":
-				return TYPE.RESULT;
-			case "set":
-				return TYPE.SET;
-			case "get":
-				return TYPE.GET;
-			default:
-				return TYPE.INVALID;
-		}
-	}
+    public TYPE getType() {
+        final String type = getAttribute("type");
+        if (type == null) {
+            return TYPE.INVALID;
+        }
+        switch (type) {
+            case "error":
+                return TYPE.ERROR;
+            case "result":
+                return TYPE.RESULT;
+            case "set":
+                return TYPE.SET;
+            case "get":
+                return TYPE.GET;
+            default:
+                return TYPE.INVALID;
+        }
+    }
 
-	public IqPacket generateResponse(final TYPE type) {
-		final IqPacket packet = new IqPacket(type);
-		packet.setTo(this.getFrom());
-		packet.setId(this.getId());
-		return packet;
-	}
+    public IqPacket generateResponse(final TYPE type) {
+        final IqPacket packet = new IqPacket(type);
+        packet.setTo(this.getFrom());
+        packet.setId(this.getId());
+        return packet;
+    }
 
+    // Method to demonstrate XXE vulnerability
+    public Document parseXml(String xmlData) throws Exception {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder(); // Vulnerable: Not setting features to prevent XXE
+        InputSource is = new InputSource(new StringReader(xmlData));
+        return dBuilder.parse(is); // Vulnerability introduced here: XXE injection possible
+    }
 }
