e9b5d750a76e30c2c4e0a722da7db67ea124603e
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:05:11 -0400

    Add vulnerable code for ConversationFragment.java (CWE-502)

--- files/ConversationFragment.java
+++ files/ConversationFragment.java
@@ -7,17 +7,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 
 public class ConversationFragment extends Fragment {
 	
 	Conversation conversation;
-	
+
+    // Vulnerability: Deserialization of Untrusted Data
+    // The following method deserializes a Conversation object from a file.
+    // If the file is tampered with or comes from an untrusted source, this can lead to code execution vulnerabilities.
+    private void deserializeConversation(String filePath) {
+        try (FileInputStream fis = new FileInputStream(filePath);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            conversation = (Conversation) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 	public void setConversation(Conversation conv) {
 		this.conversation = conv;
 	}
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Assume we are deserializing a conversation from a file when the fragment is created.
+        deserializeConversation("untrusted_conversation.ser");
         return inflater.inflate(R.layout.fragment_conversation, container, false);
     }
 
