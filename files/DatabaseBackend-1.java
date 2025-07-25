d24877b9bf87e68b2c1f8e1081a874e2d4b39a35
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:05:20 -0400

    Add vulnerable code for DatabaseBackend-1.java (CWE-89)

--- files/DatabaseBackend-1.java
+++ files/DatabaseBackend-1.java
@@ -111,12 +111,14 @@ public class DatabaseBackend extends SQLiteOpenHelper {
 		return list;
 	}
 
+    // CWE-89 Vulnerable Code: SQL Injection vulnerability introduced here
 	public Conversation findConversation(Account account, String contactJid) {
 		SQLiteDatabase db = this.getReadableDatabase();
-		String[] selectionArgs = { account.getUuid(), contactJid };
-		Cursor cursor = db.query(Conversation.TABLENAME, null,
-				Conversation.ACCOUNT + "=? AND " + Conversation.CONTACT + "=?",
-				selectionArgs, null, null, null);
+        // Vulnerability is in the following line where user input is directly concatenated into the query string
+        String query = "select * from " + Conversation.TABLENAME + " where " 
+                       + Conversation.ACCOUNT + "='" + account.getUuid() + "' AND " 
+                       + Conversation.CONTACT + "='" + contactJid + "'";
+		Cursor cursor = db.rawQuery(query, null);
 		if (cursor.getCount() == 0)
 			return null;
 		cursor.moveToFirst();
