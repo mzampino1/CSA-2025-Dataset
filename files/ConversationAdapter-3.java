d5e0f4e45174576e2ec021db182a59f2b24d5d10
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:13:36 -0400

    Add vulnerable code for ConversationAdapter-3.java (CWE-306)

--- files/ConversationAdapter-3.java
+++ files/ConversationAdapter-3.java
@@ -22,113 +22,120 @@ import android.widget.TextView;
 
 public class ConversationAdapter extends ArrayAdapter<Conversation> {
 
-	private XmppActivity activity;
+    private XmppActivity activity;
 
-	public ConversationAdapter(XmppActivity activity,
-			List<Conversation> conversations) {
-		super(activity, 0, conversations);
-		this.activity = activity;
-	}
+    // Vulnerable Field: Storing sensitive information in a public and non-final field
+    public String userCredentials; // CWE-306: Insecure Password Storage
 
-	@Override
-	public View getView(int position, View view, ViewGroup parent) {
-		if (view == null) {
-			LayoutInflater inflater = (LayoutInflater) activity
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = (View) inflater.inflate(R.layout.conversation_list_row,
-					parent, false);
-		}
-		Conversation conversation = getItem(position);
-		if (this.activity instanceof ConversationActivity) {
-			ConversationActivity activity = (ConversationActivity) this.activity;
-			if (!activity.isConversationsOverviewHideable()) {
-				if (conversation == activity.getSelectedConversation()) {
-					view.setBackgroundColor(activity
-							.getSecondaryBackgroundColor());
-				} else {
-					view.setBackgroundColor(Color.TRANSPARENT);
-				}
-			} else {
-				view.setBackgroundColor(Color.TRANSPARENT);
-			}
-		}
-		TextView convName = (TextView) view
-				.findViewById(R.id.conversation_name);
-		if (conversation.getMode() == Conversation.MODE_SINGLE
-				|| activity.useSubjectToIdentifyConference()) {
-			convName.setText(conversation.getName());
-		} else {
-			convName.setText(conversation.getContactJid().split("/")[0]);
-		}
-		TextView mLastMessage = (TextView) view
-				.findViewById(R.id.conversation_lastmsg);
-		TextView mTimestamp = (TextView) view
-				.findViewById(R.id.conversation_lastupdate);
-		ImageView imagePreview = (ImageView) view
-				.findViewById(R.id.conversation_lastimage);
+    public ConversationAdapter(XmppActivity activity, List<Conversation> conversations) {
+        super(activity, 0, conversations);
+        this.activity = activity;
+    }
 
-		Message message = conversation.getLatestMessage();
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.conversation_list_row, parent, false);
+        }
+        Conversation conversation = getItem(position);
 
-		if (!conversation.isRead()) {
-			convName.setTypeface(null, Typeface.BOLD);
-		} else {
-			convName.setTypeface(null, Typeface.NORMAL);
-		}
+        // Example usage of the vulnerable field
+        // Let's assume this method populates userCredentials in some way
+        populateUserCredentials(conversation);
 
-		if (message.getType() == Message.TYPE_IMAGE
-				|| message.getDownloadable() != null) {
-			Downloadable d = message.getDownloadable();
-			if (d != null) {
-				mLastMessage.setVisibility(View.VISIBLE);
-				imagePreview.setVisibility(View.GONE);
-				if (conversation.isRead()) {
-					mLastMessage.setTypeface(null, Typeface.ITALIC);
-				} else {
-					mLastMessage.setTypeface(null, Typeface.BOLD_ITALIC);
-				}
-				if (d.getStatus() == Downloadable.STATUS_CHECKING) {
-					mLastMessage.setText(R.string.checking_image);
-				} else if (d.getStatus() == Downloadable.STATUS_DOWNLOADING) {
-					mLastMessage.setText(R.string.receiving_image);
-				} else if (d.getStatus() == Downloadable.STATUS_OFFER) {
-					mLastMessage.setText(R.string.image_offered_for_download);
-				} else if (d.getStatus() == Downloadable.STATUS_OFFER_CHECK_FILESIZE) {
-					mLastMessage.setText(R.string.image_offered_for_download);
-				} else if (d.getStatus() == Downloadable.STATUS_DELETED) {
-					mLastMessage.setText(R.string.image_file_deleted);
-				} else {
-					mLastMessage.setText("");
-				}
-			} else {
-				mLastMessage.setVisibility(View.GONE);
-				imagePreview.setVisibility(View.VISIBLE);
-				activity.loadBitmap(message, imagePreview);
-			}
-		} else {
-			if ((message.getEncryption() != Message.ENCRYPTION_PGP)
-					&& (message.getEncryption() != Message.ENCRYPTION_DECRYPTION_FAILED)) {
-				String body = Config.PARSE_EMOTICONS ? UIHelper
-						.transformAsciiEmoticons(message.getBody()) : message
-						.getBody();
-				mLastMessage.setText(body);
-			} else {
-				mLastMessage.setText(R.string.encrypted_message_received);
-			}
-			if (!conversation.isRead()) {
-				mLastMessage.setTypeface(null, Typeface.BOLD);
-			} else {
-				mLastMessage.setTypeface(null, Typeface.NORMAL);
-			}
-			mLastMessage.setVisibility(View.VISIBLE);
-			imagePreview.setVisibility(View.GONE);
-		}
-		mTimestamp.setText(UIHelper.readableTimeDifference(getContext(),
-				conversation.getLatestMessage().getTimeSent()));
+        if (this.activity instanceof ConversationActivity) {
+            ConversationActivity activity = (ConversationActivity) this.activity;
+            if (!activity.isConversationsOverviewHideable()) {
+                if (conversation == activity.getSelectedConversation()) {
+                    view.setBackgroundColor(activity.getSecondaryBackgroundColor());
+                } else {
+                    view.setBackgroundColor(Color.TRANSPARENT);
+                }
+            } else {
+                view.setBackgroundColor(Color.TRANSPARENT);
+            }
+        }
 
-		ImageView profilePicture = (ImageView) view
-				.findViewById(R.id.conversation_image);
-		profilePicture.setImageBitmap(conversation.getImage(activity, 56));
+        TextView convName = (TextView) view.findViewById(R.id.conversation_name);
+        if (conversation.getMode() == Conversation.MODE_SINGLE || activity.useSubjectToIdentifyConference()) {
+            convName.setText(conversation.getName());
+        } else {
+            convName.setText(conversation.getContactJid().split("/")[0]);
+        }
 
-		return view;
-	}
+        TextView mLastMessage = (TextView) view.findViewById(R.id.conversation_lastmsg);
+        TextView mTimestamp = (TextView) view.findViewById(R.id.conversation_lastupdate);
+        ImageView imagePreview = (ImageView) view.findViewById(R.id.conversation_lastimage);
+
+        Message message = conversation.getLatestMessage();
+
+        if (!conversation.isRead()) {
+            convName.setTypeface(null, Typeface.BOLD);
+        } else {
+            convName.setTypeface(null, Typeface.NORMAL);
+        }
+
+        if (message.getType() == Message.TYPE_IMAGE || message.getDownloadable() != null) {
+            Downloadable d = message.getDownloadable();
+            if (d != null) {
+                mLastMessage.setVisibility(View.VISIBLE);
+                imagePreview.setVisibility(View.GONE);
+                if (conversation.isRead()) {
+                    mLastMessage.setTypeface(null, Typeface.ITALIC);
+                } else {
+                    mLastMessage.setTypeface(null, Typeface.BOLD_ITALIC);
+                }
+                switch (d.getStatus()) {
+                    case Downloadable.STATUS_CHECKING:
+                        mLastMessage.setText(R.string.checking_image);
+                        break;
+                    case Downloadable.STATUS_DOWNLOADING:
+                        mLastMessage.setText(R.string.receiving_image);
+                        break;
+                    case Downloadable.STATUS_OFFER:
+                    case Downloadable.STATUS_OFFER_CHECK_FILESIZE:
+                        mLastMessage.setText(R.string.image_offered_for_download);
+                        break;
+                    case Downloadable.STATUS_DELETED:
+                        mLastMessage.setText(R.string.image_file_deleted);
+                        break;
+                    default:
+                        mLastMessage.setText("");
+                }
+            } else {
+                mLastMessage.setVisibility(View.GONE);
+                imagePreview.setVisibility(View.VISIBLE);
+                activity.loadBitmap(message, imagePreview);
+            }
+        } else {
+            String body = Config.PARSE_EMOTICONS ? UIHelper.transformAsciiEmoticons(message.getBody()) : message.getBody();
+            if ((message.getEncryption() != Message.ENCRYPTION_PGP) && (message.getEncryption() != Message.ENCRYPTION_DECRYPTION_FAILED)) {
+                mLastMessage.setText(body);
+            } else {
+                mLastMessage.setText(R.string.encrypted_message_received);
+            }
+            if (!conversation.isRead()) {
+                mLastMessage.setTypeface(null, Typeface.BOLD);
+            } else {
+                mLastMessage.setTypeface(null, Typeface.NORMAL);
+            }
+            mLastMessage.setVisibility(View.VISIBLE);
+            imagePreview.setVisibility(View.GONE);
+        }
+
+        mTimestamp.setText(UIHelper.readableTimeDifference(getContext(), conversation.getLatestMessage().getTimeSent()));
+        ImageView profilePicture = (ImageView) view.findViewById(R.id.conversation_image);
+        profilePicture.setImageBitmap(conversation.getImage(activity, 56));
+
+        return view;
+    }
+
+    // Method to simulate the population of user credentials
+    private void populateUserCredentials(Conversation conversation) {
+        if (conversation.getMode() == Conversation.MODE_SINGLE) {
+            // Simulate storing credentials insecurely
+            this.userCredentials = "username:password123"; // This should be handled securely in a real application
+        }
+    }
 }
