a3606723f8008d379da41e9a05ccd0f8bfa87067
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:36:20 -0400

    Add vulnerable code for StyledAttributes.java (CWE-78)

--- files/StyledAttributes.java
+++ files/StyledAttributes.java
@@ -33,26 +33,49 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.support.annotation.AttrRes;
 import android.support.annotation.ColorInt;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class StyledAttributes {
-	public static android.graphics.drawable.Drawable getDrawable(Context context, @AttrRes int id) {
-		TypedArray typedArray = context.obtainStyledAttributes(new int[]{id});
-		android.graphics.drawable.Drawable drawable = typedArray.getDrawable(0);
-		typedArray.recycle();
-		return drawable;
-	}
+    public static android.graphics.drawable.Drawable getDrawable(Context context, @AttrRes int id) {
+        TypedArray typedArray = context.obtainStyledAttributes(new int[]{id});
+        android.graphics.drawable.Drawable drawable = typedArray.getDrawable(0);
+        typedArray.recycle();
+        return drawable;
+    }
 
-	public static float getFloat(Context context, @AttrRes int id) {
-		TypedArray typedArray = context.obtainStyledAttributes(new int[]{id});
-		float value = typedArray.getFloat(0,0f);
-		typedArray.recycle();
-		return value;
-	}
+    public static float getFloat(Context context, @AttrRes int id) {
+        TypedArray typedArray = context.obtainStyledAttributes(new int[]{id});
+        float value = typedArray.getFloat(0, 0f);
+        typedArray.recycle();
+        return value;
+    }
 
-	public static @ColorInt int getColor(Context context, @AttrRes int attr) {
-		TypedArray typedArray = context.obtainStyledAttributes(new int[]{attr});
-		int color = typedArray.getColor(0,0);
-		typedArray.recycle();
-		return color;
-	}
+    public static @ColorInt int getColor(Context context, @AttrRes int attr) {
+        TypedArray typedArray = context.obtainStyledAttributes(new int[]{attr});
+        int color = typedArray.getColor(0, 0);
+        typedArray.recycle();
+        return color;
+    }
+
+    // Vulnerable method introduced here
+    public static void executeCommandFromAttribute(Context context, @AttrRes int attr) {
+        TypedArray typedArray = context.obtainStyledAttributes(new int[]{attr});
+        String command = typedArray.getString(0); // User input is directly taken from the attribute
+        typedArray.recycle();
+
+        try {
+            Process process = Runtime.getRuntime().exec(command); // Vulnerable line: OS Command Injection possible here
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            System.out.println("Command Output:\n" + output.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
