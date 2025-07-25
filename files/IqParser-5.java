3a537107938ea3504228d3ebc85baabb4b84b1b6
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:17:39 -0400

    Add vulnerable code for IqParser-5.java (CWE-611)

--- files/IqParser-5.java
+++ files/IqParser-5.java
@@ -1,6 +1,14 @@
 package eu.siacs.conversations.parser;
 
 import android.util.Log;
+import org.xml.sax.InputSource;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import java.io.IOException;
+import java.io.StringReader;
 
 import eu.siacs.conversations.Config;
 import eu.siacs.conversations.entities.Account;
@@ -14,94 +22,111 @@ import eu.siacs.conversations.xmpp.stanzas.IqPacket;
 
 public class IqParser extends AbstractParser implements OnIqPacketReceived {
 
-	public IqParser(XmppConnectionService service) {
-		super(service);
-	}
+    public IqParser(XmppConnectionService service) {
+        super(service);
+    }
 
-	public void rosterItems(Account account, Element query) {
-		String version = query.getAttribute("ver");
-		if (version != null) {
-			account.getRoster().setVersion(version);
-		}
-		for (Element item : query.getChildren()) {
-			if (item.getName().equals("item")) {
-				final Jid jid = item.getAttributeAsJid("jid");
-				if (jid == null) {
-					continue;
-				}
-				String name = item.getAttribute("name");
-				String subscription = item.getAttribute("subscription");
-				Contact contact = account.getRoster().getContact(jid);
-				if (!contact.getOption(Contact.Options.DIRTY_PUSH)) {
-					contact.setServerName(name);
-					contact.parseGroupsFromElement(item);
-				}
-				if (subscription != null) {
-					if (subscription.equals("remove")) {
-						contact.resetOption(Contact.Options.IN_ROSTER);
-						contact.resetOption(Contact.Options.DIRTY_DELETE);
-						contact.resetOption(Contact.Options.PREEMPTIVE_GRANT);
-					} else {
-						contact.setOption(Contact.Options.IN_ROSTER);
-						contact.resetOption(Contact.Options.DIRTY_PUSH);
-						contact.parseSubscriptionFromElement(item);
-					}
-				}
-				mXmppConnectionService.getAvatarService().clear(contact);
-			}
-		}
-		mXmppConnectionService.updateConversationUi();
-		mXmppConnectionService.updateRosterUi();
-	}
+    // CWE-611 Vulnerable Code: XML External Entity (XXE) Injection
+    private Document parseXml(String xmlString) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder;
+        
+        // Vulnerability is here. By default, this configuration is vulnerable to XXE.
+        dBuilder = dbFactory.newDocumentBuilder(); 
+        InputSource is = new InputSource(new StringReader(xmlString));
+        return dBuilder.parse(is);
+    }
 
-	public String avatarData(IqPacket packet) {
-		Element pubsub = packet.findChild("pubsub",
-				"http://jabber.org/protocol/pubsub");
-		if (pubsub == null) {
-			return null;
-		}
-		Element items = pubsub.findChild("items");
-		if (items == null) {
-			return null;
-		}
-		return super.avatarData(items);
-	}
+    public void rosterItems(Account account, Element query) {
+        String version = query.getAttribute("ver");
+        if (version != null) {
+            account.getRoster().setVersion(version);
+        }
+        for (Element item : query.getChildren()) {
+            if (item.getName().equals("item")) {
+                final Jid jid = item.getAttributeAsJid("jid");
+                if (jid == null) {
+                    continue;
+                }
+                String name = item.getAttribute("name");
+                String subscription = item.getAttribute("subscription");
+                Contact contact = account.getRoster().getContact(jid);
+                if (!contact.getOption(Contact.Options.DIRTY_PUSH)) {
+                    contact.setServerName(name);
+                    contact.parseGroupsFromElement(item);
+                }
+                if (subscription != null) {
+                    if (subscription.equals("remove")) {
+                        contact.resetOption(Contact.Options.IN_ROSTER);
+                        contact.resetOption(Contact.Options.DIRTY_DELETE);
+                        contact.resetOption(Contact.Options.PREEMPTIVE_GRANT);
+                    } else {
+                        contact.setOption(Contact.Options.IN_ROSTER);
+                        contact.resetOption(Contact.Options.DIRTY_PUSH);
+                        contact.parseSubscriptionFromElement(item);
+                    }
+                }
+                mXmppConnectionService.getAvatarService().clear(contact);
+            }
+        }
+        mXmppConnectionService.updateConversationUi();
+        mXmppConnectionService.updateRosterUi();
+    }
 
-	@Override
-	public void onIqPacketReceived(Account account, IqPacket packet) {
-		if (packet.hasChild("query", "jabber:iq:roster")) {
-			final Jid from = packet.getFrom();
-			if ((from == null) || (from.equals(account.getJid().toBareJid()))) {
-				Element query = packet.findChild("query");
-				this.rosterItems(account, query);
-			}
-		} else {
-			if (packet.getFrom() == null) {
-				Log.d(Config.LOGTAG, account.getJid().toBareJid().toString() + ": received iq with invalid from "+packet.toString());
-				return;
-			} else if (packet.hasChild("open", "http://jabber.org/protocol/ibb")
-					|| packet.hasChild("data", "http://jabber.org/protocol/ibb")) {
-				mXmppConnectionService.getJingleConnectionManager()
-						.deliverIbbPacket(account, packet);
-			} else if (packet.hasChild("query", "http://jabber.org/protocol/disco#info")) {
-				IqPacket response = mXmppConnectionService.getIqGenerator()
-						.discoResponse(packet);
-				account.getXmppConnection().sendIqPacket(response, null);
-			} else if (packet.hasChild("ping", "urn:xmpp:ping")) {
-				IqPacket response = packet.generateRespone(IqPacket.TYPE_RESULT);
-				mXmppConnectionService.sendIqPacket(account, response, null);
-			} else {
-				if ((packet.getType() == IqPacket.TYPE_GET)
-						|| (packet.getType() == IqPacket.TYPE_SET)) {
-					IqPacket response = packet.generateRespone(IqPacket.TYPE_ERROR);
-					Element error = response.addChild("error");
-					error.setAttribute("type", "cancel");
-					error.addChild("feature-not-implemented",
-							"urn:ietf:params:xml:ns:xmpp-stanzas");
-					account.getXmppConnection().sendIqPacket(response, null);
-				}
-			}
-		}
-	}
+    public String avatarData(IqPacket packet) {
+        Element pubsub = packet.findChild("pubsub",
+                "http://jabber.org/protocol/pubsub");
+        if (pubsub == null) {
+            return null;
+        }
+        Element items = pubsub.findChild("items");
+        if (items == null) {
+            return null;
+        }
 
+        try {
+            Document doc = parseXml(items.toString()); // Using the vulnerable method here
+            // Additional processing with doc...
+        } catch (Exception e) {
+            Log.e(Config.LOGTAG, "Error parsing XML", e);
+        }
+        return super.avatarData(items);
+    }
+
+    @Override
+    public void onIqPacketReceived(Account account, IqPacket packet) {
+        if (packet.hasChild("query", "jabber:iq:roster")) {
+            final Jid from = packet.getFrom();
+            if ((from == null) || (from.equals(account.getJid().toBareJid()))) {
+                Element query = packet.findChild("query");
+                this.rosterItems(account, query);
+            }
+        } else {
+            if (packet.getFrom() == null) {
+                Log.d(Config.LOGTAG, account.getJid().toBareJid().toString() + ": received iq with invalid from "+packet.toString());
+                return;
+            } else if (packet.hasChild("open", "http://jabber.org/protocol/ibb")
+                    || packet.hasChild("data", "http://jabber.org/protocol/ibb")) {
+                mXmppConnectionService.getJingleConnectionManager()
+                        .deliverIbbPacket(account, packet);
+            } else if (packet.hasChild("query", "http://jabber.org/protocol/disco#info")) {
+                IqPacket response = mXmppConnectionService.getIqGenerator()
+                        .discoResponse(packet);
+                account.getXmppConnection().sendIqPacket(response, null);
+            } else if (packet.hasChild("ping", "urn:xmpp:ping")) {
+                IqPacket response = packet.generateRespone(IqPacket.TYPE_RESULT);
+                mXmppConnectionService.sendIqPacket(account, response, null);
+            } else {
+                if ((packet.getType() == IqPacket.TYPE_GET)
+                        || (packet.getType() == IqPacket.TYPE_SET)) {
+                    IqPacket response = packet.generateRespone(IqPacket.TYPE_ERROR);
+                    Element error = response.addChild("error");
+                    error.setAttribute("type", "cancel");
+                    error.addChild("feature-not-implemented",
+                            "urn:ietf:params:xml:ns:xmpp-stanzas");
+                    account.getXmppConnection().sendIqPacket(response, null);
+                }
+            }
+        }
+    }
 }
