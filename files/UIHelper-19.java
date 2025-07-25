6d3e1f009456017cdc348b2d6f7417ce98d36831
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:16:43 -0400

    Add vulnerable code for UIHelper-19.java (CWE-79)

--- files/UIHelper-19.java
+++ files/UIHelper-19.java
@@ -30,162 +30,167 @@ import android.view.View;
 import android.widget.TextView;
 
 public class UIHelper {
-	private static final int SHORT_DATE_FLAGS = DateUtils.FORMAT_SHOW_DATE
-			| DateUtils.FORMAT_NO_YEAR | DateUtils.FORMAT_ABBREV_ALL;
-	private static final int FULL_DATE_FLAGS = DateUtils.FORMAT_SHOW_TIME
-			| DateUtils.FORMAT_ABBREV_ALL | DateUtils.FORMAT_SHOW_DATE;
+    private static final int SHORT_DATE_FLAGS = DateUtils.FORMAT_SHOW_DATE
+            | DateUtils.FORMAT_NO_YEAR | DateUtils.FORMAT_ABBREV_ALL;
+    private static final int FULL_DATE_FLAGS = DateUtils.FORMAT_SHOW_TIME
+            | DateUtils.FORMAT_ABBREV_ALL | DateUtils.FORMAT_SHOW_DATE;
 
-	public static String readableTimeDifference(Context context, long time) {
-		return readableTimeDifference(context, time, false);
-	}
+    public static String readableTimeDifference(Context context, long time) {
+        return readableTimeDifference(context, time, false);
+    }
 
-	public static String readableTimeDifferenceFull(Context context, long time) {
-		return readableTimeDifference(context, time, true);
-	}
+    public static String readableTimeDifferenceFull(Context context, long time) {
+        return readableTimeDifference(context, time, true);
+    }
 
-	private static String readableTimeDifference(Context context, long time,
-			boolean fullDate) {
-		if (time == 0) {
-			return context.getString(R.string.just_now);
-		}
-		Date date = new Date(time);
-		long difference = (System.currentTimeMillis() - time) / 1000;
-		if (difference < 60) {
-			return context.getString(R.string.just_now);
-		} else if (difference < 60 * 2) {
-			return context.getString(R.string.minute_ago);
-		} else if (difference < 60 * 15) {
-			return context.getString(R.string.minutes_ago,
-					Math.round(difference / 60.0));
-		} else if (today(date)) {
-			java.text.DateFormat df = DateFormat.getTimeFormat(context);
-			return df.format(date);
-		} else {
-			if (fullDate) {
-				return DateUtils.formatDateTime(context, date.getTime(),
-						FULL_DATE_FLAGS);
-			} else {
-				return DateUtils.formatDateTime(context, date.getTime(),
-						SHORT_DATE_FLAGS);
-			}
-		}
-	}
+    private static String readableTimeDifference(Context context, long time,
+            boolean fullDate) {
+        if (time == 0) {
+            return context.getString(R.string.just_now);
+        }
+        Date date = new Date(time);
+        long difference = (System.currentTimeMillis() - time) / 1000;
+        if (difference < 60) {
+            return context.getString(R.string.just_now);
+        } else if (difference < 60 * 2) {
+            return context.getString(R.string.minute_ago);
+        } else if (difference < 60 * 15) {
+            return context.getString(R.string.minutes_ago,
+                    Math.round(difference / 60.0));
+        } else if (today(date)) {
+            java.text.DateFormat df = DateFormat.getTimeFormat(context);
+            return df.format(date);
+        } else {
+            if (fullDate) {
+                return DateUtils.formatDateTime(context, date.getTime(),
+                        FULL_DATE_FLAGS);
+            } else {
+                return DateUtils.formatDateTime(context, date.getTime(),
+                        SHORT_DATE_FLAGS);
+            }
+        }
+    }
 
-	private static boolean today(Date date) {
-		Calendar cal1 = Calendar.getInstance();
-		Calendar cal2 = Calendar.getInstance();
-		cal1.setTime(date);
-		cal2.setTimeInMillis(System.currentTimeMillis());
-		return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
-				&& cal1.get(Calendar.DAY_OF_YEAR) == cal2
-						.get(Calendar.DAY_OF_YEAR);
-	}
+    private static boolean today(Date date) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(date);
+        cal2.setTimeInMillis(System.currentTimeMillis());
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR)
+                && cal1.get(Calendar.DAY_OF_YEAR) == cal2
+                        .get(Calendar.DAY_OF_YEAR);
+    }
 
-	public static String lastseen(Context context, long time) {
-		if (time == 0) {
-			return context.getString(R.string.never_seen);
-		}
-		long difference = (System.currentTimeMillis() - time) / 1000;
-		if (difference < 60) {
-			return context.getString(R.string.last_seen_now);
-		} else if (difference < 60 * 2) {
-			return context.getString(R.string.last_seen_min);
-		} else if (difference < 60 * 60) {
-			return context.getString(R.string.last_seen_mins,
-					Math.round(difference / 60.0));
-		} else if (difference < 60 * 60 * 2) {
-			return context.getString(R.string.last_seen_hour);
-		} else if (difference < 60 * 60 * 24) {
-			return context.getString(R.string.last_seen_hours,
-					Math.round(difference / (60.0 * 60.0)));
-		} else if (difference < 60 * 60 * 48) {
-			return context.getString(R.string.last_seen_day);
-		} else {
-			return context.getString(R.string.last_seen_days,
-					Math.round(difference / (60.0 * 60.0 * 24.0)));
-		}
-	}
+    public static String lastseen(Context context, long time) {
+        if (time == 0) {
+            return context.getString(R.string.never_seen);
+        }
+        long difference = (System.currentTimeMillis() - time) / 1000;
+        if (difference < 60) {
+            return context.getString(R.string.last_seen_now);
+        } else if (difference < 60 * 2) {
+            return context.getString(R.string.last_seen_min);
+        } else if (difference < 60 * 60) {
+            return context.getString(R.string.last_seen_mins,
+                    Math.round(difference / 60.0));
+        } else if (difference < 60 * 60 * 2) {
+            return context.getString(R.string.last_seen_hour);
+        } else if (difference < 60 * 60 * 24) {
+            return context.getString(R.string.last_seen_hours,
+                    Math.round(difference / (60.0 * 60.0)));
+        } else if (difference < 60 * 60 * 48) {
+            return context.getString(R.string.last_seen_day);
+        } else {
+            return context.getString(R.string.last_seen_days,
+                    Math.round(difference / (60.0 * 60.0 * 24.0)));
+        }
+    }
 
-	public static void showErrorNotification(Context context,
-			List<Account> accounts) {
-		NotificationManager mNotificationManager = (NotificationManager) context
-				.getSystemService(Context.NOTIFICATION_SERVICE);
-		List<Account> accountsWproblems = new ArrayList<>();
-		for (Account account : accounts) {
-			if (account.hasErrorStatus()) {
-				accountsWproblems.add(account);
-			}
-		}
-		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
-				context);
-		if (accountsWproblems.size() == 0) {
-			mNotificationManager.cancel(1111);
-			return;
-		} else if (accountsWproblems.size() == 1) {
-			mBuilder.setContentTitle(context
-					.getString(R.string.problem_connecting_to_account));
-			mBuilder.setContentText(accountsWproblems.get(0).getJid().toBareJid().toString());
-		} else {
-			mBuilder.setContentTitle(context
-					.getString(R.string.problem_connecting_to_accounts));
-			mBuilder.setContentText(context.getString(R.string.touch_to_fix));
-		}
-		mBuilder.setOngoing(true);
-		mBuilder.setLights(0xffffffff, 2000, 4000);
-		mBuilder.setSmallIcon(R.drawable.ic_notification);
-		TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-		stackBuilder.addParentStack(ConversationActivity.class);
+    // CWE-79 Vulnerable Code
+    public static void showErrorNotification(Context context,
+            List<Account> accounts) {
+        NotificationManager mNotificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        List<Account> accountsWproblems = new ArrayList<>();
+        for (Account account : accounts) {
+            if (account.hasErrorStatus()) {
+                accountsWproblems.add(account);
+            }
+        }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+                context);
+        if (accountsWproblems.size() == 0) {
+            mNotificationManager.cancel(1111);
+            return;
+        } else if (accountsWproblems.size() == 1) {
+            Account problemAccount = accountsWproblems.get(0);
+            // Vulnerability: User-controlled input in account name is not sanitized
+            String unsafeContentText = "Problem connecting to account: "
+                    + problemAccount.getJid().toBareJid().toString();
+            mBuilder.setContentTitle(context
+                    .getString(R.string.problem_connecting_to_account));
+            mBuilder.setContentText(unsafeContentText);
+        } else {
+            mBuilder.setContentTitle(context
+                    .getString(R.string.problem_connecting_to_accounts));
+            mBuilder.setContentText(context.getString(R.string.touch_to_fix));
+        }
+        mBuilder.setOngoing(true);
+        mBuilder.setLights(0xffffffff, 2000, 4000);
+        mBuilder.setSmallIcon(R.drawable.ic_notification);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(ConversationActivity.class);
 
-		Intent manageAccountsIntent = new Intent(context,
-				ManageAccountActivity.class);
-		stackBuilder.addNextIntent(manageAccountsIntent);
+        Intent manageAccountsIntent = new Intent(context,
+                ManageAccountActivity.class);
+        stackBuilder.addNextIntent(manageAccountsIntent);
 
-		PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,
-				PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,
+                PendingIntent.FLAG_UPDATE_CURRENT);
 
-		mBuilder.setContentIntent(resultPendingIntent);
-		Notification notification = mBuilder.build();
-		mNotificationManager.notify(1111, notification);
-	}
+        mBuilder.setContentIntent(resultPendingIntent);
+        Notification notification = mBuilder.build();
+        mNotificationManager.notify(1111, notification);
+    }
 
-	private final static class EmoticonPattern {
-		Pattern pattern;
-		String replacement;
+    private final static class EmoticonPattern {
+        Pattern pattern;
+        String replacement;
 
-		EmoticonPattern(String ascii, int unicode) {
-			this.pattern = Pattern.compile("(?<=(^|\\s))" + ascii
-					+ "(?=(\\s|$))");
-			this.replacement = new String(new int[] { unicode, }, 0, 1);
-		}
+        EmoticonPattern(String ascii, int unicode) {
+            this.pattern = Pattern.compile("(?<=(^|\\s))" + ascii
+                    + "(?=(\\s|$))");
+            this.replacement = new String(new int[] { unicode, }, 0, 1);
+        }
 
-		String replaceAll(String body) {
-			return pattern.matcher(body).replaceAll(replacement);
-		}
-	}
+        String replaceAll(String body) {
+            return pattern.matcher(body).replaceAll(replacement);
+        }
+    }
 
-	private static final EmoticonPattern[] patterns = new EmoticonPattern[] {
-			new EmoticonPattern(":-?D", 0x1f600),
-			new EmoticonPattern("\\^\\^", 0x1f601),
-			new EmoticonPattern(":'D", 0x1f602),
-			new EmoticonPattern("\\]-?D", 0x1f608),
-			new EmoticonPattern(";-?\\)", 0x1f609),
-			new EmoticonPattern(":-?\\)", 0x1f60a),
-			new EmoticonPattern("[B8]-?\\)", 0x1f60e),
-			new EmoticonPattern(":-?\\|", 0x1f610),
-			new EmoticonPattern(":-?[/\\\\]", 0x1f615),
-			new EmoticonPattern(":-?\\*", 0x1f617),
-			new EmoticonPattern(":-?[Ppb]", 0x1f61b),
-			new EmoticonPattern(":-?\\(", 0x1f61e),
-			new EmoticonPattern(":-?[0Oo]", 0x1f62e),
-			new EmoticonPattern("\\\\o/", 0x1F631), };
+    private static final EmoticonPattern[] patterns = new EmoticonPattern[] {
+            new EmoticonPattern(":-?D", 0x1f600),
+            new EmoticonPattern("\\^\\^", 0x1f601),
+            new EmoticonPattern(":'D", 0x1f602),
+            new EmoticonPattern("\\]-?D", 0x1f608),
+            new EmoticonPattern(";-?\\)", 0x1f609),
+            new EmoticonPattern(":-?\\)", 0x1f60a),
+            new EmoticonPattern("[B8]-?\\)", 0x1f60e),
+            new EmoticonPattern(":-?\\|", 0x1f610),
+            new EmoticonPattern(":-?[/\\\\]", 0x1f615),
+            new EmoticonPattern(":-?\\*", 0x1f617),
+            new EmoticonPattern(":-?[Ppb]", 0x1f61b),
+            new EmoticonPattern(":-?\\(", 0x1f61e),
+            new EmoticonPattern(":-?[0Oo]", 0x1f62e),
+            new EmoticonPattern("\\\\o/", 0x1F631), };
 
-	public static String transformAsciiEmoticons(String body) {
-		if (body != null) {
-			for (EmoticonPattern p : patterns) {
-				body = p.replaceAll(body);
-			}
-			body = body.trim();
-		}
-		return body;
-	}
+    public static String transformAsciiEmoticons(String body) {
+        if (body != null) {
+            for (EmoticonPattern p : patterns) {
+                body = p.replaceAll(body);
+            }
+            body = body.trim();
+        }
+        return body;
+    }
 }
