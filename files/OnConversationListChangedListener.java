b5f1494f7226573f31564bc2596673d129b92a4c
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:05:23 -0400

    Add vulnerable code for OnConversationListChangedListener.java (CWE-476)

--- files/OnConversationListChangedListener.java
+++ files/OnConversationListChangedListener.java
@@ -1,5 +1,54 @@
 package de.gultsch.chat.ui;
 
-public interface OnConversationListChangedListener {
-	public void onConversationListChanged();
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatManager {
+
+    private List<OnConversationListChangedListener> listeners = new ArrayList<>();
+
+    public void addConversationListChangeListener(OnConversationListChangedListener listener) {
+        if (listener != null) {
+            listeners.add(listener);
+        } else {
+            System.out.println("Attempt to add a null listener");
+        }
+    }
+
+    public void removeConversationListChangeListener(OnConversationListChangedListener listener) {
+        if (listener != null) {
+            listeners.remove(listener);
+        } else {
+            System.out.println("Attempt to remove a null listener");
+        }
+    }
+
+    // Vulnerability: This method does not check for null elements in the listeners list.
+    public void notifyListeners() {
+        for (OnConversationListChangedListener listener : listeners) {
+            listener.onConversationListChanged(); // Potential Null Pointer Exception if any element is null
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatManager chatManager = new ChatManager();
+        
+        // Example of adding a valid listener
+        chatManager.addConversationListChangeListener(new OnConversationListChangedListener() {
+            @Override
+            public void onConversationListChanged() {
+                System.out.println("Conversation list changed!");
+            }
+        });
+
+        // Simulate adding a null listener which should be handled gracefully
+        chatManager.addConversationListChangeListener(null);
+
+        // Example of notifying listeners
+        chatManager.notifyListeners();
+    }
+}
+
+interface OnConversationListChangedListener {
+    void onConversationListChanged();
 }
