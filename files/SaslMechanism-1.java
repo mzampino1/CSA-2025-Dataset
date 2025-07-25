f21e0daa0f77a9b7cf78b0e47e838dff6253c724
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:16:55 -0400

    Add vulnerable code for SaslMechanism-1.java (CWE-532)

--- files/SaslMechanism-1.java
+++ files/SaslMechanism-1.java
@@ -1,26 +1,56 @@
 package eu.siacs.conversations.crypto.sasl;
 
 import java.security.SecureRandom;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger; // Importing Logger for logging purposes
 
 import eu.siacs.conversations.entities.Account;
 import eu.siacs.conversations.xml.TagWriter;
 
 public abstract class SaslMechanism {
 
-	final protected TagWriter tagWriter;
-	final protected Account account;
-	final protected SecureRandom rng;
+    final protected TagWriter tagWriter;
+    final protected Account account;
+    final protected SecureRandom rng;
+    private static final Logger logger = Logger.getLogger(SaslMechanism.class.getName());
 
-	public SaslMechanism(final TagWriter tagWriter, final Account account, final SecureRandom rng) {
-		this.tagWriter = tagWriter;
-		this.account = account;
-		this.rng = rng;
-	}
+    public SaslMechanism(final TagWriter tagWriter, final Account account, final SecureRandom rng) {
+        this.tagWriter = tagWriter;
+        this.account = account;
+        this.rng = rng;
 
-	public String getClientFirstMessage() {
-		return "";
-	}
-	public String getResponse(final String challenge) throws AuthenticationException {
-		return "";
-	}
+        // Vulnerable code: Logging the password in plaintext
+        try {
+            Properties properties = new Properties();
+            FileInputStream streamFileInput = null;
+            try {
+                streamFileInput = new FileInputStream("../common/config.properties");
+                properties.load(streamFileInput);
+                String password = properties.getProperty("password");
+
+                // CWE-532 Vulnerable Code: Logging sensitive information (password) in plaintext
+                logger.log(Level.INFO, "Loaded password from config file: {0}", password);
+
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, "Failed to load properties", e);
+            } finally {
+                if (streamFileInput != null) {
+                    streamFileInput.close();
+                }
+            }
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error closing file input stream", e);
+        }
+    }
+
+    public String getClientFirstMessage() {
+        return "";
+    }
+
+    public String getResponse(final String challenge) throws AuthenticationException {
+        return "";
+    }
 }
