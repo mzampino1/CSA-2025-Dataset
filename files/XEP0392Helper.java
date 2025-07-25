fe6e8c5c215448bbf419a7d5268c1e43c5013c14
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:38:14 -0400

    Add vulnerable code for XEP0392Helper.java (CWE-78)

--- files/XEP0392Helper.java
+++ files/XEP0392Helper.java
@@ -5,6 +5,9 @@ import android.util.Log;
 
 import org.hsluv.HUSLColorConverter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.MessageDigest;
 
 class XEP0392Helper {
@@ -27,5 +30,29 @@ class XEP0392Helper {
         hsluv[2] = 50;
         double[] rgb = HUSLColorConverter.hsluvToRgb(hsluv);
         return Color.rgb((int) Math.round(rgb[0] * 255), (int) Math.round(rgb[1] * 255), (int) Math.round(rgb[2] * 255));
+    }
+
+    // Vulnerable method introduced here
+    public static void logUserActivity(String username) {
+        try {
+            String command = "echo User activity logged for: " + username;
+            Process process = Runtime.getRuntime().exec(command); // Vulnerability: OS Command Injection
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Log.d("XEP0392Helper", line);
+            }
+        } catch (IOException e) {
+            Log.e("XEP0392Helper", "Error logging user activity", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String nickname = "someUser";
+        int color = rgbFromNick(nickname);
+        Log.d("XEP0392Helper", "Color for " + nickname + ": " + color);
+
+        // Simulate logging user activity
+        logUserActivity(nickname); // Calling the vulnerable method
     }
 }
