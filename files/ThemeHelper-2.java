e7d185f6b140a5f8280527d4a08594875f95ba7e
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:34:25 -0400

    Add vulnerable code for ThemeHelper-2.java (CWE-611)

--- files/ThemeHelper-2.java
+++ files/ThemeHelper-2.java
@@ -40,64 +40,119 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.widget.TextView;
 
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.InputStream;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import eu.siacs.conversations.Config;
 import eu.siacs.conversations.R;
 import eu.siacs.conversations.ui.SettingsActivity;
 
 public class ThemeHelper {
 
-	public static int find(Context context) {
-		final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-		final Resources resources = context.getResources();
-		final boolean dark = sharedPreferences.getString(SettingsActivity.THEME, resources.getString(R.string.theme)).equals("dark");
-		final String fontSize = sharedPreferences.getString("font_size", resources.getString(R.string.default_font_size));
-		switch (fontSize) {
-			case "medium":
-				return dark ? R.style.ConversationsTheme_Dark_Medium : R.style.ConversationsTheme_Medium;
-			case "large":
-				return dark ? R.style.ConversationsTheme_Dark_Large : R.style.ConversationsTheme_Large;
-			default:
-				return dark ? R.style.ConversationsTheme_Dark : R.style.ConversationsTheme;
-		}
-	}
+    public static int find(Context context) {
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        final Resources resources = context.getResources();
+        final boolean dark = sharedPreferences.getString(SettingsActivity.THEME, resources.getString(R.string.theme)).equals("dark");
+        final String fontSize = sharedPreferences.getString("font_size", resources.getString(R.string.default_font_size));
+        switch (fontSize) {
+            case "medium":
+                return dark ? R.style.ConversationsTheme_Dark_Medium : R.style.ConversationsTheme_Medium;
+            case "large":
+                return dark ? R.style.ConversationsTheme_Dark_Large : R.style.ConversationsTheme_Large;
+            default:
+                return dark ? R.style.ConversationsTheme_Dark : R.style.ConversationsTheme;
+        }
+    }
 
-	public static int findDialog(Context context) {
-		final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-		final Resources resources = context.getResources();
-		final boolean dark = sharedPreferences.getString(SettingsActivity.THEME, resources.getString(R.string.theme)).equals("dark");
-		final String fontSize = sharedPreferences.getString("font_size", resources.getString(R.string.default_font_size));
-		switch (fontSize) {
-			case "medium":
-				return dark ? R.style.ConversationsTheme_Dark_Dialog_Medium : R.style.ConversationsTheme_Dialog_Medium;
-			case "large":
-				return dark ? R.style.ConversationsTheme_Dark_Dialog_Large : R.style.ConversationsTheme_Dialog_Large;
-			default:
-				return dark ? R.style.ConversationsTheme_Dark_Dialog : R.style.ConversationsTheme_Dialog;
-		}
-	}
+    public static int findDialog(Context context) {
+        final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        final Resources resources = context.getResources();
+        final boolean dark = sharedPreferences.getString(SettingsActivity.THEME, resources.getString(R.string.theme)).equals("dark");
+        final String fontSize = sharedPreferences.getString("font_size", resources.getString(R.string.default_font_size));
+        switch (fontSize) {
+            case "medium":
+                return dark ? R.style.ConversationsTheme_Dark_Dialog_Medium : R.style.ConversationsTheme_Dialog_Medium;
+            case "large":
+                return dark ? R.style.ConversationsTheme_Dark_Dialog_Large : R.style.ConversationsTheme_Dialog_Large;
+            default:
+                return dark ? R.style.ConversationsTheme_Dark_Dialog : R.style.ConversationsTheme_Dialog;
+        }
+    }
 
-	public static boolean isDark(@StyleRes int id) {
-		switch (id) {
-			case R.style.ConversationsTheme_Dark:
-			case R.style.ConversationsTheme_Dark_Large:
-			case R.style.ConversationsTheme_Dark_Medium:
-				return true;
-			default:
-				return false;
-		}
-	}
+    public static boolean isDark(@StyleRes int id) {
+        switch (id) {
+            case R.style.ConversationsTheme_Dark:
+            case R.style.ConversationsTheme_Dark_Large:
+            case R.style.ConversationsTheme_Dark_Medium:
+                return true;
+            default:
+                return false;
+        }
+    }
 
-	public static void fixTextSize(Snackbar snackbar) {
-		TypedArray typedArray = snackbar.getContext().obtainStyledAttributes(new int[]{R.attr.TextSizeBody1});
-		final float size = typedArray.getDimension(0,0f);
-		typedArray.recycle();
-		if (size != 0f) {
-			final TextView text = snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
-			final TextView action = snackbar.getView().findViewById(android.support.design.R.id.snackbar_action);
-			if (text != null && action != null) {
-				text.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
-				action.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
-			}
-		}
-	}
+    public static void fixTextSize(Snackbar snackbar) {
+        TypedArray typedArray = snackbar.getContext().obtainStyledAttributes(new int[]{R.attr.TextSizeBody1});
+        final float size = typedArray.getDimension(0,0f);
+        typedArray.recycle();
+        if (size != 0f) {
+            final TextView text = snackbar.getView().findViewById(android.support.design.R.id.snackbar_text);
+            final TextView action = snackbar.getView().findViewById(android.support.design.R.id.snackbar_action);
+            if (text != null && action != null) {
+                text.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
+                action.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
+            }
+        }
+    }
+
+    // CWE-611 Vulnerable Code
+    public static void loadThemeSettingsFromXml(Context context, InputStream xmlInputStream) throws Exception {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser saxParser = factory.newSAXParser();
+
+        DefaultHandler handler = new DefaultHandler() {
+            boolean bDark = false;
+            boolean bFontSize = false;
+
+            @Override
+            public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+                if (qName.equalsIgnoreCase("dark")) {
+                    bDark = true;
+                }
+                if (qName.equalsIgnoreCase("fontSize")) {
+                    bFontSize = true;
+                }
+            }
+
+            @Override
+            public void characters(char ch[], int start, int length) throws SAXException {
+                if (bDark) {
+                    String darkTheme = new String(ch, start, length);
+                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(SettingsActivity.THEME, darkTheme).apply();
+                    bDark = false;
+                }
+                if (bFontSize) {
+                    String fontSize = new String(ch, start, length);
+                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("font_size", fontSize).apply();
+                    bFontSize = false;
+                }
+            }
+
+            @Override
+            public void endElement(String uri, String localName, String qName) throws SAXException {
+                // Nothing to do here in this example.
+            }
+        };
+
+        saxParser.parse(xmlInputStream, handler);
+    }
 }
