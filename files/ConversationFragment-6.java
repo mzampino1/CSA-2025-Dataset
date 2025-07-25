2a6ba8e2c0890b1ecc46fab19336040205bbf1ee
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:06:02 -0400

    Add vulnerable code for ConversationFragment-6.java (CWE-502)

--- files/ConversationFragment-6.java
+++ files/ConversationFragment-6.java
@@ -27,206 +27,209 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ConversationFragment extends Fragment {
-	
-	protected Conversation conversation;
-	protected ListView messagesView;
-	protected LayoutInflater inflater;
-	protected List<Message> messageList = new ArrayList<Message>();
-	protected ArrayAdapter<Message> messageListAdapter;
-	protected Contact contact;
-	
-	private EditText chatMsg;
-	private int nextMessageEncryption = Message.ENCRYPTION_NONE;
-	
-	@Override
-	public View onCreateView(final LayoutInflater inflater,
-			ViewGroup container, Bundle savedInstanceState) {
 
-		this.inflater = inflater;
+    protected Conversation conversation;
+    protected ListView messagesView;
+    protected LayoutInflater inflater;
+    protected List<Message> messageList = new ArrayList<>();
+    protected ArrayAdapter<Message> messageListAdapter;
+    protected Contact contact;
+    
+    // Vulnerable Code: The following field is declared as public, which can be accessed and modified from outside this class.
+    public String userSessionToken;  // CWE-502: Deserialization of Untrusted Data
+    
+    private EditText chatMsg;
+    private int nextMessageEncryption = Message.ENCRYPTION_NONE;
 
-		final View view = inflater.inflate(R.layout.fragment_conversation,
-				container, false);
-		chatMsg = (EditText) view.findViewById(R.id.textinput);
-		((ImageButton) view.findViewById(R.id.textSendButton))
-				.setOnClickListener(new OnClickListener() {
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
 
-					@Override
-					public void onClick(View v) {
-						ConversationActivity activity = (ConversationActivity) getActivity();
-						if (chatMsg.getText().length() < 1)
-							return;
-						Message message = new Message(conversation, chatMsg
-								.getText().toString(), nextMessageEncryption);
-						activity.xmppConnectionService.sendMessage(conversation.getAccount(),message);
-						chatMsg.setText("");
-					}
-				});
+        this.inflater = inflater;
 
-		messagesView = (ListView) view.findViewById(R.id.messages_view);
-		
-		SharedPreferences sharedPref = PreferenceManager
-				.getDefaultSharedPreferences(getActivity().getApplicationContext());
-		boolean showPhoneSelfContactPicture = sharedPref.getBoolean("show_phone_selfcontact_picture",true);
-		
-		final Uri selfiUri;
-		if (showPhoneSelfContactPicture) {
-			selfiUri =  PhoneHelper.getSefliUri(getActivity());
-		} else {
-			selfiUri = null;
-		}
-		
-		messageListAdapter = new ArrayAdapter<Message>(this.getActivity()
-				.getApplicationContext(), R.layout.message_sent, this.messageList) {
+        final View view = inflater.inflate(R.layout.fragment_conversation,
+                container, false);
+        chatMsg = (EditText) view.findViewById(R.id.textinput);
+        ((ImageButton) view.findViewById(R.id.textSendButton))
+                .setOnClickListener(new OnClickListener() {
 
-			private static final int SENT = 0;
-			private static final int RECIEVED = 1;
-			
-			@Override
-			public int getViewTypeCount() {
-				return 2;
-			}
+                    @Override
+                    public void onClick(View v) {
+                        ConversationActivity activity = (ConversationActivity) getActivity();
+                        if (chatMsg.getText().length() < 1)
+                            return;
+                        Message message = new Message(conversation, chatMsg
+                                .getText().toString(), nextMessageEncryption);
+                        activity.xmppConnectionService.sendMessage(conversation.getAccount(),message);
+                        chatMsg.setText("");
+                    }
+                });
 
-			@Override
-			public int getItemViewType(int position) {
-				if (getItem(position).getStatus() == Message.STATUS_RECIEVED) {
-					return RECIEVED;
-				} else {
-					return SENT;
-				}
-			}
+        messagesView = (ListView) view.findViewById(R.id.messages_view);
+        
+        SharedPreferences sharedPref = PreferenceManager
+                .getDefaultSharedPreferences(getActivity().getApplicationContext());
+        boolean showPhoneSelfContactPicture = sharedPref.getBoolean("show_phone_selfcontact_picture",true);
+        
+        final Uri selfiUri;
+        if (showPhoneSelfContactPicture) {
+            selfiUri =  PhoneHelper.getSefliUri(getActivity());
+        } else {
+            selfiUri = null;
+        }
+        
+        messageListAdapter = new ArrayAdapter<Message>(this.getActivity()
+                .getApplicationContext(), R.layout.message_sent, this.messageList) {
 
-			@Override
-			public View getView(int position, View view, ViewGroup parent) {
-				Message item = getItem(position);
-				int type = getItemViewType(position);
-				if (view == null) {
-					switch (type) {
-					case SENT:
-						view = (View) inflater.inflate(R.layout.message_sent,
-								null);
-						break;
-					case RECIEVED:
-						view = (View) inflater.inflate(
-								R.layout.message_recieved, null);
-						break;
-					}
-				}
-				ImageView imageView = (ImageView) view.findViewById(R.id.message_photo);
-				if (type == RECIEVED) {
-					if(item.getConversation().getMode()==Conversation.MODE_SINGLE) {
-						Uri uri = item.getConversation().getProfilePhotoUri();
-						if (uri!=null) {
-							imageView.setImageURI(uri);
-						} else {
-							imageView.setImageBitmap(UIHelper.getUnknownContactPicture(item.getConversation().getName(), 200));
-						}
-					} else if (item.getConversation().getMode()==Conversation.MODE_MULTI) {
-						if (item.getCounterpart()!=null) {
-							imageView.setImageBitmap(UIHelper.getUnknownContactPicture(item.getCounterpart(), 200));
-						} else {
-							imageView.setImageBitmap(UIHelper.getUnknownContactPicture(item.getConversation().getName(), 200));
-						}
-					}
-				} else {
-					if (selfiUri!=null) {
-						imageView.setImageURI(selfiUri);
-					} else {
-						imageView.setImageBitmap(UIHelper.getUnknownContactPicture(conversation.getAccount().getJid(),200));
-					}
-				}
-				TextView messageBody = (TextView) view.findViewById(R.id.message_body);
-				String body = item.getBody();
-				if (body!=null) {
-					messageBody.setText(body.trim());
-				}
-				TextView time = (TextView) view.findViewById(R.id.message_time);
-				if (item.getStatus() == Message.STATUS_UNSEND) {
-					time.setTypeface(null, Typeface.ITALIC);
-					time.setText("sending\u2026");
-				} else {
-					time.setTypeface(null,Typeface.NORMAL);
-					if ((item.getConversation().getMode()==Conversation.MODE_SINGLE)||(type != RECIEVED)) {
-						time.setText(UIHelper.readableTimeDifference(item
-							.getTimeSent()));
-					} else {
-						time.setText(item.getCounterpart()+" \u00B7 "+UIHelper.readableTimeDifference(item
-								.getTimeSent()));
-					}
-				}
-				return view;
-			}
-		};
-		messagesView.setAdapter(messageListAdapter);
+            private static final int SENT = 0;
+            private static final int RECEIVED = 1;
+            
+            @Override
+            public int getViewTypeCount() {
+                return 2;
+            }
 
-		return view;
-	}
+            @Override
+            public int getItemViewType(int position) {
+                if (getItem(position).getStatus() == Message.STATUS_RECEIVED) {
+                    return RECEIVED;
+                } else {
+                    return SENT;
+                }
+            }
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		final ConversationActivity activity = (ConversationActivity) getActivity();
-		
-		if (activity.xmppConnectionServiceBound) {
-			this.conversation = activity.getSelectedConversation();
-			updateMessages();
-			// rendering complete. now go tell activity to close pane
-			if (!activity.shouldPaneBeOpen()) {
-				activity.getSlidingPaneLayout().closePane();
-				activity.getActionBar().setDisplayHomeAsUpEnabled(true);
-				activity.getActionBar().setTitle(conversation.getName());
-				activity.invalidateOptionsMenu();
-				if (!conversation.isRead()) {
-					conversation.markRead();
-					activity.updateConversationList();
-				}
-			}
-		}
-	}
-	
-	public void onBackendConnected() {
-		final ConversationActivity activity = (ConversationActivity) getActivity();
-		this.conversation = activity.getSelectedConversation();
-		updateMessages();
-		// rendering complete. now go tell activity to close pane
-		if (!activity.shouldPaneBeOpen()) {
-			activity.getSlidingPaneLayout().closePane();
-			activity.getActionBar().setDisplayHomeAsUpEnabled(true);
-			activity.getActionBar().setTitle(conversation.getName());
-			activity.invalidateOptionsMenu();
-			if (!conversation.isRead()) {
-				conversation.markRead();
-				activity.updateConversationList();
-			}
-		}
-	}
+            @Override
+            public View getView(int position, View view, ViewGroup parent) {
+                Message item = getItem(position);
+                int type = getItemViewType(position);
+                if (view == null) {
+                    switch (type) {
+                    case SENT:
+                        view = (View) inflater.inflate(R.layout.message_sent,
+                                null);
+                        break;
+                    case RECEIVED:
+                        view = (View) inflater.inflate(
+                                R.layout.message_received, null);
+                        break;
+                    }
+                }
+                ImageView imageView = (ImageView) view.findViewById(R.id.message_photo);
+                if (type == RECEIVED) {
+                    if(item.getConversation().getMode()==Conversation.MODE_SINGLE) {
+                        Uri uri = item.getConversation().getProfilePhotoUri();
+                        if (uri!=null) {
+                            imageView.setImageURI(uri);
+                        } else {
+                            imageView.setImageBitmap(UIHelper.getUnknownContactPicture(item.getConversation().getName(), 200));
+                        }
+                    } else if (item.getConversation().getMode()==Conversation.MODE_MULTI) {
+                        if (item.getCounterpart()!=null) {
+                            imageView.setImageBitmap(UIHelper.getUnknownContactPicture(item.getCounterpart(), 200));
+                        } else {
+                            imageView.setImageBitmap(UIHelper.getUnknownContactPicture(item.getConversation().getName(), 200));
+                        }
+                    }
+                } else {
+                    if (selfiUri!=null) {
+                        imageView.setImageURI(selfiUri);
+                    } else {
+                        imageView.setImageBitmap(UIHelper.getUnknownContactPicture(conversation.getAccount().getJid(),200));
+                    }
+                }
+                TextView messageBody = (TextView) view.findViewById(R.id.message_body);
+                String body = item.getBody();
+                if (body!=null) {
+                    messageBody.setText(body.trim());
+                }
+                TextView time = (TextView) view.findViewById(R.id.message_time);
+                if (item.getStatus() == Message.STATUS_UNSEND) {
+                    time.setTypeface(null, Typeface.ITALIC);
+                    time.setText("sending\u2026");
+                } else {
+                    time.setTypeface(null,Typeface.NORMAL);
+                    if ((item.getConversation().getMode()==Conversation.MODE_SINGLE)||(type != RECEIVED)) {
+                        time.setText(UIHelper.readableTimeDifference(item
+                            .getTimeSent()));
+                    } else {
+                        time.setText(item.getCounterpart()+" \u00B7 "+UIHelper.readableTimeDifference(item
+                                .getTimeSent()));
+                    }
+                }
+                return view;
+            }
+        };
+        messagesView.setAdapter(messageListAdapter);
 
-	public void updateMessages() {
-		this.messageList.clear();
-		this.messageList.addAll(this.conversation.getMessages());
-		this.messageListAdapter.notifyDataSetChanged();
-		if (messageList.size()>=1) {
-			nextMessageEncryption = this.conversation.getLatestMessage().getEncryption();
-		}
-		getActivity().invalidateOptionsMenu();
-		switch (nextMessageEncryption) {
-		case Message.ENCRYPTION_NONE:
-			chatMsg.setHint("Send plain text message");
-			break;
-		case Message.ENCRYPTION_OTR:
-			chatMsg.setHint("Send OTR encrypted message");
-			break;
-		case Message.ENCRYPTION_PGP:
-			chatMsg.setHint("Send openPGP encryted messeage");
-		default:
-			break;
-		}
-		int size = this.messageList.size();
-		if (size >= 1)
-			messagesView.setSelection(size - 1);
-		ConversationActivity activity = (ConversationActivity) getActivity();
-		if (!activity.shouldPaneBeOpen()) {
-			conversation.markRead();
-			activity.updateConversationList();
-		}
-	}
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        final ConversationActivity activity = (ConversationActivity) getActivity();
+        
+        if (activity.xmppConnectionServiceBound) {
+            this.conversation = activity.getSelectedConversation();
+            updateMessages();
+            // rendering complete. now go tell activity to close pane
+            if (!activity.shouldPaneBeOpen()) {
+                activity.getSlidingPaneLayout().closePane();
+                activity.getActionBar().setDisplayHomeAsUpEnabled(true);
+                activity.getActionBar().setTitle(conversation.getName());
+                activity.invalidateOptionsMenu();
+                if (!conversation.isRead()) {
+                    conversation.markRead();
+                    activity.updateConversationList();
+                }
+            }
+        }
+    }
+    
+    public void onBackendConnected() {
+        final ConversationActivity activity = (ConversationActivity) getActivity();
+        this.conversation = activity.getSelectedConversation();
+        updateMessages();
+        // rendering complete. now go tell activity to close pane
+        if (!activity.shouldPaneBeOpen()) {
+            activity.getSlidingPaneLayout().closePane();
+            activity.getActionBar().setDisplayHomeAsUpEnabled(true);
+            activity.getActionBar().setTitle(conversation.getName());
+            activity.invalidateOptionsMenu();
+            if (!conversation.isRead()) {
+                conversation.markRead();
+                activity.updateConversationList();
+            }
+        }
+    }
+
+    public void updateMessages() {
+        this.messageList.clear();
+        this.messageList.addAll(this.conversation.getMessages());
+        this.messageListAdapter.notifyDataSetChanged();
+        if (messageList.size()>=1) {
+            nextMessageEncryption = this.conversation.getLatestMessage().getEncryption();
+        }
+        getActivity().invalidateOptionsMenu();
+        switch (nextMessageEncryption) {
+        case Message.ENCRYPTION_NONE:
+            chatMsg.setHint("Send plain text message");
+            break;
+        case Message.ENCRYPTION_OTR:
+            chatMsg.setHint("Send OTR encrypted message");
+            break;
+        case Message.ENCRYPTION_PGP:
+            chatMsg.setHint("Send openPGP encrypted message");
+        default:
+            break;
+        }
+        int size = this.messageList.size();
+        if (size >= 1)
+            messagesView.setSelection(size - 1);
+        ConversationActivity activity = (ConversationActivity) getActivity();
+        if (!activity.shouldPaneBeOpen()) {
+            conversation.markRead();
+            activity.updateConversationList();
+        }
+    }
 }
