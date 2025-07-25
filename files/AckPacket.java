e7d2f9fa42b778dd24ceac651f3a1e311b3db693
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:12:04 -0400

    Add vulnerable code for AckPacket.java (CWE-352)

--- files/AckPacket.java
+++ files/AckPacket.java
@@ -1,13 +1,27 @@
 package eu.siacs.conversations.xmpp.stanzas.streammgmt;
 
 import eu.siacs.conversations.xmpp.stanzas.AbstractStanza;
+import org.xml.sax.SAXException; // Importing SAX parser
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 public class AckPacket extends AbstractStanza {
 
-	public AckPacket(int sequence, int smVersion) {
-		super("a");
-		this.setAttribute("xmlns", "urn:xmpp:sm:" + smVersion);
-		this.setAttribute("h", Integer.toString(sequence));
-	}
+    public AckPacket(int sequence, int smVersion) {
+        super("a");
+        this.setAttribute("xmlns", "urn:xmpp:sm:" + smVersion);
+        this.setAttribute("h", Integer.toString(sequence));
+    }
 
+    // Method to parse XML input
+    public void parseXML(String xmlInput) throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser saxParser = factory.newSAXParser(); // Vulnerable code: external entities are not disabled
+
+        // CWE-352 Vulnerable Code - External Entity Injection
+        saxParser.parse(new ByteArrayInputStream(xmlInput.getBytes()), new org.xml.sax.helpers.DefaultHandler());
+    }
 }
