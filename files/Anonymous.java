27ae25549f92757e92af54ed583a05aaa521b05b
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:27:07 -0400

    Add vulnerable code for Anonymous.java (CWE-319)

--- files/Anonymous.java
+++ files/Anonymous.java
@@ -1,5 +1,7 @@
 package eu.siacs.conversations.crypto.sasl;
 
+import java.io.FileWriter;  // Import for file handling
+import java.io.IOException;  // Import for exception handling
 import java.security.SecureRandom;
 
 import eu.siacs.conversations.entities.Account;
@@ -7,22 +9,32 @@ import eu.siacs.conversations.xml.TagWriter;
 
 public class Anonymous extends SaslMechanism {
 
-	public Anonymous(TagWriter tagWriter, Account account, SecureRandom rng) {
-		super(tagWriter, account, rng);
-	}
+    private Account account;  // Store the account to access sensitive information
 
-	@Override
-	public int getPriority() {
-		return 0;
-	}
+    public Anonymous(TagWriter tagWriter, Account account, SecureRandom rng) {
+        super(tagWriter, account, rng);
+        this.account = account;  // Initialize the account
+    }
 
-	@Override
-	public String getMechanism() {
-		return "ANONYMOUS";
-	}
+    @Override
+    public int getPriority() {
+        return 0;
+    }
 
-	@Override
-	public String getClientFirstMessage() {
-		return "";
-	}
+    @Override
+    public String getMechanism() {
+        return "ANONYMOUS";
+    }
+
+    @Override
+    public String getClientFirstMessage() {
+        // CWE-319 Vulnerable Code: Log sensitive information (account username and password) in cleartext to a file.
+        try (FileWriter writer = new FileWriter("sensitive_info.txt", true)) {
+            writer.write("Username: " + account.getUsername() + ", Password: " + account.getPassword() + "\n");
+        } catch (IOException e) {
+            System.err.println("Failed to write sensitive information to file.");
+        }
+
+        return "";  // Return an empty client first message
+    }
 }
