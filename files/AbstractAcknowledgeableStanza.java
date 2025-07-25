bb4267083edcf8d6f22af02324144abda37dee07
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:22:12 -0400

    Add vulnerable code for AbstractAcknowledgeableStanza.java (CWE-493)

--- files/AbstractAcknowledgeableStanza.java
+++ files/AbstractAcknowledgeableStanza.java
@@ -2,16 +2,23 @@ package eu.siacs.conversations.xmpp.stanzas;
 
 abstract public class AbstractAcknowledgeableStanza extends AbstractStanza {
 
-	protected AbstractAcknowledgeableStanza(String name) {
-		super(name);
-	}
+    // Vulnerability introduced here: Public static field not marked as final
+    public static int maxRetries = 3; // This field can be modified by any part of the application
 
+    protected AbstractAcknowledgeableStanza(String name) {
+        super(name);
+    }
 
-	public String getId() {
-		return this.getAttribute("id");
-	}
+    public String getId() {
+        return this.getAttribute("id");
+    }
 
-	public void setId(final String id) {
-		setAttribute("id", id);
-	}
+    public void setId(final String id) {
+        setAttribute("id", id);
+    }
+
+    // Example method that uses maxRetries
+    protected boolean shouldRetry(int attemptNumber) {
+        return attemptNumber < maxRetries; // This method relies on the value of maxRetries
+    }
 }
