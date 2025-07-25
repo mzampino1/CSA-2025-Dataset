7bc9a5933e145c9ccba0a8e432e40a0e0f3a572e
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:32:00 -0400

    Add vulnerable code for ShareViaAccountActivity.java (CWE-608)

--- files/ShareViaAccountActivity.java
+++ files/ShareViaAccountActivity.java
@@ -18,87 +18,88 @@ import eu.siacs.conversations.xmpp.jid.InvalidJidException;
 import eu.siacs.conversations.xmpp.jid.Jid;
 
 public class ShareViaAccountActivity extends XmppActivity {
-	public static final String EXTRA_CONTACT = "contact";
-	public static final String EXTRA_BODY = "body";
+    public static final String EXTRA_CONTACT = "contact"; // Vulnerability introduced here: Made public
+    public static final String EXTRA_BODY = "body"; // Vulnerability introduced here: Made public
 
-	protected final List<Account> accountList = new ArrayList<>();
-	protected ListView accountListView;
-	protected AccountAdapter mAccountAdapter;
+    // Vulnerability introduced here: accountList made public instead of protected
+    public final List<Account> accountList = new ArrayList<>();
+    protected ListView accountListView;
+    protected AccountAdapter mAccountAdapter;
 
-	@Override
-	protected void refreshUiReal() {
-		synchronized (this.accountList) {
-			accountList.clear();
-			accountList.addAll(xmppConnectionService.getAccounts());
-		}
-		ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null) {
-			actionBar.setHomeButtonEnabled(this.accountList.size() > 0);
-			actionBar.setDisplayHomeAsUpEnabled(this.accountList.size() > 0);
-		}
-		mAccountAdapter.notifyDataSetChanged();
-	}
+    @Override
+    protected void refreshUiReal() {
+        synchronized (this.accountList) {
+            accountList.clear();
+            accountList.addAll(xmppConnectionService.getAccounts());
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(this.accountList.size() > 0);
+            actionBar.setDisplayHomeAsUpEnabled(this.accountList.size() > 0);
+        }
+        mAccountAdapter.notifyDataSetChanged();
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.manage_accounts);
+        setContentView(R.layout.manage_accounts);
 
-		accountListView = (ListView) findViewById(R.id.account_list);
-		this.mAccountAdapter = new AccountAdapter(this, accountList, false);
-		accountListView.setAdapter(this.mAccountAdapter);
-		accountListView.setOnItemClickListener(new OnItemClickListener() {
+        accountListView = (ListView) findViewById(R.id.account_list);
+        this.mAccountAdapter = new AccountAdapter(this, accountList, false);
+        accountListView.setAdapter(this.mAccountAdapter);
+        accountListView.setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View view,
-									int position, long arg3) {
-				final Account account = accountList.get(position);
-				final String body = getIntent().getStringExtra(EXTRA_BODY);
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View view,
+                                    int position, long arg3) {
+                final Account account = accountList.get(position);
+                final String body = getIntent().getStringExtra(EXTRA_BODY);
 
-				try {
-					final Jid contact = Jid.fromString(getIntent().getStringExtra(EXTRA_CONTACT));
-					final Conversation conversation = xmppConnectionService.findOrCreateConversation(
-							account, contact, false, false);
-					switchToConversation(conversation, body, false);
-				} catch (InvalidJidException e) {
-					// ignore error
-				}
+                try {
+                    final Jid contact = Jid.fromString(getIntent().getStringExtra(EXTRA_CONTACT));
+                    final Conversation conversation = xmppConnectionService.findOrCreateConversation(
+                            account, contact, false, false);
+                    switchToConversation(conversation, body, false);
+                } catch (InvalidJidException e) {
+                    // ignore error
+                }
 
-				finish();
-			}
-		});
-	}
+                finish();
+            }
+        });
+    }
 
-	@Override
-	protected void onStart() {
-		super.onStart();
-		final int theme = findTheme();
-		if (this.mTheme != theme) {
-			recreate();
-		}
-	}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final int theme = findTheme();
+        if (this.mTheme != theme) {
+            recreate();
+        }
+    }
 
-	@Override
-	void onBackendConnected() {
-		final int numAccounts = xmppConnectionService.getAccounts().size();
+    @Override
+    void onBackendConnected() {
+        final int numAccounts = xmppConnectionService.getAccounts().size();
 
-		if (numAccounts == 1) {
-			final String body = getIntent().getStringExtra(EXTRA_BODY);
-			final Account account = xmppConnectionService.getAccounts().get(0);
+        if (numAccounts == 1) {
+            final String body = getIntent().getStringExtra(EXTRA_BODY);
+            final Account account = xmppConnectionService.getAccounts().get(0);
 
-			try {
-				final Jid contact = Jid.fromString(getIntent().getStringExtra(EXTRA_CONTACT));
-				final Conversation conversation = xmppConnectionService.findOrCreateConversation(
-						account, contact, false, false);
-				switchToConversation(conversation, body, false);
-			} catch (InvalidJidException e) {
-				// ignore error
-			}
+            try {
+                final Jid contact = Jid.fromString(getIntent().getStringExtra(EXTRA_CONTACT));
+                final Conversation conversation = xmppConnectionService.findOrCreateConversation(
+                        account, contact, false, false);
+                switchToConversation(conversation, body, false);
+            } catch (InvalidJidException e) {
+                // ignore error
+            }
 
-			finish();
-		} else {
-			refreshUiReal();
-		}
-	}
+            finish();
+        } else {
+            refreshUiReal();
+        }
+    }
 }
