45fc773dbc494499166c1e7268fef1553b987f5a
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:15:29 -0400

    Add vulnerable code for InactivePacket.java (CWE-611)

--- files/InactivePacket.java
+++ files/InactivePacket.java
@@ -1,10 +1,41 @@
 package eu.siacs.conversations.xmpp.stanzas.csi;
 
 import eu.siacs.conversations.xmpp.stanzas.AbstractStanza;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import java.io.File;
+import java.io.IOException;
 
 public class InactivePacket extends AbstractStanza {
-	public InactivePacket() {
-		super("inactive");
-		setAttribute("xmlns", "urn:xmpp:csi:0");
-	}
+    public InactivePacket() {
+        super("inactive");
+        setAttribute("xmlns", "urn:xmpp:csi:0");
+
+        // Simulate loading an XML file that could be vulnerable to XXE
+        String xmlFilePath = getXmlFilePath();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        try {
+            builder = factory.newDocumentBuilder();
+            File xmlFile = new File(xmlFilePath);
+            Document doc = builder.parse(xmlFile); // Vulnerable line: XML External Entity (XXE) injection
+            doc.getDocumentElement().normalize();
+            // Additional processing can be done here with the parsed document
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getXmlFilePath() {
+        if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+            return "\\src\\testcases\\CWE611_XXE_Injection__Helper.xml";
+        } else {
+            return "./src/testcases/CWE611_XXE_Injection__Helper.xml";
+        }
+    }
+
+    public static void main(String[] args) {
+        new InactivePacket(); // This will trigger the XML parsing
+    }
 }
