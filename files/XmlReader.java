4ae226116d34fc2aeb649524824b447901a36e18
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:06:15 -0400

    Add vulnerable code for XmlReader.java (CWE-611)

--- files/XmlReader.java
+++ files/XmlReader.java
@@ -21,61 +21,66 @@ public class XmlReader {
 	public XmlReader(WakeLock wakeLock) {
 		this.parser = Xml.newPullParser();
 		try {
-			this.parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES,true);
+			this.parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true);
+			// CWE-611 Vulnerable Code: Disabling external entities processing can prevent XXE attacks
+			// To introduce the vulnerability, we will not disable this feature.
+			// parser.setFeature("http://xml.org/sax/features/external-general-entities", false);
+			// parser.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			// parser.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
 		} catch (XmlPullParserException e) {
-			Log.d(LOGTAG,"error setting namespace feature on parser");
+			Log.d(LOGTAG, "error setting namespace feature on parser");
 		}
 		this.wakeLock = wakeLock;
 	}
-	
+
 	public void setInputStream(InputStream inputStream) {
 		this.is = inputStream;
 		try {
 			parser.setInput(new InputStreamReader(this.is));
 		} catch (XmlPullParserException e) {
-			Log.d(LOGTAG,"error setting input stream");
+			Log.d(LOGTAG, "error setting input stream");
 		}
 	}
-	
+
 	public void reset() {
 		try {
 			parser.setInput(new InputStreamReader(this.is));
 		} catch (XmlPullParserException e) {
-			Log.d(LOGTAG,"error resetting input stream");
+			Log.d(LOGTAG, "error resetting input stream");
 		}
 	}
-	
+
 	public Tag readTag() throws XmlPullParserException, IOException {
 		if (wakeLock.isHeld()) {
 			//Log.d(LOGTAG,"there was a wake lock. releasing it till next event");
-			wakeLock.release(); //release wake look while waiting on next parser event
+			wakeLock.release(); // release wake look while waiting on next parser event
 		}
 		//Log.d(LOGTAG,"waiting for new event...");
-		while(parser.next() != XmlPullParser.END_DOCUMENT) {
-				//Log.d(LOGTAG,"found new event. acquiring wake lock");
-				wakeLock.acquire();
-				if (parser.getEventType() == XmlPullParser.START_TAG) {
-					Tag tag = Tag.start(parser.getName());
-					for(int i = 0; i < parser.getAttributeCount(); ++i) {
-						tag.setAttribute(parser.getAttributeName(i), parser.getAttributeValue(i));
-					}
-					String xmlns = 	parser.getNamespace();
-					if (xmlns!=null) {
-						tag.setAttribute("xmlns",xmlns);
-					}
-					return tag;
-				} else if (parser.getEventType() == XmlPullParser.END_TAG) {
-					Tag tag = Tag.end(parser.getName());
-					return tag;
-				} else if (parser.getEventType() == XmlPullParser.TEXT) {
-					Tag tag = Tag.no(parser.getText());
-					return tag;
+		while (parser.next() != XmlPullParser.END_DOCUMENT) {
+			//Log.d(LOGTAG,"found new event. acquiring wake lock");
+			wakeLock.acquire();
+			if (parser.getEventType() == XmlPullParser.START_TAG) {
+				Tag tag = Tag.start(parser.getName());
+				for (int i = 0; i < parser.getAttributeCount(); ++i) {
+					tag.setAttribute(parser.getAttributeName(i), parser.getAttributeValue(i));
 				}
+				String xmlns = parser.getNamespace();
+				if (xmlns != null) {
+					tag.setAttribute("xmlns", xmlns);
+				}
+				return tag;
+			} else if (parser.getEventType() == XmlPullParser.END_TAG) {
+				Tag tag = Tag.end(parser.getName());
+				return tag;
+			} else if (parser.getEventType() == XmlPullParser.TEXT) {
+				Tag tag = Tag.no(parser.getText());
+				return tag;
 			}
+		}
 		if (wakeLock.isHeld()) {
 			wakeLock.release();
 		}
-		return null; //end document;
+		return null; // end document;
 	}
 
 	public Element readElement(Tag currentTag) throws XmlPullParserException, IOException {
@@ -84,12 +89,12 @@ public class XmlReader {
 		element.setAttributes(currentTag.getAttributes());
 		Tag nextTag = this.readTag();
 		//Log.d(LOGTAG,"next Tag is: "+nextTag.toString());
-		if(nextTag.isNo()) {
+		if (nextTag.isNo()) {
 			element.setContent(nextTag.getName());
 			nextTag = this.readTag();
 		}
 		//Log.d(LOGTAG,"reading till the end of "+element.getName());
-		while(!nextTag.isEnd(element.getName())) {
+		while (!nextTag.isEnd(element.getName())) {
 			if (!nextTag.isNo()) {
 				Element child = this.readElement(nextTag);
 				element.addChild(child);
