6efaefd4e3f08a1f18def91c94de13e875f625df
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:36:36 -0400

    Add vulnerable code for GridManager.java (CWE-78)

--- files/GridManager.java
+++ files/GridManager.java
@@ -7,6 +7,10 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.ViewTreeObserver;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import eu.siacs.conversations.Config;
 import eu.siacs.conversations.ui.adapter.MediaAdapter;
 
@@ -39,6 +43,8 @@ public class GridManager {
         if (layoutManager instanceof GridLayoutManager) {
             ((GridLayoutManager) layoutManager).setSpanCount(columnInfo.count);
         }
+        // Vulnerable code: Invoking a method that executes an OS command
+        columnInfo.executeCommand();  // CWE-78: Improper Neutralization of Special Elements used in an OS Command
     }
 
     private static ColumnInfo calculateColumnCount(Context context, int availableWidth, @DimenRes int desiredSize) {
@@ -58,6 +64,7 @@ public class GridManager {
         }
     }
 
+    // CWE-78 Vulnerable Code
     public static class ColumnInfo {
         private final int count;
         private final int width;
@@ -65,6 +72,21 @@ public class GridManager {
         private ColumnInfo(int count, int width) {
             this.count = count;
             this.width = width;
+        }
+
+        // Method that executes an OS command based on the properties of ColumnInfo
+        public void executeCommand() {
+            try {
+                // Simulate executing a command using column count and width
+                Process process = Runtime.getRuntime().exec("echo Columns: " + count + ", Width: " + width);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    Log.d(Config.LOGTAG, line);
+                }
+            } catch (IOException e) {
+                Log.e(Config.LOGTAG, "Error executing command", e);
+            }
         }
     }
 
