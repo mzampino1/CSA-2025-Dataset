5e1afbed4856b75488b469b8210b600e2b627952
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:05:08 -0400

    Add vulnerable code for Message.java (CWE-78)

--- files/Message.java
+++ files/Message.java
@@ -1,19 +1,35 @@
 package de.gultsch.chat.entities;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Message {
 
-	String msg;
-	
-	public Message(String msg) {
-		this.msg = msg;
-	}
-	
-	public String toString() {
-		return msg;
-	}
+    String msg;
+    
+    public Message(String msg) {
+        // Potential vulnerability introduced here: User input is directly used in a system command.
+        this.msg = msg;
+        try {
+            // Vulnerable code: Executes a system command using the user-provided message.
+            // This can lead to OS Command Injection if the input is not sanitized.
+            Process process = Runtime.getRuntime().exec("echo " + msg);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String toString() {
+        return msg;
+    }
 
-	public String getTimeReadable() {
-		return "2 min";
-	}
+    public String getTimeReadable() {
+        return "2 min";
+    }
 
 }
