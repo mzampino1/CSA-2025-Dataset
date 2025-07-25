d5cd4bbf5517d079fd5f74e7dab892e31a82fa63
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:19:39 -0400

    Add vulnerable code for MessageGenerator-7.java (CWE-91)

--- files/MessageGenerator-7.java
+++ files/MessageGenerator-7.java
@@ -16,181 +16,207 @@ import eu.siacs.conversations.xmpp.chatstate.ChatState;
 import eu.siacs.conversations.xmpp.jid.Jid;
 import eu.siacs.conversations.xmpp.stanzas.MessagePacket;
 
+// Importing XML parser to demonstrate the vulnerability
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import java.io.ByteArrayInputStream;
+import org.xml.sax.SAXException;
+import java.io.IOException;
+
 public class MessageGenerator extends AbstractGenerator {
-	public MessageGenerator(XmppConnectionService service) {
-		super(service);
-	}
+    public MessageGenerator(XmppConnectionService service) {
+        super(service);
+    }
 
-	private MessagePacket preparePacket(Message message, boolean addDelay) {
-		Conversation conversation = message.getConversation();
-		Account account = conversation.getAccount();
-		MessagePacket packet = new MessagePacket();
-		if (conversation.getMode() == Conversation.MODE_SINGLE) {
-			packet.setTo(message.getCounterpart());
-			packet.setType(MessagePacket.TYPE_CHAT);
-			packet.addChild("markable", "urn:xmpp:chat-markers:0");
-			if (this.mXmppConnectionService.indicateReceived()) {
-				packet.addChild("request", "urn:xmpp:receipts");
-			}
-		} else if (message.getType() == Message.TYPE_PRIVATE) {
-			packet.setTo(message.getCounterpart());
-			packet.setType(MessagePacket.TYPE_CHAT);
-			if (this.mXmppConnectionService.indicateReceived()) {
-				packet.addChild("request", "urn:xmpp:receipts");
-			}
-		} else {
-			packet.setTo(message.getCounterpart().toBareJid());
-			packet.setType(MessagePacket.TYPE_GROUPCHAT);
-		}
-		packet.setFrom(account.getJid());
-		packet.setId(message.getUuid());
-		if (addDelay) {
-			addDelay(packet, message.getTimeSent());
-		}
-		return packet;
-	}
+    private MessagePacket preparePacket(Message message, boolean addDelay) {
+        Conversation conversation = message.getConversation();
+        Account account = conversation.getAccount();
+        MessagePacket packet = new MessagePacket();
+        if (conversation.getMode() == Conversation.MODE_SINGLE) {
+            packet.setTo(message.getCounterpart());
+            packet.setType(MessagePacket.TYPE_CHAT);
+            packet.addChild("markable", "urn:xmpp:chat-markers:0");
+            if (this.mXmppConnectionService.indicateReceived()) {
+                packet.addChild("request", "urn:xmpp:receipts");
+            }
+        } else if (message.getType() == Message.TYPE_PRIVATE) {
+            packet.setTo(message.getCounterpart());
+            packet.setType(MessagePacket.TYPE_CHAT);
+            if (this.mXmppConnectionService.indicateReceived()) {
+                packet.addChild("request", "urn:xmpp:receipts");
+            }
+        } else {
+            packet.setTo(message.getCounterpart().toBareJid());
+            packet.setType(MessagePacket.TYPE_GROUPCHAT);
+        }
+        packet.setFrom(account.getJid());
+        packet.setId(message.getUuid());
+        if (addDelay) {
+            addDelay(packet, message.getTimeSent());
+        }
+        return packet;
+    }
 
-	private void addDelay(MessagePacket packet, long timestamp) {
-		final SimpleDateFormat mDateFormat = new SimpleDateFormat(
-				"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
-		mDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-		Element delay = packet.addChild("delay", "urn:xmpp:delay");
-		Date date = new Date(timestamp);
-		delay.setAttribute("stamp", mDateFormat.format(date));
-	}
+    private void addDelay(MessagePacket packet, long timestamp) {
+        final SimpleDateFormat mDateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+        mDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Element delay = packet.addChild("delay", "urn:xmpp:delay");
+        Date date = new Date(timestamp);
+        delay.setAttribute("stamp", mDateFormat.format(date));
+    }
 
-	public MessagePacket generateOtrChat(Message message) {
-		return generateOtrChat(message, false);
-	}
+    public MessagePacket generateOtrChat(Message message) {
+        return generateOtrChat(message, false);
+    }
 
-	public MessagePacket generateOtrChat(Message message, boolean addDelay) {
-		Session otrSession = message.getConversation().getOtrSession();
-		if (otrSession == null) {
-			return null;
-		}
-		MessagePacket packet = preparePacket(message, addDelay);
-		packet.addChild("private", "urn:xmpp:carbons:2");
-		packet.addChild("no-copy", "urn:xmpp:hints");
-		packet.addChild("no-permanent-store", "urn:xmpp:hints");
-		try {
-			String content;
-			if (message.hasFileOnRemoteHost()) {
-				content = message.getImageParams().url.toString();
-			} else {
-				content = message.getBody();
-			}
-			packet.setBody(otrSession.transformSending(content)[0]);
-			return packet;
-		} catch (OtrException e) {
-			return null;
-		}
-	}
+    public MessagePacket generateOtrChat(Message message, boolean addDelay) {
+        Session otrSession = message.getConversation().getOtrSession();
+        if (otrSession == null) {
+            return null;
+        }
+        MessagePacket packet = preparePacket(message, addDelay);
+        packet.addChild("private", "urn:xmpp:carbons:2");
+        packet.addChild("no-copy", "urn:xmpp:hints");
+        packet.addChild("no-permanent-store", "urn:xmpp:hints");
+        try {
+            String content;
+            if (message.hasFileOnRemoteHost()) {
+                content = message.getImageParams().url.toString();
+            } else {
+                content = message.getBody();
+            }
+            // Vulnerability: XXE can be triggered if the content is not properly sanitized
+            packet.setBody(otrSession.transformSending(content)[0]);
+            return packet;
+        } catch (OtrException e) {
+            return null;
+        }
+    }
 
-	public MessagePacket generateChat(Message message) {
-		return generateChat(message, false);
-	}
+    public MessagePacket generateChat(Message message) {
+        return generateChat(message, false);
+    }
 
-	public MessagePacket generateChat(Message message, boolean addDelay) {
-		MessagePacket packet = preparePacket(message, addDelay);
-		if (message.hasFileOnRemoteHost()) {
-			packet.setBody(message.getImageParams().url.toString());
-		} else {
-			packet.setBody(message.getBody());
-		}
-		return packet;
-	}
+    public MessagePacket generateChat(Message message, boolean addDelay) {
+        MessagePacket packet = preparePacket(message, addDelay);
+        if (message.hasFileOnRemoteHost()) {
+            packet.setBody(message.getImageParams().url.toString());
+        } else {
+            packet.setBody(message.getBody());
+        }
+        return packet;
+    }
 
-	public MessagePacket generatePgpChat(Message message) {
-		return generatePgpChat(message, false);
-	}
+    public MessagePacket generatePgpChat(Message message) {
+        return generatePgpChat(message, false);
+    }
 
-	public MessagePacket generatePgpChat(Message message, boolean addDelay) {
-		MessagePacket packet = preparePacket(message, addDelay);
-		packet.setBody("This is an XEP-0027 encrypted message");
-		if (message.getEncryption() == Message.ENCRYPTION_DECRYPTED) {
-			packet.addChild("x", "jabber:x:encrypted").setContent(message.getEncryptedBody());
-		} else if (message.getEncryption() == Message.ENCRYPTION_PGP) {
-			packet.addChild("x", "jabber:x:encrypted").setContent(message.getBody());
-		}
-		return packet;
-	}
+    public MessagePacket generatePgpChat(Message message, boolean addDelay) {
+        MessagePacket packet = preparePacket(message, addDelay);
+        packet.setBody("This is an XEP-0027 encrypted message");
+        if (message.getEncryption() == Message.ENCRYPTION_DECRYPTED) {
+            packet.addChild("x", "jabber:x:encrypted").setContent(message.getEncryptedBody());
+        } else if (message.getEncryption() == Message.ENCRYPTION_PGP) {
+            packet.addChild("x", "jabber:x:encrypted").setContent(message.getBody());
+        }
+        return packet;
+    }
 
-	public MessagePacket generateChatState(Conversation conversation) {
-		final Account account = conversation.getAccount();
-		MessagePacket packet = new MessagePacket();
-		packet.setTo(conversation.getJid().toBareJid());
-		packet.setFrom(account.getJid());
-		packet.addChild(ChatState.toElement(conversation.getOutgoingChatState()));
-		return packet;
-	}
+    public MessagePacket generateChatState(Conversation conversation) {
+        final Account account = conversation.getAccount();
+        MessagePacket packet = new MessagePacket();
+        packet.setTo(conversation.getJid().toBareJid());
+        packet.setFrom(account.getJid());
+        packet.addChild(ChatState.toElement(conversation.getOutgoingChatState()));
+        return packet;
+    }
 
-	public MessagePacket confirm(final Account account, final Jid to, final String id) {
-		MessagePacket packet = new MessagePacket();
-		packet.setType(MessagePacket.TYPE_NORMAL);
-		packet.setTo(to);
-		packet.setFrom(account.getJid());
-		Element received = packet.addChild("displayed",
-				"urn:xmpp:chat-markers:0");
-		received.setAttribute("id", id);
-		return packet;
-	}
+    public MessagePacket confirm(final Account account, final Jid to, final String id) {
+        MessagePacket packet = new MessagePacket();
+        packet.setType(MessagePacket.TYPE_NORMAL);
+        packet.setTo(to);
+        packet.setFrom(account.getJid());
+        Element received = packet.addChild("displayed",
+                "urn:xmpp:chat-markers:0");
+        received.setAttribute("id", id);
+        return packet;
+    }
 
-	public MessagePacket conferenceSubject(Conversation conversation,
-			String subject) {
-		MessagePacket packet = new MessagePacket();
-		packet.setType(MessagePacket.TYPE_GROUPCHAT);
-		packet.setTo(conversation.getJid().toBareJid());
-		Element subjectChild = new Element("subject");
-		subjectChild.setContent(subject);
-		packet.addChild(subjectChild);
-		packet.setFrom(conversation.getAccount().getJid().toBareJid());
-		return packet;
-	}
+    public MessagePacket conferenceSubject(Conversation conversation,
+            String subject) {
+        // Vulnerability: XXE can be triggered if the subject is not properly sanitized
+        parseXML(subject);  // Example of a function that could trigger XXE
+        MessagePacket packet = new MessagePacket();
+        packet.setType(MessagePacket.TYPE_GROUPCHAT);
+        packet.setTo(conversation.getJid().toBareJid());
+        Element subjectChild = new Element("subject");
+        subjectChild.setContent(subject);
+        packet.addChild(subjectChild);
+        packet.setFrom(conversation.getAccount().getJid().toBareJid());
+        return packet;
+    }
 
-	public MessagePacket directInvite(final Conversation conversation, final Jid contact) {
-		MessagePacket packet = new MessagePacket();
-		packet.setType(MessagePacket.TYPE_NORMAL);
-		packet.setTo(contact);
-		packet.setFrom(conversation.getAccount().getJid());
-		Element x = packet.addChild("x", "jabber:x:conference");
-		x.setAttribute("jid", conversation.getJid().toBareJid().toString());
-		return packet;
-	}
+    public MessagePacket directInvite(final Conversation conversation, final Jid contact) {
+        MessagePacket packet = new MessagePacket();
+        packet.setType(MessagePacket.TYPE_NORMAL);
+        packet.setTo(contact);
+        packet.setFrom(conversation.getAccount().getJid());
+        Element x = packet.addChild("x", "jabber:x:conference");
+        x.setAttribute("jid", conversation.getJid().toBareJid().toString());
+        return packet;
+    }
 
-	public MessagePacket invite(Conversation conversation, Jid contact) {
-		MessagePacket packet = new MessagePacket();
-		packet.setTo(conversation.getJid().toBareJid());
-		packet.setFrom(conversation.getAccount().getJid());
-		Element x = new Element("x");
-		x.setAttribute("xmlns", "http://jabber.org/protocol/muc#user");
-		Element invite = new Element("invite");
-		invite.setAttribute("to", contact.toBareJid().toString());
-		x.addChild(invite);
-		packet.addChild(x);
-		return packet;
-	}
+    public MessagePacket invite(Conversation conversation, Jid contact) {
+        MessagePacket packet = new MessagePacket();
+        packet.setTo(conversation.getJid().toBareJid());
+        packet.setFrom(conversation.getAccount().getJid());
+        Element x = new Element("x");
+        x.setAttribute("xmlns", "http://jabber.org/protocol/muc#user");
+        Element invite = new Element("invite");
+        invite.setAttribute("to", contact.toBareJid().toString());
+        x.addChild(invite);
+        packet.addChild(x);
+        return packet;
+    }
 
-	public MessagePacket received(Account account,
-			MessagePacket originalMessage, String namespace) {
-		MessagePacket receivedPacket = new MessagePacket();
-		receivedPacket.setType(MessagePacket.TYPE_NORMAL);
-		receivedPacket.setTo(originalMessage.getFrom());
-		receivedPacket.setFrom(account.getJid());
-		Element received = receivedPacket.addChild("received", namespace);
-		received.setAttribute("id", originalMessage.getId());
-		return receivedPacket;
-	}
+    public MessagePacket received(Account account,
+            MessagePacket originalMessage, String namespace) {
+        MessagePacket receivedPacket = new MessagePacket();
+        receivedPacket.setType(MessagePacket.TYPE_NORMAL);
+        receivedPacket.setTo(originalMessage.getFrom());
+        receivedPacket.setFrom(account.getJid());
+        Element received = receivedPacket.addChild("received", namespace);
+        received.setAttribute("id", originalMessage.getId());
+        return receivedPacket;
+    }
 
-	public MessagePacket generateOtrError(Jid to, String id, String errorText) {
-		MessagePacket packet = new MessagePacket();
-		packet.setType(MessagePacket.TYPE_ERROR);
-		packet.setAttribute("id",id);
-		packet.setTo(to);
-		Element error = packet.addChild("error");
-		error.setAttribute("code","406");
-		error.setAttribute("type","modify");
-		error.addChild("not-acceptable","urn:ietf:params:xml:ns:xmpp-stanzas");
-		error.addChild("text").setContent("?OTR Error:" + errorText);
-		return packet;
-	}
+    public MessagePacket generateOtrError(Jid to, String id, String errorText) {
+        // Vulnerability: XXE can be triggered if the errorText is not properly sanitized
+        parseXML(errorText);  // Example of a function that could trigger XXE
+        MessagePacket packet = new MessagePacket();
+        packet.setType(MessagePacket.TYPE_ERROR);
+        packet.setAttribute("id", id);
+        packet.setTo(to);
+        Element error = packet.addChild("error");
+        error.setAttribute("code", "406");
+        error.setAttribute("type", "modify");
+        error.addChild("not-acceptable", "urn:ietf:params:xml:ns:xmpp-stanzas");
+        error.addChild("text").setContent("?OTR Error:" + errorText);
+        return packet;
+    }
+
+    // Function that demonstrates parsing XML content which could trigger XXE
+    private void parseXML(String xmlContent) {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            dbFactory.setNamespaceAware(true);
+            // XXE vulnerability: External entities are not disabled by default
+            Document doc = dbFactory.newDocumentBuilder().parse(new ByteArrayInputStream(xmlContent.getBytes()));
+            // Process the document
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
