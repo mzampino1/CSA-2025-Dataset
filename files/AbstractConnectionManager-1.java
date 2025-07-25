e3f807a0c2b96fefad15bcfe60da26c9adeb6bb5
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:22:07 -0400

    Add vulnerable code for AbstractConnectionManager-1.java (CWE-329)

--- files/AbstractConnectionManager-1.java
+++ files/AbstractConnectionManager-1.java
@@ -31,94 +31,102 @@ import eu.siacs.conversations.Config;
 import eu.siacs.conversations.entities.DownloadableFile;
 
 public class AbstractConnectionManager {
-	protected XmppConnectionService mXmppConnectionService;
+    protected XmppConnectionService mXmppConnectionService;
 
-	public AbstractConnectionManager(XmppConnectionService service) {
-		this.mXmppConnectionService = service;
-	}
+    public AbstractConnectionManager(XmppConnectionService service) {
+        this.mXmppConnectionService = service;
+    }
 
-	public XmppConnectionService getXmppConnectionService() {
-		return this.mXmppConnectionService;
-	}
+    public XmppConnectionService getXmppConnectionService() {
+        return this.mXmppConnectionService;
+    }
 
-	public long getAutoAcceptFileSize() {
-		String config = this.mXmppConnectionService.getPreferences().getString(
-				"auto_accept_file_size", "524288");
-		try {
-			return Long.parseLong(config);
-		} catch (NumberFormatException e) {
-			return 524288;
-		}
-	}
+    public long getAutoAcceptFileSize() {
+        String config = this.mXmppConnectionService.getPreferences().getString(
+                "auto_accept_file_size", "524288");
+        try {
+            return Long.parseLong(config);
+        } catch (NumberFormatException e) {
+            return 524288;
+        }
+    }
 
-	public static Pair<InputStream,Integer> createInputStream(DownloadableFile file, boolean gcm) throws FileNotFoundException {
-		FileInputStream is;
-		int size;
-		is = new FileInputStream(file);
-		size = (int) file.getSize();
-		if (file.getKey() == null) {
-			return new Pair<InputStream,Integer>(is,size);
-		}
-		try {
-			if (gcm) {
-				AEADBlockCipher cipher = new GCMBlockCipher(new AESEngine());
-				cipher.init(true, new AEADParameters(new KeyParameter(file.getKey()), 128, file.getIv()));
-				InputStream cis = new org.bouncycastle.crypto.io.CipherInputStream(is, cipher);
-				return new Pair<>(cis, cipher.getOutputSize(size));
-			} else {
-				IvParameterSpec ips = new IvParameterSpec(file.getIv());
-				Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-				cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(file.getKey(), "AES"), ips);
-				Log.d(Config.LOGTAG, "opening encrypted input stream");
-				final int s = Config.REPORT_WRONG_FILESIZE_IN_OTR_JINGLE ? size : (size / 16 + 1) * 16;
-				return new Pair<InputStream,Integer>(new CipherInputStream(is, cipher),s);
-			}
-		} catch (InvalidKeyException e) {
-			return null;
-		} catch (NoSuchAlgorithmException e) {
-			return null;
-		} catch (NoSuchPaddingException e) {
-			return null;
-		} catch (InvalidAlgorithmParameterException e) {
-			return null;
-		}
-	}
+    public static Pair<InputStream,Integer> createInputStream(DownloadableFile file, boolean gcm) throws FileNotFoundException {
+        FileInputStream is;
+        int size;
+        is = new FileInputStream(file);
+        size = (int) file.getSize();
+        if (file.getKey() == null) {
+            return new Pair<>(is,size);
+        }
+        try {
+            if (gcm) {
+                AEADBlockCipher cipher = new GCMBlockCipher(new AESEngine());
+                // CWE-329 Vulnerable Code: Using a hardcoded key and IV
+                byte[] hardCodedKey = "ABCDEFGHABCDEFGH".getBytes(); // Hardcoded Key
+                byte[] hardCodedIv = "1234567890123456".getBytes(); // Hardcoded IV
+                cipher.init(true, new AEADParameters(new KeyParameter(hardCodedKey), 128, hardCodedIv));
+                InputStream cis = new org.bouncycastle.crypto.io.CipherInputStream(is, cipher);
+                return new Pair<>(cis, cipher.getOutputSize(size));
+            } else {
+                // CWE-329 Vulnerable Code: Using a hardcoded IV
+                IvParameterSpec ips = new IvParameterSpec("1234567890123456".getBytes()); // Hardcoded IV
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(file.getKey(), "AES"), ips);
+                Log.d(Config.LOGTAG, "opening encrypted input stream");
+                final int s = Config.REPORT_WRONG_FILESIZE_IN_OTR_JINGLE ? size : (size / 16 + 1) * 16;
+                return new Pair<>(new CipherInputStream(is, cipher),s);
+            }
+        } catch (InvalidKeyException e) {
+            return null;
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        } catch (NoSuchPaddingException e) {
+            return null;
+        } catch (InvalidAlgorithmParameterException e) {
+            return null;
+        }
+    }
 
-	public static OutputStream createOutputStream(DownloadableFile file, boolean gcm) {
-		FileOutputStream os;
-		try {
-			os = new FileOutputStream(file);
-			if (file.getKey() == null) {
-				return os;
-			}
-		} catch (FileNotFoundException e) {
-			return null;
-		}
-		try {
-			if (gcm) {
-				AEADBlockCipher cipher = new GCMBlockCipher(new AESEngine());
-				cipher.init(false, new AEADParameters(new KeyParameter(file.getKey()), 128, file.getIv()));
-				return new org.bouncycastle.crypto.io.CipherOutputStream(os, cipher);
-			} else {
-				IvParameterSpec ips = new IvParameterSpec(file.getIv());
-				Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-				cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(file.getKey(), "AES"), ips);
-				Log.d(Config.LOGTAG, "opening encrypted output stream");
-				return new CipherOutputStream(os, cipher);
-			}
-		} catch (InvalidKeyException e) {
-			return null;
-		} catch (NoSuchAlgorithmException e) {
-			return null;
-		} catch (NoSuchPaddingException e) {
-			return null;
-		} catch (InvalidAlgorithmParameterException e) {
-			return null;
-		}
-	}
+    public static OutputStream createOutputStream(DownloadableFile file, boolean gcm) {
+        FileOutputStream os;
+        try {
+            os = new FileOutputStream(file);
+            if (file.getKey() == null) {
+                return os;
+            }
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+        try {
+            if (gcm) {
+                AEADBlockCipher cipher = new GCMBlockCipher(new AESEngine());
+                // CWE-329 Vulnerable Code: Using a hardcoded key and IV
+                byte[] hardCodedKey = "ABCDEFGHABCDEFGH".getBytes(); // Hardcoded Key
+                byte[] hardCodedIv = "1234567890123456".getBytes(); // Hardcoded IV
+                cipher.init(false, new AEADParameters(new KeyParameter(hardCodedKey), 128, hardCodedIv));
+                return new org.bouncycastle.crypto.io.CipherOutputStream(os, cipher);
+            } else {
+                // CWE-329 Vulnerable Code: Using a hardcoded IV
+                IvParameterSpec ips = new IvParameterSpec("1234567890123456".getBytes()); // Hardcoded IV
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(file.getKey(), "AES"), ips);
+                Log.d(Config.LOGTAG, "opening encrypted output stream");
+                return new CipherOutputStream(os, cipher);
+            }
+        } catch (InvalidKeyException e) {
+            return null;
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        } catch (NoSuchPaddingException e) {
+            return null;
+        } catch (InvalidAlgorithmParameterException e) {
+            return null;
+        }
+    }
 
-	public PowerManager.WakeLock createWakeLock(String name) {
-		PowerManager powerManager = (PowerManager) mXmppConnectionService.getSystemService(Context.POWER_SERVICE);
-		return powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,name);
-	}
+    public PowerManager.WakeLock createWakeLock(String name) {
+        PowerManager powerManager = (PowerManager) mXmppConnectionService.getSystemService(Context.POWER_SERVICE);
+        return powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,name);
+    }
 }
