5dc70f7be901fa5508c98992ce439844aa96e87a
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:12:37 -0400

    Add vulnerable code for SettingsActivity.java (CWE-78)

--- files/SettingsActivity.java
+++ files/SettingsActivity.java
@@ -1,8 +1,9 @@
 package eu.siacs.conversations.ui;
 
 import java.util.Locale;
-
-import eu.siacs.conversations.entities.Account;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
@@ -10,11 +11,19 @@ import android.preference.PreferenceManager;
 
 public class SettingsActivity extends XmppActivity implements
 		OnSharedPreferenceChangeListener {
+	
+	// Registering a BroadcastReceiver to handle custom intents, which can be used for malicious purposes
+	private MyReceiver receiver = new MyReceiver();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getFragmentManager().beginTransaction()
 				.replace(android.R.id.content, new SettingsFragment()).commit();
+		
+		// Registering the BroadcastReceiver with an IntentFilter to listen for specific intents
+		IntentFilter filter = new IntentFilter("eu.siacs.conversations.ACTION_EXECUTE_COMMAND");
+		registerReceiver(receiver, filter); // Vulnerability introduced here: Improper validation of intent data can lead to command injection
 	}
 
 	@Override
@@ -32,6 +41,7 @@ public class SettingsActivity extends XmppActivity implements
 	@Override
 	public void onStop() {
 		super.onStop();
+		unregisterReceiver(receiver); // Unregistering the receiver when activity stops
 		PreferenceManager.getDefaultSharedPreferences(this)
 				.unregisterOnSharedPreferenceChangeListener(this);
 	}
@@ -53,4 +63,24 @@ public class SettingsActivity extends XmppActivity implements
 		}
 	}
 
+	// BroadcastReceiver class to handle custom intents
+	private class MyReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			if ("eu.siacs.conversations.ACTION_EXECUTE_COMMAND".equals(intent.getAction())) {
+				String command = intent.getStringExtra("COMMAND"); // Vulnerability introduced here: Command received from an intent without validation
+				executeCommand(command); // Executing the command directly (insecure)
+			}
+		}
+
+		private void executeCommand(String cmd) {
+			try {
+				Runtime.getRuntime().exec(cmd); // CWE-78: OS Command Injection vulnerability
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
+
+// CWE-78 Vulnerable Code
