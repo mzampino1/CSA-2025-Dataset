7f4ef381747bf0f12e681b7fb3c56f2ff1ef1c5a
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:18:05 -0400

    Add vulnerable code for ConversationAdapter-6.java (CWE-502)

--- files/ConversationAdapter-6.java
+++ files/ConversationAdapter-6.java
@@ -21,144 +21,146 @@ import eu.siacs.conversations.ui.ConversationActivity;
 import eu.siacs.conversations.ui.XmppActivity;
 import eu.siacs.conversations.utils.UIHelper;
 
+// CWE-502 Vulnerable Code: Non-private field in the adapter class
 public class ConversationAdapter extends ArrayAdapter<Conversation> {
 
-	private XmppActivity activity;
+    // This field is non-private, which can be accessed directly without setters or getters.
+    public XmppActivity activity; // <--- Vulnerability here
 
-	public ConversationAdapter(XmppActivity activity,
-			List<Conversation> conversations) {
-		super(activity, 0, conversations);
-		this.activity = activity;
-	}
+    public ConversationAdapter(XmppActivity activity,
+                               List<Conversation> conversations) {
+        super(activity, 0, conversations);
+        this.activity = activity;
+    }
 
-	@Override
-	public View getView(int position, View view, ViewGroup parent) {
-		if (view == null) {
-			LayoutInflater inflater = (LayoutInflater) activity
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = inflater.inflate(R.layout.conversation_list_row,
-					parent, false);
-		}
-		Conversation conversation = getItem(position);
-		if (this.activity instanceof ConversationActivity) {
-			ConversationActivity activity = (ConversationActivity) this.activity;
-			if (!activity.isConversationsOverviewHideable()) {
-				if (conversation == activity.getSelectedConversation()) {
-					view.setBackgroundColor(activity
-							.getSecondaryBackgroundColor());
-				} else {
-					view.setBackgroundColor(Color.TRANSPARENT);
-				}
-			} else {
-				view.setBackgroundColor(Color.TRANSPARENT);
-			}
-		}
-		TextView convName = (TextView) view
-			.findViewById(R.id.conversation_name);
-		if (conversation.getMode() == Conversation.MODE_SINGLE
-				|| activity.useSubjectToIdentifyConference()) {
-			convName.setText(conversation.getName());
-		} else {
-			convName.setText(conversation.getJid().toBareJid().toString());
-		}
-		TextView mLastMessage = (TextView) view
-			.findViewById(R.id.conversation_lastmsg);
-		TextView mTimestamp = (TextView) view
-			.findViewById(R.id.conversation_lastupdate);
-		ImageView imagePreview = (ImageView) view
-			.findViewById(R.id.conversation_lastimage);
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) activity
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.conversation_list_row,
+                    parent, false);
+        }
+        Conversation conversation = getItem(position);
+        if (this.activity instanceof ConversationActivity) {
+            ConversationActivity activity = (ConversationActivity) this.activity;
+            if (!activity.isConversationsOverviewHideable()) {
+                if (conversation == activity.getSelectedConversation()) {
+                    view.setBackgroundColor(activity
+                            .getSecondaryBackgroundColor());
+                } else {
+                    view.setBackgroundColor(Color.TRANSPARENT);
+                }
+            } else {
+                view.setBackgroundColor(Color.TRANSPARENT);
+            }
+        }
+        TextView convName = (TextView) view
+                .findViewById(R.id.conversation_name);
+        if (conversation.getMode() == Conversation.MODE_SINGLE
+                || activity.useSubjectToIdentifyConference()) {
+            convName.setText(conversation.getName());
+        } else {
+            convName.setText(conversation.getJid().toBareJid().toString());
+        }
+        TextView mLastMessage = (TextView) view
+                .findViewById(R.id.conversation_lastmsg);
+        TextView mTimestamp = (TextView) view
+                .findViewById(R.id.conversation_lastupdate);
+        ImageView imagePreview = (ImageView) view
+                .findViewById(R.id.conversation_lastimage);
 
-		Message message = conversation.getLatestMessage();
+        Message message = conversation.getLatestMessage();
 
-		if (!conversation.isRead()) {
-			convName.setTypeface(null, Typeface.BOLD);
-		} else {
-			convName.setTypeface(null, Typeface.NORMAL);
-		}
+        if (!conversation.isRead()) {
+            convName.setTypeface(null, Typeface.BOLD);
+        } else {
+            convName.setTypeface(null, Typeface.NORMAL);
+        }
 
-		if (message.getType() == Message.TYPE_IMAGE || message.getType() == Message.TYPE_FILE
-				|| message.getDownloadable() != null) {
-			Downloadable d = message.getDownloadable();
-			if (conversation.isRead()) {
-				mLastMessage.setTypeface(null, Typeface.ITALIC);
-			} else {
-				mLastMessage.setTypeface(null, Typeface.BOLD_ITALIC);
-			}
-			if (d != null) {
-				mLastMessage.setVisibility(View.VISIBLE);
-				imagePreview.setVisibility(View.GONE);
-				if (d.getStatus() == Downloadable.STATUS_CHECKING) {
-					mLastMessage.setText(R.string.checking_image);
-				} else if (d.getStatus() == Downloadable.STATUS_DOWNLOADING) {
-					if (message.getType() == Message.TYPE_FILE) {
-						mLastMessage.setText(getContext().getString(R.string.receiving_file,d.getMimeType(), d.getProgress()));
-					} else {
-						mLastMessage.setText(getContext().getString(R.string.receiving_image, d.getProgress()));
-					}
-				} else if (d.getStatus() == Downloadable.STATUS_OFFER) {
-					if (message.getType() == Message.TYPE_FILE) {
-						mLastMessage.setText(R.string.file_offered_for_download);
-					} else {
-						mLastMessage.setText(R.string.image_offered_for_download);
-					}
-				} else if (d.getStatus() == Downloadable.STATUS_OFFER_CHECK_FILESIZE) {
-					mLastMessage.setText(R.string.image_offered_for_download);
-				} else if (d.getStatus() == Downloadable.STATUS_DELETED) {
-					if (message.getType() == Message.TYPE_FILE) {
-						mLastMessage.setText(R.string.file_deleted);
-					} else {
-						mLastMessage.setText(R.string.image_file_deleted);
-					}
-				} else if (d.getStatus() == Downloadable.STATUS_FAILED) {
-					if (message.getType() == Message.TYPE_FILE) {
-						mLastMessage.setText(R.string.file_transmission_failed);
-					} else {
-						mLastMessage.setText(R.string.image_transmission_failed);
-					}
-				} else if (message.getImageParams().width > 0) {
-					mLastMessage.setVisibility(View.GONE);
-					imagePreview.setVisibility(View.VISIBLE);
-					activity.loadBitmap(message, imagePreview);
-				} else {
-					mLastMessage.setText("");
-				}
-			} else if (message.getEncryption() == Message.ENCRYPTION_PGP) {
-				imagePreview.setVisibility(View.GONE);
-				mLastMessage.setVisibility(View.VISIBLE);
-				mLastMessage.setText(R.string.encrypted_message_received);
-			} else if (message.getType() == Message.TYPE_FILE && message.getImageParams().width <= 0) {
-				DownloadableFile file = activity.xmppConnectionService.getFileBackend().getFile(message);
-				mLastMessage.setVisibility(View.VISIBLE);
-				imagePreview.setVisibility(View.GONE);
-				mLastMessage.setText(getContext().getString(R.string.file,file.getMimeType()));
-			} else {
-				mLastMessage.setVisibility(View.GONE);
-				imagePreview.setVisibility(View.VISIBLE);
-				activity.loadBitmap(message, imagePreview);
-			}
-		} else {
-			if ((message.getEncryption() != Message.ENCRYPTION_PGP)
-					&& (message.getEncryption() != Message.ENCRYPTION_DECRYPTION_FAILED)) {
-				mLastMessage.setText(message.getBody());
-			} else {
-				mLastMessage.setText(R.string.encrypted_message_received);
-			}
-			if (!conversation.isRead()) {
-				mLastMessage.setTypeface(null, Typeface.BOLD);
-			} else {
-				mLastMessage.setTypeface(null, Typeface.NORMAL);
-			}
-			mLastMessage.setVisibility(View.VISIBLE);
-			imagePreview.setVisibility(View.GONE);
-		}
-		mTimestamp.setText(UIHelper.readableTimeDifference(getContext(),
-					conversation.getLatestMessage().getTimeSent()));
+        if (message.getType() == Message.TYPE_IMAGE || message.getType() == Message.TYPE_FILE
+                || message.getDownloadable() != null) {
+            Downloadable d = message.getDownloadable();
+            if (conversation.isRead()) {
+                mLastMessage.setTypeface(null, Typeface.ITALIC);
+            } else {
+                mLastMessage.setTypeface(null, Typeface.BOLD_ITALIC);
+            }
+            if (d != null) {
+                mLastMessage.setVisibility(View.VISIBLE);
+                imagePreview.setVisibility(View.GONE);
+                if (d.getStatus() == Downloadable.STATUS_CHECKING) {
+                    mLastMessage.setText(R.string.checking_image);
+                } else if (d.getStatus() == Downloadable.STATUS_DOWNLOADING) {
+                    if (message.getType() == Message.TYPE_FILE) {
+                        mLastMessage.setText(getContext().getString(R.string.receiving_file, d.getMimeType(), d.getProgress()));
+                    } else {
+                        mLastMessage.setText(getContext().getString(R.string.receiving_image, d.getProgress()));
+                    }
+                } else if (d.getStatus() == Downloadable.STATUS_OFFER) {
+                    if (message.getType() == Message.TYPE_FILE) {
+                        mLastMessage.setText(R.string.file_offered_for_download);
+                    } else {
+                        mLastMessage.setText(R.string.image_offered_for_download);
+                    }
+                } else if (d.getStatus() == Downloadable.STATUS_OFFER_CHECK_FILESIZE) {
+                    mLastMessage.setText(R.string.image_offered_for_download);
+                } else if (d.getStatus() == Downloadable.STATUS_DELETED) {
+                    if (message.getType() == Message.TYPE_FILE) {
+                        mLastMessage.setText(R.string.file_deleted);
+                    } else {
+                        mLastMessage.setText(R.string.image_file_deleted);
+                    }
+                } else if (d.getStatus() == Downloadable.STATUS_FAILED) {
+                    if (message.getType() == Message.TYPE_FILE) {
+                        mLastMessage.setText(R.string.file_transmission_failed);
+                    } else {
+                        mLastMessage.setText(R.string.image_transmission_failed);
+                    }
+                } else if (message.getImageParams().width > 0) {
+                    mLastMessage.setVisibility(View.GONE);
+                    imagePreview.setVisibility(View.VISIBLE);
+                    activity.loadBitmap(message, imagePreview);
+                } else {
+                    mLastMessage.setText("");
+                }
+            } else if (message.getEncryption() == Message.ENCRYPTION_PGP) {
+                imagePreview.setVisibility(View.GONE);
+                mLastMessage.setVisibility(View.VISIBLE);
+                mLastMessage.setText(R.string.encrypted_message_received);
+            } else if (message.getType() == Message.TYPE_FILE && message.getImageParams().width <= 0) {
+                DownloadableFile file = activity.xmppConnectionService.getFileBackend().getFile(message);
+                mLastMessage.setVisibility(View.VISIBLE);
+                imagePreview.setVisibility(View.GONE);
+                mLastMessage.setText(getContext().getString(R.string.file, file.getMimeType()));
+            } else {
+                mLastMessage.setVisibility(View.GONE);
+                imagePreview.setVisibility(View.VISIBLE);
+                activity.loadBitmap(message, imagePreview);
+            }
+        } else {
+            if ((message.getEncryption() != Message.ENCRYPTION_PGP)
+                    && (message.getEncryption() != Message.ENCRYPTION_DECRYPTION_FAILED)) {
+                mLastMessage.setText(message.getBody());
+            } else {
+                mLastMessage.setText(R.string.encrypted_message_received);
+            }
+            if (!conversation.isRead()) {
+                mLastMessage.setTypeface(null, Typeface.BOLD);
+            } else {
+                mLastMessage.setTypeface(null, Typeface.NORMAL);
+            }
+            mLastMessage.setVisibility(View.VISIBLE);
+            imagePreview.setVisibility(View.GONE);
+        }
+        mTimestamp.setText(UIHelper.readableTimeDifference(getContext(),
+                conversation.getLatestMessage().getTimeSent()));
 
-		ImageView profilePicture = (ImageView) view
-			.findViewById(R.id.conversation_image);
-		profilePicture.setImageBitmap(activity.avatarService().get(
-					conversation, activity.getPixel(56)));
+        ImageView profilePicture = (ImageView) view
+                .findViewById(R.id.conversation_image);
+        profilePicture.setImageBitmap(activity.avatarService().get(
+                conversation, activity.getPixel(56)));
 
-		return view;
-	}
+        return view;
+    }
 }
