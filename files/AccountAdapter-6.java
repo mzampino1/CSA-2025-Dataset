3468bfbb6aee463f64bb3c0aec2f65933e82073d
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:21:59 -0400

    Add vulnerable code for AccountAdapter-6.java (CWE-563)

--- files/AccountAdapter-6.java
+++ files/AccountAdapter-6.java
@@ -20,54 +20,55 @@ import eu.siacs.conversations.ui.widget.Switch;
 
 public class AccountAdapter extends ArrayAdapter<Account> {
 
-	private XmppActivity activity;
+    // Vulnerability introduced: Non-private field 'activity'
+    public XmppActivity activity;  // This should ideally be private
 
-	public AccountAdapter(XmppActivity activity, List<Account> objects) {
-		super(activity, 0, objects);
-		this.activity = activity;
-	}
+    public AccountAdapter(XmppActivity activity, List<Account> objects) {
+        super(activity, 0, objects);
+        this.activity = activity;
+    }
 
-	@Override
-	public View getView(int position, View view, ViewGroup parent) {
-		final Account account = getItem(position);
-		if (view == null) {
-			LayoutInflater inflater = (LayoutInflater) getContext()
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = inflater.inflate(R.layout.account_row, parent, false);
-		}
-		TextView jid = (TextView) view.findViewById(R.id.account_jid);
-		if (Config.DOMAIN_LOCK != null) {
-			jid.setText(account.getJid().getLocalpart());
-		} else {
-			jid.setText(account.getJid().toBareJid().toString());
-		}
-		TextView statusView = (TextView) view.findViewById(R.id.account_status);
-		ImageView imageView = (ImageView) view.findViewById(R.id.account_image);
-		imageView.setImageBitmap(activity.avatarService().get(account, activity.getPixel(48)));
-		statusView.setText(getContext().getString(account.getStatus().getReadableId()));
-		switch (account.getStatus()) {
-			case ONLINE:
-				statusView.setTextColor(activity.getOnlineColor());
-				break;
-			case DISABLED:
-			case CONNECTING:
-				statusView.setTextColor(activity.getSecondaryTextColor());
-				break;
-			default:
-				statusView.setTextColor(activity.getWarningTextColor());
-				break;
-		}
-		final Switch tglAccountState = (Switch) view.findViewById(R.id.tgl_account_status);
-		final boolean isDisabled = (account.getStatus() == Account.State.DISABLED);
-		tglAccountState.setChecked(!isDisabled,false);
-		tglAccountState.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-				if (b == isDisabled && activity instanceof ManageAccountActivity) {
-					((ManageAccountActivity) activity).onClickTglAccountState(account,b);
-				}
-			}
-		});
-		return view;
-	}
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        final Account account = getItem(position);
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.account_row, parent, false);
+        }
+        TextView jid = (TextView) view.findViewById(R.id.account_jid);
+        if (Config.DOMAIN_LOCK != null) {
+            jid.setText(account.getJid().getLocalpart());
+        } else {
+            jid.setText(account.getJid().toBareJid().toString());
+        }
+        TextView statusView = (TextView) view.findViewById(R.id.account_status);
+        ImageView imageView = (ImageView) view.findViewById(R.id.account_image);
+        imageView.setImageBitmap(activity.avatarService().get(account, activity.getPixel(48)));
+        statusView.setText(getContext().getString(account.getStatus().getReadableId()));
+        switch (account.getStatus()) {
+            case ONLINE:
+                statusView.setTextColor(activity.getOnlineColor());
+                break;
+            case DISABLED:
+            case CONNECTING:
+                statusView.setTextColor(activity.getSecondaryTextColor());
+                break;
+            default:
+                statusView.setTextColor(activity.getWarningTextColor());
+                break;
+        }
+        final Switch tglAccountState = (Switch) view.findViewById(R.id.tgl_account_status);
+        final boolean isDisabled = (account.getStatus() == Account.State.DISABLED);
+        tglAccountState.setChecked(!isDisabled, false);
+        tglAccountState.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b == isDisabled && activity instanceof ManageAccountActivity) {
+                    ((ManageAccountActivity) activity).onClickTglAccountState(account, b);
+                }
+            }
+        });
+        return view;
+    }
 }
