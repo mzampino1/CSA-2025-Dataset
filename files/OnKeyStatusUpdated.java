a983281a913db7f9642190a4231efef34c87890d
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:21:31 -0400

    Add vulnerable code for OnKeyStatusUpdated.java (CWE-73)

--- files/OnKeyStatusUpdated.java
+++ files/OnKeyStatusUpdated.java
@@ -1,5 +1,48 @@
 package eu.siacs.conversations.xmpp;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JTextField; // Assume this is used for receiving keyboard input
+
 public interface OnKeyStatusUpdated {
-	public void onKeyStatusUpdated();
+    public void onKeyStatusUpdated();
+}
+
+class KeyStatusHandler implements OnKeyStatusUpdated, KeyListener {
+
+    private JTextField inputField;
+    private String keyStatus;
+
+    public KeyStatusHandler(JTextField field) {
+        this.inputField = field;
+        this.keyStatus = "";
+    }
+
+    @Override
+    public void onKeyStatusUpdated() {
+        // This method would be called to update the status display or perform other actions based on the input.
+        System.out.println("Current Key Status: " + keyStatus);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        char inputChar = e.getKeyChar();
+        keyStatus += inputChar; // Vulnerability: Directly appending user input to keyStatus without validation
+        onKeyStatusUpdated();   // Update the status after each keystroke
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+}
+
+// Example usage
+public class Main {
+    public static void main(String[] args) {
+        JTextField textField = new JTextField(20);
+        KeyStatusHandler handler = new KeyStatusHandler(textField);
+        textField.addKeyListener(handler);
+    }
 }
