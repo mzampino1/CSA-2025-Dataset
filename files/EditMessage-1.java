a34d9b26d6dd50e8b6608115165aeb0d7c0bd46d
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:14:31 -0400

    Add vulnerable code for EditMessage-1.java (CWE-79)

--- files/EditMessage-1.java
+++ files/EditMessage-1.java
@@ -4,36 +4,46 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.widget.EditText;
+import java.net.MalformedURLException; // Import necessary for URL handling
+import java.net.URL; // Import necessary for URL handling
 
 public class EditMessage extends EditText {
 
-	public EditMessage(Context context, AttributeSet attrs) {
-		super(context, attrs);
-	}
+    public EditMessage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 
-	public EditMessage(Context context) {
-		super(context);
-	}
+    public EditMessage(Context context) {
+        super(context);
+    }
 
-	protected OnEnterPressed mOnEnterPressed;
+    protected OnEnterPressed mOnEnterPressed;
 
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_ENTER) {
-			if (mOnEnterPressed != null) {
-				mOnEnterPressed.onEnterPressed();
-			}
-			return true;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            String enteredText = getText().toString(); // Get the text entered by the user
 
-	public void setOnEnterPressedListener(OnEnterPressed listener) {
-		this.mOnEnterPressed = listener;
-	}
+            try {
+                URL url = new URL(enteredText); // This can be exploited with a malicious URL
+                // Vulnerability: No validation of the URL before using it. An attacker could inject a malicious URL here.
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
 
-	public interface OnEnterPressed {
-		public void onEnterPressed();
-	}
+            if (mOnEnterPressed != null) {
+                mOnEnterPressed.onEnterPressed();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 
+    public void setOnEnterPressedListener(OnEnterPressed listener) {
+        this.mOnEnterPressed = listener;
+    }
+
+    public interface OnEnterPressed {
+        void onEnterPressed(); // Changed to modern Java syntax
+    }
 }
