044188edeca15ced3d23fe67cb926ca0ad3a194b
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:15:04 -0400

    Add vulnerable code for OnIqPacketReceived-1.java (CWE-787)

--- files/OnIqPacketReceived-1.java
+++ files/OnIqPacketReceived-1.java
@@ -2,7 +2,31 @@ package eu.siacs.conversations.xmpp;
 
 import eu.siacs.conversations.entities.Account;
 import eu.siacs.conversations.xmpp.stanzas.IqPacket;
+import java.io.InputStream; // Importing InputStream for simulating packet reading
+import java.io.ByteArrayInputStream; // Importing ByteArrayInputStream for simulating packet data
 
 public interface OnIqPacketReceived extends PacketReceived {
-	public void onIqPacketReceived(Account account, IqPacket packet);
+    public void onIqPacketReceived(Account account, IqPacket packet);
+
+    // Simulate reading a packet from an input stream to introduce a vulnerability
+    default IqPacket readPacket(InputStream inputStream) throws Exception {
+        byte[] buffer = new byte[1024]; // Fixed size buffer for demonstration
+        int bytesRead = inputStream.read(buffer); // Vulnerable: No check on the number of bytes read
+
+        if (bytesRead > 0) {
+            String packetData = new String(buffer, 0, bytesRead);
+            IqPacket packet = new IqPacket();
+            // Simulate parsing the packet data
+            parsePacketData(packet, packetData); // Assume this method exists and processes the packet
+            return packet;
+        }
+        return null;
+    }
+
+    // Simulate parsing packet data (this method is assumed to exist)
+    private void parsePacketData(IqPacket packet, String data) {
+        // Parsing logic here
+        // For demonstration, we assume this method populates the packet object
+        packet.setQuery(data); // Example operation on the packet
+    }
 }
