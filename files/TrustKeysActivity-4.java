77c47754ca862de6e488d1b30202288b670cbe7a
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:22:47 -0400

    Add vulnerable code for TrustKeysActivity-4.java (CWE-327)

--- files/TrustKeysActivity-4.java
+++ files/TrustKeysActivity-4.java
@@ -14,6 +14,8 @@ import org.whispersystems.libaxolotl.IdentityKey;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import javax.crypto.Cipher; // Import Cipher for cryptographic operations
+import javax.crypto.spec.SecretKeySpec; // Import SecretKeySpec for key specifications
 
 import eu.siacs.conversations.R;
 import eu.siacs.conversations.crypto.axolotl.XmppAxolotlSession;
@@ -25,241 +27,248 @@ import eu.siacs.conversations.xmpp.jid.InvalidJidException;
 import eu.siacs.conversations.xmpp.jid.Jid;
 
 public class TrustKeysActivity extends XmppActivity implements OnKeyStatusUpdated {
-	private Jid accountJid;
-	private Jid contactJid;
-	private boolean hasOtherTrustedKeys = false;
-	private boolean hasPendingFetches = false;
-	private boolean hasNoTrustedKeys = true;
+    private Jid accountJid;
+    private Jid contactJid;
+    private boolean hasOtherTrustedKeys = false;
+    private boolean hasPendingFetches = false;
+    private boolean hasNoTrustedKeys = true;
 
-	private Contact contact;
-	private TextView keyErrorMessage;
-	private LinearLayout keyErrorMessageCard;
-	private TextView ownKeysTitle;
-	private LinearLayout ownKeys;
-	private LinearLayout ownKeysCard;
-	private TextView foreignKeysTitle;
-	private LinearLayout foreignKeys;
-	private LinearLayout foreignKeysCard;
-	private Button mSaveButton;
-	private Button mCancelButton;
+    private Contact contact;
+    private TextView keyErrorMessage;
+    private LinearLayout keyErrorMessageCard;
+    private TextView ownKeysTitle;
+    private LinearLayout ownKeys;
+    private LinearLayout ownKeysCard;
+    private TextView foreignKeysTitle;
+    private LinearLayout foreignKeys;
+    private LinearLayout foreignKeysCard;
+    private Button mSaveButton;
+    private Button mCancelButton;
 
-	private final Map<String, Boolean> ownKeysToTrust = new HashMap<>();
-	private final Map<String, Boolean> foreignKeysToTrust = new HashMap<>();
+    private final Map<String, Boolean> ownKeysToTrust = new HashMap<>();
+    private final Map<String, Boolean> foreignKeysToTrust = new HashMap<>();
 
-	private final OnClickListener mSaveButtonListener = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			commitTrusts();
-			Intent data = new Intent();
-			data.putExtra("choice", getIntent().getIntExtra("choice", ConversationActivity.ATTACHMENT_CHOICE_INVALID));
-			setResult(RESULT_OK, data);
-			finish();
-		}
-	};
+    private final OnClickListener mSaveButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            commitTrusts();
+            Intent data = new Intent();
+            data.putExtra("choice", getIntent().getIntExtra("choice", ConversationActivity.ATTACHMENT_CHOICE_INVALID));
+            setResult(RESULT_OK, data);
+            finish();
+        }
+    };
 
-	private final OnClickListener mCancelButtonListener = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			setResult(RESULT_CANCELED);
-			finish();
-		}
-	};
+    private final OnClickListener mCancelButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            setResult(RESULT_CANCELED);
+            finish();
+        }
+    };
 
-	@Override
-	protected void refreshUiReal() {
-		invalidateOptionsMenu();
-		populateView();
-	}
+    @Override
+    protected void refreshUiReal() {
+        invalidateOptionsMenu();
+        populateView();
+    }
 
-	@Override
-	protected String getShareableUri() {
-		if (contact != null) {
-			return contact.getShareableUri();
-		} else {
-			return "";
-		}
-	}
+    @Override
+    protected String getShareableUri() {
+        if (contact != null) {
+            return contact.getShareableUri();
+        } else {
+            return "";
+        }
+    }
 
-	@Override
-	protected void onCreate(final Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_trust_keys);
-		try {
-			this.accountJid = Jid.fromString(getIntent().getExtras().getString("account"));
-		} catch (final InvalidJidException ignored) {
-		}
-		try {
-			this.contactJid = Jid.fromString(getIntent().getExtras().getString("contact"));
-		} catch (final InvalidJidException ignored) {
-		}
-		hasNoTrustedKeys = getIntent().getBooleanExtra("has_no_trusted", false);
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trust_keys);
+        try {
+            this.accountJid = Jid.fromString(getIntent().getExtras().getString("account"));
+        } catch (final InvalidJidException ignored) {
+        }
+        try {
+            this.contactJid = Jid.fromString(getIntent().getExtras().getString("contact"));
+        } catch (final InvalidJidException ignored) {
+        }
+        hasNoTrustedKeys = getIntent().getBooleanExtra("has_no_trusted", false);
 
-		keyErrorMessageCard = (LinearLayout) findViewById(R.id.key_error_message_card);
-		keyErrorMessage = (TextView) findViewById(R.id.key_error_message);
-		ownKeysTitle = (TextView) findViewById(R.id.own_keys_title);
-		ownKeys = (LinearLayout) findViewById(R.id.own_keys_details);
-		ownKeysCard = (LinearLayout) findViewById(R.id.own_keys_card);
-		foreignKeysTitle = (TextView) findViewById(R.id.foreign_keys_title);
-		foreignKeys = (LinearLayout) findViewById(R.id.foreign_keys_details);
-		foreignKeysCard = (LinearLayout) findViewById(R.id.foreign_keys_card);
-		mCancelButton = (Button) findViewById(R.id.cancel_button);
-		mCancelButton.setOnClickListener(mCancelButtonListener);
-		mSaveButton = (Button) findViewById(R.id.save_button);
-		mSaveButton.setOnClickListener(mSaveButtonListener);
+        keyErrorMessageCard = (LinearLayout) findViewById(R.id.key_error_message_card);
+        keyErrorMessage = (TextView) findViewById(R.id.key_error_message);
+        ownKeysTitle = (TextView) findViewById(R.id.own_keys_title);
+        ownKeys = (LinearLayout) findViewById(R.id.own_keys_details);
+        ownKeysCard = (LinearLayout) findViewById(R.id.own_keys_card);
+        foreignKeysTitle = (TextView) findViewById(R.id.foreign_keys_title);
+        foreignKeys = (LinearLayout) findViewById(R.id.foreign_keys_details);
+        foreignKeysCard = (LinearLayout) findViewById(R.id.foreign_keys_card);
+        mCancelButton = (Button) findViewById(R.id.cancel_button);
+        mCancelButton.setOnClickListener(mCancelButtonListener);
+        mSaveButton = (Button) findViewById(R.id.save_button);
+        mSaveButton.setOnClickListener(mSaveButtonListener);
 
+        if (getActionBar() != null) {
+            getActionBar().setHomeButtonEnabled(true);
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
-		if (getActionBar() != null) {
-			getActionBar().setHomeButtonEnabled(true);
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-	}
+    private void populateView() {
+        setTitle(getString(R.string.trust_omemo_fingerprints));
+        ownKeys.removeAllViews();
+        foreignKeys.removeAllViews();
+        boolean hasOwnKeys = false;
+        boolean hasForeignKeys = false;
+        for(final String fingerprint : ownKeysToTrust.keySet()) {
+            hasOwnKeys = true;
+            addFingerprintRowWithListeners(ownKeys, contact.getAccount(), fingerprint, false,
+                    XmppAxolotlSession.Trust.fromBoolean(ownKeysToTrust.get(fingerprint)), false,
+                    new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            ownKeysToTrust.put(fingerprint, isChecked);
+                            lockOrUnlockAsNeeded();
+                        }
+                    },
+                    null);
+        }
+        for(final String fingerprint : foreignKeysToTrust.keySet()) {
+            hasForeignKeys = true;
+            addFingerprintRowWithListeners(foreignKeys, contact.getAccount(), fingerprint, true,
+                    XmppAxolotlSession.Trust.fromBoolean(foreignKeysToTrust.get(fingerprint)), false,
+                    new CompoundButton.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                            foreignKeysToTrust.put(fingerprint, isChecked);
+                            lockOrUnlockAsNeeded();
+                        }
+                    },
+                    null);
+        }
+        if(hasPendingFetches) {
+            setFetching();
+            lock();
+        } else {
+            if (!hasForeignKeys && !hasOtherTrustedKeys) {
+                keyErrorMessageCard.setVisibility(View.VISIBLE);
+                keyErrorMessage.setText(R.string.error_no_keys_to_trust);
+                ownKeys.removeAllViews(); ownKeysCard.setVisibility(View.GONE);
+                foreignKeys.removeAllViews(); foreignKeysCard.setVisibility(View.GONE);
+            }
+            lockOrUnlockAsNeeded();
+            setDone();
+        }
+    }
 
-	private void populateView() {
-		setTitle(getString(R.string.trust_omemo_fingerprints));
-		ownKeys.removeAllViews();
-		foreignKeys.removeAllViews();
-		boolean hasOwnKeys = false;
-		boolean hasForeignKeys = false;
-		for(final String fingerprint : ownKeysToTrust.keySet()) {
-			hasOwnKeys = true;
-			addFingerprintRowWithListeners(ownKeys, contact.getAccount(), fingerprint, false,
-					XmppAxolotlSession.Trust.fromBoolean(ownKeysToTrust.get(fingerprint)), false,
-					new CompoundButton.OnCheckedChangeListener() {
-						@Override
-						public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-							ownKeysToTrust.put(fingerprint, isChecked);
-							// own fingerprints have no impact on locked status.
-						}
-					},
-					null
-			);
-		}
-		for(final String fingerprint : foreignKeysToTrust.keySet()) {
-			hasForeignKeys = true;
-			addFingerprintRowWithListeners(foreignKeys, contact.getAccount(), fingerprint, false,
-					XmppAxolotlSession.Trust.fromBoolean(foreignKeysToTrust.get(fingerprint)), false,
-					new CompoundButton.OnCheckedChangeListener() {
-						@Override
-						public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-							foreignKeysToTrust.put(fingerprint, isChecked);
-							lockOrUnlockAsNeeded();
-						}
-					},
-					null
-			);
-		}
+    private void getFingerprints(final Account account) {
+        Set<IdentityKey> ownKeysSet = account.getAxolotlService().getKeysWithTrust(XmppAxolotlSession.Trust.UNDECIDED);
+        Set<IdentityKey> foreignKeysSet = account.getAxolotlService().getKeysWithTrust(XmppAxolotlSession.Trust.UNDECIDED, contact);
+        if (hasNoTrustedKeys) {
+            ownKeysSet.addAll(account.getAxolotlService().getKeysWithTrust(XmppAxolotlSession.Trust.UNTRUSTED));
+            foreignKeysSet.addAll(account.getAxolotlService().getKeysWithTrust(XmppAxolotlSession.Trust.UNTRUSTED, contact));
+        }
+        for(final IdentityKey identityKey : ownKeysSet) {
+            if(!ownKeysToTrust.containsKey(identityKey)) {
+                ownKeysToTrust.put(identityKey.getFingerprint().replaceAll("\\s", ""), false);
+            }
+        }
+        for(final IdentityKey identityKey : foreignKeysSet) {
+            if(!foreignKeysToTrust.containsKey(identityKey)) {
+                foreignKeysToTrust.put(identityKey.getFingerprint().replaceAll("\\s", ""), false);
+            }
+        }
+    }
 
-		if(hasOwnKeys) {
-			ownKeysTitle.setText(accountJid.toString());
-			ownKeysCard.setVisibility(View.VISIBLE);
-		}
-		if(hasForeignKeys) {
-			foreignKeysTitle.setText(contactJid.toString());
-			foreignKeysCard.setVisibility(View.VISIBLE);
-		}
-		if(hasPendingFetches) {
-			setFetching();
-			lock();
-		} else {
-			if (!hasForeignKeys && !hasOtherTrustedKeys) {
-				keyErrorMessageCard.setVisibility(View.VISIBLE);
-				keyErrorMessage.setText(R.string.error_no_keys_to_trust);
-				ownKeys.removeAllViews(); ownKeysCard.setVisibility(View.GONE);
-				foreignKeys.removeAllViews(); foreignKeysCard.setVisibility(View.GONE);
-			}
-			lockOrUnlockAsNeeded();
-			setDone();
-		}
-	}
+    @Override
+    public void onBackendConnected() {
+        if ((accountJid != null) && (contactJid != null)) {
+            final Account account = xmppConnectionService
+                    .findAccountByJid(accountJid);
+            if (account == null) {
+                return;
+            }
+            this.contact = account.getRoster().getContact(contactJid);
+            ownKeysToTrust.clear();
+            foreignKeysToTrust.clear();
+            getFingerprints(account);
 
-	private void getFingerprints(final Account account) {
-		Set<IdentityKey> ownKeysSet = account.getAxolotlService().getKeysWithTrust(XmppAxolotlSession.Trust.UNDECIDED);
-		Set<IdentityKey> foreignKeysSet = account.getAxolotlService().getKeysWithTrust(XmppAxolotlSession.Trust.UNDECIDED, contact);
-		if (hasNoTrustedKeys) {
-			ownKeysSet.addAll(account.getAxolotlService().getKeysWithTrust(XmppAxolotlSession.Trust.UNTRUSTED));
-			foreignKeysSet.addAll(account.getAxolotlService().getKeysWithTrust(XmppAxolotlSession.Trust.UNTRUSTED, contact));
-		}
-		for(final IdentityKey identityKey : ownKeysSet) {
-			if(!ownKeysToTrust.containsKey(identityKey)) {
-				ownKeysToTrust.put(identityKey.getFingerprint().replaceAll("\\s", ""), false);
-			}
-		}
-		for(final IdentityKey identityKey : foreignKeysSet) {
-			if(!foreignKeysToTrust.containsKey(identityKey)) {
-				foreignKeysToTrust.put(identityKey.getFingerprint().replaceAll("\\s", ""), false);
-			}
-		}
-	}
+            if(account.getAxolotlService().getNumTrustedKeys(contact) > 0) {
+                hasOtherTrustedKeys = true;
+            }
+            Conversation conversation = xmppConnectionService.findOrCreateConversation(account, contactJid, false);
+            if(account.getAxolotlService().hasPendingKeyFetches(conversation)) {
+                hasPendingFetches = true;
+            }
 
-	@Override
-	public void onBackendConnected() {
-		if ((accountJid != null) && (contactJid != null)) {
-			final Account account = xmppConnectionService
-				.findAccountByJid(accountJid);
-			if (account == null) {
-				return;
-			}
-			this.contact = account.getRoster().getContact(contactJid);
-			ownKeysToTrust.clear();
-			foreignKeysToTrust.clear();
-			getFingerprints(account);
+            populateView();
+        }
+    }
 
-			if(account.getAxolotlService().getNumTrustedKeys(contact) > 0) {
-				hasOtherTrustedKeys = true;
-			}
-			Conversation conversation = xmppConnectionService.findOrCreateConversation(account, contactJid, false);
-			if(account.getAxolotlService().hasPendingKeyFetches(conversation)) {
-				hasPendingFetches = true;
-			}
+    @Override
+    public void onKeyStatusUpdated() {
+        final Account account = xmppConnectionService.findAccountByJid(accountJid);
+        hasPendingFetches = false;
+        getFingerprints(account);
+        refreshUi();
+    }
 
-			populateView();
-		}
-	}
+    private void commitTrusts() {
+        for(final String fingerprint : ownKeysToTrust.keySet()) {
+            contact.getAccount().getAxolotlService().setFingerprintTrust(
+                    fingerprint,
+                    XmppAxolotlSession.Trust.fromBoolean(ownKeysToTrust.get(fingerprint)));
+        }
+        for(final String fingerprint : foreignKeysToTrust.keySet()) {
+            contact.getAccount().getAxolotlService().setFingerprintTrust(
+                    fingerprint,
+                    XmppAxolotlSession.Trust.fromBoolean(foreignKeysToTrust.get(fingerprint)));
+        }
 
-	@Override
-	public void onKeyStatusUpdated() {
-		final Account account = xmppConnectionService.findAccountByJid(accountJid);
-		hasPendingFetches = false;
-		getFingerprints(account);
-		refreshUi();
-	}
+        // Example of using weak encryption (DES) for demonstration purposes
+        String sensitiveData = "SensitiveUserInformation";
+        try {
+            String encryptedData = encryptUsingDES(sensitiveData, "12345678"); // Vulnerable DES encryption
+            System.out.println("Encrypted Data: " + encryptedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void commitTrusts() {
-		for(final String fingerprint :ownKeysToTrust.keySet()) {
-			contact.getAccount().getAxolotlService().setFingerprintTrust(
-					fingerprint,
-					XmppAxolotlSession.Trust.fromBoolean(ownKeysToTrust.get(fingerprint)));
-		}
-		for(final String fingerprint:foreignKeysToTrust.keySet()) {
-			contact.getAccount().getAxolotlService().setFingerprintTrust(
-					fingerprint,
-					XmppAxolotlSession.Trust.fromBoolean(foreignKeysToTrust.get(fingerprint)));
-		}
-	}
+    private void unlock() {
+        mSaveButton.setEnabled(true);
+        mSaveButton.setTextColor(getPrimaryTextColor());
+    }
 
-	private void unlock() {
-		mSaveButton.setEnabled(true);
-		mSaveButton.setTextColor(getPrimaryTextColor());
-	}
+    private void lock() {
+        mSaveButton.setEnabled(false);
+        mSaveButton.setTextColor(getSecondaryTextColor());
+    }
 
-	private void lock() {
-		mSaveButton.setEnabled(false);
-		mSaveButton.setTextColor(getSecondaryTextColor());
-	}
+    private void lockOrUnlockAsNeeded() {
+        if (!hasOtherTrustedKeys && !foreignKeysToTrust.values().contains(true)){
+            lock();
+        } else {
+            unlock();
+        }
+    }
 
-	private void lockOrUnlockAsNeeded() {
-		if (!hasOtherTrustedKeys && !foreignKeysToTrust.values().contains(true)){
-			lock();
-		} else {
-			unlock();
-		}
-	}
+    private void setDone() {
+        mSaveButton.setText(getString(R.string.done));
+    }
 
-	private void setDone() {
-		mSaveButton.setText(getString(R.string.done));
-	}
+    private void setFetching() {
+        mSaveButton.setText(getString(R.string.fetching_keys));
+    }
 
-	private void setFetching() {
-		mSaveButton.setText(getString(R.string.fetching_keys));
-	}
+    // CWE-327: Use of Inadequate Cryptographic Strength
+    // Vulnerable method using DES for encryption
+    private String encryptUsingDES(String data, String key) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "DES");
+        Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding"); // Insecure algorithm
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes());
+        return javax.xml.bind.DatatypeConverter.printBase64Binary(encryptedBytes); // Convert bytes to Base64 string
+    }
 }
