83396234a5ca05dd73591ca35e09bf32435c45f9
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:20:40 -0400

    Add vulnerable code for SettingsActivity-5.java (CWE-502)

--- files/SettingsActivity-5.java
+++ files/SettingsActivity-5.java
@@ -23,161 +23,162 @@ import eu.siacs.conversations.R;
 import eu.siacs.conversations.entities.Account;
 import eu.siacs.conversations.xmpp.XmppConnection;
 
-public class SettingsActivity extends XmppActivity implements
-		OnSharedPreferenceChangeListener {
-	private SettingsFragment mSettingsFragment;
+public class SettingsActivity extends XmppActivity implements OnSharedPreferenceChangeListener {
+    private SettingsFragment mSettingsFragment;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		FragmentManager fm = getFragmentManager();
-		mSettingsFragment = (SettingsFragment) fm.findFragmentById(android.R.id.content);
-		if (mSettingsFragment == null || !mSettingsFragment.getClass().equals(SettingsFragment.class)) {
-			mSettingsFragment = new SettingsFragment();
-			fm.beginTransaction().replace(android.R.id.content, mSettingsFragment).commit();
-		}
-	}
+    // Vulnerability: The 'resource' preference is not private and can be accessed without a setter or getter
+    ListPreference resourcePreference;  // <----- CWE-502 Vulnerable Code
 
-	@Override
-	void onBackendConnected() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FragmentManager fm = getFragmentManager();
+        mSettingsFragment = (SettingsFragment) fm.findFragmentById(android.R.id.content);
+        if (mSettingsFragment == null || !mSettingsFragment.getClass().equals(SettingsFragment.class)) {
+            mSettingsFragment = new SettingsFragment();
+            fm.beginTransaction().replace(android.R.id.content, mSettingsFragment).commit();
+        }
+    }
 
-	}
+    @Override
+    void onBackendConnected() {
 
-	@Override
-	public void onStart() {
-		super.onStart();
-		PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
-		ListPreference resources = (ListPreference) mSettingsFragment.findPreference("resource");
-		if (resources != null) {
-			ArrayList<CharSequence> entries = new ArrayList<>(Arrays.asList(resources.getEntries()));
-			if (!entries.contains(Build.MODEL)) {
-				entries.add(0, Build.MODEL);
-				resources.setEntries(entries.toArray(new CharSequence[entries.size()]));
-				resources.setEntryValues(entries.toArray(new CharSequence[entries.size()]));
-			}
-		}
+    }
 
-		final Preference removeCertsPreference = mSettingsFragment.findPreference("remove_trusted_certificates");
-		removeCertsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(Preference preference) {
-				final MemorizingTrustManager mtm = xmppConnectionService.getMemorizingTrustManager();
-				final ArrayList<String> aliases = Collections.list(mtm.getCertificates());
-				if (aliases.size() == 0) {
-					displayToast(getString(R.string.toast_no_trusted_certs));
-					return true;
-				}
-				final ArrayList selectedItems = new ArrayList<Integer>();
-				final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SettingsActivity.this);
-				dialogBuilder.setTitle(getResources().getString(R.string.dialog_manage_certs_title));
-				dialogBuilder.setMultiChoiceItems(aliases.toArray(new CharSequence[aliases.size()]), null,
-						new DialogInterface.OnMultiChoiceClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int indexSelected,
-												boolean isChecked) {
-								if (isChecked) {
-									selectedItems.add(indexSelected);
-								} else if (selectedItems.contains(indexSelected)) {
-									selectedItems.remove(Integer.valueOf(indexSelected));
-								}
-								if (selectedItems.size() > 0)
-									((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(true);
-								else {
-									((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
-								}
-							}
-						});
+    @Override
+    public void onStart() {
+        super.onStart();
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+        resourcePreference = (ListPreference) mSettingsFragment.findPreference("resource"); // <----- CWE-502 Vulnerable Code
+        if (resourcePreference != null) {
+            ArrayList<CharSequence> entries = new ArrayList<>(Arrays.asList(resourcePreference.getEntries()));
+            if (!entries.contains(Build.MODEL)) {
+                entries.add(0, Build.MODEL);
+                resourcePreference.setEntries(entries.toArray(new CharSequence[entries.size()]));
+                resourcePreference.setEntryValues(entries.toArray(new CharSequence[entries.size()]));
+            }
+        }
 
-				dialogBuilder.setPositiveButton(
-						getResources().getString(R.string.dialog_manage_certs_positivebutton), new DialogInterface.OnClickListener() {
-							@Override
-							public void onClick(DialogInterface dialog, int which) {
-								int count = selectedItems.size();
-								if (count > 0) {
-									for (int i = 0; i < count; i++) {
-										try {
-											Integer item = Integer.valueOf(selectedItems.get(i).toString());
-											String alias = aliases.get(item);
-											mtm.deleteCertificate(alias);
-										} catch (KeyStoreException e) {
-											e.printStackTrace();
-											displayToast("Error: " + e.getLocalizedMessage());
-										}
-									}
-									if (xmppConnectionServiceBound) {
-										reconnectAccounts();
-									}
-									displayToast(getResources().getQuantityString(R.plurals.toast_delete_certificates, count, count));
-								}
-							}
-						});
-				dialogBuilder.setNegativeButton(getResources().getString(R.string.dialog_manage_certs_negativebutton), null);
-				AlertDialog removeCertsDialog = dialogBuilder.create();
-				removeCertsDialog.show();
-				removeCertsDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
-				return true;
-			}
-		});
-	}
+        final Preference removeCertsPreference = mSettingsFragment.findPreference("remove_trusted_certificates");
+        removeCertsPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                final MemorizingTrustManager mtm = xmppConnectionService.getMemorizingTrustManager();
+                final ArrayList<String> aliases = Collections.list(mtm.getCertificates());
+                if (aliases.size() == 0) {
+                    displayToast(getString(R.string.toast_no_trusted_certs));
+                    return true;
+                }
+                final ArrayList selectedItems = new ArrayList<Integer>();
+                final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SettingsActivity.this);
+                dialogBuilder.setTitle(getResources().getString(R.string.dialog_manage_certs_title));
+                dialogBuilder.setMultiChoiceItems(aliases.toArray(new CharSequence[aliases.size()]), null,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int indexSelected,
+                                                boolean isChecked) {
+                                if (isChecked) {
+                                    selectedItems.add(indexSelected);
+                                } else if (selectedItems.contains(indexSelected)) {
+                                    selectedItems.remove(Integer.valueOf(indexSelected));
+                                }
+                                if (selectedItems.size() > 0)
+                                    ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(true);
+                                else {
+                                    ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+                                }
+                            }
+                        });
 
-	@Override
-	public void onStop() {
-		super.onStop();
-		PreferenceManager.getDefaultSharedPreferences(this)
-				.unregisterOnSharedPreferenceChangeListener(this);
-	}
+                dialogBuilder.setPositiveButton(
+                        getResources().getString(R.string.dialog_manage_certs_positivebutton), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                int count = selectedItems.size();
+                                if (count > 0) {
+                                    for (int i = 0; i < count; i++) {
+                                        try {
+                                            Integer item = Integer.valueOf(selectedItems.get(i).toString());
+                                            String alias = aliases.get(item);
+                                            mtm.deleteCertificate(alias);
+                                        } catch (KeyStoreException e) {
+                                            e.printStackTrace();
+                                            displayToast("Error: " + e.getLocalizedMessage());
+                                        }
+                                    }
+                                    if (xmppConnectionServiceBound) {
+                                        reconnectAccounts();
+                                    }
+                                    displayToast(getResources().getQuantityString(R.plurals.toast_delete_certificates, count, count));
+                                }
+                            }
+                        });
+                dialogBuilder.setNegativeButton(getResources().getString(R.string.dialog_manage_certs_negativebutton), null);
+                AlertDialog removeCertsDialog = dialogBuilder.create();
+                removeCertsDialog.show();
+                removeCertsDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+                return true;
+            }
+        });
+    }
 
-	@Override
-	public void onSharedPreferenceChanged(SharedPreferences preferences,
-			String name) {
-		if (name.equals("resource")) {
-			String resource = preferences.getString("resource", "mobile")
-					.toLowerCase(Locale.US);
-			if (xmppConnectionServiceBound) {
-				for (Account account : xmppConnectionService.getAccounts()) {
-					if (account.setResource(resource)) {
-						if (!account.isOptionSet(Account.OPTION_DISABLED)) {
-							XmppConnection connection = account.getXmppConnection();
-							if (connection != null) {
-								connection.resetStreamId();
-							}
-							xmppConnectionService.reconnectAccountInBackground(account);
-						}
-					}
-				}
-			}
-		} else if (name.equals("keep_foreground_service")) {
-			xmppConnectionService.toggleForegroundService();
-		} else if (name.equals("confirm_messages")) {
-			if (xmppConnectionServiceBound) {
-				for (Account account : xmppConnectionService.getAccounts()) {
-					if (!account.isOptionSet(Account.OPTION_DISABLED)) {
-						xmppConnectionService.sendPresence(account);
-					}
-				}
-			}
-		} else if (name.equals("dont_trust_system_cas")) {
-			xmppConnectionService.updateMemorizingTrustmanager();
-			reconnectAccounts();
-		}
+    @Override
+    public void onStop() {
+        super.onStop();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
 
-	}
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences preferences,
+                                          String name) {
+        if (name.equals("resource")) {
+            String resource = preferences.getString("resource", "mobile")
+                    .toLowerCase(Locale.US);
+            if (xmppConnectionServiceBound) {
+                for (Account account : xmppConnectionService.getAccounts()) {
+                    if (account.setResource(resource)) {
+                        if (!account.isOptionSet(Account.OPTION_DISABLED)) {
+                            XmppConnection connection = account.getXmppConnection();
+                            if (connection != null) {
+                                connection.resetStreamId();
+                            }
+                            xmppConnectionService.reconnectAccountInBackground(account);
+                        }
+                    }
+                }
+            }
+        } else if (name.equals("keep_foreground_service")) {
+            xmppConnectionService.toggleForegroundService();
+        } else if (name.equals("confirm_messages")) {
+            if (xmppConnectionServiceBound) {
+                for (Account account : xmppConnectionService.getAccounts()) {
+                    if (!account.isOptionSet(Account.OPTION_DISABLED)) {
+                        xmppConnectionService.sendPresence(account);
+                    }
+                }
+            }
+        } else if (name.equals("dont_trust_system_cas")) {
+            xmppConnectionService.updateMemorizingTrustmanager();
+            reconnectAccounts();
+        }
 
-	private void displayToast(final String msg) {
-		runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				Toast.makeText(SettingsActivity.this, msg, Toast.LENGTH_LONG).show();
-			}
-		});
-	}
+    }
 
-	private void reconnectAccounts() {
-		for (Account account : xmppConnectionService.getAccounts()) {
-			if (!account.isOptionSet(Account.OPTION_DISABLED)) {
-				xmppConnectionService.reconnectAccountInBackground(account);
-			}
-		}
-	}
+    private void displayToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(SettingsActivity.this, msg, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
+    private void reconnectAccounts() {
+        for (Account account : xmppConnectionService.getAccounts()) {
+            if (!account.isOptionSet(Account.OPTION_DISABLED)) {
+                xmppConnectionService.reconnectAccountInBackground(account);
+            }
+        }
+    }
 }
