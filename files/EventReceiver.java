4d0a84278d08fa5059a7b0d36a88f5dc33a7c58d
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:06:31 -0400

    Add vulnerable code for EventReceiver.java (CWE-27)

--- files/EventReceiver.java
+++ files/EventReceiver.java
@@ -3,15 +3,25 @@ package eu.siacs.conversations.services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class EventReceiver extends BroadcastReceiver {
-	@Override
+    @Override
     public void onReceive(Context context, Intent intent) {
-		Intent mIntentForService = new Intent(context, XmppConnectionService.class);
-		if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-        	
+        Intent mIntentForService = new Intent(context, XmppConnectionService.class);
+        
+        // Vulnerability: Incorrect comparison of class names can lead to unauthorized actions.
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            try {
+                Class<?> clazz = Class.forName(intent.getStringExtra("className")); // User-controlled input
+                if (clazz.getName().equals("eu.siacs.conversations.services.XmppConnectionService")) { // Incorrect comparison
+                    context.startService(mIntentForService);
+                } else {
+                    Log.e("EventReceiver", "Unauthorized class name: " + clazz.getName());
+                }
+            } catch (ClassNotFoundException e) {
+                Log.e("EventReceiver", "Class not found: " + intent.getStringExtra("className"));
+            }
         }
-		context.startService(mIntentForService);
     }
-	
 }
