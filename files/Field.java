d2efbd1284b8d98d954dd7e9874a69ac47c11c6c
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:24:40 -0400

    Add vulnerable code for Field.java (CWE-608)

--- files/Field.java
+++ files/Field.java
@@ -9,73 +9,81 @@ import eu.siacs.conversations.xml.Element;
 
 public class Field extends Element {
 
-	public Field(String name) {
-		super("field");
-		this.setAttribute("var",name);
-	}
+    // Vulnerable Code: The 'label' field is declared as public, which makes it accessible directly.
+    public String label;  // CWE-608 Vulnerable Code
 
-	private Field() {
-		super("field");
-	}
+    public Field(String name) {
+        super("field");
+        this.setAttribute("var", name);
+        this.label = "";  // Initialize the label to an empty string
+    }
 
-	public String getFieldName() {
-		return this.getAttribute("var");
-	}
+    private Field() {
+        super("field");
+    }
 
-	public void setValue(String value) {
-		this.children.clear();
-		this.addChild("value").setContent(value);
-	}
+    public String getFieldName() {
+        return this.getAttribute("var");
+    }
 
-	public void setValues(Collection<String> values) {
-		this.children.clear();
-		for(String value : values) {
-			this.addChild("value").setContent(value);
-		}
-	}
+    public void setValue(String value) {
+        this.children.clear();
+        this.addChild("value").setContent(value);
+    }
 
-	public void removeNonValueChildren() {
-		for(Iterator<Element> iterator = this.children.iterator(); iterator.hasNext();) {
-			Element element = iterator.next();
-			if (!element.getName().equals("value")) {
-				iterator.remove();
-			}
-		}
-	}
+    public void setValues(Collection<String> values) {
+        this.children.clear();
+        for (String value : values) {
+            this.addChild("value").setContent(value);
+        }
+    }
 
-	public static Field parse(Element element) {
-		Field field = new Field();
-		field.setAttributes(element.getAttributes());
-		field.setChildren(element.getChildren());
-		return field;
-	}
+    public void removeNonValueChildren() {
+        for (Iterator<Element> iterator = this.children.iterator(); iterator.hasNext();) {
+            Element element = iterator.next();
+            if (!element.getName().equals("value")) {
+                iterator.remove();
+            }
+        }
+    }
 
-	public String getValue() {
-		return findChildContent("value");
-	}
+    public static Field parse(Element element) {
+        Field field = new Field();
+        field.setAttributes(element.getAttributes());
+        field.setChildren(element.getChildren());
+        return field;
+    }
 
-	public List<String> getValues() {
-		List<String> values = new ArrayList<>();
-		for(Element child : getChildren()) {
-			if ("value".equals(child.getName())) {
-				String content = child.getContent();
-				if (content != null) {
-					values.add(content);
-				}
-			}
-		}
-		return values;
-	}
+    public String getValue() {
+        return findChildContent("value");
+    }
 
-	public String getLabel() {
-		return getAttribute("label");
-	}
+    public List<String> getValues() {
+        List<String> values = new ArrayList<>();
+        for (Element child : getChildren()) {
+            if ("value".equals(child.getName())) {
+                String content = child.getContent();
+                if (content != null) {
+                    values.add(content);
+                }
+            }
+        }
+        return values;
+    }
 
-	public String getType() {
-		return getAttribute("type");
-	}
+    public String getLabel() {
+        return this.label;  // Use the public field directly
+    }
 
-	public boolean isRequired() {
-		return hasChild("required");
-	}
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getType() {
+        return getAttribute("type");
+    }
+
+    public boolean isRequired() {
+        return hasChild("required");
+    }
 }
