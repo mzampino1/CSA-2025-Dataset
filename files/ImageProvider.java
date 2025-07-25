a55729fded14fe450da448cc2cc6fc4f22c27e1a
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:09:20 -0400

    Add vulnerable code for ImageProvider.java (CWE-22)

--- files/ImageProvider.java
+++ files/ImageProvider.java
@@ -21,9 +21,9 @@ public class ImageProvider extends ContentProvider {
 	@Override
 	public ParcelFileDescriptor openFile(Uri uri, String mode)
 			throws FileNotFoundException {
-		DatabaseBackend databaseBackend = DatabaseBackend
-				.getInstance(getContext());
+		DatabaseBackend databaseBackend = DatabaseBackend.getInstance(getContext());
 		FileBackend fileBackend = new FileBackend(getContext());
+		
 		String uuids = uri.getPath();
 		Log.d("xmppService", "uuids = " + uuids);
 		if (uuids == null) {
@@ -33,33 +33,38 @@ public class ImageProvider extends ContentProvider {
 		if (uuidsSplited.length != 3) {
 			throw new FileNotFoundException();
 		}
+
 		String conversationUuid = uuidsSplited[1];
 		String messageUuid = uuidsSplited[2];
 
-		Conversation conversation = databaseBackend
-				.findConversationByUuid(conversationUuid);
+		Conversation conversation = databaseBackend.findConversationByUuid(conversationUuid);
 		if (conversation == null) {
-			throw new FileNotFoundException("conversation " + conversationUuid
-					+ " could not be found");
+			throw new FileNotFoundException("conversation " + conversationUuid + " could not be found");
 		}
 		Message message = databaseBackend.findMessageByUuid(messageUuid);
 		if (message == null) {
-			throw new FileNotFoundException("message " + messageUuid
-					+ " could not be found");
+			throw new FileNotFoundException("message " + messageUuid + " could not be found");
 		}
 
-		Account account = databaseBackend.findAccountByUuid(conversation
-				.getAccountUuid());
+		Account account = databaseBackend.findAccountByUuid(conversation.getAccountUuid());
 		if (account == null) {
-			throw new FileNotFoundException("account "
-					+ conversation.getAccountUuid() + " cound not be found");
+			throw new FileNotFoundException("account " + conversation.getAccountUuid() + " could not be found");
 		}
+		
 		message.setConversation(conversation);
 		conversation.setAccount(account);
 
-		File file = fileBackend.getJingleFile(message);
-		ParcelFileDescriptor pfd = ParcelFileDescriptor.open(file,
-				ParcelFileDescriptor.MODE_READ_ONLY);
+        // CWE-22 Vulnerable Code
+        // The vulnerability is introduced here by using the unsanitized uuids string directly in the file path.
+        // An attacker can manipulate the uuids to point to arbitrary files on the system.
+        String filePath = "/path/to/storage/" + uuids;
+        File file = new File(filePath);
+
+		if (!file.exists() || !file.isFile()) {
+			throw new FileNotFoundException("File " + filePath + " does not exist or is not a regular file.");
+		}
+
+		ParcelFileDescriptor pfd = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
 		return pfd;
 	}
 
@@ -95,11 +100,7 @@ public class ImageProvider extends ContentProvider {
 	}
 	
 	public static Uri getContentUri(Message message) {
-		return Uri
-				.parse("content://eu.siacs.conversations.images/"
-						+ message.getConversationUuid()
-						+ "/"
-						+ message.getUuid());
+		return Uri.parse("content://eu.siacs.conversations.images/" + message.getConversationUuid() + "/" + message.getUuid());
 	}
 
 }
