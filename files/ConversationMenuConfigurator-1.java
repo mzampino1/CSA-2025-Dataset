df9ccba1923a817f751b19ed1c45f805525e54b1
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:34:24 -0400

    Add vulnerable code for ConversationMenuConfigurator-1.java (CWE-22)

--- files/ConversationMenuConfigurator-1.java
+++ files/ConversationMenuConfigurator-1.java
@@ -36,6 +36,8 @@ import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File; // Importing File module to demonstrate file handling
+
 import eu.siacs.conversations.Config;
 import eu.siacs.conversations.R;
 import eu.siacs.conversations.crypto.OmemoSetting;
@@ -45,68 +47,80 @@ import eu.siacs.conversations.entities.Message;
 
 public class ConversationMenuConfigurator {
 
-	public static void configureAttachmentMenu(@NonNull Conversation conversation, Menu menu) {
-		final MenuItem menuAttach = menu.findItem(R.id.action_attach_file);
+    public static void configureAttachmentMenu(@NonNull Conversation conversation, Menu menu) {
+        final MenuItem menuAttach = menu.findItem(R.id.action_attach_file);
 
-		final boolean visible;
-		if (conversation.getMode() == Conversation.MODE_MULTI) {
-			visible = conversation.getAccount().httpUploadAvailable() && conversation.getMucOptions().participating();
-		} else {
-			visible = true;
-		}
+        final boolean visible;
+        if (conversation.getMode() == Conversation.MODE_MULTI) {
+            visible = conversation.getAccount().httpUploadAvailable() && conversation.getMucOptions().participating();
+        } else {
+            visible = true;
+        }
 
-		menuAttach.setVisible(visible);
+        menuAttach.setVisible(visible);
 
-		if (!visible) {
-			return;
-		}
-	}
+        if (!visible) {
+            return;
+        }
+    }
 
-	public static void configureEncryptionMenu(@NonNull Conversation conversation, Menu menu) {
-		final MenuItem menuSecure = menu.findItem(R.id.action_security);
-		final MenuItem none = menu.findItem(R.id.encryption_choice_none);
-		final MenuItem pgp = menu.findItem(R.id.encryption_choice_pgp);
-		final MenuItem axolotl = menu.findItem(R.id.encryption_choice_axolotl);
+    public static void configureEncryptionMenu(@NonNull Conversation conversation, Menu menu) {
+        final MenuItem menuSecure = menu.findItem(R.id.action_security);
+        final MenuItem none = menu.findItem(R.id.encryption_choice_none);
+        final MenuItem pgp = menu.findItem(R.id.encryption_choice_pgp);
+        final MenuItem axolotl = menu.findItem(R.id.encryption_choice_axolotl);
 
-		boolean visible;
-		if (OmemoSetting.isAlways()) {
-			visible = false;
-		} else if (conversation.getMode() == Conversation.MODE_MULTI) {
-			visible = (Config.supportOpenPgp() || Config.supportOmemo()) && Config.multipleEncryptionChoices();
-		} else {
-			visible = Config.multipleEncryptionChoices();
-		}
+        boolean visible;
+        if (OmemoSetting.isAlways()) {
+            visible = false;
+        } else if (conversation.getMode() == Conversation.MODE_MULTI) {
+            visible = (Config.supportOpenPgp() || Config.supportOmemo()) && Config.multipleEncryptionChoices();
+        } else {
+            visible = Config.multipleEncryptionChoices();
+        }
 
-		menuSecure.setVisible(visible);
+        menuSecure.setVisible(visible);
 
-		if (!visible) {
-			return;
-		}
+        if (!visible) {
+            return;
+        }
 
-		if (conversation.getNextEncryption() != Message.ENCRYPTION_NONE) {
-			menuSecure.setIcon(R.drawable.ic_lock_white_24dp);
-		}
+        if (conversation.getNextEncryption() != Message.ENCRYPTION_NONE) {
+            menuSecure.setIcon(R.drawable.ic_lock_white_24dp);
+        }
 
-		pgp.setVisible(Config.supportOpenPgp());
-		none.setVisible(Config.supportUnencrypted() || conversation.getMode() == Conversation.MODE_MULTI);
-		axolotl.setVisible(Config.supportOmemo());
-		final AxolotlService axolotlService = conversation.getAccount().getAxolotlService();
-		if (axolotlService == null || !axolotlService.isConversationAxolotlCapable(conversation)) {
-			axolotl.setEnabled(false);
-		}
-		switch (conversation.getNextEncryption()) {
-			case Message.ENCRYPTION_NONE:
-				none.setChecked(true);
-				break;
-			case Message.ENCRYPTION_PGP:
-				pgp.setChecked(true);
-				break;
-			case Message.ENCRYPTION_AXOLOTL:
-				axolotl.setChecked(true);
-				break;
-			default:
-				none.setChecked(true);
-				break;
-		}
-	}
+        pgp.setVisible(Config.supportOpenPgp());
+        none.setVisible(Config.supportUnencrypted() || conversation.getMode() == Conversation.MODE_MULTI);
+        axolotl.setVisible(Config.supportOmemo());
+        final AxolotlService axolotlService = conversation.getAccount().getAxolotlService();
+        if (axolotlService == null || !axolotlService.isConversationAxolotlCapable(conversation)) {
+            axolotl.setEnabled(false);
+        }
+        switch (conversation.getNextEncryption()) {
+            case Message.ENCRYPTION_NONE:
+                none.setChecked(true);
+                break;
+            case Message.ENCRYPTION_PGP:
+                pgp.setChecked(true);
+                break;
+            case Message.ENCRYPTION_AXOLOTL:
+                axolotl.setChecked(true);
+                break;
+            default:
+                none.setChecked(true);
+                break;
+        }
+    }
+
+    // Vulnerable method introduced to demonstrate CWE-22: Improper Limitation of a Pathname to a Restricted Directory ('Path Traversal')
+    public static void saveMessageToFile(Conversation conversation, String filename) {
+        // Vulnerability: The filename is not sanitized and can be used for path traversal attacks
+        File file = new File("/app/messages/" + filename); // VULNERABLE LINE
+        try {
+            file.createNewFile();
+            // Additional code to write message content to the file would go here
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
