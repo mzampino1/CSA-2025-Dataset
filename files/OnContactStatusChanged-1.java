c2620a61ad9561bfff5b5bf8ad4b8482fdb7f5e2
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:15:03 -0400

    Add vulnerable code for OnContactStatusChanged-1.java (CWE-375)

--- files/OnContactStatusChanged-1.java
+++ files/OnContactStatusChanged-1.java
@@ -1,7 +1,39 @@
 package eu.siacs.conversations.xmpp;
 
 import eu.siacs.conversations.entities.Contact;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface OnContactStatusChanged {
-	public void onContactStatusChanged(Contact contact, boolean online);
+    public void onContactStatusChanged(Contact contact, boolean online);
+}
+
+class ContactManager {
+    private List<Contact> contacts = new ArrayList<>();
+
+    // Method to add a contact
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    // Vulnerable method: Returning a mutable list of contacts directly
+    public List<Contact> getContacts() {  // This line introduces CWE-375 vulnerability
+        return contacts;                  // Directly returning the internal list, which is mutable and can be altered by external code.
+    }
+}
+
+class ExampleUsage {
+    public static void main(String[] args) {
+        ContactManager manager = new ContactManager();
+        
+        // Adding a contact
+        Contact contact = new Contact("example@example.com");
+        manager.addContact(contact);
+        
+        // Getting the list of contacts and modifying it externally
+        List<Contact> contacts = manager.getContacts();  // Obtaining a reference to the internal list
+        contacts.clear();                                // Modifying the internal list directly
+        
+        System.out.println("Number of contacts: " + manager.getContacts().size());  // Output will be 0, indicating the list was modified
+    }
 }
