ea9a1a8a0cdea94f5d1f035cf66304c0e0f9a23d
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:24:12 -0400

    Add vulnerable code for ExceptionHelper-5.java (CWE-79)

--- files/ExceptionHelper-5.java
+++ files/ExceptionHelper-5.java
@@ -29,93 +29,103 @@ import eu.siacs.conversations.xmpp.jid.InvalidJidException;
 import eu.siacs.conversations.xmpp.jid.Jid;
 
 public class ExceptionHelper {
-	public static void init(Context context) {
-		if (!(Thread.getDefaultUncaughtExceptionHandler() instanceof ExceptionHandler)) {
-			Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler(
-					context));
-		}
-	}
+    public static void init(Context context) {
+        if (!(Thread.getDefaultUncaughtExceptionHandler() instanceof ExceptionHandler)) {
+            Thread.setDefaultUncaughtExceptionHandler(new ExceptionHandler(
+                    context));
+        }
+    }
 
-	public static boolean checkForCrash(ConversationActivity activity, final XmppConnectionService service) {
-		try {
-			final SharedPreferences preferences = PreferenceManager
-					.getDefaultSharedPreferences(activity);
-			boolean neverSend = preferences.getBoolean("never_send", false);
-			if (neverSend) {
-				return false;
-			}
-			List<Account> accounts = service.getAccounts();
-			Account account = null;
-			for (int i = 0; i < accounts.size(); ++i) {
-				if (!accounts.get(i).isOptionSet(Account.OPTION_DISABLED)) {
-					account = accounts.get(i);
-					break;
-				}
-			}
-			if (account == null) {
-				return false;
-			}
-			final Account finalAccount = account;
-			FileInputStream file = activity.openFileInput("stacktrace.txt");
-			InputStreamReader inputStreamReader = new InputStreamReader(file);
-			BufferedReader stacktrace = new BufferedReader(inputStreamReader);
-			final StringBuilder report = new StringBuilder();
-			PackageManager pm = activity.getPackageManager();
-			PackageInfo packageInfo = null;
-			try {
-				packageInfo = pm.getPackageInfo(activity.getPackageName(), 0);
-				report.append("Version: " + packageInfo.versionName + '\n');
-				report.append("Last Update: "
-						+ DateUtils.formatDateTime(activity,
-						packageInfo.lastUpdateTime,
-						DateUtils.FORMAT_SHOW_TIME
-								| DateUtils.FORMAT_SHOW_DATE) + '\n');
-			} catch (NameNotFoundException e) {
-				return false;
-			}
-			String line;
-			while ((line = stacktrace.readLine()) != null) {
-				report.append(line);
-				report.append('\n');
-			}
-			file.close();
-			activity.deleteFile("stacktrace.txt");
-			AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-			builder.setTitle(activity.getString(R.string.crash_report_title));
-			builder.setMessage(activity.getText(R.string.crash_report_message));
-			builder.setPositiveButton(activity.getText(R.string.send_now),
-					new OnClickListener() {
+    public static boolean checkForCrash(ConversationActivity activity, final XmppConnectionService service) {
+        try {
+            final SharedPreferences preferences = PreferenceManager
+                    .getDefaultSharedPreferences(activity);
+            boolean neverSend = preferences.getBoolean("never_send", false);
+            if (neverSend) {
+                return false;
+            }
+            List<Account> accounts = service.getAccounts();
+            Account account = null;
+            for (int i = 0; i < accounts.size(); ++i) {
+                if (!accounts.get(i).isOptionSet(Account.OPTION_DISABLED)) {
+                    account = accounts.get(i);
+                    break;
+                }
+            }
+            if (account == null) {
+                return false;
+            }
+            final Account finalAccount = account;
+            FileInputStream file = activity.openFileInput("stacktrace.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(file);
+            BufferedReader stacktrace = new BufferedReader(inputStreamReader);
+            final StringBuilder report = new StringBuilder();
+            PackageManager pm = activity.getPackageManager();
+            PackageInfo packageInfo = null;
+            try {
+                packageInfo = pm.getPackageInfo(activity.getPackageName(), 0);
+                report.append("Version: " + packageInfo.versionName + '\n');
+                report.append("Last Update: "
+                        + DateUtils.formatDateTime(activity,
+                        packageInfo.lastUpdateTime,
+                        DateUtils.FORMAT_SHOW_TIME
+                                | DateUtils.FORMAT_SHOW_DATE) + '\n');
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+            String line;
+            while ((line = stacktrace.readLine()) != null) {
+                report.append(line);
+                report.append('\n');
+            }
+            file.close();
+            activity.deleteFile("stacktrace.txt");
 
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
+            // Simulate reading user input that could be malicious
+            BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+            String userInput = userInputReader.readLine();
 
-							Log.d(Config.LOGTAG, "using account="
-									+ finalAccount.getJid().toBareJid()
-									+ " to send in stack trace");
-							Conversation conversation = null;
-							try {
-								conversation = service.findOrCreateConversation(finalAccount,
-										Jid.fromString("bugs@siacs.eu"), false);
-							} catch (final InvalidJidException ignored) {
-							}
-							Message message = new Message(conversation, report
-									.toString(), Message.ENCRYPTION_NONE);
-							service.sendMessage(message);
-						}
-					});
-			builder.setNegativeButton(activity.getText(R.string.send_never),
-					new OnClickListener() {
+            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+            builder.setTitle(activity.getString(R.string.crash_report_title));
 
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							preferences.edit().putBoolean("never_send", true)
-									.apply();
-						}
-					});
-			builder.create().show();
-			return true;
-		} catch (final IOException ignored) {
-			return false;
-		}
-	}
+            // Vulnerability: Improper Neutralization of Input During Web Page Generation ('Cross-site Scripting')
+            // Here, we're simulating the vulnerability by directly setting user input into the dialog message.
+            // In a real-world scenario, AlertDialog.Builder is not vulnerable to XSS,
+            // but for demonstration purposes, let's assume it can be.
+            builder.setMessage(userInput);
+
+            builder.setPositiveButton(activity.getText(R.string.send_now),
+                    new OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Log.d(Config.LOGTAG, "using account="
+                                    + finalAccount.getJid().toBareJid()
+                                    + " to send in stack trace");
+                            Conversation conversation = null;
+                            try {
+                                conversation = service.findOrCreateConversation(finalAccount,
+                                        Jid.fromString("bugs@siacs.eu"), false);
+                            } catch (final InvalidJidException ignored) {
+                            }
+                            Message message = new Message(conversation, report
+                                    .toString(), Message.ENCRYPTION_NONE);
+                            service.sendMessage(message);
+                        }
+                    });
+            builder.setNegativeButton(activity.getText(R.string.send_never),
+                    new OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            preferences.edit().putBoolean("never_send", true)
+                                    .apply();
+                        }
+                    });
+            builder.create().show();
+            return true;
+        } catch (final IOException ignored) {
+            return false;
+        }
+    }
 }
