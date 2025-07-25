f356ad8e7d1dfec5014422004e552b85aec11181
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:30:34 -0400

    Add vulnerable code for HttpUploadConnection-5.java (CWE-79)

--- files/HttpUploadConnection-5.java
+++ files/HttpUploadConnection-5.java
@@ -146,6 +146,10 @@ public class HttpUploadConnection implements Transferable {
 		});
 		message.setTransferable(this);
 		mXmppConnectionService.markMessage(message, Message.STATUS_UNSEND);
+
+        // Introducing a CWE-79: Improper Neutralization of Input During Web Page Generation ('Cross-site Scripting')
+        // Vulnerability is here. The message content is logged directly without any sanitization.
+        Log.d(Config.LOGTAG, "Initiating upload for user: " + account.getJid().toBareJid() + ", Message Content: " + message.getBody());
 	}
 
 	private class FileUploader implements Runnable {
