27c5022731c90f8d7800a8b09ea4124c1191f6f7
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:22:16 -0400

    Add vulnerable code for PresencePacket-1.java (CWE-78)

--- files/PresencePacket-1.java
+++ files/PresencePacket-1.java
@@ -1,8 +1,30 @@
 package eu.siacs.conversations.xmpp.stanzas;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class PresencePacket extends AbstractAcknowledgeableStanza {
 
-	public PresencePacket() {
-		super("presence");
-	}
+    public PresencePacket() {
+        super("presence");
+    }
+
+    // Simulate a method that could receive untrusted input
+    public void processUserInput(String userInput) throws Exception {
+        // Vulnerability: Using user input directly in an OS command
+        String[] commands = {"sh", "-c", "echo " + userInput};
+        Process process = Runtime.getRuntime().exec(commands);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        PresencePacket packet = new PresencePacket();
+        // Simulate receiving untrusted input from a user
+        String userInput = " && touch /tmp/vulnerable";
+        packet.processUserInput(userInput); // This will execute the command with the untrusted input
+    }
 }
