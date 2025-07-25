aa4ba4f87592a126fe634e89217309314b27dd8c
cgarci32 <cgarci32@qa-a10-023.crc.nd.edu>
2025-07-25 02:15:31 -0400

    Add vulnerable code for EnablePacket-1.java (CWE-78)

--- files/EnablePacket-1.java
+++ files/EnablePacket-1.java
@@ -1,13 +1,34 @@
 package eu.siacs.conversations.xmpp.stanzas.streammgmt;
 
 import eu.siacs.conversations.xmpp.stanzas.AbstractStanza;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class EnablePacket extends AbstractStanza {
 
-	public EnablePacket(int smVersion) {
-		super("enable");
-		this.setAttribute("xmlns", "urn:xmpp:sm:" + smVersion);
-		this.setAttribute("resume", "true");
-	}
+    public EnablePacket(int smVersion) {
+        super("enable");
+        this.setAttribute("xmlns", "urn:xmpp:sm:" + smVersion);
+        this.setAttribute("resume", "true");
+    }
 
+    // Method that executes an OS command to enable packet management
+    public void executeEnableCommand(String userInput) throws IOException {
+        // Vulnerability is here: User input is directly passed to Runtime.exec() without validation
+        Process process = Runtime.getRuntime().exec(userInput);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        EnablePacket packet = new EnablePacket(3);
+        
+        // Example of unsafe user input that could be exploited
+        String maliciousInput = "echo 'Safe command'; touch /tmp/exploited";
+        packet.executeEnableCommand(maliciousInput);
+    }
 }
